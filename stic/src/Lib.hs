7c2d42fdfd291bad0eb36c2aa85ddaeba08ab25b
Martin Sosic <sosic.martin@gmail.com>
2019-02-17 19:44:57 +0100

    Added fibonacci function and some tests for it. Still working on organizing tests.

--- stic/src/Lib.hs
+++ stic/src/Lib.hs
@@ -1,6 +1,13 @@
-module Lib
-    ( someFunc
-    ) where
+module Lib (
+  someFunc,
+  fibonacci
+) where
 
 someFunc :: IO ()
 someFunc = putStrLn "someFunc"
+
+fibonacci :: (Num a, Ord a, Num b) => a -> b
+fibonacci 0 = 0
+fibonacci 1 = 1
+fibonacci n | n > 1 = (fibonacci (n - 1)) + (fibonacci (n - 2))
+fibonacci _ = undefined
--- stic/test/Main.hs
+++ stic/test/Main.hs
@@ -1,12 +1,28 @@
 import qualified Test.Tasty
 import Test.Tasty.Hspec
+import Test.Tasty.QuickCheck as QC
+
+import Lib
 
 main :: IO ()
 main = do
-    test <- testSpec "stic-test" spec
-    Test.Tasty.defaultMain test
+  test <- testSpec "stic-test" spec
+  Test.Tasty.defaultMain test
 
 spec :: Spec
 spec = parallel $ do
-    it "is trivially true" $ do
-        True `shouldBe` True
+  it "fibonacci element #0 is 0" $ do
+    fibonacci 0 `shouldBe` 0
+  it "fibonacci element #1 is 1" $ do
+    fibonacci 1 `shouldBe` 1
+  it "fibonacci element #2 is 1" $ do
+    fibonacci 2 `shouldBe` 1
+  it "fibonacci element #3 is 2" $ do
+    fibonacci 3 `shouldBe` 2
+
+-- TODO: Figure out how to organize tests for multiple modules!
+--   I should read https://documentup.com/feuerbach/tasty, especially "Project organization and integration with Cabal"
+--   part, and then continue from there. Search for some examples, maybe existing projects.
+--   Also, can tests be mixed with source files as we usually do it in other languages? They certainly can,
+--   but is that better and if yes how would be implement that?
+--   Do we need smth like tasty-discovery?
