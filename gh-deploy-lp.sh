83e325aa6133e27a38afd9b30788da5fa7179eb0
Martin Sosic <sosic.martin@gmail.com>
2019-10-29 15:57:27 +0100

    Moved landing page deployment script to landing page directory.

--- gh-deploy-lp.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/sh
-git push origin `git subtree split --prefix landing-page master`:gh-pages --force
---
+++ landing-page/deploy-master-to-gh-pages.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+GIT_ROOT=`git rev-parse --show-toplevel`
+DIR_TO_DEPLOY=`pwd`
+DIR_TO_DEPLOY_REL_TO_GIT_ROOT="${DIR_TO_DEPLOY#"$GIT_ROOT"/}"
+cd $GIT_ROOT
+SUBTREE_MASTER_REF=`git subtree split --prefix "$DIR_TO_DEPLOY_REL_TO_GIT_ROOT" master`
+echo "$SUBTREE_MASTER_REF"
+git push origin "$SUBTREE_MASTER_REF":gh-pages --force
