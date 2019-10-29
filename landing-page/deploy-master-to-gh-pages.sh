d1e39c9229736ded5fe46acacf8679e252cfec8f
Martin Sosic <sosic.martin@gmail.com>
2019-10-29 16:32:32 +0100

    Tiny fix on landing page deployment script.

--- landing-page/deploy-master-to-gh-pages.sh
+++ landing-page/deploy-master-to-gh-pages.sh
@@ -4,5 +4,4 @@ DIR_TO_DEPLOY=`pwd`
 DIR_TO_DEPLOY_REL_TO_GIT_ROOT="${DIR_TO_DEPLOY#"$GIT_ROOT"/}"
 cd $GIT_ROOT
 SUBTREE_MASTER_REF=`git subtree split --prefix "$DIR_TO_DEPLOY_REL_TO_GIT_ROOT" master`
-echo "$SUBTREE_MASTER_REF"
 git push origin "$SUBTREE_MASTER_REF":gh-pages --force
