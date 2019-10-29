0ca25c2eae36f7a6d7b68a905d1fd49a0d2eff4c
Martin Sosic <sosic.martin@gmail.com>
2019-10-29 16:59:16 +0100

    Removed landing page.

--- landing-page/deploy-master-to-gh-pages.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-GIT_ROOT=`git rev-parse --show-toplevel`
-DIR_TO_DEPLOY=`pwd`
-DIR_TO_DEPLOY_REL_TO_GIT_ROOT="${DIR_TO_DEPLOY#"$GIT_ROOT"/}"
-cd $GIT_ROOT
-SUBTREE_MASTER_REF=`git subtree split --prefix "$DIR_TO_DEPLOY_REL_TO_GIT_ROOT" master`
-git push origin "$SUBTREE_MASTER_REF":gh-pages --force
