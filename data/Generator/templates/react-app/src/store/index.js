e9d0ea9781adccb970a8d5914cfc27dfb415850b
Martin Sosic <sosic.martin@gmail.com>
2020-02-04 18:14:44 +0100

    Replaced redux-starter-kit with @reduxjs/toolkit.

--- data/Generator/templates/react-app/src/store/index.js
+++ data/Generator/templates/react-app/src/store/index.js
@@ -1,5 +1,5 @@
 {{={= =}=}}
-import * as reduxStarterKit from 'redux-starter-kit'
+import * as RTK from '@reduxjs/toolkit'
 
 import loggerMiddleware from './middleware/logger'
 
@@ -10,13 +10,13 @@ import loggerMiddleware from './middleware/logger'
  */
 export const configureStore = (reducer, preloadedState) => {
   const middleware = [
-    loggerMiddleware,
-    ...reduxStarterKit.getDefaultMiddleware()
+    ...RTK.getDefaultMiddleware(),
+    loggerMiddleware
   ]
 
   const enhancers = []
 
-  const store = reduxStarterKit.configureStore({
+  const store = RTK.configureStore({
     reducer,
     preloadedState,
     middleware,
