2e00d5a34e9c68f8b912e0be460b54f4dc8cc560
Matija Sosic <matija.sosic@gmail.com>
2020-08-31 13:58:53 +0200

    Added in-code routing and url/query params examples.

--- waspc/examples/todoApp/ext/pages/About.js
+++ waspc/examples/todoApp/ext/pages/About.js
@@ -1,11 +1,15 @@
 import React, { Component } from 'react'
 
+import { Link } from "react-router-dom"
 
 export default class About extends Component {
 
   render() {
     return (
-      <div>I am About page!</div>
+      <>
+        <div>I am About page!</div>
+        <Link to="/">Go to dashboard</Link>
+      </>
     )
   }
 }
