69f45c40a79d70b036cfa034aff46690553ad4d9
Martin Sosic <sosic.martin@gmail.com>
2020-09-03 17:28:29 +0200

    Added HttpError on server, to be used in operations.

--- waspc/data/Generator/templates/react-app/src/queries/_query.js
+++ waspc/data/Generator/templates/react-app/src/queries/_query.js
@@ -8,13 +8,24 @@ const {= queryFnName =} = async (args) => {
     const response = await axios.post(config.apiUrl + '/{= queryRoute =}', args)
     return response.data
   } catch (error) {
-    // TODO: This is a really crude error handling for now, and we should look into improving it,
-    //   once we figure out what we need. We should start from the server side probably.
-    const e = new Error(error.message)
-    if (error?.response?.data) {
-      e.data = error.response.data
+    if (error?.response) {
+      // If error came from HTTP response, we capture most informative message
+      // and also add .statusCode information to it.
+      // If error had JSON response, we assume it is of format { message, data } and
+      // add that info to the error.
+      // TODO: We might want to use HttpError here instead of just Error, since
+      //   HttpError is also used on server to throw errors like these.
+      //   That would require copying HttpError code to web-app also and using it here.
+      const responseJson = error.response?.data
+      const responseStatusCode = error.response.status
+      const e = new Error(responseJson?.message || error.message)
+      e.statusCode = responseStatusCode
+      e.data = responseJson?.data
+      throw e
+    } else {
+      // If any other error, we just propagate it.
+      throw error
     }
-    throw e
   }
 }
 
