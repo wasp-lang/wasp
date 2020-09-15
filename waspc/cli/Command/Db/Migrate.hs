1f02809b3da8ec527295733d45ecc6b8a77ac0c7
Martin Sosic <sosic.martin@gmail.com>
2020-09-15 17:39:56 +0200

    Fixed compile error from previous commit.

--- waspc/cli/Command/Db/Migrate.hs
+++ waspc/cli/Command/Db/Migrate.hs
@@ -7,21 +7,22 @@ import Control.Monad.IO.Class (liftIO)
 
 import StrongPath ((</>))
 import Command (Command, CommandError(..))
-import Command.Common (findWaspProjectRootFromCwd, waspSays)
+import Command.Common (findWaspProjectRootDirFromCwd, waspSaysC)
 import qualified Common
 import qualified Generator.DbGenerator.Operations as DbOps
 
+
 migrate :: String -> Command ()
 migrate migrationName = do
-    waspRootDir <- findWaspProjectRootFromCwd
+    waspRootDir <- findWaspProjectRootDirFromCwd
     let genProjectRootDir = waspRootDir </> Common.dotWaspDirInWaspProjectDir </>
                          Common.generatedCodeDirInDotWaspDir
 
-    waspSays "Saving db migration..."
+    waspSaysC "Saving db migration..."
     migrateSaveResult <- liftIO $ DbOps.migrateSave genProjectRootDir migrationName
     case migrateSaveResult of
         Left migrateSaveError -> throwError $ CommandError $ "Migrate save failed: " ++ migrateSaveError
-        Right () -> waspSays "Migration has been successfully saved."
+        Right () -> waspSaysC "Migration has been successfully saved."
 
-    waspSays "All done!"
+    waspSaysC "All done!"
 
