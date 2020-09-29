d936eeba283cffc654fc64d20abfdbcea4d81ca0
Matija Sosic <matija.sosic@gmail.com>
2020-09-29 10:44:27 +0200

    Todo example: task isDone can be toggled now. (#57)

--- waspc/examples/todoApp/ext/actions.js
+++ waspc/examples/todoApp/ext/actions.js
@@ -4,13 +4,22 @@ import Prisma from '@prisma/client'
 const prisma = new Prisma.PrismaClient()
 
 export const createTask = async (task, context) => {
+  /*
   if (Math.random() < 0.5) {
     throw new HttpError(400, 'Failed to create task, random error!')
   }
+  */
 
   const newTask = await prisma.task.create({
     data: {
       description: task.description
     }
+  })
+}
+
+export const updateTaskIsDone = async ({taskId, newIsDoneVal}, context) => {
+  await prisma.task.update({
+    where: { id: taskId },
+    data: { isDone: newIsDoneVal }
   })
 }
