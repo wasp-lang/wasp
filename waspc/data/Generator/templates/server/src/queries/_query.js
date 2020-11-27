6527319581eb313b8014e9622f46670507a9d432
Matija Sosic <matija.sosic@gmail.com>
2020-11-27 18:11:54 +0100

    Added password hashing middleware to Prisma instance - dev cannot change/update password without it being hashed. (#149)

--- waspc/data/Generator/templates/server/src/queries/_query.js
+++ waspc/data/Generator/templates/server/src/queries/_query.js
@@ -1,12 +1,10 @@
 {{={= =}=}}
-import Prisma from '@prisma/client'
+import prisma from '../dbClient.js'
 
 {=& jsFnImportStatement =}
 
 {=! TODO: This template is exactly the same at the moment as one for queries,
           consider in the future if it is worth removing this duplication. =}
-
-const prisma = new Prisma.PrismaClient()
 
 export default async (args, context) => {
   context = { ...context, entities: {
