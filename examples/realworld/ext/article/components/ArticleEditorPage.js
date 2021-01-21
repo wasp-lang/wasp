b8a1b0a7bd7650b346174aa6deae0a818c2245ce
Matija Sosic <matija.sosic@gmail.com>
2021-01-21 12:19:50 +0100

    Made auth more flexible - not generating pages & routes anymore, just the forms.

--- examples/realworld/ext/article/components/ArticleEditorPage.js
+++ examples/realworld/ext/article/components/ArticleEditorPage.js
@@ -23,7 +23,7 @@ const ArticleEditorPage = (props) => {
   // TODO: Instead of this logic here, I wish I could use ACL via Wasp and just
   //   receive user via props instead of useAuth().
   if (!user || isError) {
-    return <span> Please <Link to='/signin'>log in</Link>. </span>
+    return <span> Please <Link to='/login'>log in</Link>. </span>
   }
 
   return articleError
