47c080f41cfe6fb9e3d21e1400eec6977283cda7
bashovski <anurbasic01@outlook.com>
2021-04-04 21:50:08 +0200

    Improve CORS config - allow localhost by default; Allow ORIGINS env var

--- waspc/data/Generator/templates/server/src/app.js
+++ waspc/data/Generator/templates/server/src/app.js
@@ -5,13 +5,17 @@ import cors from 'cors'
 
 import HttpError from './core/HttpError.js'
 import indexRouter from './routes/index.js'
+import config from './config.js'
 
 // TODO: Consider extracting most of this logic into createApp(routes, path) function so that
 //   it can be used in unit tests to test each route individually.
 
 const app = express()
 
-app.use(cors()) // TODO: Consider configuring CORS to be more restrictive, right now it allows all CORS requests.
+app.use(cors({
+  origin: config.origins,
+  credentials: true
+})) // TODO: Consider configuring CORS to be more restrictive, right now it allows all CORS requests.
 app.use(logger('dev'))
 app.use(express.json())
 app.use(express.urlencoded({ extended: false }))
--- waspc/data/Generator/templates/server/src/config.js
+++ waspc/data/Generator/templates/server/src/config.js
@@ -8,10 +8,32 @@ const env = process.env.NODE_ENV || 'development'
 //   - Use convict library to define schema and validate env vars.
 //  https://codingsans.com/blog/node-config-best-practices
 
+const defaultOrigins = [
+    'localhost',
+    'https://localhost:3000',
+    'http://localhost:3000'
+];
+
+/**
+ * @method
+ * @summary Parses and returns all allowed origins.
+ * @description All allowed origins for CORS settings are stored in an ORIGINS environment variable.
+ *              Each origin is delimited by a comma.
+ * @returns {Array<string>}
+ */
+const parseAllowedOrigins = () => {
+  const { ORIGINS: origins } = process.env;
+  return [
+      ...origins.split(','),
+      ...defaultOrigins
+  ];
+}
+
 const config = {
   all: {
     env,
     port: parseInt(process.env.PORT) || 3001,
+    origins: parseAllowedOrigins(),
     {=# isAuthEnabled =}
     auth: {
       jwtSecret: undefined
@@ -33,6 +55,8 @@ const config = {
     {=/ isAuthEnabled =}
   }
 }
+
+console.log('config: ', config.all);
 
 const resolvedConfig = _.merge(config.all, config[env])
 export default resolvedConfig
