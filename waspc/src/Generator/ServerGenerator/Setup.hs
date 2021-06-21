076d683544a8d0c796b77edc80efa97c845a20e5
Damiano Presa <tolarianwiz@gmail.com>
2021-06-21 10:02:14 +0200

    use script to cheat npm install into thinking it's being run interactively like prisma commands

--- waspc/src/Generator/ServerGenerator/Setup.hs
+++ waspc/src/Generator/ServerGenerator/Setup.hs
@@ -8,9 +8,18 @@ import qualified Generator.Job as J
 import Generator.Job.Process (runNodeCommandAsJob)
 import qualified Generator.ServerGenerator.Common as Common
 import StrongPath (Abs, Dir, Path, (</>))
+import qualified System.Info
 
 setupServer :: Path Abs (Dir ProjectRootDir) -> J.Job
 setupServer projectDir = do
   let serverDir = projectDir </> Common.serverRootDirInProjectRootDir
-  _ <- runNodeCommandAsJob serverDir "npm" ["install"] J.Server
-  runNodeCommandAsJob serverDir "npm" ["ls"] J.Server
+
+  let npmInstallServerCmd = ["npm", "install"]
+  let scriptArgs =
+        if System.Info.os == "darwin"
+          then -- NOTE: On MacOS, command that `script` should execute is treated as multiple arguments.
+            ["-Fq", "/dev/null"] ++ npmInstallServerCmd
+          else -- NOTE: On Linux, command that `script` should execute is treated as one argument.
+            ["-feqc", unwords npmInstallServerCmd, "/dev/null"]
+
+  runNodeCommandAsJob serverDir "script" scriptArgs J.Server
--- waspc/src/Generator/WebAppGenerator/Setup.hs
+++ waspc/src/Generator/WebAppGenerator/Setup.hs
@@ -8,9 +8,18 @@ import qualified Generator.Job as J
 import Generator.Job.Process (runNodeCommandAsJob)
 import qualified Generator.WebAppGenerator.Common as Common
 import StrongPath (Abs, Dir, Path, (</>))
+import qualified System.Info
 
 setupWebApp :: Path Abs (Dir ProjectRootDir) -> J.Job
 setupWebApp projectDir = do
   let webAppDir = projectDir </> Common.webAppRootDirInProjectRootDir
-  _ <- runNodeCommandAsJob webAppDir "npm" ["install"] J.WebApp
-  runNodeCommandAsJob webAppDir "npm" ["ls"] J.WebApp
+
+  let npmInstallWebAppCmd = ["npm", "install"]
+  let scriptArgs =
+        if System.Info.os == "darwin"
+          then -- NOTE: On MacOS, command that `script` should execute is treated as multiple arguments.
+            ["-Fq", "/dev/null"] ++ npmInstallWebAppCmd
+          else -- NOTE: On Linux, command that `script` should execute is treated as one argument.
+            ["-feqc", unwords npmInstallWebAppCmd, "/dev/null"]
+
+  runNodeCommandAsJob webAppDir "script" scriptArgs J.WebApp
