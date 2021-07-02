423b1879a00d487a785779c9770e7f4dedeb45bd
craigmc08 <craigmc08@gmail.com>
2021-07-02 16:17:38 -0400

    Refactor TypeError
    Instead of just a string, now the possible errors are enumerated. Recursive
    error structures are used for dictionary/list type errors. Tests are updated
    to check behavior of the new type errors.

--- waspc/src/Analyzer/TypeChecker/AST.hs
+++ waspc/src/Analyzer/TypeChecker/AST.hs
@@ -4,6 +4,7 @@ module Analyzer.TypeChecker.AST
     TypedExpr (..),
     exprType,
     TypeError (..),
+    TypeCoerceReason (..),
   )
 where
 
@@ -41,4 +42,32 @@ exprType (Var _ t) = t
 exprType (JSON _) = QuoterType "json"
 exprType (PSL _) = QuoterType "psl"
 
-data TypeError = TypeError String deriving (Eq, Show)
+data TypeError
+  = UnificationError TypeCoerceReason Type Type
+  | WeakenError TypeCoerceReason TypedExpr Type
+  | NoDeclarationType String
+  | UndefinedIdentifier String
+  | QuoterUnknownTag String
+  | QuoterDifferentTags String String
+  | DictDuplicateField String
+  | -- | Temperory "solution" to missing type inference for empty lists
+    EmptyListNotImplemented
+  deriving (Eq, Show)
+
+-- | Describes a reason that a @UnificationError@ or @WeakenError@ happened
+data TypeCoerceReason
+  = -- | A coercion involving a DeclType and a different type happened
+    ReasonDecl
+  | -- | A coercion involving an EnuMType and a different type happened
+    ReasonEnum
+  | -- | There is no relationship between the types in the coercion
+    ReasonUncoercable
+  | -- | A coercion of the type contained in a list fialed
+    ReasonList TypeError
+  | -- | A coercion failed because a dictionary was missing a key
+    ReasonDictNoKey String
+  | -- | A weakening failed because a dictionary contained an extra key
+    ReasonDictExtraKey String
+  | -- | A coercion failed because two dictionaries had uncoercable types for a key
+    ReasonDictWrongKeyType String TypeError
+  deriving (Eq, Show)
