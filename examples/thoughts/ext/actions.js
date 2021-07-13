fd7568ed17b8765be2981259c27fa012517d7ad2
Martin Sosic <sosic.martin@gmail.com>
2021-07-13 20:31:15 +0200

    Connected thoughts and tags to user.

--- examples/thoughts/ext/actions.js
+++ examples/thoughts/ext/actions.js
@@ -1,6 +1,8 @@
 import HttpError from '@wasp/core/HttpError.js'
 
 export const createThought = async (args, context) => {
+  if (!context.user) { throw new HttpError(403) }
+
   args.tagNames?.map(tagName => {
     if (!/^[a-z](\.?[a-z0-9])*$/.test(tagName)) {
       throw new HttpError(400, "Tag must contain only lowercase letters and dots.")
@@ -11,10 +13,11 @@ export const createThought = async (args, context) => {
       textMarkdown: args.textMarkdown,
       tags: {
         connectOrCreate: args.tagNames?.map(tagName => ({
-          where: { name: tagName },
-          create: { name: tagName }
+          where: { name_userId: { name: tagName, userId: context.user.id } },
+          create: { name: tagName, user: { connect: { id: context.user.id } } }
         }))
-      }
+      },
+      user: { connect: { id: context.user.id } }
     }
   })
 }
