a68bf3b4375ba974d5e0541804d972198e553fa2
gonza <gonzalo.christobal@gmail.com>
2021-08-21 01:41:39 -0400

    Update functions

--- waspc/cli/Command/Info.hs
+++ waspc/cli/Command/Info.hs
@@ -14,10 +14,12 @@ import Common (WaspProjectDir)
 import Control.Monad.IO.Class (liftIO)
 import Lib (findWaspFile)
 import qualified Parser
-import StrongPath (Abs, Dir, File', Path', toFilePath)
+import qualified Path as P
+import StrongPath (Abs, Dir, Path', toFilePath)
 import StrongPath.Operations
+import StrongPath.Path (toPathAbsDir)
 import System.Directory (doesFileExist, getFileSize)
-import System.Directory.Recursive (getDirRecursive)
+import Util.IO (listDirectoryDeep)
 import qualified Util.Terminal as Term
 import Wasp (Wasp, appName, getApp)
 
@@ -25,41 +27,41 @@ info :: Command ()
 info =
   do
     waspDir <- findWaspProjectRootDirFromCwd
-    let dotWaspInfoFile = waspDir </> Cli.Common.dotWaspDirInWaspProjectDir </> Cli.Common.generatedCodeDirInDotWaspDir </> Cli.Common.dotWaspInfoFileInGeneratedCodeDir
     waspAstOrError <- liftIO $ parseWaspFile waspDir
-    case eitherStringWasp of
+    case waspAstOrError of
       Left err -> waspSaysC err
       Right wasp -> do
-        compileInfo <- liftIO $ readDotWaspInfoFile dotWaspInfoFile
-        projectSize <- liftIO $ getDirectorySize $ toFilePath waspDir
+        -- compileInfo <- liftIO $ readDotWaspInfoFile waspDir
+        projectSize <- liftIO $ getDirectorySizeMB waspDir
         waspSaysC $
-            unlines
-              [ "",
-                title "Project information",
-                printInfo
-                  "Name"
-                  (appName $ getApp wasp),
-                printInfo
-                  "Compilation"
-                  compileInfo,
-                printInfo
-                  "Project size"
-                  (show $ projectSize `div` 1000)
-                  ++ " "
-                  ++ "KB"
-              ]
+          unlines
+            [ "",
+              title "Project information",
+              printInfo
+                "Name"
+                (appName $ getApp wasp),
+              -- printInfo
+              --   "Last compilation"
+              --   compileInfo,
+              printInfo
+                "Project size"
+                (show projectSize)
+                ++ " "
+                ++ "MB"
+            ]
 
 printInfo :: String -> String -> String
 printInfo key value = Term.applyStyles [Term.Cyan] key ++ ": " <> Term.applyStyles [Term.White] value
 
-getDirectorySize :: FilePath -> IO Integer
-getDirectorySize path = sum <$> (getDirRecursive path >>= mapM getFileSize)
+getDirectorySizeMB :: Path' Abs (Dir WaspProjectDir) -> IO Integer
+getDirectorySizeMB path = (`div` 1000000) . sum <$> (listDirectoryDeep (toPathAbsDir path) >>= mapM (getFileSize . P.toFilePath))
 
-readDotWaspInfoFile :: Path' Abs File' -> IO String
-readDotWaspInfoFile path = do
-  dotWaspInfoFileExists <- doesFileExist $ toFilePath path
+readDotWaspInfoFile :: Path' Abs (Dir WaspProjectDir) -> IO String
+readDotWaspInfoFile waspDir = do
+  let dotWaspInfoFile = waspDir </> Cli.Common.dotWaspDirInWaspProjectDir </> Cli.Common.generatedCodeDirInDotWaspDir </> Cli.Common.dotWaspInfoFileInGeneratedCodeDir
+  dotWaspInfoFileExists <- doesFileExist $ toFilePath dotWaspInfoFile
   if dotWaspInfoFileExists
-    then do readFile (toFilePath path)
+    then do readFile (toFilePath waspDir)
     else return "No compile information found"
 
 parseWaspFile :: Path' Abs (Dir WaspProjectDir) -> IO (Either String Wasp)
