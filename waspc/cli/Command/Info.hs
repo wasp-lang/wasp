f055b56485d375c282847af8cddd4bcf821fe8cf
gonza <gonzalo.christobal@gmail.com>
2021-08-23 08:06:11 -0400

    Format

--- waspc/cli/Command/Info.hs
+++ waspc/cli/Command/Info.hs
@@ -11,18 +11,18 @@ import Command.Common
     waspSaysC,
   )
 import Common (WaspProjectDir)
+import Control.Arrow (ArrowChoice (left))
 import Control.Monad.IO.Class (liftIO)
 import Lib (findWaspFile)
 import qualified Parser
 import qualified Path as P
-import StrongPath (Abs, Dir, Path', toFilePath, fromAbsFile)
+import StrongPath (Abs, Dir, Path', fromAbsFile, toFilePath)
 import StrongPath.Operations
 import StrongPath.Path (toPathAbsDir)
 import System.Directory (doesFileExist, getFileSize)
 import Util.IO (listDirectoryDeep)
 import qualified Util.Terminal as Term
 import Wasp (Wasp, appName, getApp)
-import Control.Arrow (ArrowChoice(left))
 
 info :: Command ()
 info =
