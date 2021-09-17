674c3ec3a7157ea3a6dd1c2b758e5bcdb320ec15
Martin Sosic <sosic.martin@gmail.com>
2021-09-17 12:09:44 +0200

    fix

--- waspc/src/Analyzer/Evaluator/Evaluation/TypedDictExpr/Combinators.hs
+++ waspc/src/Analyzer/Evaluator/Evaluation/TypedDictExpr/Combinators.hs
@@ -14,29 +14,29 @@ import qualified Analyzer.Evaluator.EvaluationError as EvaluationError
 import qualified Analyzer.TypeChecker.AST as TypedAST
 import Control.Arrow (left)
 
--- | An evaluator that runs a "DictEvaluator". Expects a "Dict" expression and
--- uses its entries to run the "DictEvaluator".
+-- | An evaluation that runs a "TypedDictExprEvaluation". Expects a "Dict" expression and
+-- uses its entries to run the "TypedDictExprEvaluation".
 dict :: TypedDictExprEvaluation a -> TypedExprEvaluation a
 dict dictEvalutor = evaluation $ \(typeDefs, bindings) -> \case
   TypedAST.Dict entries _ -> runEvaluation dictEvalutor typeDefs bindings $ TypedDictEntries entries
   expr -> Left $ EvaluationError.ExpectedDictType $ TypedAST.exprType expr
 
--- | A dictionary evaluator that requires the field to exist.
+-- | A dictionary evaluation that requires the field to exist.
 field :: String -> TypedExprEvaluation a -> TypedDictExprEvaluation a
-field key valueEvaluator = evaluation $
+field key valueEvaluation = evaluation $
   \(typeDefs, bindings) (TypedDictEntries entries) -> case lookup key entries of
     Nothing -> Left $ EvaluationError.MissingField key
     Just value ->
       left (EvaluationError.WithContext (EvaluationError.InField key)) $
-        runEvaluation valueEvaluator typeDefs bindings value
+        runEvaluation valueEvaluation typeDefs bindings value
 
--- | A dictionary evaluator that allows the field to be missing.
+-- | A dictionary evaluation that allows the field to be missing.
 maybeField :: String -> TypedExprEvaluation a -> TypedDictExprEvaluation (Maybe a)
-maybeField key valueEvaluator = evaluation $
+maybeField key valueEvaluation = evaluation $
   \(typeDefs, bindings) (TypedDictEntries entries) -> case lookup key entries of
     Nothing -> pure Nothing
     Just value ->
       Just
         <$> left
           (EvaluationError.WithContext (EvaluationError.InField key))
-          (runEvaluation valueEvaluator typeDefs bindings value)
+          (runEvaluation valueEvaluation typeDefs bindings value)
