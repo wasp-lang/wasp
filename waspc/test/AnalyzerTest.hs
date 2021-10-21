88cd02fbaa66776df9838992471474fd235dd6b4
Martin Sosic <sosic.martin@gmail.com>
2021-10-21 20:16:51 +0200

    fix

--- waspc/test/AnalyzerTest.hs
+++ waspc/test/AnalyzerTest.hs
@@ -5,6 +5,8 @@ module AnalyzerTest where
 import AST (App (..), AuthMethod (..), Page (..))
 import AST.Core.Ref (Ref (..))
 import Analyzer
+import qualified Analyzer.TypeChecker as T
+import Data.Either (isRight)
 import Test.Tasty.Hspec
 
 spec_Analyzer :: Spec
@@ -13,15 +15,39 @@ spec_Analyzer = do
     it "Analyzes a well-typed example" $ do
       let source =
             unlines
-              [ "page HomePage { content: \"Hello world\" }",
-                "app Todo {",
+              [ "app Todo {",
                 "  title: \"Todo App\",",
                 "  authMethod: EmailAndPassword,",
                 "  defaultPage: HomePage",
-                "}"
+                "}",
+                "page HomePage { content: \"Hello world\" }"
               ]
       let decls = analyze source
       let expectedApps = [("Todo", App {title = "Todo App", authMethod = EmailAndPassword, defaultPage = Ref "HomePage"})]
       takeDecls @App <$> decls `shouldBe` Right expectedApps
       let expectedPages = [("HomePage", Page {content = "Hello world"})]
       takeDecls @Page <$> decls `shouldBe` Right expectedPages
+
+    it "Returns an error if unexisting declaration is referenced" $ do
+      let source =
+            unlines
+              [ "page HomePage { content: \"Hello world\" }",
+                "app Todo {",
+                "  title: \"Todo App\",",
+                "  authMethod: EmailAndPassword,",
+                "  defaultPage: NonExistentPage",
+                "}"
+              ]
+      takeDecls @App <$> analyze source `shouldBe` Left (TypeError $ T.UndefinedIdentifier "NonExistentPage")
+
+    it "Works when referenced declaration is declared after the reference." $ do
+      let source =
+            unlines
+              [ "app Todo {",
+                "  title: \"Todo App\",",
+                "  authMethod: EmailAndPassword,",
+                "  defaultPage: HomePage",
+                "}",
+                "page HomePage { content: \"Hello world\" }"
+              ]
+      isRight (analyze source) `shouldBe` True
