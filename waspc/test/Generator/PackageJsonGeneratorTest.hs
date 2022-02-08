d1727abc4d22ce8995b16ad2bdb7b16b7dc7266d
Martijn Faassen <faassen@startifact.com>
2022-02-08 10:40:47 +0100

    Some minor PR tweaks

--- waspc/test/Generator/PackageJsonGeneratorTest.hs
+++ waspc/test/Generator/PackageJsonGeneratorTest.hs
@@ -25,10 +25,10 @@ spec_resolveNpmDeps = do
     resolveDependencies waspDeps userDeps
       `shouldBe` Left
         [ DependencyConflictError
-            (D.create ("b", "2"))
-            (D.create ("b", "3"))
+            (D.make ("b", "2"))
+            (D.make ("b", "3"))
         ]
-  it "same versions so no conflict" $ do
+  it "wasp deps completely overlap with user deps, so no user deps required" $ do
     let userDeps =
           D.fromList
             [ ("a", "1"),
@@ -37,7 +37,7 @@ spec_resolveNpmDeps = do
     resolveDependencies waspDeps userDeps
       `shouldBe` Right (waspDeps, [])
 
-  it "no version overlap so no conflict" $ do
+  it "no dependency name overlap so no conflict" $ do
     let userDeps =
           D.fromList
             [ ("c", "1"),
@@ -46,14 +46,14 @@ spec_resolveNpmDeps = do
     resolveDependencies waspDeps userDeps
       `shouldBe` Right (waspDeps, userDeps)
 
-  it "partial overlap without conflict" $ do
+  it "some dependencies names overlap, with the same version so dependency is not in user dep" $ do
     let userDeps =
           D.fromList
             [ ("a", "1"),
               ("d", "2")
             ]
     resolveDependencies waspDeps userDeps
-      `shouldBe` Right (waspDeps, [D.create ("d", "2")])
+      `shouldBe` Right (waspDeps, [D.make ("d", "2")])
 
   it "Reports error if user dep version does not match wasp dep version" $ do
     let userDeps =
@@ -63,7 +63,7 @@ spec_resolveNpmDeps = do
             ]
     let Left conflicts = resolveNpmDeps waspDeps userDeps
     conflicts
-      `shouldBe` [ ( D.create ("a", "2"),
+      `shouldBe` [ ( D.make ("a", "2"),
                      "Error: Dependency conflict for user dependency (a, 2): "
                        ++ "Version must be set to the exactly "
                        ++ "the same version as the one wasp is using: 1"
