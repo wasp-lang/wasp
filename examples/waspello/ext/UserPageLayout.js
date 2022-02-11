9546dcf200a02b0ec380e2a0b1861f5eb8a8acdf
Matija Sosic <matija.sosic@gmail.com>
2022-02-11 10:28:27 +0100

    Waspello: added Wasp top bar.

--- examples/waspello/ext/UserPageLayout.js
+++ examples/waspello/ext/UserPageLayout.js
@@ -1,10 +1,14 @@
 import React from 'react'
 
 import Navbar from './Navbar'
+import WaspExampleHeader from './WaspExampleHeader.js'
+
 import './UserPageLayout.css'
 
 const UserPageLayout = ({ user, children }) => (
   <div className='layout-root'>
+    <WaspExampleHeader name="Waspello" />
+
     <Navbar user={user} />
     <div className='layout-content'>
       {children}
