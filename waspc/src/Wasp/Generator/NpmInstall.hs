b93f3bc97d5d06246de5bcb90a1bade1ede2b8d6
Martijn Faassen <faassen@startifact.com>
2022-02-18 09:45:27 +0100

    Modify the text a bit more

--- waspc/src/Wasp/Generator/NpmInstall.hs
+++ waspc/src/Wasp/Generator/NpmInstall.hs
@@ -25,9 +25,12 @@ import qualified Wasp.Generator.WebAppGenerator.Setup as WebAppSetup
 
 -- | Run npm install if needed
 
+-- Redundant npm installs can be avoided if the dependencies specified
+-- by the user and wasp have not changed since the last time this ran.
+
 -- It only does this if the dependencies described in the user wasp file are
 -- different from the dependencies that we just installed. To this end, this
--- code keeps track of the dependencies installed a file, which it updates
+-- code keeps track of the dependencies installed with a file, which it updates
 -- after each install.
 ensureNpmInstall :: AppSpec -> Path' Abs (Dir ProjectRootDir) -> IO ([GeneratorWarning], [GeneratorError])
 ensureNpmInstall spec dstDir = do
