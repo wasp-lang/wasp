c786540cef1ab24daf6b3e615b937eb26fd26419
shayneczyzewski <shayne.czyzewski@gmail.com>
2022-02-24 08:33:11 -0500

    remove goldens, split into separate PR later

--- waspc/e2e-test/goldens/waspBuild-golden/waspBuild/.wasp/build/server/src/routes/operations/index.js
+++
@@ -1,8 +0,0 @@
-import express from 'express'
-
-
-
-const router = express.Router()
-
-
-export default router
--- waspc/e2e-test/goldens/waspMigrate-golden/waspMigrate/.wasp/out/db/migrations/no-date-foo/migration.sql
+++
@@ -1,6 +0,0 @@
--- CreateTable
-CREATE TABLE "Task" (
-    "id" INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
-    "description" TEXT NOT NULL,
-    "isDone" BOOLEAN NOT NULL DEFAULT false
-);
