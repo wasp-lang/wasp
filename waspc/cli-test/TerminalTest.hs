640fa4746814212582d7fd4348bef5fb6c7238d8
shayneczyzewski <shayne.czyzewski@gmail.com>
2022-03-21 13:25:05 -0400

    round out asWasp*Message tests

--- waspc/cli-test/TerminalTest.hs
+++ waspc/cli-test/TerminalTest.hs
@@ -1,9 +1,19 @@
 module TerminalTest where
 
 import Test.Tasty.Hspec
-import Wasp.Cli.Terminal (asWaspSuccessMessage)
+import Wasp.Cli.Terminal
+  ( asWaspFailureMessage,
+    asWaspMessage,
+    asWaspStartMessage,
+    asWaspSuccessMessage,
+    asWaspWarningMessage,
+  )
 
 spec_terminalMessages :: Spec
 spec_terminalMessages = do
   it "it can format messages" $ do
+    asWaspMessage "Hello, world!" `shouldBe` "\n --- Hello, world! ---------------------------------------------------------------\n"
+    asWaspStartMessage "Hello, world!" `shouldBe` "\n🐝 --- Hello, world! --------------------------------------------------------------\n"
     asWaspSuccessMessage "Hello, world!" `shouldBe` "\n✅ --- Hello, world! --------------------------------------------------------------\n"
+    asWaspWarningMessage "Hello, world!" `shouldBe` "\n\n👀 --- [Warning] Hello, world! ----------------------------------------------------\n\n"
+    asWaspFailureMessage "Hello, world!" `shouldBe` "\n\n❌ --- [Error] Hello, world! ------------------------------------------------------\n\n"
