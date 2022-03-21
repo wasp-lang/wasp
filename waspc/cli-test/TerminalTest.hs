84f59e2d42afe1e126ad9f1d28a9ca3822a98927
shayneczyzewski <shayne.czyzewski@gmail.com>
2022-03-21 14:14:14 -0400

    change placeholder test and add a todo

--- waspc/cli-test/TerminalTest.hs
+++ waspc/cli-test/TerminalTest.hs
@@ -2,18 +2,11 @@ module TerminalTest where
 
 import Test.Tasty.Hspec
 import Wasp.Cli.Terminal
-  ( asWaspFailureMessage,
-    asWaspMessage,
-    asWaspStartMessage,
-    asWaspSuccessMessage,
-    asWaspWarningMessage,
+  ( asWaspMessage,
   )
 
+-- TODO: Showing it can be done. Remove me when writing first real CLI test.
 spec_terminalMessages :: Spec
 spec_terminalMessages = do
-  it "it can format messages" $ do
+  it "can format messages of various kinds" $ do
     asWaspMessage "Hello, world!" `shouldBe` "\n --- Hello, world! ---------------------------------------------------------------\n"
-    asWaspStartMessage "Hello, world!" `shouldBe` "\n🐝 --- Hello, world! --------------------------------------------------------------\n"
-    asWaspSuccessMessage "Hello, world!" `shouldBe` "\n✅ --- Hello, world! --------------------------------------------------------------\n"
-    asWaspWarningMessage "Hello, world!" `shouldBe` "\n\n👀 --- [Warning] Hello, world! ----------------------------------------------------\n\n"
-    asWaspFailureMessage "Hello, world!" `shouldBe` "\n\n❌ --- [Error] Hello, world! ------------------------------------------------------\n\n"
