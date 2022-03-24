4abdbf9328ec8d45870761de699870f3ec2505db
shayneczyzewski <shayne.czyzewski@gmail.com>
2022-03-24 11:12:18 -0400

    starting to play with Brick

--- waspc/cli/src/Wasp/Cli/Command/Start.hs
+++ waspc/cli/src/Wasp/Cli/Command/Start.hs
@@ -3,11 +3,12 @@ module Wasp.Cli.Command.Start
   )
 where
 
+import Brick (Widget, simpleMain, str)
 import Control.Concurrent.Async (race)
-import Control.Monad.Except (throwError)
+import Control.Monad.Except (runExceptT, throwError)
 import Control.Monad.IO.Class (liftIO)
 import StrongPath ((</>))
-import Wasp.Cli.Command (Command, CommandError (..))
+import Wasp.Cli.Command (Command, CommandError (..), runCommand)
 import Wasp.Cli.Command.Common
   ( findWaspProjectRootDirFromCwd,
   )
@@ -22,22 +23,35 @@ import qualified Wasp.Message as Msg
 
 -- | Does initial compile of wasp code and then runs the generated project.
 -- It also listens for any file changes and recompiles and restarts generated project accordingly.
+-- start :: Command ()
+-- start = do
+--   waspRoot <- findWaspProjectRootDirFromCwd
+--   let outDir = waspRoot </> Common.dotWaspDirInWaspProjectDir </> Common.generatedCodeDirInDotWaspDir
+
+--   cliSendMessageC $ Msg.Start "Starting compilation and setup phase. Hold tight..."
+--   compilationResult <- liftIO $ compileIO waspRoot outDir
+--   case compilationResult of
+--     Left compileError -> throwError $ CommandError "Compilation failed" compileError
+--     Right () -> cliSendMessageC $ Msg.Success "Code has been successfully compiled, project has been generated."
+
+--   cliSendMessageC $ Msg.Start "Listening for file changes..."
+--   cliSendMessageC $ Msg.Start "Starting up generated project..."
+--   watchOrStartResult <- liftIO $ race (watch waspRoot outDir) (Wasp.Lib.start outDir)
+--   case watchOrStartResult of
+--     Left () -> error "This should never happen, listening for file changes should never end but it did."
+--     Right startResult -> case startResult of
+--       Left startError -> throwError $ CommandError "Start failed" startError
+--       Right () -> error "This should never happen, start should never end but it did."
+
+buildInterface :: Widget ()
+buildInterface = do
+  str "Starting compilation and setup phase. Hold tight..."
+
 start :: Command ()
 start = do
   waspRoot <- findWaspProjectRootDirFromCwd
   let outDir = waspRoot </> Common.dotWaspDirInWaspProjectDir </> Common.generatedCodeDirInDotWaspDir
-
-  cliSendMessageC $ Msg.Start "Starting compilation and setup phase. Hold tight..."
-  compilationResult <- liftIO $ compileIO waspRoot outDir
-  case compilationResult of
-    Left compileError -> throwError $ CommandError "Compilation failed" compileError
-    Right () -> cliSendMessageC $ Msg.Success "Code has been successfully compiled, project has been generated."
-
-  cliSendMessageC $ Msg.Start "Listening for file changes..."
-  cliSendMessageC $ Msg.Start "Starting up generated project..."
-  watchOrStartResult <- liftIO $ race (watch waspRoot outDir) (Wasp.Lib.start outDir)
-  case watchOrStartResult of
-    Left () -> error "This should never happen, listening for file changes should never end but it did."
-    Right startResult -> case startResult of
-      Left startError -> throwError $ CommandError "Start failed" startError
-      Right () -> error "This should never happen, start should never end but it did."
+  compRes <- liftIO $ compileIO waspRoot outDir
+  case compRes of
+    Left e -> liftIO $ simpleMain (str e :: Widget ())
+    Right () -> liftIO $ simpleMain buildInterface
