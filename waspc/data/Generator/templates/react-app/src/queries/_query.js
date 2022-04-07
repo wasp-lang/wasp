f357677cc60f444c28b69eb8d06376928efc0389
Filip Sodić <filip.sodic@fer.hr>
2022-04-07 16:35:53 +0200

    Apply minor syntax fixes to templates

--- waspc/data/Generator/templates/react-app/src/queries/_query.js
+++ waspc/data/Generator/templates/react-app/src/queries/_query.js
@@ -2,14 +2,14 @@
 import { callOperation } from '../operations'
 import * as resources from '../operations/resources'
 
-const {= queryFnName =} = async (args) => {
-  return await callOperation('{= queryRoute =}', args)
+function {= queryFnName =}(args) {
+  return callOperation('{= queryRoute =}', args)
 }
 
 const queryCacheKey = '{= queryRoute =}'
 {= queryFnName =}.queryCacheKey = queryCacheKey
 
-export const entitiesUsed = {=& entitiesArray =}
+const entitiesUsed = {=& entitiesArray =}
 resources.addResourcesUsedByQuery(queryCacheKey, entitiesUsed)
 
 export default {= queryFnName =}
--- waspc/data/Generator/templates/react-app/src/queries/index.js
+++ waspc/data/Generator/templates/react-app/src/queries/index.js
@@ -1,18 +1,16 @@
 import { useQuery as rqUseQuery } from 'react-query'
 
-export const useQuery = (queryFn, queryFnArgs, config) => {
+export function useQuery(queryFn, queryFnArgs, config) {
   if (typeof queryFn !== 'function') {
-    throw new Error('useQuery requires queryFn to be a function.')
+    throw new TypeError('useQuery requires queryFn to be a function.')
   }
   if (!queryFn.queryCacheKey) {
-    throw new Error('queryFn needs to have queryCacheKey property defined.')
+    throw new TypeError('queryFn needs to have queryCacheKey property defined.')
   }
 
-  const rqResult = rqUseQuery({
+  return rqUseQuery({
     queryKey: [queryFn.queryCacheKey, queryFnArgs],
     queryFn: (_key, args) => queryFn(args),
     config
   })
-
-  return rqResult
 }
