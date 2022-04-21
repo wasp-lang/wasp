441b43d9bbbe15e8135df2d1d181f8b014d1818e
shayneczyzewski <shayne.czyzewski@gmail.com>
2022-04-21 09:56:37 -0400

    test

--- waspc/src/Wasp/Generator/Start.hs
+++ waspc/src/Wasp/Generator/Start.hs
@@ -3,19 +3,24 @@ module Wasp.Generator.Start
   )
 where
 
-import Control.Concurrent (newChan)
+import Control.Concurrent (newChan, dupChan, forkIO, readChan)
 import Control.Concurrent.Async (concurrently, race)
 import StrongPath (Abs, Dir, Path')
 import Wasp.Generator.Common (ProjectRootDir)
 import Wasp.Generator.Job.IO (readJobMessagesAndPrintThemPrefixed)
 import Wasp.Generator.ServerGenerator.Start (startServer)
 import Wasp.Generator.WebAppGenerator.Start (startWebApp)
+import Control.Monad (forever)
 
 -- | This is a blocking action, that will start the processes that run web app and server.
 --   It will run as long as one of those processes does not fail.
 start :: Path' Abs (Dir ProjectRootDir) -> IO (Either String ())
 start projectDir = do
   chan <- newChan
+  childChan <- dupChan chan
+  forkIO $ forever $ do 
+    contents <- readChan childChan
+    appendFile "/tmp/test.html" (show contents)
   let runStartJobs = race (startServer projectDir chan) (startWebApp projectDir chan)
   (_, serverOrWebExitCode) <- concurrently (readJobMessagesAndPrintThemPrefixed chan) runStartJobs
   case serverOrWebExitCode of
