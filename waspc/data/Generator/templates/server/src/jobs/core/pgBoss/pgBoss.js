ed057ce7e0aaa7e560cffb01676d0c8281235d0f
shayneczyzewski <shayne.czyzewski@gmail.com>
2022-04-28 13:56:26 -0400

    final updates

--- waspc/data/Generator/templates/server/src/jobs/core/pgBoss/pgBoss.js
+++ waspc/data/Generator/templates/server/src/jobs/core/pgBoss/pgBoss.js
@@ -10,7 +10,7 @@ let hasPgBossBeenStarted = false
  * Prepares the target PostgreSQL database and begins job monitoring.
  * If the required database objects do not exist in the specified database,
  * `boss.start()` will automatically create them.
-  * Ref: https://github.com/timgit/pg-boss/blob/master/docs/readme.md#start
+ * Ref: https://github.com/timgit/pg-boss/blob/master/docs/readme.md#start
  * 
  * After making this call, we can send PgBoss jobs and they will be persisted and acted upon.
  * This should only be called once during a server's lifetime.
