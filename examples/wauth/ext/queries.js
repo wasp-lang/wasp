4c2e0e9d512c9bb6af9dc8671300283063fe1e56
shayneczyzewski <shayne.czyzewski@gmail.com>
2022-05-27 13:22:48 -0400

    example of using proxy for automatic entity checks

--- examples/wauth/ext/queries.js
+++ examples/wauth/ext/queries.js
@@ -1,8 +1,8 @@
-import HttpError from '@wasp/core/HttpError.js'
+import HttpError from '../core/HttpError.js'
 import userAbility from './userAbility.js';
 import { accessibleBy } from '@casl/prisma';
 
-export const getTasks = async (args, context) => {
+export const getTasksOld = async (args, context) => {
   const ability = userAbility(context.user);
   if (ability.cannot('read', 'Task')) {
     throw new HttpError(403);
@@ -15,4 +15,35 @@ export const getTasks = async (args, context) => {
   return context.entities.Task.findMany({
     where: accessibleBy(ability).Task
   });
+}
+
+// Example of doing checks via Proxy objects.
+export const getTasks = async (_args, context) => {
+  setupProxy(context, 'Task'); // Can set these up automatically by looking at entities[] in .wasp file.
+  return context.checked.Task.findMany({ where: { userId: context.user.id } });
+}
+
+function setupProxy(context, entityName) {
+  const ability = userAbility(context.user);
+  const entity = context.entities[entityName];
+
+  const readFunctions = ["findUnique", "findFirst", "findMany"];
+
+  const entityProxy = {
+    get: function (target, prop) {
+
+      if (typeof target[prop] === 'function') {
+        if (readFunctions.includes(prop)) {
+          console.log("Before read check")
+          if (ability.cannot('read', entityName)) {
+            throw new Error("Read check failed!");
+          }
+        } // and so on for other verbs.
+
+        return target[prop].bind(target);
+      }
+    }
+  };
+
+  context.checked = { ...context.checked, [entityName]: new Proxy(entity, entityProxy) };
 }
