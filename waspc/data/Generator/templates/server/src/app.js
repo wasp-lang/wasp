522905316535ca58640072aff9b401e663042709
shayneczyzewski <shayne.czyzewski@gmail.com>
2022-06-07 16:10:42 -0400

    POC working, hacky

--- waspc/data/Generator/templates/server/src/app.js
+++ waspc/data/Generator/templates/server/src/app.js
@@ -19,6 +19,52 @@ app.use(express.json())
 app.use(express.urlencoded({ extended: false }))
 app.use(cookieParser())
 
+import passport from 'passport';
+app.use(passport.initialize());
+
+import prisma from './dbClient.js';
+import { sign } from './core/auth.js';
+import GoogleStrategy from 'passport-google-oauth2';
+
+console.log('setting up Google');
+
+passport.use(new GoogleStrategy.Strategy({
+  clientID: process.env['GOOGLE_CLIENT_ID'],
+  clientSecret: process.env['GOOGLE_CLIENT_SECRET'],
+  callbackURL: 'http://localhost:3001/oauth2/redirect/google',
+  scope: ['email'],
+  passReqToCallback: true
+}, async function (request, accessToken, refreshToken, profile, done) {
+  console.log("In Google OAuth callback")
+
+  let user = await prisma.user.findUnique({ where: { email: profile.email } });
+  if (!user) {
+    user = await prisma.user.create({ data: { email: profile.email, password: "password123!" } });
+  }
+
+  request.wasp = { ...request.wasp, user_id: user.id };
+
+  return done(null, user);
+}));
+
+// Redirect user to Google
+app.get('/login/federated/google', passport.authenticate('google', { session: false }));
+
+// Handle Google callback
+app.get('/oauth2/redirect/google',
+  passport.authenticate('google', {
+    session: false,
+    failureRedirect: 'http://localhost:3000/login',
+    failureMessage: true
+  }),
+  async function(req, res) {
+    console.log("In Passport success callback")
+
+    console.log("Tyring to find user_id: ", req.wasp.user_id);
+    const token = await sign(req.wasp.user_id);
+    res.redirect('http://localhost:3000/login?token=' + token);
+});
+
 app.use('/', indexRouter)
 
 // Custom error handler.
--- waspc/examples/todoApp/ext/pages/auth/Login.js
+++ waspc/examples/todoApp/ext/pages/auth/Login.js
@@ -2,8 +2,19 @@ import React from 'react'
 import { Link } from 'react-router-dom'
 
 import LoginForm from '@wasp/auth/forms/Login'
+import { setAuthToken } from '@wasp/api.js'
 
 const Login = () => {
+  const queryString = window.location.search;
+  const urlParams = new URLSearchParams(queryString);
+  const token = urlParams.get('token');
+
+  if (token) {
+    console.log('Setting token: ', token);
+    setAuthToken(token);
+    window.location.replace("http://localhost:3000/profile");
+  }
+
   return (
     <>
       <LoginForm/>
@@ -11,6 +22,8 @@ const Login = () => {
       <span>
         I don't have an account yet (<Link to="/signup">go to signup</Link>).
       </span>
+
+      <a class="button google" href="http://localhost:3001/login/federated/google">Sign in with Google</a>
     </>
   )
 }
