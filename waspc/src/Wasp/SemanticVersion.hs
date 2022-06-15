f6750b173120a40cbde746171f82354a47374d96
Martin Sosic <sosic.martin@gmail.com>
2022-06-15 12:04:49 +0200

    Limited Wasp node version to ^16.0.0 && <=16.15.0.
    In the process, I removed .nvmrc files from generated code and
    refactored SemanticVersion to follow npm spec.
    I also updated CI to use node 16.15.0, and not any 16.

--- waspc/src/Wasp/SemanticVersion.hs
+++ waspc/src/Wasp/SemanticVersion.hs
@@ -1,12 +1,21 @@
 module Wasp.SemanticVersion
   ( Version (..),
-    VersionBounds (..),
-    isVersionInBounds,
+    Operator (..),
+    Comparator (..),
+    ComparatorSet (..),
+    Range (..),
+    isVersionInRange,
+    rangeFromVersion,
+    rangeFromVersionsIntersection,
   )
 where
 
+import Data.List (intercalate)
+import Data.List.NonEmpty (NonEmpty, toList)
 import Numeric.Natural
 import Text.Printf (printf)
+
+-- Implements semantic version by following spec from https://github.com/npm/node-semver .
 
 data Version = Version
   { major :: Natural,
@@ -18,24 +27,51 @@ data Version = Version
 instance Show Version where
   show (Version mjr mnr ptc) = printf "%d.%d.%d" mjr mnr ptc
 
-data VersionBounds
-  = -- | Allows only the version exactly equal to the one specified in the bounds
-    Exact Version
-  | -- | Allows changes that do not modify the leftmost non-zero digit in major.minor.patch, as described
-    -- in node semver docs: https://github.com/npm/node-semver#caret-ranges-123-025-004
-    BackwardsCompatibleWith Version
+data Operator = Equal | LessThanOrEqual | BackwardsCompatibleWith
 
-instance Show VersionBounds where
-  show (BackwardsCompatibleWith version) = "^" ++ show version
-  show (Exact version) = show version
+instance Show Operator where
+  show Equal = "="
+  show LessThanOrEqual = "<="
+  show BackwardsCompatibleWith = "^"
 
-isVersionInBounds :: Version -> VersionBounds -> Bool
-isVersionInBounds version bounds = case bounds of
-  (BackwardsCompatibleWith reference) -> version >= reference && version < nextBreakingChangeVersion reference
-  (Exact reference) -> version == reference
+data Comparator = Comparator Operator Version
+
+instance Show Comparator where
+  show (Comparator op v) = show op ++ show v
+
+data ComparatorSet = ComparatorSet (NonEmpty Comparator)
+
+instance Show ComparatorSet where
+  show (ComparatorSet comps) = unwords $ show <$> toList comps
+
+data Range = Range (NonEmpty ComparatorSet)
+
+instance Show Range where
+  show (Range compSets) = intercalate " || " $ show <$> toList compSets
+
+instance Semigroup Range where
+  (Range csets1) <> (Range csets2) = Range $ csets1 <> csets2
 
 nextBreakingChangeVersion :: Version -> Version
 nextBreakingChangeVersion version = case version of
   (Version 0 0 x) -> Version 0 0 (succ x)
   (Version 0 x _) -> Version 0 (succ x) 0
   (Version x _ _) -> Version (succ x) 0 0
+
+doesVersionSatisfyComparator :: Version -> Comparator -> Bool
+doesVersionSatisfyComparator version (Comparator operator compVersion) = case operator of
+  Equal -> version == compVersion
+  LessThanOrEqual -> version <= compVersion
+  BackwardsCompatibleWith -> version >= compVersion && version < nextBreakingChangeVersion compVersion
+
+doesVersionSatisfyComparatorSet :: Version -> ComparatorSet -> Bool
+doesVersionSatisfyComparatorSet version (ComparatorSet comps) = all (doesVersionSatisfyComparator version) comps
+
+isVersionInRange :: Version -> Range -> Bool
+isVersionInRange version (Range compSets) = any (doesVersionSatisfyComparatorSet version) compSets
+
+rangeFromVersion :: (Operator, Version) -> Range
+rangeFromVersion = Range . pure . ComparatorSet . pure . uncurry Comparator
+
+rangeFromVersionsIntersection :: NonEmpty (Operator, Version) -> Range
+rangeFromVersionsIntersection = Range . pure . ComparatorSet . (uncurry Comparator <$>)
--- waspc/test/SemanticVersionTest.hs
+++ waspc/test/SemanticVersionTest.hs
@@ -1,39 +1,91 @@
 module SemanticVersionTest where
 
+import Data.List.NonEmpty (fromList)
+import Numeric.Natural
 import Test.Tasty.Hspec
 import Wasp.SemanticVersion
 
-spec_isVersionInBounds :: Spec
-spec_isVersionInBounds = do
-  it "Correctly determines satisfied exact version bounds" $
-    Version 1 2 3 `isVersionInBounds` Exact (Version 1 2 3)
-  it "Correctly determines unsatisfied exact version bounds" $
-    not $
-      any
-        (`isVersionInBounds` Exact (Version 1 2 3))
-        [Version 2 2 3, Version 1 3 3, Version 1 2 4]
-  it "Correctly determine satisfied backwards compatible version bounds" $
-    all
-      (`isVersionInBounds` BackwardsCompatibleWith (Version 1 2 3))
-      [Version 1 2 3, Version 1 2 4, Version 1 3 0]
-  it "Correctly determines unsatisfied backwards compatible version bounds" $
-    not $
-      any
-        (`isVersionInBounds` BackwardsCompatibleWith (Version 1 2 3))
-        [Version 1 2 0, Version 1 2 2, Version 2 0 0]
-  it "Correctly determines satisfied backwards compatible version bounds with 0.x versions" $
-    all
-      (`isVersionInBounds` BackwardsCompatibleWith (Version 0 2 3))
-      [Version 0 2 3, Version 0 2 4]
-  it "Correctly determines unsatisfied backwards compatible version bounds with 0.x versions" $
-    not $
-      any
-        (`isVersionInBounds` BackwardsCompatibleWith (Version 0 2 3))
-        [Version 0 0 0, Version 0 1 3, Version 0 2 0, Version 0 2 2, Version 0 3 0, Version 1 0 0]
-  it "Correctly determines satisfied backwards compatible version bounds with 0.0.x versions" $
-    Version 0 0 2 `isVersionInBounds` BackwardsCompatibleWith (Version 0 0 2)
-  it "Correctly determines unsatisfied backwards compatible version bounds with 0.0.x versions" $
-    not $
-      any
-        (`isVersionInBounds` BackwardsCompatibleWith (Version 0 0 2))
-        [Version 0 0 1, Version 0 0 3, Version 0 1 0, Version 1 0 0]
+spec_SemanticVersion :: Spec
+spec_SemanticVersion = do
+  describe "isVersionInRange" $ do
+    it "Recognizes only version v to be in range '=v'" $
+      testRange
+        (rangeFromVersion (Equal, Version 1 2 3))
+        [ ((0, 5, 5), False),
+          ((1, 0, 0), False),
+          ((1, 2, 3), True),
+          ((1, 2, 4), False),
+          ((1, 3, 0), False),
+          ((2, 0, 0), False)
+        ]
+    it "Recognizes only versions lesser or equal to v to be in range '<=v'" $
+      testRange
+        (rangeFromVersion (LessThanOrEqual, Version 1 2 3))
+        [ ((0, 5, 5), True),
+          ((1, 0, 0), True),
+          ((1, 2, 3), True),
+          ((1, 2, 4), False),
+          ((1, 3, 0), False),
+          ((2, 0, 0), False)
+        ]
+    describe "Recognizes only versions >=v but smaller than next breaking change to be in range '^v'" $ do
+      it "when v is of shape x.y.z where x != 0." $
+        testRange
+          (rangeFromVersion (BackwardsCompatibleWith, Version 1 2 3))
+          [ ((0, 5, 5), False),
+            ((1, 0, 0), False),
+            ((1, 2, 3), True),
+            ((1, 2, 4), True),
+            ((1, 3, 0), True),
+            ((2, 0, 0), False)
+          ]
+      it "when v is of shape 0.y.z where y != 0." $
+        testRange
+          (rangeFromVersion (BackwardsCompatibleWith, Version 0 2 3))
+          [ ((0, 0, 0), False),
+            ((0, 1, 3), False),
+            ((0, 2, 0), False),
+            ((0, 2, 2), False),
+            ((0, 2, 3), True),
+            ((0, 2, 4), True),
+            ((0, 3, 0), False),
+            ((1, 0, 0), False)
+          ]
+      it "when v is of shape 0.0.z." $
+        testRange
+          (rangeFromVersion (BackwardsCompatibleWith, Version 0 0 2))
+          [ ((0, 0, 1), False),
+            ((0, 0, 2), True),
+            ((0, 0, 3), False),
+            ((0, 1, 0), False),
+            ((1, 0, 0), False)
+          ]
+      it "Correctly works for complex version range." $
+        testRange
+          ( ( rangeFromVersionsIntersection
+                ( fromList
+                    [ (LessThanOrEqual, Version 1 2 3),
+                      (BackwardsCompatibleWith, Version 1 1 0)
+                    ]
+                )
+            )
+              <> rangeFromVersion (Equal, Version 0 5 6)
+          )
+          [ ((0, 5, 5), False),
+            ((0, 5, 6), True),
+            ((0, 5, 7), False),
+            ((1, 0, 9), False),
+            ((1, 1, 0), True),
+            ((1, 1, 9), True),
+            ((1, 2, 3), True),
+            ((1, 2, 4), False),
+            ((1, 3, 0), False),
+            ((2, 0, 0), False)
+          ]
+  where
+    testRange :: Range -> [((Natural, Natural, Natural), Bool)] -> Expectation
+    testRange range versionsWithResults =
+      ( (`isVersionInRange` range) . (\(x, y, z) -> Version x y z)
+          <$> map fst versionsWithResults
+      )
+        `shouldBe` map snd versionsWithResults
