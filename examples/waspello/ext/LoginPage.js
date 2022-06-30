ed6736012a68e093666460bc3e4ff21497d6ef5b
h4r1337 <72344536+h4r1337@users.noreply.github.com>
2022-06-30 15:47:10 +0530

    Update LoginPage.js

--- examples/waspello/ext/LoginPage.js
+++ examples/waspello/ext/LoginPage.js
@@ -9,8 +9,6 @@ import './Signup.css'
 
 const LoginPage = (props) => {
   return (
-    <>
-    <WaspSourceHeader name="Waspello" />
     <div className="auth-root-container">
 
       <img alt="Waspello" className="main-logo" src={mainLogo} />
@@ -23,7 +21,6 @@ const LoginPage = (props) => {
       </div>
 
     </div>
-    </>
   )
 }
 
