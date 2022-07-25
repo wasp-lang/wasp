a0f8821a6022cb08bf8aff203678138a627ae881
shayneczyzewski <shayne.czyzewski@gmail.com>
2022-07-25 15:34:02 -0400

    reload on logout

--- waspc/data/Generator/templates/react-app/src/auth/logout.js
+++ waspc/data/Generator/templates/react-app/src/auth/logout.js
@@ -3,7 +3,15 @@ import { invalidateAndRemoveQueries } from '../operations/resources'
 
 export default async function logout() {
   clearLocalStorage()
+
   // TODO(filip): We are currently invalidating and removing  all the queries, but
   // we should remove only the non-public, user-dependent ones.
   await invalidateAndRemoveQueries()
+
+  // NOTE: Unfortunately, in react-router v6 you cannot use navigate outside a React context.
+  // You also cannot manipulate anything like a history in the standard BrowserRouter.
+  // Using HistoryRouter is a workaround, but feels like a bit of a hack.
+  // In this case, I think a forced reload is an acceptable comprimise.
+  // Ref: https://github.com/remix-run/react-router/issues/8264
+  window.location.reload()
 }
