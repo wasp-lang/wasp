0dd8c93c1e589602d4fa601ff7cb70ce46223e06
Max Khamrovskyi <maksim36ua@gmail.com>
2022-08-06 10:11:25 +0300

    Add dev excuse application (#680)
    * Add dev excuse application
    
    * Update Readme
    
    * Add app description
    
    * Bugfix and formatting update
    
    * Move axios dependency
    
    * Fix typo
    
    * Code fix, image relocation, update indentation
    
    Co-authored-by: mkhamrov <mkhamrov@redhat.com>

---
+++ examples/tutorials/ItWaspsOnMyMachine/ext/MainPage.js
@@ -0,0 +1,63 @@
+import React, { useState } from 'react'
+import { useQuery } from '@wasp/queries'
+import getExcuse from '@wasp/queries/getExcuse'
+import getAllExcuses from '@wasp/queries/getAllExcuses'
+import saveExcuse from '@wasp/actions/saveExcuse'
+import './Main.css'
+
+const MainPage = () => {
+  const [currentExcuse, setCurrentExcuse] = useState({ text: "" })
+  const { data: excuses } = useQuery(getAllExcuses)
+
+  const handleGetExcuse = async () => {
+    try {
+      setCurrentExcuse(await getExcuse())
+    } catch (err) {
+      window.alert('Error while getting the excuse: ' + err.message)
+    }
+  }
+
+  const handleSaveExcuse = async () => {
+    if (currentExcuse.text) {
+      try {
+        await saveExcuse(currentExcuse)
+      } catch (err) {
+        window.alert('Error while saving the excuse: ' + err.message)
+      }
+    }
+  }
+
+  return (
+    <div className="grid grid-cols-2 text-3xl">
+      <div>
+        <div className="bg-white space-x-2 rounded-lg border border-gray-200 text-gray-900">
+          <div className="px-6 py-2 border-b border-gray-200 w-full rounded-t-lg bg-blue-600 text-white"> Current excuse: </div>
+          <Excuse excuse={currentExcuse} />
+          <button onClick={handleGetExcuse} className="items-center bg-blue-600 text-white hover:bg-blue-400 p-2 rounded w-auto"> Get excuse </button>
+          <button onClick={handleSaveExcuse} className="items-center bg-blue-600 text-white hover:bg-blue-400 p-2 rounded w-auto"> Save excuse </button>
+        </div>
+      </div>
+      <div>
+        <div className="bg-white rounded-lg border border-gray-200 text-gray-900">
+          <div className="px-6 py-2 border-b border-gray-200 w-full rounded-t-lg bg-blue-600 text-white"> Saved excuses: </div>
+          {excuses && <ExcuseList excuses={excuses} />}
+        </div>
+      </div>
+    </div>
+  )
+}
+
+const ExcuseList = (props) => {
+  if (!props.excuses?.length) return 'No saved excuses'
+  return props.excuses.map((excuse, idx) => <Excuse excuse={excuse} key={idx} />)
+}
+
+const Excuse = ({ excuse }) => {
+  return (
+    <div className="px-6 py-2 border-b border-gray-200 w-full">
+      {excuse.text}
+    </div>
+  )
+}
+
+export default MainPage
---
+++ examples/tutorials/ItWaspsOnMyMachine/ext/actions.js
@@ -0,0 +1,8 @@
+export const saveExcuse = async (excuse, context) => {
+  console.log(excuse.text);
+
+  var text = excuse.text;
+  return context.entities.Excuse.create({
+    data: { text }
+  })
+}
