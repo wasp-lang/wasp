d0eb6a1ebe97ecd85ce30bb7db68bcc46a2af91c
Craig McIlwrath <craigmc08@gmail.com>
2022-08-09 15:25:39 -0400

    Add all old parser tests as golden tests

--- waspc/test/Analyzer/ParserTest.hs
+++ waspc/test/Analyzer/ParserTest.hs
@@ -5,10 +5,30 @@ import qualified Data.ByteString.Lazy.Char8 as BSC
 import System.FilePath (replaceExtension, takeBaseName)
 import Test.Tasty (TestTree, testGroup)
 import Test.Tasty.Golden (findByExtension, goldenVsStringDiff)
+import Test.Tasty.Hspec
 import Wasp.Analyzer.Parser hiding (withCtx)
 import Wasp.Analyzer.Parser.ParseError (getErrorMessageAndCtx)
 import Wasp.Analyzer.Parser.SourceRegion (SourceRegion (SourceRegion))
 import Wasp.Util (indent)
+
+spec_IsValidWaspIdentifier :: Spec
+spec_IsValidWaspIdentifier = do
+  it "Correctly identifies valid identifiers" $ do
+    all isValidWaspIdentifier ["foo", "foo12", "_12", "_"]
+  it "Correctly identifies invalid identifiers" $ do
+    not (any isValidWaspIdentifier ["true", "12", "12_", "1foo"])
+
+spec_ParseExpression :: Spec
+spec_ParseExpression = do
+  it "Parses identifiers" $ do
+    parseExpression "foo" `shouldBe` Right (Var "foo")
+    parseExpression "foo12" `shouldBe` Right (Var "foo12")
+    parseExpression "_" `shouldBe` Right (Var "_")
+    parseExpression "_12" `shouldBe` Right (Var "_12")
+  it "Parses double literals" $ do
+    parseExpression "0.5" `shouldBe` Right (DoubleLiteral 0.5)
+  it "Parses int literals" $ do
+    parseExpression "5" `shouldBe` Right (IntegerLiteral 5)
 
 test_Parser :: IO TestTree
 test_Parser = do
@@ -32,7 +52,7 @@ showResult :: Either ParseError AST -> String
 showResult (Left err) =
   let (message, ctx) = getErrorMessageAndCtx err
       locationStr = "at " ++ showCtx ctx
-   in "Parse error " ++ locationStr ++ indent 2 message
+   in "Parse error " ++ locationStr ++ ":\n" ++ indent 2 message ++ "\n"
 showResult (Right ast) = showAST ast
 
 showAST :: AST -> String
@@ -49,7 +69,7 @@ showExpr (WithCtx ctx expr) = "(" ++ withCtx (exprName expr) ctx ++ showDetails 
   where
     exprName (Dict _) = "Dict"
     exprName (List _) = "List"
-    exprName (Tuple _) = "List"
+    exprName (Tuple _) = "Tuple"
     exprName (StringLiteral _) = "String"
     exprName (IntegerLiteral _) = "Integer"
     exprName (DoubleLiteral _) = "Double"
@@ -58,16 +78,18 @@ showExpr (WithCtx ctx expr) = "(" ++ withCtx (exprName expr) ctx ++ showDetails 
     exprName (Var _) = "Var"
     exprName (Quoter _ _) = "Quoter"
 
+    showDetails (Dict []) = ""
     showDetails (Dict entries) = "\n" ++ indent 2 (concatMap showEntry entries)
+    showDetails (List []) = ""
     showDetails (List values) = "\n" ++ indent 2 (concatMap showExpr values)
     showDetails (Tuple (a, b, cs)) = "\n" ++ indent 2 (concatMap showExpr (a : b : cs))
     showDetails (StringLiteral s) = showLiteral s
     showDetails (IntegerLiteral n) = showLiteral n
     showDetails (DoubleLiteral n) = showLiteral n
     showDetails (BoolLiteral b) = showLiteral b
-    showDetails (ExtImport name path) = " " ++ showExtImportName name ++ " path=" ++ show path ++ "\n"
+    showDetails (ExtImport name path) = " " ++ showExtImportName name ++ " path=" ++ show path
     showDetails (Var v) = " variable=" ++ v
-    showDetails (Quoter tag contents) = " tag=" ++ tag ++ "\n" ++ indent 2 contents
+    showDetails (Quoter tag contents) = " tag=" ++ tag ++ "\n" ++ indent 2 ("{=" ++ contents ++ "=}") ++ "\n"
 
     showEntry (key, value) = "(DictEntry key=" ++ key ++ "\n" ++ indent 2 (showExpr value) ++ ")\n"
 
