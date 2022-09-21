4a94c9851e3a1203ac92d17d98f4814dffc7b0e5
Filip Sodić <filip.sodic@gmail.com>
2022-09-21 17:40:06 +0200

    Implement optimistic updates
    * Remove flickering fixes for optimistic updates
    
    * Add optimistic updates to Task.js
    
    * First round of code review feedback
    
    * Remove react query's additional options from hook
    
    * Futher review feedback and code docs
    
    * Fix and document error handling for optimistic updates
    
    * Further document optimistic update mechanisms
    
    * Update optimistic update docs
    
    * Improve comment for useAction
    
    * Further improve comments in actions/index
    
    * Heavily comment actions/index
    
    * Fix capitalization in action/index
    
    * Add types to actions/index
    
    * Fix jsdoc types in actions/index
    
    * Comment testing sleeps
    
    * Comment sleep function in todoApp/ext/actions
    
    * Properly handle type errors in actions/index
    
    * Fix formatting in actions/index
    
    * Move updateFn change to optimistic update instantiation
    
    * Extract common functionality in actions/index
    
    * Fix formatting in actions/index
    
    * Add jsdoc types to actions/index
    
    * Revise jsdoc in actions/index
    
    * Address PR comments
    
    * Update e2e tests
    
    * Mention optimistic updates in cache invalidation

--- waspc/examples/todoApp/ext/actions.js
+++ waspc/examples/todoApp/ext/actions.js
@@ -1,6 +1,7 @@
 import HttpError from '@wasp/core/HttpError.js'
 import { getSomeResource } from './serverSetup.js'
 
+
 export const createTask = async (task, context) => {
   if (!context.user) {
     throw new HttpError(401)
@@ -24,6 +25,10 @@ export const updateTaskIsDone = async ({ id, isDone }, context) => {
   if (!context.user) {
     throw new HttpError(401)
   }
+
+  // Uncomment to test optimistic updates
+  // const sleep = (ms) => new Promise(res => setTimeout(res, ms))
+  // await sleep(3000);
 
   const Task = context.entities.Task
   return Task.updateMany({
