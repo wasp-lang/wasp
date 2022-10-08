9a49486bee31471566d4a72085273a1497c6c6cb
Gautier DI FOLCO <gautier.difolco@gmail.com>
2022-10-08 17:28:10 +0200

    fixup! Add cli update command

--- waspc/cli/src/Wasp/Cli/Command/Update.hs
+++ waspc/cli/src/Wasp/Cli/Command/Update.hs
@@ -4,8 +4,10 @@ module Wasp.Cli.Command.Update
 where
 
 import Control.Monad.IO.Class (liftIO)
+import System.Environment
 import System.Exit (ExitCode (..), exitWith)
-import System.Process (system)
+import System.IO
+import System.Info
 import Wasp.Cli.Command (Command)
 import Wasp.Cli.Command.Message (cliSendMessageC)
 import qualified Wasp.Cli.Common as Common
@@ -14,9 +16,28 @@ import qualified Wasp.Message as Msg
 update :: Command ()
 update = do
   cliSendMessageC $ Msg.Start "Updating wasp..."
-  status <- liftIO $ system "curl -sSL https://get.wasp-lang.dev/installer.sh | sh -- --force"
+  ensureWritability
+  status <- liftIO $ system "curl -sSL " <> archiveUrl <> " | tar xvzf - -C ~/.local/share/wasp"
   if status == ExitSuccess
     then cliSendMessageC $ Msg.Success "Wasp updated!"
     else do
       cliSendMessageC $ Msg.Success "Update failure"
       liftIO $ exitWith status
+
+ensureWritability :: Command ()
+ensureWritability = do
+  exe <- liftIO getExecutablePath
+  isWritable <- liftIO $ withFile exe WriteMode hIsWritable
+  unless isWritable $
+    failedOn "unable to rewrite wasp binary"
+
+failedOn :: String -> Command ()
+failedOn reason = do
+  cliSendMessageC $ Msg.Success $ "Update failure: " <> reason
+  liftIO $ exitWith status
+
+archiveUrl :: String
+archiveUrl = "https://github.com/wasp-lang/wasp/releases/latest/download/" <> archiveName
+
+archiveName :: String
+archiveName = "wasp-" <> os <> "-" <> arch <> ".tar.gz"
