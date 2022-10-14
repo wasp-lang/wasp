8cd522029329934ade7e532e302361e553bfaba7
Filip Sodić <filip.sodic@gmail.com>
2022-10-14 15:16:35 +0200

    Remove rudandant aboslute API URLs (#755)
    * Remove rudandant aboslute API URLs
    
    * Remove redundant imports from js files
    
    * Update e2e tests

--- waspc/data/Generator/templates/react-app/src/operations/index.js
+++ waspc/data/Generator/templates/react-app/src/operations/index.js
@@ -1,9 +1,8 @@
 import api, { handleApiError } from '../api.js'
-import config from '../config.js'
 
 export async function callOperation(operationRoute, args) {
   try {
-    const response = await api.post(config.apiUrl + '/' + operationRoute, args)
+    const response = await api.post(`/${operationRoute}`, args)
     return response.data
   } catch (error) {
     handleApiError(error)
