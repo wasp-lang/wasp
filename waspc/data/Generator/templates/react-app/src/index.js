1b129188b0bc1fe76cf941377d62175c64b3c8a4
Filip Sodić <filip.sodic@gmail.com>
2022-10-25 11:41:03 +0200

    Update React Query to version 4 (#782)
    * Update to React Query 4, pass 1
    
    * Fix array key for React Query 4
    
    * Fix logout bug after React Query 4 update
    
    * Update e2e tests

--- waspc/data/Generator/templates/react-app/src/index.js
+++ waspc/data/Generator/templates/react-app/src/index.js
@@ -1,7 +1,7 @@
 {{={= =}=}}
 import React from 'react'
 import ReactDOM from 'react-dom'
-import { QueryClientProvider } from 'react-query'
+import { QueryClientProvider } from '@tanstack/react-query'
 
 import router from './router'
 import { 
