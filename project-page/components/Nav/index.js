5ca1000be23cb4414a86faf658d43854c422697b
Matija Sosic <matija.sosic@gmail.com>
2022-10-31 08:57:44 +0100

    Added hamburger button, not working yet.

--- project-page/components/Nav/index.js
+++ project-page/components/Nav/index.js
@@ -4,6 +4,79 @@ import { Star } from 'react-feather'
 
 const Nav = () => {
 
+  const Logo = () => (
+    <div className='flex flex-shrink-0 items-center'>
+      <Link href='/' as='/'>
+        <a>
+          <Image
+            src='/wasp-logo.png'
+            width={35}
+            height={35}
+            alt='Wasp Logo'
+          />
+        </a>
+      </Link>
+      <span className='ml-3 font-semibold text-lg'>
+        Wasp <sup className='text-base text-yellow-500'>βeta</sup>
+      </span>
+    </div>
+  )
+
+  const GitHubButton = () => (
+    <a href='/'
+      className={`
+        px-2.5 py-1 rounded
+        hover:bg-neutral-200
+        transition ease-out duration-200
+        hidden lg:flex items-center space-x-2 text-xs
+        group
+      `}
+    >
+      <div
+        className={`
+          flex h-3 w-3 items-center justify-center
+          group-hover:h-4 group-hover:w-4
+          group-hover:text-yellow-500
+        `}
+      >
+        <Star strokeWidth={2} />
+      </div>
+      <span className='truncate'>Star us on GitHub</span>
+    </a>
+  )
+
+  const HamburgerButton = () => (
+    <div className='absolute inset-y-0 left-0 px-2 flex items-center lg:hidden'>
+      <button
+        className={`
+          inline-flex items-center p-2
+          rounded-md hover:bg-gray-50
+          focus:ring-yellow-500 focus:outline-none focus:ring-2 focus:ring-inset
+        `}
+      >
+
+        <span className="sr-only">Open menu</span>
+
+        <svg
+          className="block h-6 w-6"
+          xmlns="http://www.w3.org/2000/svg"
+          fill="none"
+          viewBox="0 0 24 24"
+          stroke="currentColor"
+          aria-hidden="true"
+        >
+          <path
+            strokeLinecap="round"
+            strokeLinejoin="round"
+            strokeWidth="2"
+            d="M4 6h16M4 12h16M4 18h16"
+          />
+        </svg>
+      </button>
+    </div>
+
+  )
+
   return (
     <>
       <div className='sticky top-0 z-50'>
@@ -15,7 +88,7 @@ const Nav = () => {
               lg:container lg:px-16 xl:px-20
             '
           >
-            {/* TODO: HamburgerButton goes here */}
+            <HamburgerButton />
             <div className='
                 flex flex-1
                 items-center justify-center
@@ -24,21 +97,7 @@ const Nav = () => {
               '
             >
               <div className='flex items-center'> {/* Navbar left side */}
-                <div className='flex flex-shrink-0 items-center'> {/* Logo */}
-                  <Link href='/' as='/'>
-                    <a>
-                      <Image
-                        src='/wasp-logo.png'
-                        width={35}
-                        height={35}
-                        alt='Wasp Logo'
-                      />
-                    </a>
-                  </Link>
-                  <span className='ml-3 font-semibold text-lg'>
-                    Wasp <sup className='text-base text-yellow-500'>βeta</sup>
-                  </span>
-                </div> {/* EOF logo */}
+                <Logo />
 
                 <div className='hidden pl-4 sm:ml-6 sm:space-x-4 lg:flex'> {/* Left items */}
                   <Link href='/docs'>
@@ -70,28 +129,7 @@ const Nav = () => {
               </div> {/* EOF left side */}
 
               <div className='flex items-center gap-2'> {/* Navbar right side */}
-                {/* GitHub button */}
-                <a href='/'
-                  className={`
-                    px-2.5 py-1 rounded
-                    hover:bg-neutral-200
-                    transition ease-out duration-200
-                    hidden lg:flex items-center space-x-2 text-xs
-                    group
-                  `}
-                >
-                  <div
-                    className={`
-                      flex h-3 w-3 items-center justify-center
-                      group-hover:h-4 group-hover:w-4
-                      group-hover:text-yellow-500
-                    `}
-                  >
-                    <Star strokeWidth={2} />
-                  </div>
-                  <span className='truncate'>Star us on GitHub</span>
-                </a>{/* EOF GitHub button */}
-                
+                <GitHubButton />
               </div> {/* EOF right side */}
 
             </div>
