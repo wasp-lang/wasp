81e10bf7df955584431dbc39c4c4cff3898d17ba
Matija Sosic <matija.sosic@gmail.com>
2022-11-10 14:33:51 +0100

    more progress.

--- project-page/components/Benefits.js
+++ project-page/components/Benefits.js
@@ -6,7 +6,7 @@ const Lang = () => (
   <>
     <span className='underline decoration-yellow-500 font-bold'>
       language
-    </span>;
+    </span>
   </>
 )
 
@@ -16,8 +16,8 @@ const Benefits = () => {
       <div className='grid grid-cols-12'>
         <div className='col-span-12 text-center'>
           <h2 className='text-xl lg:text-2xl text-neutral-700 mb-4'>
-            Yet another web framework {'=>'} Except it is
-            a <Lang />
+            Yet another web framework. Except it is
+            a <Lang />.
           </h2>
           <p className='text-neutral-500'>
             Don't worry, it takes less than 30 minutes to learn.
@@ -26,16 +26,27 @@ const Benefits = () => {
       </div>
 
       <dl className='grid grid-cols-1 lg:grid-cols-4'>
-        <div className='mb-10 md:mb-0'>
+
+        <div className='mb-10 md:mb-0 space-y-4'>
           <div className='flex items-center'>
-            <Terminal />
-            <dt className=''>
+            <div
+              className={`
+                inline-flex h-8 w-8 rounded-md
+                items-center justify-center
+                text-yellow-500 bg-neutral-700
+              `}
+            >
+              <Terminal size={20} />
+            </div>
+            <dt className='ml-4'>
               Benefit name
             </dt>
           </div>
-
-          <p className=''>This is benefit description.</p>
-
+          <p className=''>
+            This is benefit description.
+            It has multiple lines and explains several things about
+            this benefit.
+          </p>
         </div>
         
       </dl>
