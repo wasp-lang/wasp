28c26be2e2a12d69a53b1bb31e5220ce1d0e2888
Matija Sosic <matija.sosic@gmail.com>
2022-11-15 10:20:54 +0100

    more progress on hiw section.

--- project-page/components/Hero.js
+++ project-page/components/Hero.js
@@ -9,8 +9,8 @@ const StartIcon = () => (
   <svg
     xmlns="http://www.w3.org/2000/svg"
     width="16" height="16" viewBox="0 0 24 24" fill="none"
-    stroke="currentColor" stroke-width="2" stroke-linecap="round"
-    stroke-linejoin="round" opacity="0.5"
+    stroke="currentColor" strokeWidth="2" strokeLinecap="round"
+    strokeLinejoin="round" opacity="0.5"
   >
     <polyline points="13 17 18 12 13 7"></polyline>
     <polyline points="6 17 11 12 6 7"></polyline>
--- project-page/components/HowItWorks.js
+++ project-page/components/HowItWorks.js
@@ -4,16 +4,76 @@ const HowItWorks = () => {
   return (
     <SectionContainer>
       <div className='grid grid-cols-12'>
+
         <div className='col-span-12 lg:col-span-5'>
           <h2 className='text-xl lg:text-2xl text-neutral-700 mb-4'>
-            How it works?
+            Under the hood 🚕
           </h2>
           <p className='text-neutral-500'>
             Given <code>.wasp</code> and <code>.js(x)/.css/...</code>, source files,
             Wasp compiler generates the full source of your web app in
             the target stack - front-end, back-end and deployment.
           </p>
+
+          {/* Features */}
+          <div className='py-8'>
+            <dl className='grid grid-cols-12 gap-y-4 md:gap-8'>
+
+              <div className='col-span-12 md:col-span-6'>
+                <div className='lg:mt-5'>
+                  <dt>
+                    <h4 className='mb-4'>Typescript support</h4>
+                    <p className='text-neutral-500'>
+                      This is some description of our cool Typescript support.
+                    </p>
+                    <a href='/' className='mt-3 block cursor-pointer text-sm text-neutral-500'>
+                      <span>Learn more</span>
+                    </a>
+                  </dt>
+                </div>
+              </div>
+
+              <div className='col-span-12 md:col-span-6'>
+                <div className='lg:mt-5'>
+                  <dt>
+                    <h4 className='mb-4'>Awesome CLI</h4>
+                    <p className='text-neutral-500'>
+                      Our CLI is super helpful and you will feel amazing using it.
+                    </p>
+                    <a href='/' className='mt-3 block cursor-pointer text-sm text-neutral-500'>
+                      <span>Learn more</span>
+                    </a>
+                  </dt>
+                </div>
+              </div>
+
+              <div className='col-span-12 md:col-span-6'>
+                <div className='lg:mt-5'>
+                  <dt>
+                    <h4 className='mb-4'>Custom made LSP</h4>
+                    <p className='text-neutral-500'>
+                      Our LSP is here, enjoy it VS Code.
+                    </p>
+                    <a href='/' className='mt-3 block cursor-pointer text-sm text-neutral-500'>
+                      <span>Learn more</span>
+                    </a>
+                  </dt>
+                </div>
+              </div>
+
+            </dl>
+
+          </div> {/* EOF Features */}
         </div>
+
+        <div className='col-span-12 lg:col-span-7 xl:col-span-6 xl:col-start-7'>
+          <img
+            className=''
+            src='/wasp-compilation-diagram.png'
+            alt='React'
+          />
+        </div>
+
       </div>
     </SectionContainer>
     
