3b5d6407b28853217b6146eaa6a3f5ef22a83213
Matija Sosic <matija.sosic@gmail.com>
2022-11-16 11:33:37 +0100

    Added content for showcases.

--- project-page/components/ShowcaseGallery.js
+++ project-page/components/ShowcaseGallery.js
@@ -1,13 +1,20 @@
+import classNames from 'classnames'
 import Image from 'next/image'
 import SectionContainer from './Layouts/SectionContainer'
 
-const Tag = ({ text, color }) => (
-  <span className={`text-sm ${color}`}>
+const Tag = ({ text, className }) => (
+  <span
+    className={classNames(`
+      text-sm border rounded-md px-2.5 py-0.5
+
+    `, className
+    )}
+  >
     {text}
   </span>
 )
 
-const ShowcaseItem = ({ url, thumb, title, description }) => (
+const ShowcaseItem = ({ url, thumb, title, description, children }) => (
   <div>
     <a href={url}>
       <div className='group inline-block min-w-full'>
@@ -30,7 +37,7 @@ const ShowcaseItem = ({ url, thumb, title, description }) => (
           <h3 className='text-xl'>{title}</h3>
 
           <div className='flex space-x-2'>
-            <Tag text='hackathon' color='text-yellow-500' />
+            {children}
           </div>
 
           <p className='text-base'>{description}</p>
@@ -66,22 +73,30 @@ const ShowcaseGallery = () => {
           url='/'
           thumb='/showcase/farnance-dashboard.png'
           title='Farnance: SaaS marketplace for farmers'
-          description='Check out how we made this first showcase post.'
-        />
+          description="See how Julian won HackLBS 2021 among 250 participants and why Wasp was instrumental for the team's victory."
+        >
+          <Tag text='hackathon' className='text-yellow-600 border-yellow-600 bg-yellow-50' />
+          <Tag text='material-ui' className='text-blue-500 border-blue-500 bg-slate-50' />
+        </ShowcaseItem>
 
         <ShowcaseItem
           url='/'
-          thumb='/wasp-compilation-diagram.png'
-          title='Second showcase post'
-          description='Check out how we made this second showcase post.'
-        />
+          thumb='/showcase/grabbit-hero.png'
+          title='Grabbit: Easily manage dev environments'
+          description='See how Michael built and deployed an internal tool for managing dev resources at StudentBeans.'
+        >
+          <Tag text='internal-tools' className='text-green-600 border-green-600 bg-green-50' />
+        </ShowcaseItem>
 
         <ShowcaseItem
           url='/'
-          thumb='/wasp-compilation-diagram.png'
-          title='Third showcase post'
-          description='Check out how we made this third showcase post.'
-        />
+          thumb='/showcase/amicus-landing.png'
+          title='Amicus: Task and workflow management for legal teams'
+          description='See how Erlis rolled out fully-fledged SaaS as a team of one in record time and got first paying customers.'
+        >
+          <Tag text='startup' className='text-fuchsia-600 border-fuchsia-600 bg-fuchsia-50' />
+          <Tag text='material-ui' className='text-blue-500 border-blue-500 bg-slate-50' />
+        </ShowcaseItem>
 
       </div>
 
