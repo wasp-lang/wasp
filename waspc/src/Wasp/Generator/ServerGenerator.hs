627c6325bce34546d047173487a2989cdec9d69e
Filip Sodić <filip.sodic@fer.hr>
2022-11-23 14:53:15 +0100

    Remove server-specific typescript stuff

--- waspc/src/Wasp/Generator/ServerGenerator.hs
+++ waspc/src/Wasp/Generator/ServerGenerator.hs
@@ -124,10 +124,7 @@ npmDepsForWasp spec =
             ("helmet", "^6.0.0"),
             ("patch-package", "^6.4.7"),
             ("uuid", "^9.0.0"),
-            ("lodash", "^4.17.21"),
-            -- TODO: make optional
-            -- TODO: types for node
-            ("typescript", "^4.8.4")
+            ("lodash", "^4.17.21")
           ]
           ++ depsRequiredByPassport spec
           ++ depsRequiredByJobs spec,
