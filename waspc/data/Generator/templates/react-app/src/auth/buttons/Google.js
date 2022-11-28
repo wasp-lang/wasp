210179eb89ed2f5acf164c8fee39277dca68be66
shayneczyzewski <shayne.czyzewski@gmail.com>
2022-11-28 13:56:12 -0500

    start of customizing google login button

--- waspc/data/Generator/templates/react-app/src/auth/buttons/Google.js
+++ waspc/data/Generator/templates/react-app/src/auth/buttons/Google.js
@@ -4,8 +4,11 @@ export const googleSignInUrl = `${config.apiUrl}/auth/external/google/login`
 
 export function GoogleSignInButton(props) {
   return (
-    <a href={googleSignInUrl}>
-      <img alt="Sign in with Google" height={props?.height || 40} src="/images/btn_google_signin_dark_normal_web@2x.png" />
+    <a {...props.a} href={googleSignInUrl}>
+      <img alt="Sign in with Google"
+        style={{ height: 40 }}
+        {...props.img}
+        src="/images/btn_google_signin_dark_normal_web@2x.png" />
     </a>
   )
 }
