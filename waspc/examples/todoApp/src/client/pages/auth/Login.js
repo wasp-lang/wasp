e1add3fc3381a7b706fd160dcfb21db565a14413
shayneczyzewski <shayne.czyzewski@gmail.com>
2022-12-14 13:13:27 -0500

    do not enable social auth in example by default

--- waspc/examples/todoApp/src/client/pages/auth/Login.js
+++ waspc/examples/todoApp/src/client/pages/auth/Login.js
@@ -2,8 +2,8 @@ import React from 'react'
 import { Link } from 'react-router-dom'
 
 import LoginForm from '@wasp/auth/forms/Login'
-import { SignInButton as GoogleSignInButton } from '@wasp/auth/buttons/Google'
-import { SignInButton as GitHubSignInButton } from '@wasp/auth/buttons/GitHub'
+// import { SignInButton as GoogleSignInButton } from '@wasp/auth/buttons/Google'
+// import { SignInButton as GitHubSignInButton } from '@wasp/auth/buttons/GitHub'
 
 const Login = () => {
   return (
@@ -14,10 +14,10 @@ const Login = () => {
         I don't have an account yet (<Link to="/signup">go to signup</Link>).
       </span>
 
-      <div>
+      {/* <div>
         <GoogleSignInButton/>
         <GitHubSignInButton/>
-      </div>
+      </div> */}
     </>
   )
 }
