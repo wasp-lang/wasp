b1e3279adcd4b4216744c78ca3133ecbd39501b4
shayneczyzewski <shayne.czyzewski@gmail.com>
2022-12-19 16:52:56 -0500

    using JSON serialization as GET param

--- waspc/data/Generator/templates/react-app/src/operations/index.js
+++ waspc/data/Generator/templates/react-app/src/operations/index.js
@@ -11,7 +11,9 @@ export async function callAction(actionRoute, args) {
 
 export async function callQuery(queryRoute, args) {
   try {
-    const response = await api.get(`/${queryRoute}`, { params: args })
+    // TODO: This is to help us preserve the types of the args.
+    // In future, we should get type info from users via wasp file.
+    const response = await api.get(`/${queryRoute}`, { params: { args: JSON.stringify(args) } })
     return response.data
   } catch (error) {
     handleApiError(error)
--- waspc/data/Generator/templates/server/src/routes/operations/_query.js
+++ waspc/data/Generator/templates/server/src/routes/operations/_query.js
@@ -4,15 +4,7 @@ import { handleRejection } from '../../utils.js'
 import {= operationName =} from "{= operationImportPath =}"
 
 export default handleRejection(async (req, res) => {
-  const args = req.query || {}
-
-  // TODO: Have a flag for if we should autoconvert numbers.
-  // Just doing some testing right now.
-  if (true) {
-    if (req.query.id) {
-      req.query.id = parseInt(req.query.id);
-    }
-  }
+  const args = JSON.parse(req?.query?.args || '{}')
 
   const context = {
     {=# userEntityLower =}
