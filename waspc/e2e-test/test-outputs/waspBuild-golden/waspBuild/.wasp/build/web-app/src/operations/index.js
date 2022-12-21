6a6bec2d4f9d60d5b20aa48712106162516a2c4a
shayneczyzewski <shayne.czyzewski@gmail.com>
2022-12-21 11:15:39 -0500

    fix e2e

--- waspc/e2e-test/test-outputs/waspBuild-golden/waspBuild/.wasp/build/web-app/src/operations/index.js
+++ waspc/e2e-test/test-outputs/waspBuild-golden/waspBuild/.wasp/build/web-app/src/operations/index.js
@@ -1,11 +1,21 @@
 import api, { handleApiError } from '../api.js'
 
-export async function callOperation(operationRoute, args) {
+export async function callAction(actionRoute, args) {
   try {
-    const response = await api.post(`/${operationRoute}`, args)
+    const response = await api.post(`/${actionRoute}`, args)
     return response.data
   } catch (error) {
     handleApiError(error)
   }
 }
 
+export async function callQuery(queryRoute, args) {
+  try {
+    // TODO: This is to help us preserve the types of the args.
+    // In future, we should get type info from users via wasp file.
+    const response = await api.get(`/${queryRoute}`, { params: { args: JSON.stringify(args) } })
+    return response.data
+  } catch (error) {
+    handleApiError(error)
+  }
+}
