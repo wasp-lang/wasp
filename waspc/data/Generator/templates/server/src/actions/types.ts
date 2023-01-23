587258f7dc972bac6af63ff6c734aa7e011ae32e
Filip Sodić <filip.sodic@fer.hr>
2023-01-23 17:03:46 +0100

    Add duplication comment

--- waspc/data/Generator/templates/server/src/actions/types.ts
+++ waspc/data/Generator/templates/server/src/actions/types.ts
@@ -1,4 +1,6 @@
 {{={= =}=}}
+{=! TODO: This template is exactly the same at the moment as one for action
+          types, consider whether it makes sense to address this in the future. =}
 import {
   {=# allEntities =}
   {= . =},
