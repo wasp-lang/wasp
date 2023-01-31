3f107d1089f25f70d39227d9cc02edd9d89adfab
Filip Sodić <filip.sodic@fer.hr>
2023-01-31 15:20:39 +0100

    Remove leftover testing artifacts

--- waspc/examples/todoApp/src/server/queries.ts
+++ waspc/examples/todoApp/src/server/queries.ts
@@ -1,7 +1,6 @@
 import HttpError from '@wasp/core/HttpError.js'
 import { Task } from '@wasp/entities'
 import {
-  GetFoo,
   GetNumTasks,
   GetTask,
   GetTasks
@@ -22,15 +21,6 @@ export const getTasks: GetTasks<Task[]> = async (args, context) => {
     }
   )
   return tasks
-}
-
-export const getFoo: GetFoo<number> = async (args, context) => {
-  if (!context.user) {
-    throw new HttpError(401)
-  }
-  const x = await context.entities.Foo.count()
-  const y = await context.entities.Task.count()
-  return x + y
 }
 
 export const getNumTasks: GetNumTasks<number> = async (args, context) => {
