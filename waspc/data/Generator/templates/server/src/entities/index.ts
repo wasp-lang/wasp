d22fb47f162ea2b476c789e0d90ec876574873a3
Filip Sodić <filip.sodic@fer.hr>
2023-02-09 13:22:15 +0100

    Remove leftover code

--- waspc/data/Generator/templates/server/src/entities/index.ts
+++ waspc/data/Generator/templates/server/src/entities/index.ts
@@ -22,22 +22,3 @@ export type EntityName =
   | "{= name =}"
   {=/ entities =}
   | never
-
-// Wasp internally uses the following types for typing entity maps in
-// operation contexts.
-//
-// We must explicitly tag all entities with their name to avoid issues with
-// structural typing. See https://github.com/wasp-lang/wasp/pull/982 for details.
-
-{=# entities =}
-export type {= internalTypeName =} = WithName<{= name =}, "{= name =}">
-{=/ entities =}
-
-export type _Entity = 
-  {=# entities =}
-  | {= internalTypeName =}
-  {=/ entities =}
-  | never
-
-type WithName<E extends Entity, Name extends EntityName> = 
-  E & { _entityName: Name }
