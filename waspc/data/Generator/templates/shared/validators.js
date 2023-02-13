f2bd8995e2f8661c590601d167aa960813da7bd9
Mihovil Ilakovac <mihovil@ilakovac.com>
2023-02-13 22:40:03 +0100

    Simplify validator, fixes tsconfig bug

--- waspc/data/Generator/templates/shared/validators.js
+++ waspc/data/Generator/templates/shared/validators.js
@@ -1,24 +1,15 @@
 export function isValidAbsoluteURL(rawUrl) {
     try {
-        new URL(rawUrl);
+        const url = new URL(rawUrl);
+        /*
+            URL constructor will parse some invalid absolute URLs as valid URLs
+            so we need to do some extra checks.
+
+            Example: "localhost:3000" will be parsed as URL with protocol of
+            "localhost:" and host of "3000"
+        */
+        return url.protocol === "http:" || url.protocol === "https:";
     } catch (e) {
         return false;
     }
-
-    /*
-        URL constrctor will parse some invalid absolute URLs as valid URLs
-        so we need to do some extra checks to make sure the URL is valid.
-
-        Example: "localhost:3000" will be parsed as URL with protocol of
-        "localhost:" and host of "3000"
-    */
-    const urlParts = rawUrl.split("//");
-    const isProtocolSpecified = urlParts.length > 1;
-    if (!isProtocolSpecified) {
-        return false;
-    }
-
-    const protocol = urlParts[0];
-
-    return protocol === "http:" || protocol === "https:";
 }
