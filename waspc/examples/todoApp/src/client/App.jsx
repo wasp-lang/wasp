473b89ebda6b3335a77903b34f6cb684ec06ed60
Mihovil Ilakovac <mihovil@ilakovac.com>
2023-02-20 14:28:03 +0100

    Use Link in root component in example app

--- waspc/examples/todoApp/src/client/App.jsx
+++ waspc/examples/todoApp/src/client/App.jsx
@@ -1,8 +1,12 @@
+import { Link } from "react-router-dom";
+
 export function App({ children }) {
     return (
         <div className="p-6">
             <header className="mb-6">
-                <h1 className="text-3xl font-bold">ToDo App</h1>
+                <h1 className="text-3xl font-bold">
+                    <Link to="/">ToDo App</Link>
+                </h1>
             </header>
             {children}
         </div>
