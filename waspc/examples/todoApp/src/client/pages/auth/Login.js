dbaddb45132ff53ecf1dc0bb17afdf05d65c3ff4
Mihovil Ilakovac <mihovil@ilakovac.com>
2023-02-21 15:31:08 +0100

    Updates social auth button styles
    Signed-off-by: Mihovil Ilakovac <mihovil@ilakovac.com>

--- waspc/examples/todoApp/src/client/pages/auth/Login.js
+++ waspc/examples/todoApp/src/client/pages/auth/Login.js
@@ -2,23 +2,21 @@ import React from 'react'
 import { Link } from 'react-router-dom'
 
 import LoginForm from '@wasp/auth/forms/Login'
-// import { SignInButton as GoogleSignInButton } from '@wasp/auth/helpers/Google'
-// import { SignInButton as GitHubSignInButton } from '@wasp/auth/helpers/GitHub'
+import { SignInButton as GoogleSignInButton } from '@wasp/auth/helpers/Google'
+import { SignInButton as GitHubSignInButton } from '@wasp/auth/helpers/GitHub'
 
 const Login = () => {
   return (
-    <>
+    <div className="flex flex-col gap-5">
       <LoginForm/>
-      <br/>
       <span>
         I don't have an account yet (<Link to="/signup">go to signup</Link>).
       </span>
-
-      {/* <div>
+      <div className="flex flex-col gap-2 max-w-xs">
         <GoogleSignInButton/>
         <GitHubSignInButton/>
-      </div> */}
-    </>
+      </div>
+    </div>
   )
 }
 
