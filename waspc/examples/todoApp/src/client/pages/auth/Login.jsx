020379b37f437ea4cd094c64397e74d164b91659
Mihovil Ilakovac <mihovil@ilakovac.com>
2023-03-02 16:58:19 +0100

    Revert changes to the example app
    Signed-off-by: Mihovil Ilakovac <mihovil@ilakovac.com>

--- waspc/examples/todoApp/src/client/pages/auth/Login.jsx
+++ waspc/examples/todoApp/src/client/pages/auth/Login.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 import { Link } from 'react-router-dom'
 
 import LoginForm from '@wasp/auth/forms/Login'
-import { SignInButton as GoogleSignInButton } from '@wasp/auth/helpers/Google'
+// import { SignInButton as GoogleSignInButton } from '@wasp/auth/helpers/Google'
 // import { SignInButton as GitHubSignInButton } from '@wasp/auth/helpers/GitHub'
 
 const Login = () => {
@@ -12,10 +12,10 @@ const Login = () => {
       <span>
         I don't have an account yet (<Link to="/signup">go to signup</Link>).
       </span>
-      <div className="flex flex-col gap-2 max-w-xs">
-        <GoogleSignInButton />
-        {/* <GitHubSignInButton/> */}
-      </div>
+      {/* <div className="flex flex-col gap-2 max-w-xs">
+        <GoogleSignInButton/>
+        <GitHubSignInButton/>
+      </div> */}
     </div>
   )
 }
