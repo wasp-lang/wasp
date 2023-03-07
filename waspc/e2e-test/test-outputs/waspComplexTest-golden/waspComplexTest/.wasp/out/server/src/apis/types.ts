465640f778b4f379479f49d3f9dc5a8186a232a2
shayneczyzewski <shayne.czyzewski@gmail.com>
2023-03-07 16:51:02 -0500

    update e2e

--- waspc/e2e-test/test-outputs/waspComplexTest-golden/waspComplexTest/.wasp/out/server/src/apis/types.ts
+++ waspc/e2e-test/test-outputs/waspComplexTest-golden/waspComplexTest/.wasp/out/server/src/apis/types.ts
@@ -1,7 +1,10 @@
 
 import prisma from '../dbClient.js'
 
+import { User } from '../entities'
+
 export type FooBarContext = {
+  user: User,
   entities: {
   }
 }
--- waspc/e2e-test/test-outputs/waspComplexTest-golden/waspComplexTest/.wasp/out/server/src/routes/apis/index.ts
+++ waspc/e2e-test/test-outputs/waspComplexTest-golden/waspComplexTest/.wasp/out/server/src/routes/apis/index.ts
@@ -1,13 +1,16 @@
 import express, { Request, Response } from 'express';
 import prisma from '../../dbClient.js'
 import { handleRejection } from '../../utils.js'
+import auth from '../../core/auth.js'
+import { User } from '../../entities'
 
 import { fooBar } from '../../ext-src/apis.js'
 
 const router = express.Router()
 
-router.get('/foo/bar', handleRejection((req: Request, res: Response) => {
+router.get('/foo/bar', auth, handleRejection((req: Request & { user: User }, res: Response) => {
   const context = {
+    user: req.user,
     entities: {
     },
   }
