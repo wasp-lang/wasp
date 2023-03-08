971e1a7928d5b504187bec774788f550eb3b0dca
shayneczyzewski <shayne.czyzewski@gmail.com>
2023-03-08 08:02:41 -0500

    show username from auth protected custom route

--- waspc/examples/todoApp/src/server/apis.ts
+++ waspc/examples/todoApp/src/server/apis.ts
@@ -2,6 +2,6 @@ import { Request, Response } from '@wasp/types'
 import { FooBarContext } from '@wasp/apis/types'
 
 export function fooBar(req: Request, res: Response, context: FooBarContext) {
-  res.set('Access-Control-Allow-Origin', '*')
-  res.json({ msg: "Hello, world!" })
+  res.set('Access-Control-Allow-Origin', '*') // Example of modifying headers to override CORS middleware.
+  res.json({ msg: `Hello, ${context.user.username}!` })
 }
