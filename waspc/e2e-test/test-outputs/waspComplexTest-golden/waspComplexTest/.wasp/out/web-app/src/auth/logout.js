9da6e6f6e39e5f3921de907f2329e10639bfce60
Mihovil Ilakovac <mihovil@ilakovac.com>
2023-03-15 15:40:21 +0100

    Fixes formatting and e2e tests

--- waspc/e2e-test/test-outputs/waspComplexTest-golden/waspComplexTest/.wasp/out/web-app/src/auth/logout.js
+++ waspc/e2e-test/test-outputs/waspComplexTest-golden/waspComplexTest/.wasp/out/web-app/src/auth/logout.js
@@ -1,8 +1,8 @@
-import { cleanupStorage } from '../api.js'
+import { removeLocalUserData } from '../api.js'
 import { invalidateAndRemoveQueries } from '../operations/resources'
 
 export default async function logout() {
-  cleanupStorage()
+  removeLocalUserData()
   // TODO(filip): We are currently invalidating and removing  all the queries, but
   // we should remove only the non-public, user-dependent ones.
   await invalidateAndRemoveQueries()
