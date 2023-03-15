d45628c6a5b0744196e16884638676457cb935d9
Mihovil Ilakovac <mihovil@ilakovac.com>
2023-03-15 14:06:22 +0100

    Fixes localstorage clearing bug
    Signed-off-by: Mihovil Ilakovac <mihovil@ilakovac.com>

--- waspc/data/Generator/templates/react-app/src/auth/logout.js
+++ waspc/data/Generator/templates/react-app/src/auth/logout.js
@@ -1,8 +1,8 @@
-import { clearLocalStorage } from '../api.js'
+import { cleanupStorage } from '../api.js'
 import { invalidateAndRemoveQueries } from '../operations/resources'
 
 export default async function logout() {
-  clearLocalStorage()
+  cleanupStorage()
   // TODO(filip): We are currently invalidating and removing  all the queries, but
   // we should remove only the non-public, user-dependent ones.
   await invalidateAndRemoveQueries()
