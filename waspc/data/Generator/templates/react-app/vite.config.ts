2d06e1c72f8af161b775c99b9c5cfc2a99b1d2df
shayneczyzewski <shayne.czyzewski@gmail.com>
2023-03-16 08:43:38 -0400

    change vitest config

--- waspc/data/Generator/templates/react-app/vite.config.ts
+++ waspc/data/Generator/templates/react-app/vite.config.ts
@@ -1,4 +1,5 @@
-import { defineConfig } from 'vitest/config'
+/// <reference types="vitest" />
+import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
 
 // https://vitejs.dev/config/
