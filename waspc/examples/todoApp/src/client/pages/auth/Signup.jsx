c56c131ce4ae6b0b1f84c824b92986dc9791550a
Mihovil Ilakovac <mihovil@ilakovac.com>
2023-03-27 16:11:16 +0200

    Adds a task count unit test

--- waspc/examples/todoApp/src/client/pages/auth/Signup.jsx
+++ waspc/examples/todoApp/src/client/pages/auth/Signup.jsx
@@ -4,21 +4,19 @@ import { Link } from 'react-router-dom'
 import SignupForm from '@wasp/auth/forms/Signup'
 import getNumTasks from '@wasp/queries/getNumTasks'
 import { useQuery } from '@wasp/queries'
-
+import { getTotalTaskCountMessage } from './helpers'
 
 const Signup = () => {
   const { data: numTasks } = useQuery(getNumTasks)
   return (
     <>
-      <SignupForm/>
-      <br/>
+      <SignupForm />
+      <br />
       <span>
         I already have an account (<Link to="/login">go to login</Link>).
       </span>
-      <br/>
-      <span>
-        Number of tasks already created: {numTasks}
-      </span>
+      <br />
+      <span>{getTotalTaskCountMessage(numTasks)}</span>
     </>
   )
 }
