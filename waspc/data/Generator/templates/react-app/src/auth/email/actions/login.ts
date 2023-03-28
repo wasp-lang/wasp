90a47d77f177eab0cf1e7ccc668b9dd4fcf7bb62
Mihovil Ilakovac <mihovil@ilakovac.com>
2023-03-28 12:51:55 +0200

    Updates error handling

--- waspc/data/Generator/templates/react-app/src/auth/email/actions/login.ts
+++ waspc/data/Generator/templates/react-app/src/auth/email/actions/login.ts
@@ -1,9 +1,13 @@
 {{={= =}=}}
+import type { AxiosError } from 'axios';
 import api, { handleApiError } from '../../../api'
 import { initSession } from '../../helpers/user';
 
 export async function login(data: { email: string; password: string }): Promise<void> {
-    const response = await api.post('{= loginPath =}', data)
-        .catch(handleApiError);
-    await initSession(response.data.token);
+    try {
+        const response = await api.post('{= loginPath =}', data);
+        await initSession(response.data.token);
+    } catch (e: AxiosError) {
+        handleApiError(e);
+    }
 }
