4e66805d4060ab9bf32e72328037496918147eb7
Mihovil Ilakovac <mihovil@ilakovac.com>
2023-04-03 12:25:56 +0200

    Updates migration

--- waspc/examples/todoApp/migrations/20230328100331_update_default_value/migration.sql
+++
@@ -1,2 +0,0 @@
--- AlterTable
-ALTER TABLE "User" ALTER COLUMN "isEmailVerified" SET DEFAULT false;
--- waspc/examples/todoApp/migrations/20230330105235_add_datetime_for_email_sends/migration.sql
+++
@@ -1,3 +0,0 @@
--- AlterTable
-ALTER TABLE "User" ADD COLUMN     "emailVerificationSentAt" TIMESTAMP(3),
-ADD COLUMN     "passwordResetSentAt" TIMESTAMP(3);
