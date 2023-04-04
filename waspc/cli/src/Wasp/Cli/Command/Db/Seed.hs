f6ddedcf9a4becee954045940fcd0f278c86b2fa
Martin Sosic <sosic.martin@gmail.com>
2023-04-04 18:13:36 +0200

    fix

--- waspc/cli/src/Wasp/Cli/Command/Db/Seed.hs
+++ waspc/cli/src/Wasp/Cli/Command/Db/Seed.hs
@@ -6,6 +6,8 @@ where
 import qualified Control.Monad.Except as E
 import Control.Monad.IO.Class (liftIO)
 import Data.List (intercalate)
+import Data.List.NonEmpty (NonEmpty ((:|)))
+import qualified Data.List.NonEmpty as NE
 import StrongPath ((</>))
 import Text.Printf (printf)
 import qualified Wasp.AppSpec as AS
@@ -30,7 +32,7 @@ seed maybeUserProvidedSeedName = do
 
   appSpec <- analyze waspProjectDir
 
-  nameOfSeedToRun <- figureOutNameOfExistingSeedToRun maybeUserProvidedSeedName appSpec
+  nameOfSeedToRun <- obtainNameOfExistingSeedToRun maybeUserProvidedSeedName appSpec
 
   cliSendMessageC $ Msg.Start "Seeding the database..."
 
@@ -38,50 +40,55 @@ seed maybeUserProvidedSeedName = do
     Left errorMsg -> cliSendMessageC $ Msg.Failure "Database seeding failed" errorMsg
     Right () -> cliSendMessageC $ Msg.Success "Database seeded successfully!"
 
-getDbSeeds :: AS.AppSpec -> Maybe [AS.ExtImport]
-getDbSeeds spec = AS.Db.seeds =<< AS.App.db (snd $ ASV.getApp spec)
-
-figureOutNameOfExistingSeedToRun :: Maybe String -> AS.AppSpec -> Command String
-figureOutNameOfExistingSeedToRun maybeUserProvidedSeedName spec = case getDbSeeds spec of
-  Just seeds@(_ : _) -> do
-    let seedNames = map AS.ExtImport.importIdentifier seeds
-    case maybeUserProvidedSeedName of
-      Just name -> validateUserProvidedSeedName name seedNames
-      Nothing -> case seedNames of
-        [seedName] -> return seedName
-        _seedNames -> liftIO $ askUserToChooseFromSeedNames seedNames
-  _noSeeds ->
-    (E.throwError . CommandError "No seeds defined") $
-      "You haven't defined any database seeding functions, so there is nothing to run!\n"
-        <> "To do so, define seeding functions via app.db.seeds in your Wasp config."
+obtainNameOfExistingSeedToRun :: Maybe String -> AS.AppSpec -> Command String
+obtainNameOfExistingSeedToRun maybeUserProvidedSeedName spec = do
+  seedNames <- getSeedNames <$> getSeedsFromAppSpecOrThrowIfNone
+  case maybeUserProvidedSeedName of
+    Just name -> parseUserProvidedSeedName name seedNames
+    Nothing -> case seedNames of
+      seedName :| [] -> return seedName
+      _seedNames -> liftIO $ askUserToChooseFromSeedNames seedNames
   where
-    askUserToChooseFromSeedNames :: [String] -> IO String
+    askUserToChooseFromSeedNames :: NE.NonEmpty String -> IO String
     askUserToChooseFromSeedNames seedNames = do
       putStrLn "Choose a seed to run:"
-      mapM_ (\(i, n) -> putStrLn $ printf "  %d: %s" i n) $ zip [1 :: Int ..] seedNames
+      mapM_ (\(i, n) -> putStrLn $ printf "  %d: %s" i n) $ zip [1 :: Int ..] (NE.toList seedNames)
       putStrLn "Type a number (e.g. 1 or 2):"
       chosenNumber <- getLine
       case parseNumberInRange (1, length seedNames) chosenNumber of
-        Right idx -> return $ seedNames !! (idx - 1)
+        Right idx -> return $ seedNames NE.!! (idx - 1)
         Left errMsg -> do
-          putStrLn $ "Invalid number (" <> errMsg <> "), please try again."
+          putStrLn $ "Invalid number (" <> errMsg <> "), please try again.\n"
           askUserToChooseFromSeedNames seedNames
 
-    -- TODO: Move to Wasp.Util?
     parseNumberInRange :: (Int, Int) -> String -> Either String Int
     parseNumberInRange (minNum, maxNum) strNum =
       case reads strNum of
         [(num, _)] ->
           if num >= minNum && num <= maxNum
             then Right num
-            else Left "Number out of range."
-        _notANum -> Left "Not a number."
+            else Left "number out of range"
+        _notANum -> Left "not a number"
 
-    validateUserProvidedSeedName :: String -> [String] -> Command String
-    validateUserProvidedSeedName userProvidedSeedName seedNames =
+    parseUserProvidedSeedName :: String -> NE.NonEmpty String -> Command String
+    parseUserProvidedSeedName userProvidedSeedName seedNames =
       if userProvidedSeedName `elem` seedNames
         then return userProvidedSeedName
         else
           (E.throwError . CommandError "Invalid seed name") $
-            "There is no seed with the name: " <> userProvidedSeedName <> " ."
-              <> ("\nValid seed names are: " <> intercalate ", " seedNames <> ".")
+            "There is no seed with the name " <> userProvidedSeedName <> "."
+              <> ("\nValid seed names are: " <> intercalate ", " (NE.toList seedNames) <> ".")
+
+    getSeedsFromAppSpecOrThrowIfNone :: Command (NE.NonEmpty AS.ExtImport.ExtImport)
+    getSeedsFromAppSpecOrThrowIfNone = case getDbSeeds spec of
+      Just seeds@(_ : _) -> return $ NE.fromList seeds
+      _noSeeds ->
+        (E.throwError . CommandError "No seeds defined") $
+          "You haven't defined any database seeding functions, so there is nothing to run!\n"
+            <> "To do so, define seeding functions via app.db.seeds in your Wasp config."
+
+    getSeedNames :: (Functor f) => f AS.ExtImport.ExtImport -> f String
+    getSeedNames seeds = AS.ExtImport.importIdentifier <$> seeds
+
+getDbSeeds :: AS.AppSpec -> Maybe [AS.ExtImport]
+getDbSeeds spec = AS.Db.seeds =<< AS.App.db (snd $ ASV.getApp spec)
