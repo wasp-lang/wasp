7291e49b843132597f906871ad82f4ee53673cef
Martin Sosic <sosic.martin@gmail.com>
2023-04-07 15:48:24 +0200

    Updated app to v0.10, added db seeding.

--- examples/thoughts/src/client/LoginPage.jsx
+++ examples/thoughts/src/client/LoginPage.jsx
@@ -1,17 +1,19 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
 
-import LoginForm from '@wasp/auth/forms/Login'
+import { LoginForm } from '@wasp/auth/forms/Login'
 import addWaspSourceHeader from './addWaspSourceHeader'
 
 const LoginPage = (props) => {
   return (
     <>
-      <LoginForm/>
-      <br/>
-      <span>
-        I don't have an account yet (<Link to="/signup">go to signup</Link>).
-      </span>
+      <div style={{maxWidth: "400px", margin: "0 auto"}}>
+        <LoginForm/>
+        <br/>
+        <span>
+          I don't have an account yet (<Link to="/signup">go to signup</Link>).
+        </span>
+      </div>
     </>
   )
 }
