17bbfaee96b73ad90415df6649bfa3cf3b471795
Mihovil Ilakovac <mihovil@ilakovac.com>
2023-04-11 18:08:18 +0200

    Updates e2e tests
    Signed-off-by: Mihovil Ilakovac <mihovil@ilakovac.com>

--- waspc/e2e-test/test-outputs/waspComplexTest-golden/waspComplexTest/.wasp/out/web-app/src/auth/useAuth.ts
+++ waspc/e2e-test/test-outputs/waspComplexTest-golden/waspComplexTest/.wasp/out/web-app/src/auth/useAuth.ts
@@ -6,28 +6,34 @@ import { HttpMethod } from '../types'
 import { type SanitizedUser as User } from '../../../server/src/_types/' 
 import { addMetadataToQuery } from '../queries/core'
 
+export const getMe = createUserGetter()
+
 export default function useAuth(queryFnArgs?: unknown, config?: any) {
   return useQuery(getMe, queryFnArgs, config)
-}
+}  
 
-const getMeRelativePath = 'auth/me'
-const getMeRoute = { method: HttpMethod.Get, path: `/${getMeRelativePath}` }
-export async function getMe(): Promise<User | null> {
-  try {
-    const response = await api.get(getMeRoute.path)
-
-    return superjsonDeserialize(response.data)
-  } catch (error) {
-    if (error.response?.status === 401) {
-      return null
-    } else {
-      handleApiError(error)
+function createUserGetter() {
+  const getMeRelativePath = 'auth/me'
+  const getMeRoute = { method: HttpMethod.Get, path: `/${getMeRelativePath}` }
+  async function getMe(): Promise<User | null> {
+    try {
+      const response = await api.get(getMeRoute.path)
+  
+      return superjsonDeserialize(response.data)
+    } catch (error) {
+      if (error.response?.status === 401) {
+        return null
+      } else {
+        handleApiError(error)
+      }
     }
   }
-}
+  
+  addMetadataToQuery(getMe, {
+    relativeQueryPath: getMeRelativePath,
+    queryRoute: getMeRoute,
+    entitiesUsed: ['"User"'],
+  })
 
-addMetadataToQuery(getMe, {
-  relativeQueryPath: getMeRelativePath,
-  queryRoute: getMeRoute,
-  entitiesUsed: ['User'],
-})
+  return getMe
+}
