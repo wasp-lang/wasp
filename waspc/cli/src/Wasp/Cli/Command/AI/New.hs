25b5b62e14744cc3479109b39c67f1e5688ea0ba
Martin Sosic <sosic.martin@gmail.com>
2023-04-20 21:35:33 +0200

    Wrote instructions for myself.

--- waspc/cli/src/Wasp/Cli/Command/AI/New.hs
+++ waspc/cli/src/Wasp/Cli/Command/AI/New.hs
@@ -77,15 +77,50 @@ new = do
     -- TODO: Also check if it compiles and if not, send errors to GPT.
     aiWriteWaspFile :: Path' Abs (Dir WaspProjectDir) -> String -> String -> Command Text
     aiWriteWaspFile absWaspProjectDir appTitle appDesc = do
+      -- TODO: Tell GPT about Wasp in general, shortly.
+      -- Also give it an example of a Wasp file that is pretty rich. It can even be done as a previous part of the conversation, so it has an example of what is good.
+      -- Then, tell it to generate a Wasp file for the given prompt, while also adding comments in it for every ExtImport, where comments explain what that file is supposed to contain / do, so basically to serve as instrutioncs to itself.
+      -- Or probably best to tell it to provide those instructions separately, in a JSON object where key is the name of each page or whatever.
+      -- Once it does, let's feed that Wasp file to the Wasp analyzer and see if it returns any errors. If it does, send it back to chat GPT for repairs.
+      -- Finally, write it to disk.
+      -- In the example wasp file, we can put in a lot of comments explaining stuff, but then we can ask it to not produce those once it produces a wasp file, so we save some tokens.
+      -- We should also make it clear which feature in wasp file is related to which part of the prompt, so it can know to skip them properly.
+      -- TODO: What if it fails to repair it? Well we can just pretend all is ok, let user fix it.
+      -- TODO: As chatGPT to compress our prompt for us.
+      let chatMessages =
+            [ ChatMessage {role = System, content = "You are an expert Wasp developer, helping set up a new Wasp project."},
+              ChatMessage
+                { role = User,
+                  content =
+                    "Wasp is a framework for building web apps with React (frontend), NodeJS (backend) and Prisma (db)."
+                      <> "\nIt let's you define high-level overview of your app via specialized Wasp DSL in main.wasp file, and then implements the details in JS/JSX files."
+                      <> "\nMain Wasp features are: frontend routes and pages, operations (queries and actions -> it is really an RPC system), entities (data models), jobs (cron / scheduled), ... ."
+                      <> "\nHere is an example main.wasp file:"
+                      <> ""
+                }
+            ]
       error "TODO"
 
     aiWriteWaspPages :: Path' Abs (Dir WaspProjectDir) -> Text -> Command ()
     aiWriteWaspPages absWaspProjectDir waspFileContent = do
+      -- TODO: Actually it should recieve AppSpec I think, right?
+      -- So idea is here that for each page we give Wasp file to Chat GPT, also all the basic knowledge about Wasp,
+      -- and then ask it to generate JS for that page. If page already exists, we also pass that and tell it to add stuff to it.
+      -- How do we get comments to it about it? Well, maybe it is smart enough to pick them up from the wasp file?
+      -- Or, we give them separately, but then we need them in a good format. I think it will be able to pick them up on its own.
+      -- Oh and we also need to give it info about the concept of the page itself! And example. Uff.
+      -- Maybe that is too much and we can't give it all that. In that case we should drop the idea of passing whole Wasp file,
+      -- and we need to give it only instructions for that page. We can have it write those instructions separately, for each page, in that case. Yeah probably that is the best.
+      -- Hm and we also need initial prompt by user here.
       error "TODO"
 
     aiWriteWaspOperations :: Path' Abs (Dir WaspProjectDir) -> Text -> Command ()
     aiWriteWaspOperations absWaspProjectDir waspFileContent = do
+      -- Here we do everything analogous as we did for the pages, but it becomes extra important to be able to reuse the already written file, so we should make sure we have that going,
+      -- because often it is normal to put multiple operations in the same file.
       error "TODO"
+
+-- TODO: What about other ext imports? Make sure we cover all of them: jobs, setupFn (server, client), api, something else?
 
 -- In general, gpt-3.5-turbo-0301 does not pay strong attention to the system message, and therefore important instructions are often better placed in a user message.
 
