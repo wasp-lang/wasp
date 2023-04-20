6b5bd70cf320679b1efbccc6bcbc2edc7fdc7239
Oleg Gulevskyy <43781031+OlegGulevskyy@users.noreply.github.com>
2023-04-20 19:36:00 +0200

    global options appliance

--- waspc/packages/deploy/src/index.ts
+++ waspc/packages/deploy/src/index.ts
@@ -3,15 +3,21 @@
 import { Command } from 'commander';
 import { addFlyCommand } from './providers/fly/index.js';
 import { addRailwayCommand } from './providers/railway/index.js';
+import { createGlobalOptions } from './providers/shared/options.js';
 
 const program = new Command();
 
 program
-	.name('wasp deploy')
-	.description('CLI for deploying Wasp apps to various clouds')
-	.allowUnknownOption();
+    .name('wasp deploy')
+    .description('CLI for deploying Wasp apps to various clouds')
+    .allowUnknownOption();
 
 addFlyCommand(program);
 addRailwayCommand(program);
+
+// add global options to all subcommands
+program.commands.forEach((cmd) => {
+    cmd.commands.forEach((subCmd) => createGlobalOptions(subCmd));
+});
 
 program.parseAsync();
