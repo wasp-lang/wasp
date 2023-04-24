b358dc848e3ee1487fa3835fa23cddd484cb3ff7
Mihovil Ilakovac <mihovil@ilakovac.com>
2023-04-24 16:59:41 +0200

    Fixes e2e tests
    Signed-off-by: Mihovil Ilakovac <mihovil@ilakovac.com>

--- waspc/e2e-test/ShellCommands.hs
+++ waspc/e2e-test/ShellCommands.hs
@@ -87,7 +87,7 @@ insertCodeIntoWaspFileAtLineNumber atLineNumber line =
 waspCliNew :: ShellCommandBuilder ShellCommand
 waspCliNew = do
   context <- ask
-  return $ "wasp-cli new " ++ _ctxtCurrentProjectName context ++ " --force-fallback-template"
+  return $ "wasp-cli new " ++ _ctxtCurrentProjectName context ++ " -t basic"
 
 waspCliCompile :: ShellCommandBuilder ShellCommand
 waspCliCompile = return "wasp-cli compile"
