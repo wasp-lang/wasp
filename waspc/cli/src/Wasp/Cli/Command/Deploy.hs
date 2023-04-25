a6a077de06771d1cb617388543f4b5107368897e
shayneczyzewski <shayne.czyzewski@gmail.com>
2023-04-25 08:30:48 -0400

    update name

--- waspc/cli/src/Wasp/Cli/Command/Deploy.hs
+++ waspc/cli/src/Wasp/Cli/Command/Deploy.hs
@@ -13,13 +13,13 @@ import qualified Wasp.Project.Deployment
 deploy :: [String] -> Command ()
 deploy cmdArgs = do
   waspProjectDir <- findWaspProjectRootDirFromCwd
-  res <- liftIO $ do
+  deployResult <- liftIO $ do
     -- `getExecutablePath` has some caveats:
     --   https://frasertweedale.github.io/blog-fp/posts/2022-05-10-improved-executable-path-queries.html
     -- Once we upgrade to GHC 9.4 we should change to `executablePath`, but this should be ok for
     -- our purposes.
     waspExePath <- getExecutablePath
     Wasp.Project.Deployment.deploy waspExePath waspProjectDir cmdArgs
-  case res of
+  case deployResult of
     Left err -> throwError $ CommandError "Deploy command failed" err
     Right () -> return ()
