d3e6739e6947ee0847e6d8d68fd42c471a97516a
Mihovil Ilakovac <mihovil@ilakovac.com>
2023-04-27 01:26:47 +0200

    Updates e2e tests
    Signed-off-by: Mihovil Ilakovac <mihovil@ilakovac.com>

--- waspc/e2e-test/test-outputs/waspComplexTest-golden/waspComplexTest/.wasp/out/server/src/routes/auth/me.js
+++ waspc/e2e-test/test-outputs/waspComplexTest-golden/waspComplexTest/.wasp/out/server/src/routes/auth/me.js
@@ -1,6 +1,6 @@
 import { serialize as superjsonSerialize } from 'superjson'
 import { handleRejection } from '../../utils.js'
-import { throwInvalidCredentialsError } from '../../auth/utils.js'
+import { throwInvalidCredentialsError } from '../../core/auth.js'
 
 export default handleRejection(async (req, res) => {
   if (req.user) {
