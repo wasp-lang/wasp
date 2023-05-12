6f125c2966600af7b1bf74f4829441bcee4a3998
Mihovil Ilakovac <mihovil@ilakovac.com>
2023-05-12 13:30:38 +0200

    Update website banner

--- web/src/components/Nav/Announcement.js
+++ web/src/components/Nav/Announcement.js
@@ -1,12 +1,12 @@
-import React from 'react'
-import classNames from 'classnames'
-import { useHistory } from '@docusaurus/router'
-import { ChevronRight, X } from 'react-feather'
+import React from "react";
+import classNames from "classnames";
+import { useHistory } from "@docusaurus/router";
+import { ChevronRight, X } from "react-feather";
 
-import styles from '../../pages/styles.module.css'
+import styles from "../../pages/styles.module.css";
 
 const Announcement = () => {
-  let history = useHistory()
+  let history = useHistory();
 
   const handleLink = () => {
     //history.push('/blog/2023/04/13/db-start-and-seed')
@@ -15,18 +15,21 @@ const Announcement = () => {
     //window.open('https://twitter.com/MatijaSosic/status/1646532181324603395')
     //window.open('https://twitter.com/WaspLang/status/1647979490180575234')
     //window.open('https://www.producthunt.com/posts/free-saas-template-gpt-stripe-auth')
-    window.open('https://hackathon.wasp-lang.dev')
-  }
+    window.open("https://hackathon.wasp-lang.dev");
+  };
 
   return (
     <div
       onClick={handleLink}
-      className={classNames(styles.gradientBackground, `
+      className={classNames(
+        styles.gradientBackground,
+        `
         overflow-hidden
         cursor-pointer flex-row
         space-x-3
         text-white
-      `)}
+      `
+      )}
     >
       <div
         className={`
@@ -35,29 +38,23 @@ const Announcement = () => {
           lg:container lg:divide-x lg:px-16 xl:px-20
         `}
       >
-        <span className='item-center flex gap-2 px-3'>
-
-          <span>🏆 Wasp Hackathon 2.0 ends on May 8th! You can still participate 🏆</span>
+        <span className="item-center flex gap-2 px-3">
+          <span>🏆 Wasp Hackathon 2.0 finished</span>
         </span>
 
-        <span className='hidden items-center space-x-2 px-3 lg:flex'>
+        <span className="hidden items-center space-x-2 px-3 lg:flex">
           <span
             className={`
               bg-neutral-700 px-2.5 py-1 text-xs rounded-full cursor-pointer
               hover:bg-neutral-600
             `}
           >
-            Register now 📝
+            Check out the submissions 👀
           </span>
         </span>
-
       </div>
-
-
     </div>
+  );
+};
 
-  )
-
-}
-
-export default Announcement
+export default Announcement;
