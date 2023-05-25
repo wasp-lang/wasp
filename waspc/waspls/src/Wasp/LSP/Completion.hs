e784b5166e9944eb0201ffb25533e1813c6e380d
Craig McIlwrath <craigmc08@gmail.com>
2023-05-25 12:25:57 -0400

    refactor waspls completions code

--- waspc/waspls/src/Wasp/LSP/Completion.hs
+++ waspc/waspls/src/Wasp/LSP/Completion.hs
@@ -3,18 +3,16 @@ module Wasp.LSP.Completion
   )
 where
 
-import Control.Lens ((?~), (^.))
+import Control.Lens ((^.))
 import Control.Monad.Log.Class (MonadLog (logM))
+import Control.Monad.Reader (MonadReader, runReaderT)
 import Control.Monad.State.Class (MonadState, gets)
-import Data.Maybe (maybeToList)
-import qualified Data.Text as Text
 import qualified Language.LSP.Types as LSP
-import qualified Language.LSP.Types.Lens as LSP
-import Wasp.Analyzer.Parser.CST (SyntaxNode)
-import qualified Wasp.Analyzer.Parser.CST as S
 import Wasp.Analyzer.Parser.CST.Traverse
-import Wasp.LSP.ServerState
-import Wasp.LSP.Syntax (findChild, isAtExprPlace, lexemeAt, lspPositionToOffset, showNeighborhood, toOffset)
+import Wasp.LSP.Completions.Common (CompletionContext (..), CompletionProvider)
+import qualified Wasp.LSP.Completions.ExprCompletion as ExprCompletion
+import Wasp.LSP.ServerState (ServerState, cst, currentWaspSource)
+import Wasp.LSP.Syntax (lspPositionToOffset, toOffset)
 
 -- | Get the list of completions at a (line, column) position in the source.
 getCompletionsAtPosition ::
@@ -31,75 +29,15 @@ getCompletionsAtPosition position = do
       let offset = lspPositionToOffset src position
       -- 'location' is a traversal through the syntax tree that points to 'position'
       let location = toOffset offset (fromSyntaxForest syntax)
-      logM $ "[getCompletionsAtPosition] neighborhood=\n" ++ showNeighborhood location
-      exprCompletions <-
-        if isAtExprPlace location
-          then do
-            logM $ "[getCompletionsAtPosition] offset=" ++ show offset ++ " position=" ++ show position ++ " atExpr=True"
-            getExprCompletions src syntax
-          else do
-            logM $ "[getCompletionsAtPosition] offset=" ++ show offset ++ " position=" ++ show position ++ " atExpr=False"
-            return []
-      let completions = exprCompletions
-      return completions
+      logM $ "[getCompletionsAtPosition] position=" ++ show position ++ " offset=" ++ show offset
+      let completionContext = CompletionContext {_src = src, _cst = syntax}
+      -- Run all completion providers and concatenate results
+      concat
+        <$> mapM
+          (\m -> runReaderT (m location) completionContext)
+          completionProviders
 
--- | If the location is at an expression, find declaration names in the file
--- and return them as autocomplete suggestions
---
--- TODO: include completions for enum variants (use standard type defs from waspc)
-getExprCompletions ::
-  (MonadLog m) =>
-  String ->
-  [SyntaxNode] ->
-  m [LSP.CompletionItem]
-getExprCompletions src syntax = do
-  let declNames = findDeclNames src syntax
-  logM $ "[getExprCompletions] declnames=" ++ show declNames
-  return $
-    map
-      ( \(name, typ) ->
-          makeBasicCompletionItem (Text.pack name)
-            & (LSP.kind ?~ LSP.CiVariable)
-            & (LSP.detail ?~ Text.pack (":: " ++ typ ++ " (declaration type)"))
-      )
-      declNames
-
--- | Search through the CST and collect all @(declName, declType)@ pairs.
-findDeclNames :: String -> [SyntaxNode] -> [(String, String)]
-findDeclNames src syntax = traverseForDeclNames $ fromSyntaxForest syntax
-  where
-    traverseForDeclNames :: Traversal -> [(String, String)]
-    traverseForDeclNames t = case kindAt t of
-      S.Program -> maybe [] traverseForDeclNames $ down t
-      S.Decl ->
-        let declNameAndType = maybeToList $ getDeclNameAndType t
-         in declNameAndType ++ maybe [] traverseForDeclNames (right t)
-      _ -> maybe [] traverseForDeclNames $ right t
-    getDeclNameAndType :: Traversal -> Maybe (String, String)
-    getDeclNameAndType t = do
-      nameT <- findChild S.DeclName t
-      typeT <- findChild S.DeclType t
-      return (lexemeAt src nameT, lexemeAt src typeT)
-
--- | Create a completion item containing only a label.
-makeBasicCompletionItem :: Text.Text -> LSP.CompletionItem
-makeBasicCompletionItem name =
-  LSP.CompletionItem
-    { _label = name,
-      _kind = Nothing,
-      _tags = Nothing,
-      _detail = Nothing,
-      _documentation = Nothing,
-      _deprecated = Nothing,
-      _preselect = Nothing,
-      _sortText = Nothing,
-      _filterText = Nothing,
-      _insertText = Nothing,
-      _insertTextFormat = Nothing,
-      _insertTextMode = Nothing,
-      _textEdit = Nothing,
-      _additionalTextEdits = Nothing,
-      _commitCharacters = Nothing,
-      _command = Nothing,
-      _xdata = Nothing
-    }
+completionProviders :: (MonadReader CompletionContext m, MonadLog m) => [CompletionProvider m]
+completionProviders =
+  [ ExprCompletion.getCompletions
+  ]
---
+++ waspc/waspls/src/Wasp/LSP/Completions/ExprCompletion.hs
@@ -0,0 +1,60 @@
+module Wasp.LSP.Completions.ExprCompletion
+  ( getCompletions,
+  )
+where
+
+import Control.Lens ((?~), (^.))
+import Control.Monad.Log.Class (MonadLog (logM))
+import Control.Monad.Reader.Class (MonadReader, asks)
+import Data.Maybe (maybeToList)
+import qualified Data.Text as Text
+import qualified Language.LSP.Types as LSP
+import qualified Language.LSP.Types.Lens as LSP
+import Wasp.Analyzer.Parser.CST (SyntaxNode)
+import qualified Wasp.Analyzer.Parser.CST as S
+import Wasp.Analyzer.Parser.CST.Traverse
+import Wasp.LSP.Completions.Common (CompletionContext, CompletionProvider, makeBasicCompletionItem)
+import qualified Wasp.LSP.Completions.Common as Ctx
+import Wasp.LSP.Syntax (findChild, isAtExprPlace, lexemeAt)
+
+-- | If the location is at an expression, find declaration names in the file
+-- and return them as autocomplete suggestions
+--
+-- TODO: include completions for enum variants (use standard type defs from waspc)
+getCompletions :: (MonadReader CompletionContext m, MonadLog m) => CompletionProvider m
+getCompletions location =
+  if not (isAtExprPlace location)
+    then do
+      logM "[ExprCompletion] not at expression"
+      return []
+    else do
+      logM "[ExprCompletion] at expression"
+      src <- asks (^. Ctx.src)
+      syntax <- asks (^. Ctx.cst)
+      let declNames = findDeclNames src syntax
+      logM $ "[ExprCompletion] declnames=" ++ show declNames
+      return $
+        map
+          ( \(name, typ) ->
+              makeBasicCompletionItem (Text.pack name)
+                & (LSP.kind ?~ LSP.CiVariable)
+                & (LSP.detail ?~ Text.pack (":: " ++ typ ++ " (declaration type)"))
+          )
+          declNames
+
+-- | Search through the CST and collect all @(declName, declType)@ pairs.
+findDeclNames :: String -> [SyntaxNode] -> [(String, String)]
+findDeclNames src syntax = traverseForDeclNames $ fromSyntaxForest syntax
+  where
+    traverseForDeclNames :: Traversal -> [(String, String)]
+    traverseForDeclNames t = case kindAt t of
+      S.Program -> maybe [] traverseForDeclNames $ down t
+      S.Decl ->
+        let declNameAndType = maybeToList $ getDeclNameAndType t
+         in declNameAndType ++ maybe [] traverseForDeclNames (right t)
+      _ -> maybe [] traverseForDeclNames $ right t
+    getDeclNameAndType :: Traversal -> Maybe (String, String)
+    getDeclNameAndType t = do
+      nameT <- findChild S.DeclName t
+      typeT <- findChild S.DeclType t
+      return (lexemeAt src nameT, lexemeAt src typeT)
