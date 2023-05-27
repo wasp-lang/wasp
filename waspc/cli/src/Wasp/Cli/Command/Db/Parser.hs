6d7ec5f422f3fb19114c4f5d4079182ce4062f9d
modotte <runecroon@gmail.com>
2023-05-27 20:36:25 +0800

    Fix migrate-dev help formatting

--- waspc/cli/src/Wasp/Cli/Command/Db/Parser.hs
+++ waspc/cli/src/Wasp/Cli/Command/Db/Parser.hs
@@ -6,6 +6,7 @@ import Options.Applicative
     Parser,
   )
 import qualified Options.Applicative as O
+import qualified Options.Applicative.Help as OH
 import Wasp.Cli.Command.Call
   ( Call (Db),
     DbArgs
@@ -17,7 +18,7 @@ import Wasp.Cli.Command.Call
       ),
     DbMigrateDevArgs (DbMigrateDevArgs),
   )
-import Wasp.Cli.Parser.Util (mkNormalCommand)
+import Wasp.Cli.Parser.Util (mkCommand, mkNormalCommand)
 
 db :: Mod CommandFields Call
 db = mkNormalCommand "db" parseDb "Executes a database command. Run 'wasp db --help' for more info."
@@ -25,15 +26,23 @@ db = mkNormalCommand "db" parseDb "Executes a database command. Run 'wasp db --h
 parseDb :: Parser Call
 parseDb = Db <$> parseDbArgs
   where
+    customMigrateDevDescription =
+      O.progDescDoc $
+        Just $
+          mconcat
+            [ OH.string "Ensures dev database corresponds to the current state of schema(entities):\n",
+              OH.string "  - Generates a new migration if there are changes in the schema.\n",
+              OH.string "  - Applies any pending migrations to the database either using the\n",
+              OH.string "    supplied migration name or asking for one."
+            ]
     parseDbArgs :: Parser DbArgs
     parseDbArgs =
       O.subparser $
         mconcat
           [ mkNormalCommand "start" (pure DbStart) "Alias for `wasp start db`.",
             mkNormalCommand "reset" (pure DbReset) "Drops all data and tables from development database and re-applies all migrations.",
-            -- FIXME: Fix stdout formatting.
             mkNormalCommand "seed" parseDbSeedArg "Executes a db seed function (specified via app.db.seeds). If there are multiple seeds, you can specify a seed to execute by providing its name, or if not then you will be asked to provide the name interactively.",
-            mkNormalCommand "migrate-dev" parseDbMigrateDevArgs "Ensures dev database corresponds to the current state of schema(entities):\n  - Generates a new migration if there are changes in the schema.\n  - Applies any pending migrations to the database either using the\n    supplied migration name or asking for one.",
+            mkCommand "migrate-dev" [customMigrateDevDescription] parseDbMigrateDevArgs "",
             mkNormalCommand "studio" (pure DbStudio) "GUI for inspecting your database."
           ]
 
