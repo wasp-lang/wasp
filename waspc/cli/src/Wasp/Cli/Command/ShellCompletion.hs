916109d4ec38b6c7f995b0e1ce9ed3b6c2ce5eb3
modotte <runecroon@gmail.com>
2023-05-30 23:39:06 +0800

    Use shellCompletionScript functions from optparse

--- waspc/cli/src/Wasp/Cli/Command/ShellCompletion.hs
+++ waspc/cli/src/Wasp/Cli/Command/ShellCompletion.hs
@@ -4,6 +4,7 @@ module Wasp.Cli.Command.ShellCompletion
 where
 
 import Control.Monad.IO.Class (liftIO)
+import qualified Options.Applicative.BashCompletion as OC
 import System.Environment (getProgName)
 import Wasp.Cli.Command (Command)
 import Wasp.Cli.Command.Call
@@ -24,9 +25,9 @@ printShellCompletionScript shell = liftIO $ do
   where
     completionScript :: ShellCompletionScript
     completionScript = case shell of
-      Bash -> bashCompletionScript
-      Zsh -> zshCompletionScript
-      Fish -> fishCompletionScript
+      Bash -> OC.bashCompletionScript
+      Zsh -> OC.zshCompletionScript
+      Fish -> OC.fishCompletionScript
 
 printShellCompletionInstruction :: Command ()
 printShellCompletionInstruction =
@@ -75,85 +76,3 @@ printShellCompletionInstruction =
         "",
         "then reset your terminal session."
       ]
-
--- TODO: Use newly exposed`*shCompletionScript` functions from optparse-applicative >=0.17
--- once it is released instead of using these text functions manually.
-bashCompletionScript :: String -> String -> String
-bashCompletionScript prog progn =
-  unlines
-    [ "_" ++ progn ++ "()",
-      "{",
-      "    local CMDLINE",
-      "    local IFS=$'\\n'",
-      "    CMDLINE=(--bash-completion-index $COMP_CWORD)",
-      "",
-      "    for arg in ${COMP_WORDS[@]}; do",
-      "        CMDLINE=(${CMDLINE[@]} --bash-completion-word $arg)",
-      "    done",
-      "",
-      "    COMPREPLY=( $(" ++ prog ++ " \"${CMDLINE[@]}\") )",
-      "}",
-      "",
-      "complete -o filenames -F _" ++ progn ++ " " ++ progn
-    ]
-
-fishCompletionScript :: String -> String -> String
-fishCompletionScript prog progn =
-  unlines
-    [ " function _" ++ progn,
-      "    set -l cl (commandline --tokenize --current-process)",
-      "    # Hack around fish issue #3934",
-      "    set -l cn (commandline --tokenize --cut-at-cursor --current-process)",
-      "    set -l cn (count $cn)",
-      "    set -l tmpline --bash-completion-enriched --bash-completion-index $cn",
-      "    for arg in $cl",
-      "      set tmpline $tmpline --bash-completion-word $arg",
-      "    end",
-      "    for opt in (" ++ prog ++ " $tmpline)",
-      "      if test -d $opt",
-      "        echo -E \"$opt/\"",
-      "      else",
-      "        echo -E \"$opt\"",
-      "      end",
-      "    end",
-      "end",
-      "",
-      "complete --no-files --command " ++ progn ++ " --arguments '(_" ++ progn ++ ")'"
-    ]
-
-zshCompletionScript :: String -> String -> String
-zshCompletionScript prog progn =
-  unlines
-    [ "#compdef " ++ progn,
-      "",
-      "local request",
-      "local completions",
-      "local word",
-      "local index=$((CURRENT - 1))",
-      "",
-      "request=(--bash-completion-enriched --bash-completion-index $index)",
-      "for arg in ${words[@]}; do",
-      "  request=(${request[@]} --bash-completion-word $arg)",
-      "done",
-      "",
-      "IFS=$'\\n' completions=($( " ++ prog ++ " \"${request[@]}\" ))",
-      "",
-      "for word in $completions; do",
-      "  local -a parts",
-      "",
-      "  # Split the line at a tab if there is one.",
-      "  IFS=$'\\t' parts=($( echo $word ))",
-      "",
-      "  if [[ -n $parts[2] ]]; then",
-      "     if [[ $word[1] == \"-\" ]]; then",
-      "       local desc=(\"$parts[1] ($parts[2])\")",
-      "       compadd -d desc -- $parts[1]",
-      "     else",
-      "       local desc=($(print -f  \"%-019s -- %s\" $parts[1] $parts[2]))",
-      "       compadd -l -d desc -- $parts[1]",
-      "     fi",
-      "  else",
-      "    compadd -f -- $word",
-      "  fi",
-      "done"
-    ]
