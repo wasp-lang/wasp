a498ce2120d973348ef9ebbe5c1b31e1b2f47a6f
shayneczyzewski <shayne.czyzewski@gmail.com>
2023-05-31 09:58:54 -0400

    track auth token events

--- waspc/data/Generator/templates/react-app/src/webSocket.ts
+++ waspc/data/Generator/templates/react-app/src/webSocket.ts
@@ -4,14 +4,14 @@ import { useState, useEffect } from 'react'
 import { io, Socket } from 'socket.io-client'
 import { DefaultEventsMap, EventsMap } from '@socket.io/component-emitter'
 
-import { getAuthToken } from './api'
+import { getAuthToken, events as apiEvents } from './api'
 import config from './config'
 
 // TODO: In the future, it would be nice if users could pass more
 // options to `io`, likely via some `configFn`.
 export const socket = io(config.apiUrl, { autoConnect: {= autoConnect =} })
 
-export function refreshAuthToken() {
+function refreshAuthToken() {
   // NOTE: In the future, we should consider making this explicit in the Wasp file when
   // we make the change for how auth works in Operations.
   // For now, it is fine, and can be ignored on the server if not needed.
@@ -25,13 +25,16 @@ export function refreshAuthToken() {
   }
 }
 
+refreshAuthToken()
+apiEvents.on('authToken.set', refreshAuthToken)
+apiEvents.on('authToken.clear', refreshAuthToken)
+
 export function useSocket<
   ServerToClientEvents extends EventsMap = DefaultEventsMap,
   ClientToServerEvents extends EventsMap = DefaultEventsMap
 >(): {
   socket: Socket<ServerToClientEvents, ClientToServerEvents>;
-  isConnected: boolean;
-  refreshAuthToken: () => void
+  isConnected: boolean
 } {
 
   const [isConnected, setIsConnected] = useState(socket.connected)
@@ -48,13 +51,11 @@ export function useSocket<
     socket.on('connect', onConnect)
     socket.on('disconnect', onDisconnect)
 
-    refreshAuthToken()
-
     return () => {
       socket.off('connect', onConnect)
       socket.off('disconnect', onDisconnect)
     }
   }, [])
 
-  return { socket, isConnected, refreshAuthToken }
+  return { socket, isConnected }
 }
