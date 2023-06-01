680977c86629bb9f7b6acac6c28fee92f8f99af0
Mihovil Ilakovac <mihovil@ilakovac.com>
2023-06-01 13:33:57 +0200

    Simplify CRUD middleware usage

--- waspc/data/Generator/templates/server/src/routes/crud/_crud.ts
+++ waspc/data/Generator/templates/server/src/routes/crud/_crud.ts
@@ -8,38 +8,37 @@ import auth from '../../core/auth.js'
 
 const _waspRouter = express.Router()
 
+{=# isAuthEnabled =}
+_waspRouter.use(auth)
+{=/ isAuthEnabled =}
+
 {=# crud.operations.Get =}
 _waspRouter.post(
     '/{= route =}',
-    {=# isAuthEnabled =}auth,{=/ isAuthEnabled =}
     withSuperJsonSerialization(crud.getFn),
 )
 {=/ crud.operations.Get =}
 {=# crud.operations.GetAll =}
 _waspRouter.post(
     '/{= route =}',
-    {=# isAuthEnabled =}auth,{=/ isAuthEnabled =}
     withSuperJsonSerialization(crud.getAllFn),
 )
 {=/ crud.operations.GetAll =}
 {=# crud.operations.Create =}
 _waspRouter.post(
     '/{= route =}',
-    {=# isAuthEnabled =}auth,{=/ isAuthEnabled =}
     withSuperJsonSerialization(crud.createFn),
 )
 {=/ crud.operations.Create =}
 {=# crud.operations.Update =}
 _waspRouter.post(
     '/{= route =}',
-    {=# isAuthEnabled =}auth,{=/ isAuthEnabled =}
     withSuperJsonSerialization(crud.updateFn),
 )
 {=/ crud.operations.Update =}
 {=# crud.operations.Delete =}
 _waspRouter.post(
     '/{= route =}',
-    {=# isAuthEnabled =}auth,{=/ isAuthEnabled =}
     withSuperJsonSerialization(crud.deleteFn),
 )
 {=/ crud.operations.Delete =}
--- waspc/e2e-test/test-outputs/waspComplexTest-golden/waspComplexTest/.wasp/out/server/src/routes/crud/tasks.ts
+++ waspc/e2e-test/test-outputs/waspComplexTest-golden/waspComplexTest/.wasp/out/server/src/routes/crud/tasks.ts
@@ -5,19 +5,18 @@ import auth from '../../core/auth.js'
 
 const _waspRouter = express.Router()
 
+_waspRouter.use(auth)
+
 _waspRouter.post(
     '/get',
-    auth,
     withSuperJsonSerialization(crud.getFn),
 )
 _waspRouter.post(
     '/get-all',
-    auth,
     withSuperJsonSerialization(crud.getAllFn),
 )
 _waspRouter.post(
     '/create',
-    auth,
     withSuperJsonSerialization(crud.createFn),
 )
 
