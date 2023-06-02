2c593dab0ac4c92f47458d1a8adf9a95f8f9834d
modotte <runecroon@gmail.com>
2023-06-02 18:46:04 +0800

    Simplify ParserTest

--- waspc/cli/test/Wasp/Cli/ParserTest.hs
+++ waspc/cli/test/Wasp/Cli/ParserTest.hs
@@ -6,70 +6,71 @@ import Wasp.Cli.Command.Call
 import Wasp.Cli.Command.ShellCompletion.CompletionArgs
 import Wasp.Cli.Parser (parserRunnerSettings)
 
+(~>) :: String -> CommandCall -> SpecWith ()
+(~>) = testParsingCliArgs
+
+testParsingCliArgs :: String -> CommandCall -> SpecWith ()
+testParsingCliArgs cliArgs expectedCmdCall =
+  it ("`" <> cliArgs <> "`") $ parseArgs cliArgs `shouldBe` Just expectedCmdCall
+
 parseArgs :: String -> Maybe CommandCall
 parseArgs args = O.getParseResult $ uncurry O.execParserPure parserRunnerSettings $ words args
 
-runTest :: String -> String -> CommandCall -> SpecWith ()
-runTest cliArgs description expectedResult =
-  it description $ parseArgs cliArgs `shouldBe` Just expectedResult
-
-runTestWithoutCommandArgs :: String -> CommandCall -> SpecWith ()
-runTestWithoutCommandArgs cliArgs expectedResult = runTest cliArgs "no args" expectedResult
-
 spec_cliArgsParser :: Spec
 spec_cliArgsParser =
-  describe "top level commands that takes no arguments at all, which should pass" $ do
-    (\(cmd, expectedResult) -> runTest cmd cmd expectedResult)
-      `mapM_` [ ("version", Version),
-                ("uninstall", Uninstall),
-                ("compile", Compile),
-                ("clean", Clean),
-                ("build", Build),
-                ("telemetry", Telemetry),
-                ("deps", Deps),
-                ("dockerfile", Dockerfile),
-                ("info", Info)
-              ]
+  describe "CLI command calls should parse correctly, returning correct Command.Call" $ do
+    describe "Top level commands that take no arguments" $ do
+      "version" ~> Version
+      "uninstall" ~> Uninstall
+      "compile" ~> Compile
+      "clean" ~> Clean
+      "build" ~> Build
+      "telemetry" ~> Telemetry
+      "deps" ~> Deps
+      "dockerfile" ~> Dockerfile
+      "info" ~> Info
 
-    describe "new should correctly pass with" $ do
-      runTestWithoutCommandArgs "new" $
-        New (NewProjectArgs {newProjectName = Nothing, newTemplateName = Nothing})
-      runTest "new Awn28WW2 --template todo-ts" "both projectName and --template arg" $
-        New (NewProjectArgs {newProjectName = Just "Awn28WW2", newTemplateName = Just "todo-ts"})
+    describe "`new` command" $ do
+      "new"
+        ~> New (NewProjectArgs {newProjectName = Nothing, newTemplateName = Nothing})
+      "new MyTodoApp --template todo-ts"
+        ~> New (NewProjectArgs {newProjectName = Just "MyTodoApp", newTemplateName = Just "todo-ts"})
 
-    describe "waspls should correctly pass with" $ do
-      runTestWithoutCommandArgs "waspls" $ WaspLS (WaspLSArgs {wlsLogFile = Nothing, wlsUseStudio = False})
-      runTest "waspls --stdio --log zero2.txt" "both --stdio and --log arg" $
-        WaspLS (WaspLSArgs {wlsLogFile = Just "zero2.txt", wlsUseStudio = True})
+    describe "`waspls` command" $ do
+      "waspls"
+        ~> WaspLS (WaspLSArgs {wlsLogFile = Nothing, wlsUseStudio = False})
+      "waspls --stdio --log myLogFile.txt"
+        ~> WaspLS (WaspLSArgs {wlsLogFile = Just "myLogFile.txt", wlsUseStudio = True})
 
-    describe "completion should correctly pass with" $ do
-      runTestWithoutCommandArgs "completion" $ Completion PrintInstruction
-      runTest "completion generate bash" "generate bash" $ Completion (PrintScript Bash)
-      runTest "completion generate zsh" "generate zsh" $ Completion (PrintScript Zsh)
-      runTest "completion generate fish" "generate fish" $ Completion (PrintScript Fish)
+    describe "`completion` command" $ do
+      "completion" ~> Completion PrintInstruction
+      "completion generate bash" ~> Completion (PrintScript Bash)
+      "completion generate zsh" ~> Completion (PrintScript Zsh)
+      "completion generate fish" ~> Completion (PrintScript Fish)
 
-    describe "start should correctly pass with" $ do
-      runTestWithoutCommandArgs "start" $ Start StartApp
-      runTest "start db" "db arg" $ Start StartDb
+    describe "`start` command" $ do
+      "start" ~> Start StartApp
+      "start db" ~> Start StartDb
 
-    describe "db should correctly pass with" $ do
-      runTest "db start" "start" $ Db DbStart
-      runTest "db migrate-dev" "migrate-dev without args" $
-        Db (DbMigrateDev (DbMigrateDevArgs {dbMigrateName = Nothing, dbMigrateCreateOnly = False}))
-      runTest "db migrate-dev --create-only --name \"dev9-2\"" "migrate-dev with both --create-only and --name arg" $
-        Db (DbMigrateDev (DbMigrateDevArgs {dbMigrateName = Just "\"dev9-2\"", dbMigrateCreateOnly = True}))
-      runTest "db reset" "reset" $ Db DbReset
-      runTest "db seed" "seed without arg" $ Db (DbSeed Nothing)
-      runTest "db seed apple" "seed arg" $ Db (DbSeed (Just "apple"))
-      runTest "db studio" "studio" $ Db DbStudio
+    describe "`db` command" $ do
+      "db start" ~> Db DbStart
+      describe "`db migrate-dev` command" $ do
+        "db migrate-dev"
+          ~> Db (DbMigrateDev (DbMigrateDevArgs {dbMigrateName = Nothing, dbMigrateCreateOnly = False}))
+        "db migrate-dev --create-only --name \"myMigrationName\""
+          ~> Db (DbMigrateDev (DbMigrateDevArgs {dbMigrateName = Just "\"myMigrationName\"", dbMigrateCreateOnly = True}))
+      "db reset" ~> Db DbReset
+      "db seed" ~> Db (DbSeed Nothing)
+      "db seed apple" ~> Db (DbSeed (Just "apple"))
+      "db studio" ~> Db DbStudio
 
-    describe "deploy should correctly pass with" $ do
-      runTestWithoutCommandArgs "deploy" $ Deploy []
-      let cliArgs = "deploy fly launch my-wasp-app mia --server-secret GOOGLE_CLIENT_ID=\"EXAMPLE@ID\" --server-secret GOOGLE_CLIENT_SECRET=EXAMPLE_SECRET"
-      runTest cliArgs "with args" $
-        Deploy ["fly", "launch", "my-wasp-app", "mia", "--server-secret", "GOOGLE_CLIENT_ID=\"EXAMPLE@ID\"", "--server-secret", "GOOGLE_CLIENT_SECRET=EXAMPLE_SECRET"]
+    describe "`deploy` command" $ do
+      "deploy" ~> Deploy []
+      "deploy fly launch my-wasp-app mia --server-secret GOOGLE_CLIENT_ID=\"EXAMPLE@ID\" --server-secret GOOGLE_CLIENT_SECRET=EXAMPLE_SECRET"
+        ~> Deploy ["fly", "launch", "my-wasp-app", "mia", "--server-secret", "GOOGLE_CLIENT_ID=\"EXAMPLE@ID\"", "--server-secret", "GOOGLE_CLIENT_SECRET=EXAMPLE_SECRET"]
 
-    describe "test should correctly pass with" $ do
-      runTest "test client" "client without args" $ Test $ TestClient []
-      runTest "test client --ui banana apple zero33 49 -q" "client with args" $
-        Test (TestClient ["--ui", "banana", "apple", "zero33", "49", "-q"])
+    describe "`test` command" $ do
+      describe "`test client` command" $ do
+        "test client" ~> Test (TestClient [])
+        "test client --ui run someRandomArgs nextArgs moreRandomArgs restOfArgs"
+          ~> Test (TestClient ["--ui", "run", "someRandomArgs", "nextArgs", "moreRandomArgs", "restOfArgs"])
