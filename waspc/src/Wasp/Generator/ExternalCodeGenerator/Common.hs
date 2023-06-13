ea34968b2b4b13eeacf960556743b45c59f2d5f7
Mihovil Ilakovac <mihovil@ilakovac.com>
2023-06-13 15:02:36 +0200

    Updates a comment

--- waspc/src/Wasp/Generator/ExternalCodeGenerator/Common.hs
+++ waspc/src/Wasp/Generator/ExternalCodeGenerator/Common.hs
@@ -14,7 +14,8 @@ import Wasp.Generator.Common (ProjectRootDir)
 
 -- TODO: consider refactoring the usage of GeneratedExternalCodeDir since
 -- generated code might end up in multiple places (e.g. ext-src/ but also public/).
--- Name should probably be narrowed down to something like UserDefinedCodeDir.
+-- Name should probably be narrowed down to something that represent only the ext-src/
+-- directory. Maybe GeneratedExtSrcDir?
 
 -- | Path to the directory where ext code will be generated.
 data GeneratedExternalCodeDir
