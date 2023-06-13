e840835d4bf99ded18ae06b2e9f33c8aeff112de
Mihovil Ilakovac <mihovil@ilakovac.com>
2023-06-13 12:24:13 +0200

    Auth UI component refactor (#1216)

--- waspc/e2e-test/test-outputs/waspComplexTest-golden/waspComplexTest/.wasp/out/web-app/src/auth/forms/types.ts
+++ waspc/e2e-test/test-outputs/waspComplexTest-golden/waspComplexTest/.wasp/out/web-app/src/auth/forms/types.ts
@@ -1,15 +1,17 @@
 import { createTheme } from '@stitches/react'
 
 export enum State {
-    Login = "login",
-    Signup = "signup",
-    ForgotPassword = "forgot-password",
-    ResetPassword = "reset-password",
-    VerifyEmail = "verify-email",
+  Login = 'login',
+  Signup = 'signup',
 }
 
 export type CustomizationOptions = {
-    logo?: string; 
-    socialLayout?: "horizontal" | "vertical";
-    appearance?: Parameters<typeof createTheme>[0];
+  logo?: string
+  socialLayout?: 'horizontal' | 'vertical'
+  appearance?: Parameters<typeof createTheme>[0]
+}
+
+export type ErrorMessage = {
+  title: string
+  description?: string
 }
