5ab7958d28762b741fff0a30b5a4097ed1502a62
Mihovil Ilakovac <mihovil@ilakovac.com>
2023-06-15 13:30:03 +0200

    Updates file path tests for Windows
    Signed-off-by: Mihovil Ilakovac <mihovil@ilakovac.com>

--- waspc/test/Util/FilePathTest.hs
+++ waspc/test/Util/FilePathTest.hs
@@ -2,18 +2,23 @@ module Util.FilePathTest where
 
 import Test.Tasty.Hspec (Spec, describe, it, shouldBe)
 import Wasp.Util.FilePath (removePathPrefix)
+import qualified System.FilePath as FP
 
 spec_FilePathTest :: Spec
 spec_FilePathTest = do
   describe "removePathPrefix" $ do
     it "removes path prefix from a path that includes that prefix" $ do
-      removePathPrefix "src/" "src/Component.js" `shouldBe` Just "Component.js"
+      removePathPrefix prefixWithTrailingSlash ("src" FP.</> "Component.js") `shouldBe` Just "Component.js"
 
     it "removes path prefix even if the prefix doesn't have trailing slash" $ do
-      removePathPrefix "src" "src/Component.js" `shouldBe` Just "Component.js"
+      removePathPrefix prefixWithoutTrailingSlash ("src" FP.</> "Component.js") `shouldBe` Just "Component.js"
 
     it "returns Nothing if path doesn't contain the prefix" $ do
-      removePathPrefix "src/" "Component.js" `shouldBe` Nothing
+      removePathPrefix prefixWithTrailingSlash "Component.js" `shouldBe` Nothing
 
     it "returns Nothing if prefix is only partially matched" $ do
-      removePathPrefix "src" "srce/Component.js" `shouldBe` Nothing
+      removePathPrefix prefixWithoutTrailingSlash ("srce" FP.</> "Component.js") `shouldBe` Nothing
+    
+    where
+      prefixWithTrailingSlash = "src" ++ [FP.pathSeparator]
+      prefixWithoutTrailingSlash = "src"
