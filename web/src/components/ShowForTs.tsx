5594e1626b7e4c649689bb82cdd28b43ad13dc49
Filip Sodić <filip.sodic@fer.hr>
2023-06-21 11:15:22 +0200

    Update ShowForTs component in docs

--- web/src/components/ShowForTs.tsx
+++ web/src/components/ShowForTs.tsx
@@ -1,4 +1,4 @@
-// Copied from 
+// Copied from (and adapted)
 // https://github.com/redwoodjs/redwood/blob/bd903c5755925ea7174775a2fdaba371b700c910/docs/src/components/ShowForTs.tsx
 
 import * as React from 'react'
@@ -20,11 +20,24 @@ interface Props {
  * ### Mdx Formatted content
  * </ShowForTs>
  * **/
-
-export default function ShowForTs({ children }: Props) {
+export function ShowForTs({ children }: Props) {
   const [jsTs] = useStorageSlot('docusaurus.tab.js-ts')
-
-  console.log(jsTs);
-
   return jsTs === 'ts' && <MDXContent>{children}</MDXContent>
 }
+
+
+/**
+ * Only renders this block if user has selected TS in the codeblocks
+ * @Note leave a blank space after opening the tag e.g.
+ *
+ * @example
+ * <ShowForJs>
+ * // {blank space}
+ * ### Mdx Formatted content
+ * </ShowForJs>
+ * **/
+export function ShowForJs({ children }: Props) {
+  const [jsTs] = useStorageSlot('docusaurus.tab.js-ts')
+  return jsTs === 'js' && <MDXContent>{children}</MDXContent>
+}
+
