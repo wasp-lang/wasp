757ce940c9a167c087b83178e4ad2fd28de8d8b2
Martin Sosic <sosic.martin@gmail.com>
2023-06-24 19:11:56 +0200

    one more test.

--- waspc/test/Util/IO/RetryTest.hs
+++ waspc/test/Util/IO/RetryTest.hs
@@ -10,11 +10,15 @@ spec_RetryTest :: Spec
 spec_RetryTest = do
   describe "retry" $ do
     it "runs action only once if it succeeds on the first try" $ do
-      let action = modify (++ [ActionCall]) >> return (Right ())
-      let (_result, events) = runState (R.retry (R.constPause 42) 2 action) []
-      events `shouldBe` [ActionCall]
+      let action = modify (++ [ActionCall]) >> return (Right () :: Either () ())
+      let (result, events) = runState (R.retry (R.constPause 42) 2 action) []
+      (result, events) `shouldBe` (Right (), [ActionCall])
+    it "runs always failing action until it spends all retries" $ do
+      let action = modify (++ [ActionCall]) >> return (Left () :: Either () ())
+      let (result, events) = runState (R.retry (R.constPause 42) 2 action) []
+      (result, events) `shouldBe` (Left (), [ActionCall, ThreadDelayCall 42, ActionCall, ThreadDelayCall 42, ActionCall])
 
--- TODO: Write more tests
+-- TODO: Write more tests, refactor/simplify.
 
 data Event = ThreadDelayCall Int | ActionCall
   deriving (Show, Eq)
