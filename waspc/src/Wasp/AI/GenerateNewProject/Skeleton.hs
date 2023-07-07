a3216849591ecb3b1f77631d7c96f6a0e9ddf1b4
Martin Sosic <sosic.martin@gmail.com>
2023-07-07 17:46:30 +0200

    Login/Signup page now have nice width.

--- waspc/src/Wasp/AI/GenerateNewProject/Skeleton.hs
+++ waspc/src/Wasp/AI/GenerateNewProject/Skeleton.hs
@@ -110,21 +110,27 @@ generateLoginJsPage =
 
       export default function Login() {
         return (
-          <div>
-            <LoginForm
-              appearance={{
-                colors: {
-                  brand: 'var(--auth-form-brand)',
-                  brandAccent: 'var(--auth-form-brand-accent)',
-                  submitButtonText: 'var(--auth-form-submit-button-text-color)',
-                }
-              }}
-            />
-            <div className="mt-4 text-center">
-              If you don't have an account go to{" "}
-              <Link to="/signup" className="text-primary-500 hover:text-primary-800">
-                sign up
-              </Link>
+          <div className="w-full h-full bg-white">
+            <div className="min-w-full min-h-[75vh] flex items-center justify-center">
+              <div className="w-full h-full max-w-sm p-5 bg-white">
+                <div>
+                  <LoginForm
+                    appearance={{
+                      colors: {
+                        brand: 'var(--auth-form-brand)',
+                        brandAccent: 'var(--auth-form-brand-accent)',
+                        submitButtonText: 'var(--auth-form-submit-button-text-color)',
+                      }
+                    }}
+                  />
+                  <div className="mt-4 text-center">
+                    If you don't have an account go to{" "}
+                    <Link to="/signup" className="text-primary-500 hover:text-primary-800">
+                      sign up
+                    </Link>
+                  </div>
+                </div>
+              </div>
             </div>
           </div>
         );
@@ -142,21 +148,27 @@ generateSignupJsPage =
 
       export default function Signup() {
         return (
-          <div>
-            <SignupForm
-              appearance={{
-                colors: {
-                  brand: 'var(--auth-form-brand)',
-                  brandAccent: 'var(--auth-form-brand-accent)',
-                  submitButtonText: 'var(--auth-form-submit-button-text-color)',
-                }
-              }}
-            />
-            <div className="mt-4 text-center">
-              If you already have an account go to{" "}
-              <Link to="/login" className="text-primary-500 hover:text-primary-800">
-                login
-              </Link>
+          <div className="w-full h-full bg-white">
+            <div className="min-w-full min-h-[75vh] flex items-center justify-center">
+              <div className="w-full h-full max-w-sm p-5 bg-white">
+                <div>
+                  <SignupForm
+                    appearance={{
+                      colors: {
+                        brand: 'var(--auth-form-brand)',
+                        brandAccent: 'var(--auth-form-brand-accent)',
+                        submitButtonText: 'var(--auth-form-submit-button-text-color)',
+                      }
+                    }}
+                  />
+                  <div className="mt-4 text-center">
+                    If you already have an account go to{" "}
+                    <Link to="/login" className="text-primary-500 hover:text-primary-800">
+                      login
+                    </Link>
+                  </div>
+                </div>
+              </div>
             </div>
           </div>
         );
