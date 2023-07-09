7be240b0fad323b12837e21305069552d9b57fe7
Martin Sosic <sosic.martin@gmail.com>
2023-07-09 17:58:15 +0200

    Updated FAQ.

--- wasp-ai/src/client/components/Faq.jsx
+++ wasp-ai/src/client/components/Faq.jsx
@@ -1,46 +1,60 @@
 import React, { useState } from 'react'
-import { Link } from 'react-router-dom'
 import { FiChevronDown, FiChevronRight } from 'react-icons/fi'
+
+function l(title, overrideTitle) {
+  const links = {
+    "Wasp": "https://wasp-lang.dev/",
+    "web app": "https://github.com/wasp-lang/wasp/tree/wasp-ai/wasp-ai",
+    "GPT code agent": "https://github.com/wasp-lang/wasp/tree/wasp-ai/waspc/src/Wasp/AI",
+    "blog post": "https://wasp-lang.dev/blog/gpt-web-app-generator/"
+  };
+
+  return <a href={links[title]} className="underline decoration-2 decoration-yellow-500">{overrideTitle || title}</a>
+}
 
 const faqs = [
   {
     question: 'What is GPT Web App Generator?',
     answer: <p>
-      <strong>TL;DR</strong> - GPT Web App Generator scaffolds a complete full-stack application based on your description.
+      GTP Web App Generator is an experiment by the {l("Wasp")} team:
+      it is a web app where you can shortly describe the web app you would like to create, and in a matter of minutes,
+      a full-stack web app codebase, written in React, Node.js, Prisma, and Wasp, will be generated right in front of you,
+      and made available to download and run locally.
+
       <br/><br/>
-      Paragraph 2
+
+      All the code behind GPT Web App Generator is open source: {l("web app")} and {l("GPT code agent")}.
+
       <br/><br/>
-      Paragraph 3
+
+      To learn more about the GPT Web App Generator and how exactly it works, check out our {l("blog post")}.
     </p>
   },
   {
     question: 'What is Wasp?',
     answer: <p>
-      Wasp is an open-source, full-stack framework for React & Node.js. It covers everything from front-end and back-end to deployment.
-      You can think of it as a modern version of Ruby on Rails - opinionated, provides best practices and helps you move faster.
-      Learn more about it&nbsp;
-      <a 
-        href='https://wasp-lang.dev/'
-        className='underline decoration-2 decoration-yellow-500 font-medium'
-      >here</a>
+      {l("Wasp")} is an open-source, full-stack framework for React & Node.js. It covers everything from front-end, back-end, database, to deployment.
+      You can think of it as a modern version of Ruby on Rails for JS/TS - opinionated, provides best practices and helps you move faster.
     </p>
   },
   {
-    question: 'Does it always work flawlessly?',
+    question: 'How well does it work?',
     answer: <p>
-      Since this is a GPT-powered solution, it does not provide a 100% deterministic output. That also means that, from time to time, it will introduce small mistakes in the generated code.
+      Due to GPT being non-deterministic, it sometimes introduces (small) mistakes, especially for more complex apps, but altogether it works better than we expected!
+      The code it generates is often very reasonable, and for very simple apps, it can even produce a working app out of the box, while for a bit more complex apps
+      it currently serves more like a super-intelligent starter that needs a couple of tweeks to get it going.
+      With GPT4 increasing its availability and with LLMs improving in general, the quality of generated code will only get better!
     </p>
   },
   {
-    question: 'How complex applications can it produce?',
+    question: 'What kind of apps can it generate?',
     answer: <p>
-      Take a look at our examples.
-    </p>
-  },
-  {
-    question: 'Which stack is the code generated in?',
-    answer: <p>
-      React, Node.js, Prisma, and Wasp.
+      It can generate full stack web apps written in React, Node.js, Prisma, and Wasp.
+      You can download the generated app, run it locally on your machine, even easily deploy if you wish.
+
+      In order to keep things simpler for this first version, we enforced some limitations: no additional npm deps, no additional files, no TS, no advanced Wasp features.
+
+      We believe most of these limitations can be removed in the future with more work!
     </p>
   },
 ]
