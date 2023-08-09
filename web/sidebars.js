67328bd05ece709c8939e1d0432396a6fb265919
Filip Sodić <filip.sodic@gmail.com>
2023-08-10 01:36:56 +0200

    Write data model docs (#1374)

--- web/sidebars.js
+++ web/sidebars.js
@@ -1,115 +1,118 @@
 module.exports = {
   docs: [
     {
-      type: "category",
-      label: "Getting Started",
+      type: 'category',
+      label: 'Getting Started',
       collapsed: false,
       collapsible: false,
       items: [
-        "introduction/what-is-wasp",
-        "introduction/getting-started",
-        "introduction/editor-setup",
+        'introduction/what-is-wasp',
+        'introduction/getting-started',
+        'introduction/editor-setup',
       ],
     },
     {
-      type: "category",
-      label: "Essentials",
+      type: 'category',
+      label: 'Essentials',
       collapsed: false,
       collapsible: false,
       items: [
-        "essentials/create",
-        "essentials/project-structure",
-        "essentials/pages",
-        "essentials/entities",
-        "essentials/queries",
-        "essentials/actions",
-        "essentials/auth"
-      ]
+        'essentials/create',
+        'essentials/project-structure',
+        'essentials/pages',
+        'essentials/entities',
+        'essentials/queries',
+        'essentials/actions',
+        'essentials/auth',
+      ],
     },
     {
-      type: "category",
-      label: "Data Model",
+      type: 'category',
+      label: 'Data Model',
       collapsed: false,
       collapsible: false,
       items: [
-        "database/entities",
-        "database/operations",
-        "database/crud",
-        "database/backends",
-      ]
-    },
-    {
-      type: "category",
-      label: "Advanced Features",
-      collapsed: false,
-      collapsible: false,
-      items: [
-        "advanced/email",
-        "advanced/jobs",
-        "advanced/web-sockets",
-        "advanced/apis",
-        "advanced/middleware-config",
+        'database/entities',
         {
-          type: "category",
-          label: "Deployment",
+          type: 'category',
+          label: 'Operations',
+          collapsed: false,
+          items: [
+            'database/operations/overview',
+            'database/operations/queries',
+            'database/operations/actions',
+          ],
+        },
+        'database/crud',
+        'database/backends',
+      ],
+    },
+    {
+      type: 'category',
+      label: 'Advanced Features',
+      collapsed: false,
+      collapsible: false,
+      items: [
+        'advanced/email',
+        'advanced/jobs',
+        'advanced/web-sockets',
+        'advanced/apis',
+        'advanced/middleware-config',
+        {
+          type: 'category',
+          label: 'Deployment',
           collapsed: true,
           items: [
-            "advanced/deployment/overview",
-            "advanced/deployment/cli",
-            "advanced/deployment/manually",
-          ]
+            'advanced/deployment/overview',
+            'advanced/deployment/cli',
+            'advanced/deployment/manually',
+          ],
         },
-      ]
+      ],
     },
     {
-      type: "category",
-      label: "Authentication",
+      type: 'category',
+      label: 'Authentication',
       collapsed: false,
       collapsible: false,
       items: [
-        "auth/overview",
-        "auth/ui",
-        "auth/username-and-pass",
-        "auth/email",
+        'auth/overview',
+        'auth/ui',
+        'auth/username-and-pass',
+        'auth/email',
         {
-          type: "category",
-          label: "Social Auth",
+          type: 'category',
+          label: 'Social Auth',
           collapsed: true,
           items: [
-            "auth/social-auth/overview",
-            "auth/social-auth/github",
-            "auth/social-auth/google"
-          ]
+            'auth/social-auth/overview',
+            'auth/social-auth/github',
+            'auth/social-auth/google',
+          ],
         },
-      ]
+      ],
     },
     {
-      type: "category",
-      label: "Project Setup",
+      type: 'category',
+      label: 'Project Setup',
       collapsed: false,
       collapsible: false,
       items: [
-        "project/starter-templates",
-        "project/client-config",
-        "project/server-config",
-        "project/public-files",
-        "project/env-vars",
-        "project/testing",
-        "project/dependencies",
-        "project/css-frameworks",
-      ]
+        'project/starter-templates',
+        'project/client-config',
+        'project/server-config',
+        'project/public-files',
+        'project/env-vars',
+        'project/testing',
+        'project/dependencies',
+        'project/css-frameworks',
+      ],
     },
     {
-      type: "category",
-      label: "Miscellaneous",
+      type: 'category',
+      label: 'Miscellaneous',
       collapsed: false,
-      collapsible: false,
-      items: [
-        "contributing",
-        "telemetry",
-        "vision",
-        "contact"
-      ]
-    }
+      items: ['contributing', 'telemetry', 'vision', 'contact'],
+    },
   ],
-};
+}
