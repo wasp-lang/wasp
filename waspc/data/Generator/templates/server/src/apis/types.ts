cb7910a56552b4979c08b2531431d0aaa04ca30b
Mihovil Ilakovac <mihovil@ilakovac.com>
2023-08-18 16:27:52 +0200

    Uses export type

--- waspc/data/Generator/templates/server/src/apis/types.ts
+++ waspc/data/Generator/templates/server/src/apis/types.ts
@@ -2,7 +2,7 @@
 
 import { type ParamsDictionary as ExpressParams, type Query as ExpressQuery } from 'express-serve-static-core'
 
-export { ParamsDictionary as ExpressParams, Query as ExpressQuery } from 'express-serve-static-core'
+export type { ParamsDictionary as ExpressParams, Query as ExpressQuery } from 'express-serve-static-core'
 
 import {
   {=# allEntities =}
