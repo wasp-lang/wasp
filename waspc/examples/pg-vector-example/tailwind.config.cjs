cb88dfc618e143caf7539f7e9464c6465596c2c1
Mihovil Ilakovac <mihovil@ilakovac.com>
2023-09-12 13:03:57 +0200

    Adding support for Postgres Extensions (#1443)

---
+++ waspc/examples/pg-vector-example/tailwind.config.cjs
@@ -0,0 +1,14 @@
+const { nextui } = require("@nextui-org/react");
+
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./src/**/*.{js,jsx,ts,tsx}",
+    "./node_modules/@nextui-org/theme/dist/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {},
+  },
+  darkMode: "class",
+  plugins: [nextui()],
+};
