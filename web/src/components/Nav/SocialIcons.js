4f5f4d963f3768cf2376705d983552f4b4d39ed3
khanjan sanghavi <khanjannsanghavi0@gmail.com>
2023-10-19 14:56:40 +0000

    final fix

--- web/src/components/Nav/SocialIcons.js
+++ web/src/components/Nav/SocialIcons.js
@@ -8,10 +8,6 @@ export const DiscordIcon = () => (
 )
 
 export const TwitterIcon = () => (
-  // <svg width='24' height='24' fill='currentColor' viewBox='0 0 335 276'>
-  //   <title>Twitter</title>
-  //   <path d='m302 70a195 195 0 0 1 -299 175 142 142 0 0 0 97 -30 70 70 0 0 1 -58 -47 70 70 0 0 0 31 -2 70 70 0 0 1 -57 -66 70 70 0 0 0 28 5 70 70 0 0 1 -18 -90 195 195 0 0 0 141 72 67 67 0 0 1 116 -62 117 117 0 0 0 43 -17 65 65 0 0 1 -31 38 117 117 0 0 0 39 -11 65 65 0 0 1 -32 35'></path>
-  // </svg>
   <svg
     width="24"
     height="24"
@@ -19,6 +15,7 @@ export const TwitterIcon = () => (
     class="bi bi-twitter-x"
     viewBox="0 0 16 16"
   >
+    <title>X</title>
     <path d="M12.6.75h2.454l-5.36 6.142L16 15.25h-4.937l-3.867-5.07-4.425 5.07H.316l5.733-6.57L0 .75h5.063l3.495 4.633L12.601.75Zm-.86 13.028h1.36L4.323 2.145H2.865l8.875 11.633Z" />
   </svg>
 )
