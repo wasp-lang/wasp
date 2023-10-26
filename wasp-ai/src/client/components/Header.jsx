9322d97d93a4e6c6017a22a08ace084a52d066b5
vincanger <70215737+vincanger@users.noreply.github.com>
2023-10-26 12:40:48 +0200

    update og:title

--- wasp-ai/src/client/components/Header.jsx
+++ wasp-ai/src/client/components/Header.jsx
@@ -2,15 +2,20 @@ import { StatusPill } from './StatusPill';
 import { Title } from './Title';
 import { signInUrl as gitHubSignInUrl } from '@wasp/auth/helpers/GitHub';
 import { AiFillGithub } from 'react-icons/ai';
+import useAuth from '@wasp/auth/useAuth';
 
 export function Header({ currentStatus, isStatusVisible }) {
+  const { data: user } = useAuth();
+
   return (
     <div className='mb-4 bg-slate-50 p-8 rounded-xl md:flex justify-between items-center'>
       <Title />
-      {isStatusVisible && (
+      {!!user && isStatusVisible ? (
         <StatusPill status={currentStatus.status} className='hidden md:flex'>
           {currentStatus.message}
         </StatusPill>
+      ) : (
+        <GithubLoginButton />
       )}
     </div>
   );
@@ -18,8 +23,11 @@ export function Header({ currentStatus, isStatusVisible }) {
 
 function GithubLoginButton() {
   return (
-        <button className='button gray flex !text-gray-800 hover:bg-slate-300 shadow-md' onClick={() => window.location.href = gitHubSignInUrl}>
-          <AiFillGithub className='w-6 h-6 mr-2' /> Sign in with GitHub
-        </button>
-      )
+    <button
+      className='button gray flex !text-gray-800 hover:bg-slate-300 shadow-md'
+      onClick={() => (window.location.href = gitHubSignInUrl)}
+    >
+      <AiFillGithub className='w-6 h-6 mr-2' /> Sign in with GitHub
+    </button>
+  );
 }
