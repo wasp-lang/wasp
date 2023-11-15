0d7087f4d6b43675a532f08a9eed21f09c65c7b0
Filip Sodić <filip.sodic@fer.hr>
2023-11-15 17:55:25 +0100

    Add prototype migration script and fix some issues

--- waspc/data/Generator/templates/react-app/vite.config.ts
+++ waspc/data/Generator/templates/react-app/vite.config.ts
@@ -5,6 +5,9 @@ import react from '@vitejs/plugin-react-swc'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  optimizeDeps: {
+    exclude: ['@wasp']
+  },
   server: {
     port: 3000,
     host: '0.0.0.0',
