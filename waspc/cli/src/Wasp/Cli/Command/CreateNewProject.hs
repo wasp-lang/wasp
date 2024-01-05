52a0993a03715cad3c90c19b0827a0530e4e9cf2
Martin Sosic <sosic.martin@gmail.com>
2024-01-05 19:16:06 +0100

    Starting instructions are not adjustable per template.

--- waspc/cli/src/Wasp/Cli/Command/CreateNewProject.hs
+++ waspc/cli/src/Wasp/Cli/Command/CreateNewProject.hs
@@ -5,11 +5,12 @@ where
 
 import Control.Monad.IO.Class (liftIO)
 import Data.Function ((&))
+import qualified StrongPath as SP
 import Wasp.Cli.Command (Command)
 import Wasp.Cli.Command.Call (Arguments)
 import qualified Wasp.Cli.Command.CreateNewProject.AI as AI
 import Wasp.Cli.Command.CreateNewProject.ArgumentsParser (parseNewProjectArgs)
-import Wasp.Cli.Command.CreateNewProject.Common (printGettingStartedInstructions, throwProjectCreationError)
+import qualified Wasp.Cli.Command.CreateNewProject.Common as Common
 import Wasp.Cli.Command.CreateNewProject.ProjectDescription
   ( NewProjectDescription (..),
     obtainNewProjectDescription,
@@ -18,22 +19,24 @@ import Wasp.Cli.Command.CreateNewProject.StarterTemplates
   ( DirBasedTemplateMetadata (_path),
     StarterTemplate (..),
     getStarterTemplates,
+    getTemplateStartingInstructions,
   )
 import Wasp.Cli.Command.CreateNewProject.StarterTemplates.GhRepo (createProjectOnDiskFromGhRepoTemplate)
 import Wasp.Cli.Command.CreateNewProject.StarterTemplates.Local (createProjectOnDiskFromLocalTemplate)
 import Wasp.Cli.Command.Message (cliSendMessageC)
 import qualified Wasp.Message as Msg
+import qualified Wasp.Util.Terminal as Term
 
 -- | It receives all of the arguments that were passed to the `wasp new` command.
 createNewProject :: Arguments -> Command ()
 createNewProject args = do
-  newProjectArgs <- parseNewProjectArgs args & either throwProjectCreationError return
+  newProjectArgs <- parseNewProjectArgs args & either Common.throwProjectCreationError return
   starterTemplates <- liftIO getStarterTemplates
 
   newProjectDescription <- obtainNewProjectDescription newProjectArgs starterTemplates
 
   createProjectOnDisk newProjectDescription
-  liftIO $ printGettingStartedInstructions $ _absWaspProjectDir newProjectDescription
+  liftIO $ printGettingStartedInstructionsForProject newProjectDescription
 
 createProjectOnDisk :: NewProjectDescription -> Command ()
 createProjectOnDisk
@@ -51,3 +54,11 @@ createProjectOnDisk
         liftIO $ createProjectOnDiskFromLocalTemplate absWaspProjectDir projectName appName $ _path metadata
       AiGeneratedStarterTemplate ->
         AI.createNewProjectInteractiveOnDisk absWaspProjectDir appName
+
+-- | This function assumes that the project dir was created inside the current working directory.
+printGettingStartedInstructionsForProject :: NewProjectDescription -> IO ()
+printGettingStartedInstructionsForProject projectDescription = do
+  let projectDirName = init . SP.toFilePath . SP.basename $ _absWaspProjectDir projectDescription
+  let instructions = getTemplateStartingInstructions projectDirName $ _template projectDescription
+  putStrLn $ Term.applyStyles [Term.Green] $ "Created new Wasp app in ./" ++ projectDirName ++ " directory!"
+  putStrLn instructions
