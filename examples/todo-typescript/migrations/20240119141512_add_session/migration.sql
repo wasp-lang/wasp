723c9c579d4d7de7bd322607be70de99188f71ee
Mihovil Ilakovac <mihovil@ilakovac.com>
2024-01-19 15:16:26 +0100

    Update todo-typescript to 0.12

---
+++ examples/todo-typescript/migrations/20240119141512_add_session/migration.sql
@@ -0,0 +1,13 @@
+-- CreateTable
+CREATE TABLE "Session" (
+    "id" TEXT NOT NULL PRIMARY KEY,
+    "expiresAt" DATETIME NOT NULL,
+    "userId" TEXT NOT NULL,
+    CONSTRAINT "Session_userId_fkey" FOREIGN KEY ("userId") REFERENCES "Auth" ("id") ON DELETE CASCADE ON UPDATE CASCADE
+);
+
+-- CreateIndex
+CREATE UNIQUE INDEX "Session_id_key" ON "Session"("id");
+
+-- CreateIndex
+CREATE INDEX "Session_userId_idx" ON "Session"("userId");
