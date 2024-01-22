1c47aa00f63b8ff06bcc84b95fd47a7c801deb80
Martin Sosic <sosic.martin@gmail.com>
2024-01-22 17:07:01 +0100

    Fixed import api from '@wasp/api'.

--- waspc/examples/todo-typescript/src/task/queries.ts
+++ waspc/examples/todo-typescript/src/task/queries.ts
@@ -1,12 +1,16 @@
 import HttpError from 'wasp/core/HttpError'
 import type { GetTasks } from 'wasp/server/queries/types'
 import type { Task } from 'wasp/entities'
+import api from "wasp/api";
 
 //Using TypeScript's new 'satisfies' keyword, it will infer the types of the arguments and return value
 export const getTasks = ((_args, context) => {
   if (!context.user) {
     throw new HttpError(401)
   }
+
+  console.log("test");
+  console.log(api);
 
   return context.entities.Task.findMany({
     where: { user: { id: context.user.id } },
