22b48e0f74b1302f0393316bdceb722eddd8cbde
Mihovil Ilakovac <mihovil@ilakovac.com>
2024-01-22 13:13:48 +0100

    SDK fixes for Email and Social auth (#1654)

---
+++ waspc/examples/todo-typescript/src/user/auth.tsx
@@ -0,0 +1,59 @@
+import { ResetPasswordForm } from "wasp/auth/forms/ResetPassword";
+import { LoginForm } from "wasp/auth/forms/Login";
+import { VerifyEmailForm } from "wasp/auth/forms/VerifyEmail";
+import { SignupForm } from "wasp/auth/forms/Signup";
+import { ForgotPasswordForm } from "wasp/auth/forms/ForgotPassword";
+import { Link } from "react-router-dom";
+
+export function SignupPage() {
+  return (
+    <main>
+      {/** Wasp has built-in auth forms & flows, which you can customize or opt-out of, if you wish :)
+       * https://wasp-lang.dev/docs/guides/auth-ui
+       */}
+      <SignupForm
+        additionalFields={[
+          {
+            type: "input",
+            label: "Address",
+            name: "address",
+            validations: {
+              required: "Address is required",
+            },
+          },
+        ]}
+      />
+      <br />
+      <span>
+        I already have an account (<Link to="/login">go to login</Link>).
+      </span>
+    </main>
+  );
+}
+
+export function LoginPage() {
+  return (
+    <main>
+      {/** Wasp has built-in auth forms & flows, which you can customize or opt-out of, if you wish :)
+       * https://wasp-lang.dev/docs/guides/auth-ui
+       */}
+      <LoginForm />
+      <br />
+      <span>
+        I don't have an account yet (<Link to="/signup">go to signup</Link>).
+      </span>
+    </main>
+  );
+}
+
+export function RequestPasswordResetPage() {
+  return <ForgotPasswordForm />;
+}
+
+export function PasswordResetPage() {
+  return <ResetPasswordForm />;
+}
+
+export function EmailVerificationPage() {
+  return <VerifyEmailForm />;
+}
---
+++ waspc/examples/todo-typescript/src/user/userSignupFields.ts
@@ -0,0 +1,18 @@
+import { defineUserSignupFields } from "wasp/auth/providers/types";
+
+export const googleUserSignupFields = defineUserSignupFields({
+  address: () => "Placeholder address",
+});
+
+export const githubUserSignupFields = defineUserSignupFields({
+  address: () => "Placeholder address",
+});
+
+export const userSignupFields = defineUserSignupFields({
+  address: (data) => {
+    if (typeof data.address !== "string") {
+      throw new Error("Address must be provided on signup.");
+    }
+    return data.address;
+  },
+});
