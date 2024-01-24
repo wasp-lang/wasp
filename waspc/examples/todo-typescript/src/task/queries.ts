e4bb5fe1cc82c05c84dae8c13427b61dc36d4ddf
Martin Sosic <sosic.martin@gmail.com>
2024-01-24 16:43:15 +0100

    Added small test to todo-typescript for wasp/auth/validation.

--- waspc/examples/todo-typescript/src/task/queries.ts
+++ waspc/examples/todo-typescript/src/task/queries.ts
@@ -1,12 +1,15 @@
 import HttpError from 'wasp/core/HttpError'
 import type { GetTasks } from 'wasp/server/queries/types'
 import type { Task } from 'wasp/entities'
+import { ensureValidEmail } from 'wasp/auth/validation'
 
 //Using TypeScript's new 'satisfies' keyword, it will infer the types of the arguments and return value
 export const getTasks = ((_args, context) => {
   if (!context.user) {
     throw new HttpError(401)
   }
+
+  ensureValidEmail({ email: "wasp@gmail.com"});
 
   return context.entities.Task.findMany({
     where: { user: { id: context.user.id } },
