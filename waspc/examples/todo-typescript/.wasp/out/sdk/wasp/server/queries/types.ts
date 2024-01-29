f9978e2af7606fa7f95cb417086658cdf2a4aba8
Filip Sodić <filip.sodic@fer.hr>
2024-01-29 15:33:38 +0100

    Remove generator output from Git

--- waspc/examples/todo-typescript/.wasp/out/sdk/wasp/server/queries/types.ts
+++
@@ -1,6 +0,0 @@
-import { type _Task, type AuthenticatedQuery, type Payload } from "../_types";
-
-export type GetTasks<
-  Input extends Payload = never,
-  Output extends Payload = Payload
-> = AuthenticatedQuery<[_Task], Input, Output>;
