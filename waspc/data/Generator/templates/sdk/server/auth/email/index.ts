7a21dd740ecb4b48bac45d93d188377ff79e48df
Mihovil Ilakovac <mihovil@ilakovac.com>
2024-01-30 15:34:37 +0100

    Adds wasp/server/auth API (#1694)

--- waspc/data/Generator/templates/sdk/server/auth/email/index.ts
+++ waspc/data/Generator/templates/sdk/server/auth/email/index.ts
@@ -1,5 +1,7 @@
+// PUBLIC API
 export type GetVerificationEmailContentFn = (params: { verificationLink: string }) => EmailContent;
 
+// PUBLIC API
 export type GetPasswordResetEmailContentFn = (params: { passwordResetLink: string }) => EmailContent;
 
 type EmailContent = {
@@ -7,3 +9,12 @@ type EmailContent = {
     html: string;
     text: string;
 }
+
+export {
+    createEmailVerificationLink,
+    sendEmailVerificationEmail,
+    createPasswordResetLink,
+    sendPasswordResetEmail,
+    isEmailResendAllowed,
+} from './utils.js'
+export { ensureValidEmail } from '../../../auth/validation.js'
