b8dc9ea2e55e25a09b62f3c17b9302190c91426f
Mihovil Ilakovac <mihovil@ilakovac.com>
2024-02-08 15:16:03 +0100

    Cleanup

--- waspc/data/Generator/templates/sdk/client/index.ts
+++ waspc/data/Generator/templates/sdk/client/index.ts
@@ -1,5 +1,6 @@
 import * as path from 'path';
 
+// PUBLIC API
 /**
  * Make a pattern relative from Wasp's web-app directory to the project's root.
  * This enables users to define a glob pattern that is relative to the project's root.
