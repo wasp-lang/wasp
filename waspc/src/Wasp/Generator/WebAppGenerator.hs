87e576d66d9ffec70047fe87c5a426386d835e70
Mihovil Ilakovac <mihovil@ilakovac.com>
2024-02-16 12:25:31 +0100

    Fixes Tailwind setup in client tests (#1766)

--- waspc/src/Wasp/Generator/WebAppGenerator.hs
+++ waspc/src/Wasp/Generator/WebAppGenerator.hs
@@ -36,7 +36,6 @@ import Wasp.Generator.Common
   ( makeJsArrayFromHaskellList,
     makeJsonWithEntityData,
   )
-import qualified Wasp.Generator.ConfigFile as G.CF
 import qualified Wasp.Generator.DbGenerator.Auth as DbAuth
 import Wasp.Generator.FileDraft (FileDraft, createTextFileDraft)
 import qualified Wasp.Generator.FileDraft as FD
@@ -120,7 +119,7 @@ genNpmrc =
       Nothing
 
 npmDepsForWasp :: AppSpec -> N.NpmDepsForWasp
-npmDepsForWasp spec =
+npmDepsForWasp _spec =
   N.NpmDepsForWasp
     { N.waspDependencies =
         AS.Dependency.fromList
@@ -133,8 +132,7 @@ npmDepsForWasp spec =
             ("mitt", "3.0.0"),
             -- Used for Auth UI
             ("react-hook-form", "^7.45.4")
-          ]
-          ++ depsRequiredByTailwind spec,
+          ],
       N.waspDevDependencies =
         AS.Dependency.fromList
           [ -- TODO: Allow users to choose whether they want to use TypeScript
@@ -150,17 +148,6 @@ npmDepsForWasp spec =
             ("@tsconfig/vite-react", "^2.0.0")
           ]
     }
-
-depsRequiredByTailwind :: AppSpec -> [AS.Dependency.Dependency]
-depsRequiredByTailwind spec =
-  if G.CF.isTailwindUsed spec
-    then
-      AS.Dependency.fromList
-        [ ("tailwindcss", "^3.2.7"),
-          ("postcss", "^8.4.21"),
-          ("autoprefixer", "^10.4.13")
-        ]
-    else []
 
 genGitignore :: Generator FileDraft
 genGitignore =
