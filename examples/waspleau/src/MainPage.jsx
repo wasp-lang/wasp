67d6cd43decdfd5393a3035e0354ba9689a15674
Mihovil Ilakovac <mihovil@ilakovac.com>
2024-02-27 10:50:28 +0100

    Updates imports

--- examples/waspleau/src/MainPage.jsx
+++ examples/waspleau/src/MainPage.jsx
@@ -1,8 +1,8 @@
-import { useQuery, dashboard as refreshDashboardData } from "wasp/client/operations";
 import React from 'react'
-
 import './style.css'
 import addWaspSourceHeader from './addWaspSourceHeader'
+
+import { useQuery, dashboard as refreshDashboardData } from "wasp/client/operations";
 
 const MainPage = () => {
   const { data: dashboardData, isFetching, error } = useQuery(refreshDashboardData, null, { refetchInterval: 60 * 1000 })
