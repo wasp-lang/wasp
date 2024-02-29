1669357dbeb2b71399bbb9bb52f9e0219fc81d77
Mihovil Ilakovac <mihovil@ilakovac.com>
2024-02-29 18:48:09 +0100

    Conditionally apply Web Crypto API polyfill (#1834)
    * Conditionally apply Web Crypto API polyfill
    
    Signed-off-by: Mihovil Ilakovac <mihovil@ilakovac.com>
    
    * Updates e2e tests
    
    * Update waspc/data/Generator/templates/server/src/polyfill.ts
    
    ---------
    
    Signed-off-by: Mihovil Ilakovac <mihovil@ilakovac.com>
    Co-authored-by: Martin Šošić <Martinsos@users.noreply.github.com>

--- waspc/data/Generator/templates/server/src/polyfill.ts
+++ waspc/data/Generator/templates/server/src/polyfill.ts
@@ -3,5 +3,9 @@
 
 import { webcrypto } from "node:crypto";
 
-// @ts-ignore
-globalThis.crypto = webcrypto as Crypto;
+// NOTE: node < 19 doesn't have Crypto API, which we need for Lucia, so we apply the polyfill if Crypto API is not defined.
+if (typeof globalThis.crypto === "undefined") {
+  // @ts-ignore
+  globalThis.crypto = webcrypto as Crypto;
+}
+
