5ba64b11e410f41c2175f4ac1e31c753b59f0a85
Mihovil Ilakovac <mihovil@ilakovac.com>
2024-03-08 19:05:34 +0100

    Replaces `jsonwebtoken` with `oslo/jwt` (#1852)

--- waspc/data/Generator/templates/sdk/wasp/auth/utils.ts
+++ waspc/data/Generator/templates/sdk/wasp/auth/utils.ts
@@ -1,6 +1,5 @@
 {{={= =}=}}
 import { hashPassword } from './password.js'
-import { verify } from './jwt.js'
 import { prisma, HttpError } from 'wasp/server'
 import { sleep } from 'wasp/server/utils'
 import {
@@ -172,11 +171,6 @@ export async function deleteUserByAuthId(authId: string): Promise<{ count: numbe
   return prisma.{= userEntityLower =}.deleteMany({ where: { auth: {
     id: authId,
   } } })
-}
-
-// PRIVATE API
-export async function verifyToken<T = unknown>(token: string): Promise<T> {
-  return verify(token);
 }
 
 // PRIVATE API
