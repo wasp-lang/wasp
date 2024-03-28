052c569531636567f5f5ba46fc63659367025ab0
Filip Sodić <filip.sodic@fer.hr>
2024-03-28 15:07:08 +0100

    Remove even more redundant files

--- waspc/src/Wasp/Generator/ServerGenerator/OperationsG.hs
+++ waspc/src/Wasp/Generator/ServerGenerator/OperationsG.hs
@@ -10,8 +10,7 @@ where
 
 import Data.Aeson (object, (.=))
 import qualified Data.Aeson as Aeson
-import Data.List (nub)
-import Data.Maybe (fromJust, fromMaybe)
+import Data.Maybe (fromJust)
 import StrongPath (Dir, File', Path, Path', Posix, Rel, reldir, reldirP, relfile, (</>))
 import qualified StrongPath as SP
 import Wasp.AppSpec (AppSpec)
@@ -20,8 +19,7 @@ import qualified Wasp.AppSpec.Action as AS.Action
 import Wasp.AppSpec.Operation (getName)
 import qualified Wasp.AppSpec.Operation as AS.Operation
 import qualified Wasp.AppSpec.Query as AS.Query
-import Wasp.AppSpec.Valid (isAuthEnabled)
-import Wasp.Generator.Common (ServerRootDir, makeJsonWithEntityData)
+import Wasp.Generator.Common (makeJsonWithEntityData)
 import Wasp.Generator.FileDraft (FileDraft)
 import Wasp.Generator.Monad (Generator)
 import qualified Wasp.Generator.ServerGenerator.Common as C
@@ -30,11 +28,7 @@ import Wasp.Util (toUpperFirst, (<++>))
 
 genOperations :: AppSpec -> Generator [FileDraft]
 genOperations spec =
-  sequence
-    [ genQueryTypesFile spec,
-      genActionTypesFile spec
-    ]
-    <++> genQueries spec
+  genQueries spec
     <++> genActions spec
 
 genQueries :: AppSpec -> Generator [FileDraft]
@@ -42,22 +36,6 @@ genQueries = mapM genQuery . AS.getQueries
 
 genActions :: AppSpec -> Generator [FileDraft]
 genActions = mapM genAction . AS.getActions
-
-genQueryTypesFile :: AppSpec -> Generator FileDraft
-genQueryTypesFile spec = genOperationTypesFile tmplFile dstFile operations isAuthEnabledGlobally
-  where
-    tmplFile = [relfile|src/queries/types.ts|]
-    dstFile = [relfile|src/queries/types.ts|]
-    operations = map (uncurry AS.Operation.QueryOp) $ AS.getQueries spec
-    isAuthEnabledGlobally = isAuthEnabled spec
-
-genActionTypesFile :: AppSpec -> Generator FileDraft
-genActionTypesFile spec = genOperationTypesFile tmplFile dstFile operations isAuthEnabledGlobally
-  where
-    tmplFile = [relfile|src/actions/types.ts|]
-    dstFile = [relfile|src/actions/types.ts|]
-    operations = map (uncurry AS.Operation.ActionOp) $ AS.getActions spec
-    isAuthEnabledGlobally = isAuthEnabled spec
 
 -- | Here we generate JS file that basically imports JS query function provided by user,
 --   decorates it (mostly injects stuff into it) and exports. Idea is that the rest of the server,
@@ -69,31 +47,6 @@ genQuery (queryName, query) = return $ C.mkTmplFdWithDstAndData tmplFile dstFile
     tmplFile = C.asTmplFile [relfile|src/queries/_query.ts|]
     dstFile = C.serverSrcDirInServerRootDir </> queryFileInSrcDir queryName
     tmplData = operationTmplData operation
-
-genOperationTypesFile ::
-  Path' (Rel C.ServerTemplatesDir) File' ->
-  Path' (Rel ServerRootDir) File' ->
-  [AS.Operation.Operation] ->
-  Bool ->
-  Generator FileDraft
-genOperationTypesFile tmplFile dstFile operations isAuthEnabledGlobally =
-  return $ C.mkTmplFdWithDstAndData tmplFile dstFile (Just tmplData)
-  where
-    tmplData =
-      object
-        [ "operations" .= map operationTypeData operations,
-          "shouldImportAuthenticatedOperation" .= any usesAuth operations,
-          "shouldImportNonAuthenticatedOperation" .= not (all usesAuth operations),
-          "allEntities" .= nub (concatMap getEntities operations)
-        ]
-    operationTypeData operation =
-      object
-        [ "typeName" .= toUpperFirst (getName operation),
-          "entities" .= getEntities operation,
-          "usesAuth" .= usesAuth operation
-        ]
-    getEntities = map makeJsonWithEntityData . maybe [] (map AS.refName) . AS.Operation.getEntities
-    usesAuth = fromMaybe isAuthEnabledGlobally . AS.Operation.getAuth
 
 -- | Analogous to genQuery.
 genAction :: (String, AS.Action.Action) -> Generator FileDraft
