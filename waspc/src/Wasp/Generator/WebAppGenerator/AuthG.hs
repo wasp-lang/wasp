18d60e7e30094c02c89cdba003b616d70cb7b2b4
Martin Sosic <sosic.martin@gmail.com>
2024-04-16 21:08:43 +0200

    Added ability to define custom page loader (app.pageLoader).

--- waspc/src/Wasp/Generator/WebAppGenerator/AuthG.hs
+++ waspc/src/Wasp/Generator/WebAppGenerator/AuthG.hs
@@ -4,7 +4,7 @@ module Wasp.Generator.WebAppGenerator.AuthG
 where
 
 import Data.Aeson (object, (.=))
-import StrongPath (relfile)
+import StrongPath (reldirP, relfile)
 import Wasp.AppSpec (AppSpec)
 import qualified Wasp.AppSpec.App as AS.App
 import qualified Wasp.AppSpec.App.Auth as AS.Auth
@@ -14,24 +14,37 @@ import Wasp.Generator.FileDraft (FileDraft)
 import Wasp.Generator.Monad (Generator)
 import Wasp.Generator.WebAppGenerator.Auth.Common (getOnAuthSucceededRedirectToOrDefault)
 import Wasp.Generator.WebAppGenerator.Common as C
-  ( mkTmplFdWithData,
+  ( mkTmplFd,
+    mkTmplFdWithData,
   )
+import Wasp.Generator.WebAppGenerator.JsImport (extImportToImportJson)
+import Wasp.Util ((<:>))
 
 genAuth :: AppSpec -> Generator [FileDraft]
 genAuth spec =
   case maybeAuth of
     Nothing -> return []
-    Just auth -> (:) <$> genCreateAuthRequiredPage auth <*> genOAuthAuth auth
+    Just auth ->
+      genCreateAuthRequiredPage spec auth
+        <:> pure (C.mkTmplFd [relfile|src/auth/pages/DefaultPageLoader.jsx|])
+        <:> genOAuthAuth auth
   where
     maybeAuth = AS.App.auth $ snd $ getApp spec
 
 -- | Generates HOC that handles auth for the given page.
-genCreateAuthRequiredPage :: AS.Auth.Auth -> Generator FileDraft
-genCreateAuthRequiredPage auth =
+genCreateAuthRequiredPage :: AppSpec -> AS.Auth.Auth -> Generator FileDraft
+genCreateAuthRequiredPage spec auth =
   return $
     C.mkTmplFdWithData
       [relfile|src/auth/pages/createAuthRequiredPage.jsx|]
-      (object ["onAuthFailedRedirectTo" .= AS.Auth.onAuthFailedRedirectTo auth])
+      ( object
+          [ "onAuthFailedRedirectTo" .= AS.Auth.onAuthFailedRedirectTo auth,
+            "pageLoader" .= extImportToImportJson relPathToWebAppSrcDir maybePageLoaderComponent
+          ]
+      )
+  where
+    maybePageLoaderComponent = AS.App.pageLoader $ snd $ getApp spec
+    relPathToWebAppSrcDir = [reldirP|../../|]
 
 genOAuthAuth :: AS.Auth.Auth -> Generator [FileDraft]
 genOAuthAuth auth = sequence [genOAuthCodeExchange auth | AS.Auth.isExternalAuthEnabled auth]
