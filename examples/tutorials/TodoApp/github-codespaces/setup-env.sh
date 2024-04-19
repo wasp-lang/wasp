a9d4a8964a0b9f43dbc5364e72de0c84004b272c
vincanger <70215737+vincanger@users.noreply.github.com>
2024-04-19 14:00:27 +0200

    fix script

--- examples/tutorials/TodoApp/github-codespaces/setup-env.sh
+++ examples/tutorials/TodoApp/github-codespaces/setup-env.sh
@@ -8,10 +8,10 @@ wasp_web_client_url="${base_url}-3000.app.github.dev/"
 react_app_api_url="${base_url}-3001.app.github.dev/"
 
 # Write to .env.server
-echo "WASP_WEB_CLIENT_URL=${wasp_web_client_url}" > .env.server
+echo "WASP_WEB_CLIENT_URL=${wasp_web_client_url}" > ../.env.server
 
 # Write to .env.client
-echo "REACT_APP_API_URL=${react_app_api_url}" > .env.client
+echo "REACT_APP_API_URL=${react_app_api_url}" > ../.env.client
 
 echo "Environment variables set:"
 echo ".env.server: WASP_WEB_CLIENT_URL=${wasp_web_client_url}"
