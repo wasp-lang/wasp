8b1b35b39d83eb983f7c9cb4c9c713fa5af0a952
Martin Sosic <sosic.martin@gmail.com>
2024-04-22 16:20:21 +0200

    Moved DefaultLoader.

--- waspc/data/Generator/templates/react-app/src/auth/pages/createAuthRequiredPage.jsx
+++ waspc/data/Generator/templates/react-app/src/auth/pages/createAuthRequiredPage.jsx
@@ -3,35 +3,8 @@ import React from 'react'
 
 import { Redirect } from 'react-router-dom'
 import { useAuth } from 'wasp/client/auth'
-import { styled, keyframes } from 'wasp/core/stitches.config'
 
-// TODO: I should probably extract Loader into separate file as a component?
-
-const spinKeyframes = keyframes({
-  '0%': { transform: 'rotate(0deg)' },
-  '100%': { transform: 'rotate(360deg)' }
-});
-
-// TODO: Do I need to add !important on all of this stuff because of Tailwind css?
-const Loader = styled('div', {
-  display: 'flex',
-  justifyContent: 'center',
-  alignItems: 'center',
-  height: '100vh',
-  width: '100vw',
-  color: '$gray900',
-
-  // Spinner that is two quarters of a circle across each other.
-  '&::after': {
-    content: '',
-    width: '40px',
-    height: '40px',
-    border: '5px solid currentColor',
-    borderColor: 'currentColor transparent currentColor transparent',
-    borderRadius: '50%',
-    animation: `${spinKeyframes} 1s linear infinite`
-  }
-});
+import { DefaultLoader } from '../../DefaultLoader'
 
 const createAuthRequiredPage = (Page) => {
   return (props) => {
@@ -47,11 +20,10 @@ const createAuthRequiredPage = (Page) => {
           return <Redirect to="{= onAuthFailedRedirectTo =}" />;
         }
       case 'loading':
-        return <Loader/>;
+        return <DefaultLoader/>;
     case 'error':
-      // TODO: Add global error boundary instead and remove this.
-      //   But then I need to throw an error I think.
-      return <span>ERROR {error?.message}</span>
+      // TODO: User ErrorBoundary instead (therefore I will likely want to throw error here).
+      return <span>Error occurred, try reloading the page. {error?.message}</span>
     }
   }
 }
