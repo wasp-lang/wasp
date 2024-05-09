7abb79fd38924ca43e02902059d560ebfd505f46
Mihovil Ilakovac <mihovil@ilakovac.com>
2024-05-09 14:17:39 +0200

    Cleanup

--- waspc/src/Wasp/AppSpec.hs
+++ waspc/src/Wasp/AppSpec.hs
@@ -92,9 +92,6 @@ data AppSpec = AppSpec
 getDecls :: IsDecl a => AppSpec -> [(String, a)]
 getDecls = takeDecls . decls
 
--- NOTE: if you are adding a new declaration type, you need to update the Valid.hs
--- to make sure we check for duplicate names for that declaration type.
-
 getQueries :: AppSpec -> [(String, Query)]
 getQueries = getDecls
 
