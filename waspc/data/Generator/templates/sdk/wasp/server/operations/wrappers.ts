a4abb30e5ef481c5837bb686f9b31e45abc8c98d
Filip Sodić <filip.sodic@fer.hr>
2024-05-22 17:23:36 +0200

    Don't unpack user in server-side operation wrapper

--- waspc/data/Generator/templates/sdk/wasp/server/operations/wrappers.ts
+++ waspc/data/Generator/templates/sdk/wasp/server/operations/wrappers.ts
@@ -93,76 +93,23 @@ export function createAuthenticatedOperation<
   entities: EntityMapFor<OperationDefinition>
 ): AuthenticatedOperationFor<OperationDefinition> {
   async function operation(...args: AuthenticatedOperationArgsFor<OperationDefinition>) {
-      /*
-    An authenticated operation expects either a single argument or two arguments,
-    depending on whether it was defined to accept a payload.
-    The possible mutually-exclusive options for its type signatures are
-    therefore:
-     - action(payload, { user })
-     - action({ user })
-
-    Since neither the Generator nor the runtime know the function's true type
-    signature (i.e., its true number of arguments), the runtime must "guess"
-    which of the two possible signatures is right (i.e., whether
-    the operation expects a payload or not) and act accordingly.
-    
-    When the user's using TypeScript, TypeScript takes care of enforcing that the
-    user sends the correct number of arguments to this function, while the
-    function's runtime decides on the correct implementation based on the number
-    of arguments it received. As long as the user's using TypeScript,
-    everything's OK 
-
-    What happens when the user isn't using TypeScript or decides to bypass it?
-    In this case, there are possible discrepancies between the function's true
-    type and the type runtime decides on.
-
-    Let's see what happens in all scenarios:
-
-      - If the operation's meant to accept a payload, it should be called as:
-
-        action(payload, { user })
-
-      This is also the type advertised by TypeScript.
-      Here's what happens when:
-        - User calls it without arguments, `()` - All good, an appropriate error is
-        thrown.
-        - User calls it with a single argument, `action(arg1)` - Not good, the
-        first arg is interpreted as the user, the action is called with
-        `(undefined, { user: arg1 })`, while the correct call would be `(arg1, {
-        user: undefined })`
-        - User calls it with two arguments, `action(arg1, arg2)` - All good, `arg1`
-        is treated as the payload, `arg2` is treated as the context.
-
-      - If the operation isn't meant to accept a payload, it should be called as
-
-        action({ user })
-
-      This is also the type advertised by TypeScript.
-      Here's what happens when:
-        - User calls it with zero arguments, `action()` -> All good, an error is
-        thrown.
-        - User calls it with a single argument, `(arg1)` - All good, `arg1` is
-        treated as the context
-        - User calls it with two arguments, `action(arg1, arg2)` - Not good, `arg1`
-        is treated as the payload, `arg2` is treated as the context.  The action
-        is called with (arg1, { user: arg2 }), while the correct call would be
-        `(undefined, { user: arg1 })`
-    */
+    // To understand this function and its limitations, read
+    // https://github.com/wasp-lang/wasp/issues/2050
     if (args.length < 1) {
       // No arguments sent -> no user and no payload specified -> there's no way this was called correctly.
       throw new Error('Invalid number of arguments')
     } else if (includesPayload(args)) {
       // Two arguments sent -> the first argument is the payload, the second is the context.
-      const [payload, { user }] = args
+      const [payload, context] = args
       return userOperation(payload, {
-        user,
+        ...context,
         entities,
       })
     } else {
       // One argument sent -> the first and only argument is the user.
-      const [{ user }] = args
+      const [context] = args
       return userOperation(undefined as Parameters<OperationDefinition>[0], {
-        user,
+        ...context,
         entities,
       })
     }
