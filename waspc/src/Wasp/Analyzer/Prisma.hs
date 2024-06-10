5402a7e901cc446ddaae662c9d20e30cce9b4122
Mihovil Ilakovac <mihovil@ilakovac.com>
2024-06-10 17:52:17 +0200

    Update Analyzer.Prisma

--- waspc/src/Wasp/Analyzer/Prisma.hs
+++ waspc/src/Wasp/Analyzer/Prisma.hs
@@ -1,4 +1,7 @@
-module Wasp.Analyzer.Prisma where
+module Wasp.Analyzer.Prisma
+  ( injectEntitiesFromPrismaSchema,
+  )
+where
 
 import Wasp.Analyzer.Parser as Parser
 import qualified Wasp.Psl.Ast.Schema as Psl.Ast
@@ -7,22 +10,27 @@ import qualified Wasp.Psl.Generator.Schema as Psl.Generator
 injectEntitiesFromPrismaSchema :: Psl.Ast.Schema -> Parser.AST -> Either a Parser.AST
 injectEntitiesFromPrismaSchema schema ast = Right $ ast {Parser.astStmts = stmts ++ entityStmts}
   where
-    entityStmts = makeEntityStmt <$> generatePrismaModels schema
+    entityStmts = makeEntityStmt <$> generatePrismaModelSources schema
     stmts = Parser.astStmts ast
 
-makeEntityStmt :: (String, String) -> WithCtx Parser.Stmt
+type ModelName = String
+
+type ModelBody = String
+
+makeEntityStmt :: (ModelName, ModelBody) -> WithCtx Parser.Stmt
 makeEntityStmt (name, body) = wrapWithCtx $ Parser.Decl "entity" name $ wrapWithCtx $ Parser.Quoter "psl" body
   where
     wrapWithCtx = WithCtx (Ctx mockSourceRegion)
     -- Since we didn't parse the entities from the Wasp source file
-    -- we don't have a real source region.
+    -- but the Prisma schema file, we don't have a real source region.
+    -- We mock the source region to be from 0 0 to 0 0.
     -- TODO: In the future, it would be nice to have the source region
     -- of the entity from the Prisma schema file.
     mockSourceRegion = SourceRegion (SourcePosition 0 0) (SourcePosition 0 0)
 
 -- | Generates Prisma models source code so that it can be injected into Wasp AST.
-generatePrismaModels :: Psl.Ast.Schema -> [(String, String)]
-generatePrismaModels schema =
+generatePrismaModelSources :: Psl.Ast.Schema -> [(ModelName, ModelBody)]
+generatePrismaModelSources schema =
   [ ( name,
       Psl.Generator.generateModelBody body
     )
