491e22e96867e102ebddb86b21d2376b2ed9b563
Mihovil Ilakovac <mihovil@ilakovac.com>
2024-06-11 14:57:48 +0200

    Update naming

--- waspc/waspls/src/Wasp/LSP/Prisma/Util.hs
+++ waspc/waspls/src/Wasp/LSP/Prisma/Util.hs
@@ -1,10 +1,13 @@
-module Wasp.LSP.Prisma.Util where
+module Wasp.LSP.Prisma.Util
+  ( showModelNames,
+  )
+where
 
 import qualified Wasp.Psl.Ast.Model as Psl.Model
 import qualified Wasp.Psl.Ast.Schema as Psl.Schema
 
-showModels :: Psl.Schema.Schema -> String
-showModels = unwords . getModelNames
+showModelNames :: Psl.Schema.Schema -> String
+showModelNames = unwords . getModelNames
 
 getModelNames :: Psl.Schema.Schema -> [String]
 getModelNames = fmap (\(Psl.Model.Model name _) -> name) . Psl.Schema.getModels
