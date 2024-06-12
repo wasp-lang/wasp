41eb0474302de5e72dfd1b5c5dc299e2b0e62188
Filip Sodić <filip.sodic@fer.hr>
2024-06-12 19:16:37 +0200

    Add explanation for generic types

--- waspc/examples/todoApp/src/testTypes/operations/helpers.ts
+++ waspc/examples/todoApp/src/testTypes/operations/helpers.ts
@@ -1,3 +1,5 @@
+// All generics in this file are taken from
+// https://github.com/type-challenges/type-challenges/blob/9080836b5923f1716815e643342195d23b6a1617/utils/index.d.ts#L7
 export type Expect<T extends true> = T
 
 export type Equal<X, Y> = (<T>() => T extends X ? 1 : 2) extends <
