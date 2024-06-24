20b87d9a953ddc9496fb2ee932011b8e24b48dc2
wardbox <dylank@startmail.com>
2024-06-24 09:30:38 -0700

    E2E test diff

--- waspc/e2e-test/test-outputs/waspComplexTest-golden/waspComplexTest/.wasp/out/sdk/wasp/dist/auth/forms/internal/social/SocialIcons.d.ts
+++ waspc/e2e-test/test-outputs/waspComplexTest-golden/waspComplexTest/.wasp/out/sdk/wasp/dist/auth/forms/internal/social/SocialIcons.d.ts
@@ -1,5 +1,4 @@
-/// <reference types="react" />
-export declare const Discord: () => import("react").JSX.Element;
 export declare const Google: () => import("react").JSX.Element;
 export declare const Keycloak: () => import("react").JSX.Element;
 export declare const GitHub: () => import("react").JSX.Element;
+export declare const Discord: () => import("react").JSX.Element;
