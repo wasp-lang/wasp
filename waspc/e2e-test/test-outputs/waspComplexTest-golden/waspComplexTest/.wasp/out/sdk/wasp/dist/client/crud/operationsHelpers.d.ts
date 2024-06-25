6e36ae32e727fd46667aec4c28fe30f3264ffc7f
Mihovil Ilakovac <mihovil@ilakovac.com>
2024-06-25 13:13:51 +0200

    e2e tests
    Signed-off-by: Mihovil Ilakovac <mihovil@ilakovac.com>

--- waspc/e2e-test/test-outputs/waspComplexTest-golden/waspComplexTest/.wasp/out/sdk/wasp/dist/client/crud/operationsHelpers.d.ts
+++ waspc/e2e-test/test-outputs/waspComplexTest-golden/waspComplexTest/.wasp/out/sdk/wasp/dist/client/crud/operationsHelpers.d.ts
@@ -2,4 +2,4 @@ import { useAction, useQuery } from "../operations";
 export declare function makeUseQueryFor<Input, Output>(query: Parameters<typeof useQuery<Input, Output>>[0]): (queryFnArgs?: Input, options?: any) => import("@tanstack/react-query").UseQueryResult<Output, Error>;
 export declare function makeUseActionFor<Input = unknown, Output = unknown>(action: Parameters<typeof useAction<Input, Output>>[0]): (actionOptions?: {
     optimisticUpdates: import("../operations").OptimisticUpdateDefinition<Input, any>[];
-}) => [Input] extends [never] ? (args?: unknown) => Promise<Output> : (args: Input) => Promise<Output>;
+}) => [Input] extends [never] ? (args?: unknown) => Promise<Output> : [Input] extends [void] ? () => Promise<Output> : (args: Input) => Promise<Output>;
