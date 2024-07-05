134fc1aa9aaf160b6932e5cdb6865e4f8331109a
Mihovil Ilakovac <mihovil@ilakovac.com>
2024-07-05 22:35:17 +0200

    Removes realworld app

--- examples/realworld/src/client/utils.jsx
+++
@@ -1,8 +0,0 @@
-export const errorMessage = (e) => {
-  return (
-    <span>
-      Error: {e.message}
-      {e.data?.message && <span style={{display: 'block'}}>Details: {e.data.message}</span>}
-    </span>
-  );
-};
