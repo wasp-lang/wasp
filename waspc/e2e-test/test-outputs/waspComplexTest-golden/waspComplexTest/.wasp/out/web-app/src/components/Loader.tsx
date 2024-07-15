4053ea312921321db93894fed2760f4cebfdbeb3
Mihovil Ilakovac <mihovil@ilakovac.com>
2024-07-15 16:37:26 +0200

    e2e tests
    Signed-off-by: Mihovil Ilakovac <mihovil@ilakovac.com>

--- waspc/e2e-test/test-outputs/waspComplexTest-golden/waspComplexTest/.wasp/out/web-app/src/components/Loader.tsx
+++ waspc/e2e-test/test-outputs/waspComplexTest-golden/waspComplexTest/.wasp/out/web-app/src/components/Loader.tsx
@@ -2,7 +2,7 @@ import styles from './Loader.module.css'
 
 // Spans a accross the whole screen and displays a spinner in the very middle.
 export function Loader() {
-  const loaderClassName = ['loader', styles.loader].join(' ')
+  const loaderClassName = ['wasp-loader', styles.loader].join(' ')
   return (
     <div className={loaderClassName}>
       <div className={styles.loaderCircle} />
