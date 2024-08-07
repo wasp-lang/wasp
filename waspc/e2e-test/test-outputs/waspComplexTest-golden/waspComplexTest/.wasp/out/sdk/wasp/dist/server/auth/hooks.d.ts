3c091896c5edd1283309b40aea290bb45344df2f
Mihovil Ilakovac <mihovil@ilakovac.com>
2024-08-07 16:44:07 +0200

    e2e tests

--- waspc/e2e-test/test-outputs/waspComplexTest-golden/waspComplexTest/.wasp/out/sdk/wasp/dist/server/auth/hooks.d.ts
+++ waspc/e2e-test/test-outputs/waspComplexTest-golden/waspComplexTest/.wasp/out/sdk/wasp/dist/server/auth/hooks.d.ts
@@ -17,80 +17,88 @@ export type OnAfterLoginHook = (params: Expand<OnAfterLoginHookParams>) => void 
 export type InternalAuthHookParams = {
     /**
      * Prisma instance that can be used to interact with the database.
-     */
+    */
     prisma: typeof prisma;
 };
 type OnBeforeSignupHookParams = {
     /**
      * Provider ID object that contains the provider name and the provide user ID.
-     */
+    */
     providerId: ProviderId;
     /**
      * Request object that can be used to access the incoming request.
-     */
+    */
     req: ExpressRequest;
 } & InternalAuthHookParams;
 type OnAfterSignupHookParams = {
     /**
      * Provider ID object that contains the provider name and the provide user ID.
-     */
+    */
     providerId: ProviderId;
     /**
      * User object that was created during the signup process.
-     */
+    */
     user: Awaited<ReturnType<typeof createUser>>;
+    /**
+     * OAuth flow data that was generated during the OAuth flow. This is only
+     * available if the user signed up using OAuth.
+    */
     oauth?: OAuthParams;
     /**
      * Request object that can be used to access the incoming request.
-     */
+    */
     req: ExpressRequest;
 } & InternalAuthHookParams;
 type OnBeforeOAuthRedirectHookParams = {
     /**
      * URL that the OAuth flow should redirect to.
-     */
+    */
     url: URL;
     /**
      * Unique request ID that was generated during the OAuth flow.
-     */
+    */
     uniqueRequestId: OAuthParams['uniqueRequestId'];
     /**
      * Request object that can be used to access the incoming request.
-     */
+    */
     req: ExpressRequest;
 } & InternalAuthHookParams;
 type OnBeforeLoginHookParams = {
     /**
      * Provider ID object that contains the provider name and the provide user ID.
-     */
+    */
     providerId: ProviderId;
     /**
      * Request object that can be used to access the incoming request.
-     */
+    */
     req: ExpressRequest;
 } & InternalAuthHookParams;
 type OnAfterLoginHookParams = {
     /**
      * Provider ID object that contains the provider name and the provide user ID.
-     */
+    */
     providerId: ProviderId;
     /**
      * User that is logged in.
-     */
+    */
     user: Awaited<ReturnType<typeof findAuthWithUserBy>>['user'];
+    /**
+     * OAuth flow data that was generated during the OAuth flow. This is only
+     * available if the user logged in using OAuth.
+    */
+    oauth?: OAuthParams;
     /**
      * Request object that can be used to access the incoming request.
     */
     req: ExpressRequest;
-    oauth?: OAuthParams;
 } & InternalAuthHookParams;
 export type OAuthParams = {
     /**
      * Unique request ID that was generated during the OAuth flow.
-     */
+    */
     uniqueRequestId: string;
 } & ({
-    provider: 'google';
+    providerName: 'google';
     tokens: import('arctic').GoogleTokens;
 } | never);
 export {};
