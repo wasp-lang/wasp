9c138d7fede98bd061034ef0832b4578747223df
case-E <kc@schooltendance.com>
2024-08-12 20:00:57 +0530

    added an env variable after rebasing from main

--- waspc/packages/deploy/src/providers/fly/deploy/deploy.ts
+++ waspc/packages/deploy/src/providers/fly/deploy/deploy.ts
@@ -103,8 +103,11 @@ async function deployClient(deploymentInfo: DeploymentInfo<DeployOptions>, { bui
 	copyProjectClientTomlLocally(deploymentInfo.tomlFilePaths);
 
 	waspSays('Building web client for production...');
+	waspSays('If you configured a custom domain for the server, please run the command with an env variable: REACT_APP_API_URL=https://serverUrl.com wasp deploy fly deploy');
+
+	const serverUrl = process.env.REACT_APP_API_URL || deploymentInfo.serverUrl;
 	await $`npm install`;
-	await $`REACT_APP_API_URL=${deploymentInfo.serverUrl} npm run build`;
+	await $`REACT_APP_API_URL=${serverUrl} npm run build`;
 
 	// Creates the necessary Dockerfile for deploying static websites to Fly.io.
 	// Adds dummy .dockerignore to supress CLI question.
