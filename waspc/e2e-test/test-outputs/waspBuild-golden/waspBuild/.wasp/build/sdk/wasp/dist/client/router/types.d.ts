da359a2682cfe06446a6e0fa61e98bad7c04ba01
Mihovil Ilakovac <mihovil@ilakovac.com>
2024-09-06 15:03:17 +0200

    e2e tests
    Signed-off-by: Mihovil Ilakovac <mihovil@ilakovac.com>

--- waspc/e2e-test/test-outputs/waspBuild-golden/waspBuild/.wasp/build/sdk/wasp/dist/client/router/types.d.ts
+++ waspc/e2e-test/test-outputs/waspBuild-golden/waspBuild/.wasp/build/sdk/wasp/dist/client/router/types.d.ts
@@ -10,7 +10,7 @@ export type Params = {
 export type Search = string[][] | Record<string, string> | string | URLSearchParams;
 type RouteDefinitionsToRoutesObj<Routes extends RoutesDefinition> = {
     [K in keyof Routes]: {
-        to: Routes[K]['to'];
+        to: GenerateRoute<Routes[K]['to']>;
     } & ParamsFromBuildFn<Routes[K]['build']>;
 };
 type RoutesDefinition = {
@@ -27,4 +27,23 @@ type ParamsFromBuildFn<BF extends BuildFn> = Parameters<BF>[0] extends {
 } : {
     params?: never;
 };
+type GenerateRoute<S extends string> = S extends '/' ? '/' : `/${JoinPath<JoinSegments<ExplodeOptionalSegments<SplitPath<S>>>>}`;
+type ExplodeOptionalSegments<T> = T extends [infer Head, ...infer Tail] ? Head extends '' ? [...ExplodeOptionalSegments<Tail>] : [_ExplodeOptionalSegment<Head>, ...ExplodeOptionalSegments<Tail>] : T;
+type _ExplodeOptionalSegment<T> = T extends `:${infer P}` ? {
+    segment: T;
+} : T extends `${infer S}?` ? {
+    optionalSegment: S;
+} : {
+    segment: T;
+};
+type Segment = {
+    segment: string;
+};
+type OptionalSegment = {
+    optionalSegment: string;
+};
+type Elem = Segment | OptionalSegment;
+type JoinSegments<T extends Elem[]> = T extends [] ? [] : T extends [infer First extends Elem, ...infer Rest extends Elem[]] ? First extends Segment ? [First['segment'], ...JoinSegments<Rest>] : First extends OptionalSegment ? [First['optionalSegment'], ...JoinSegments<Rest>] | JoinSegments<Rest> : [] : [];
+type SplitPath<S extends string> = S extends `${infer T}/${infer U}` ? [T, ...SplitPath<U>] : [S];
+type JoinPath<T extends string[]> = T extends [infer Only extends string] ? Only : T extends [infer First extends string, ...infer Rest extends string[]] ? `${First}/${JoinPath<Rest>}` : never;
 export {};
--- waspc/e2e-test/test-outputs/waspCompile-golden/waspCompile/.wasp/out/sdk/wasp/client/router/types.ts
+++ waspc/e2e-test/test-outputs/waspCompile-golden/waspCompile/.wasp/out/sdk/wasp/client/router/types.ts
@@ -21,7 +21,7 @@ export type Search =
 
 type RouteDefinitionsToRoutesObj<Routes extends RoutesDefinition> = {
   [K in keyof Routes]: {
-    to: Routes[K]['to']
+    to: GenerateRoute<Routes[K]['to']>
   } & ParamsFromBuildFn<Routes[K]['build']>
 }
 
@@ -39,3 +39,49 @@ type ParamsFromBuildFn<BF extends BuildFn> = Parameters<BF>[0] extends {
 }
   ? { params: Params }
   : { params?: never }
+
+// Optional static segments handling
+type GenerateRoute<S extends string> = S extends '/'
+  ? '/'
+  : `/${JoinPath<JoinSegments<ExplodeOptionalSegments<SplitPath<S>>>>}`
+
+type ExplodeOptionalSegments<T> = T extends [infer Head, ...infer Tail]
+  ? Head extends ''
+    ? [...ExplodeOptionalSegments<Tail>]
+    : [_ExplodeOptionalSegment<Head>, ...ExplodeOptionalSegments<Tail>]
+  : T
+
+type _ExplodeOptionalSegment<T> = T extends `:${infer P}`
+  // Param segment
+  ? { segment: T }
+  : T extends `${infer S}?`
+  // Optional segment
+  ? { optionalSegment: S }
+  // Regular segment
+  : { segment: T }
+
+type Segment = { segment: string }
+type OptionalSegment = { optionalSegment: string }
+
+type Elem = Segment | OptionalSegment
+
+type JoinSegments<T extends Elem[]> = T extends []
+  ? []
+  : T extends [infer First extends Elem, ...infer Rest extends Elem[]]
+  ? First extends Segment
+    ? [First['segment'], ...JoinSegments<Rest>]
+    : First extends OptionalSegment
+    ? [First['optionalSegment'], ...JoinSegments<Rest>] | JoinSegments<Rest>
+    : []
+  : []
+
+type SplitPath<S extends string> = S extends `${infer T}/${infer U}`
+  ? [T, ...SplitPath<U>]
+  : [S]
+
+type JoinPath<T extends string[]> = T extends [infer Only extends string]
+  ? Only
+  : T extends [infer First extends string, ...infer Rest extends string[]]
+  ? `${First}/${JoinPath<Rest>}`
+  : never
+  
