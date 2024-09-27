b755293f7b376f42c9f589420e75cd1f1f0a6f2f
Mihovil Ilakovac <mihovil@ilakovac.com>
2024-09-27 19:58:32 +0200

    Userland files validation (#2297)

---
+++ waspc/src/Wasp/ExternalConfig/TsConfig.hs
@@ -0,0 +1,45 @@
+{-# LANGUAGE DeriveAnyClass #-}
+{-# LANGUAGE DeriveGeneric #-}
+{-# LANGUAGE FlexibleInstances #-}
+
+module Wasp.ExternalConfig.TsConfig
+  ( TsConfig (..),
+    CompilerOptions (..),
+  )
+where
+
+import Data.Aeson
+  ( FromJSON,
+    genericParseJSON,
+    parseJSON,
+  )
+import qualified Data.Aeson as Aeson
+import GHC.Generics (Generic)
+
+data TsConfig = TsConfig
+  { compilerOptions :: !CompilerOptions
+  }
+  deriving (Show, Generic, FromJSON)
+
+data CompilerOptions = CompilerOptions
+  { _module :: !(Maybe String),
+    target :: !(Maybe String),
+    moduleResolution :: !(Maybe String),
+    jsx :: !(Maybe String),
+    strict :: !(Maybe Bool),
+    esModuleInterop :: !(Maybe Bool),
+    lib :: !(Maybe [String]),
+    allowJs :: !(Maybe Bool),
+    typeRoots :: !(Maybe [String]),
+    outDir :: !(Maybe String)
+  }
+  deriving (Show, Generic)
+
+instance FromJSON CompilerOptions where
+  parseJSON =
+    genericParseJSON $
+      Aeson.defaultOptions {Aeson.fieldLabelModifier = modifyFieldLabel}
+    where
+      -- "module" is a reserved keyword in Haskell, so we use "_module" instead.
+      modifyFieldLabel "_module" = "module"
+      modifyFieldLabel other = other
---
+++ waspc/src/Wasp/Util/Json.hs
@@ -0,0 +1,18 @@
+module Wasp.Util.Json (parseJsonWithComments) where
+
+import Data.Aeson (FromJSON)
+import System.Exit (ExitCode (..))
+import qualified System.Process as P
+import Wasp.Util.Aeson (decodeFromString)
+
+-- | Uses Node.js to parse JSON with comments by treating it as a JavaScript object.
+-- We use this technique because Aeson can't read JSON with comments and we didn't want to write
+-- a custom parser.
+parseJsonWithComments :: FromJSON a => String -> IO (Either String a)
+parseJsonWithComments jsonStr = do
+  let evalScript = "const v = " ++ jsonStr ++ ";console.log(JSON.stringify(v));"
+  let cp = P.proc "node" ["-e", evalScript]
+  (exitCode, response, stderr) <- P.readCreateProcessWithExitCode cp ""
+  case exitCode of
+    ExitSuccess -> return $ decodeFromString response
+    _exitFailure -> return $ Left stderr
