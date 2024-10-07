1699df25fc991e7ae95f9258c2d66e3ce2f7ba6c
Filip Sodić <filip.sodic@fer.hr>
2024-10-07 22:21:57 +0200

    Revert Show instance for Decl

--- waspc/src/Wasp/AppSpec/Core/Decl.hs
+++ waspc/src/Wasp/AppSpec/Core/Decl.hs
@@ -1,6 +1,5 @@
 {-# LANGUAGE GADTs #-}
 {-# LANGUAGE TupleSections #-}
-{-# LANGUAGE TypeApplications #-}
 
 module Wasp.AppSpec.Core.Decl
   ( Decl,
@@ -10,40 +9,15 @@ module Wasp.AppSpec.Core.Decl
   )
 where
 
-import Control.Applicative ((<|>))
 import Data.Maybe (mapMaybe)
 import Data.Typeable (cast)
-import Wasp.AppSpec.Action (Action)
-import Wasp.AppSpec.Api (Api)
-import Wasp.AppSpec.ApiNamespace (ApiNamespace)
-import Wasp.AppSpec.App (App)
 import Wasp.AppSpec.Core.IsDecl (IsDecl)
-import Wasp.AppSpec.Crud (Crud)
-import Wasp.AppSpec.Entity (Entity)
-import Wasp.AppSpec.Job (Job)
-import Wasp.AppSpec.Page (Page)
-import Wasp.AppSpec.Query (Query)
-import Wasp.AppSpec.Route (Route)
 
 -- | A container for any (IsDecl a) type, allowing you to have a heterogenous list of
 --   Wasp declarations as [Decl].
 --   Declarations make the top level of AppSpec.
 data Decl where
   Decl :: (IsDecl a) => String -> a -> Decl
-
-instance Show Decl where
-  show decl =
-    show $
-      (show <$> fromDecl @Api decl)
-        <|> (show <$> fromDecl @Route decl)
-        <|> (show <$> fromDecl @Crud decl)
-        <|> (show <$> fromDecl @App decl)
-        <|> (show <$> fromDecl @Action decl)
-        <|> (show <$> fromDecl @Job decl)
-        <|> (show <$> fromDecl @Entity decl)
-        <|> (show <$> fromDecl @Page decl)
-        <|> (show <$> fromDecl @ApiNamespace decl)
-        <|> (show <$> fromDecl @Query decl)
 
 -- | Extracts all declarations of a certain type from a @[Decl]@s
 takeDecls :: (IsDecl a) => [Decl] -> [(String, a)]
