734705899af9f983f447d13f5217dc3fa2ee99b5
Mihovil Ilakovac <mihovil@ilakovac.com>
2024-11-22 11:10:55 +0100

    Explicitly encode job data

--- waspc/data/Generator/templates/sdk/wasp/server/jobs/_job.ts
+++ waspc/data/Generator/templates/sdk/wasp/server/jobs/_job.ts
@@ -12,10 +12,28 @@ const entities = {
 // PUBLIC API
 export type {= typeName =}<Input extends JSONObject, Output extends JSONValue | void> = JobFn<Input, Output, typeof entities>
 
+{=# jobSchedule.isDefined =}
+const jobSchedule = {
+  cron: "{= jobSchedule.cron =}",
+  {=# jobSchedule.args.isDefined =}
+  args: {=& jobSchedule.args.json =},
+  {=/ jobSchedule.args.isDefined =}
+  {=# jobSchedule.options.isDefined =}
+  options: {=& jobSchedule.options.json =},
+  {=/ jobSchedule.options.isDefined =}
+  {=^ jobSchedule.options.isDefined =}
+  options: {},
+  {=/ jobSchedule.options.isDefined =}
+}
+{=/ jobSchedule.isDefined =}
+{=^ jobSchedule.isDefined =}
+const jobSchedule = null
+{=/ jobSchedule.isDefined =}
+
 // PUBLIC API
 export const {= jobName =} = createJobDefinition({
   jobName: '{= jobName =}',
   defaultJobOptions: {=& jobPerformOptions =},
-  jobSchedule: {=& jobSchedule =},
+  jobSchedule,
   entities,
 })
