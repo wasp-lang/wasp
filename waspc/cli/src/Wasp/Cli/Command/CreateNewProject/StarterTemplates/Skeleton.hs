98a4d3d7d6e06c4a6200d18fce8bdd518bc6a02d
Martin Sosic <sosic.martin@gmail.com>
2024-12-12 10:11:52 +0100

    fix

--- waspc/cli/src/Wasp/Cli/Command/CreateNewProject/StarterTemplates/Skeleton.hs
+++ waspc/cli/src/Wasp/Cli/Command/CreateNewProject/StarterTemplates/Skeleton.hs
@@ -5,6 +5,7 @@ module Wasp.Cli.Command.CreateNewProject.StarterTemplates.Skeleton
   )
 where
 
+import Data.Functor ((<&>))
 import Data.Text (Text)
 import StrongPath (File', Path, Rel, System, reldir, (</>))
 import qualified Wasp.Data as Data
@@ -13,6 +14,6 @@ import Wasp.Util.IO (listDirectoryDeep, readFileStrict)
 
 readWaspProjectSkeletonFiles :: IO [(Path System (Rel WaspProjectDir) File', Text)]
 readWaspProjectSkeletonFiles = do
-  skeletonFilesDir <- (</> [reldir|Cli/templates/skeleton|]) <$> Data.getAbsDataDirPath
+  skeletonFilesDir <- Data.getAbsDataDirPath <&> (</> [reldir|Cli/templates/skeleton|])
   skeletonFilePaths <- listDirectoryDeep skeletonFilesDir
   mapM (\path -> (path,) <$> readFileStrict (skeletonFilesDir </> path)) skeletonFilePaths
