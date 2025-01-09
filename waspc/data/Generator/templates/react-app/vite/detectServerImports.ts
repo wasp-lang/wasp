1d038d82ad16f1f5839309a09af384556d471cc1
Mihovil Ilakovac <mihovil@ilakovac.com>
2025-01-09 09:50:39 +0100

    Use generator. Cleanup.

--- waspc/data/Generator/templates/react-app/vite/detectServerImports.ts
+++ waspc/data/Generator/templates/react-app/vite/detectServerImports.ts
@@ -3,26 +3,52 @@ import { type Plugin } from "vite";
 export function detectServerImports(): Plugin {
   return {
     name: 'wasp-detect-server-imports',
-    transform(code, id) {
-      const isInDotWaspFolder = id.includes("/.wasp/");
+    transform(code, filePath) {
+      const isInDotWaspFolder = filePath.includes("/.wasp/");
 
+      // We don't want to check for server imports in the Wasp 
+      // framework code.
       if (isInDotWaspFolder) {
-        // Skip checking files in .wasp folder.
         return;
       }
 
-      const regex = /\s*import\s+(?:(?:[\w*\s{},]*)\s+from\s+)?(['"`])([^'"`]+)\1\s*/g;
+      const imports = getImportsFromCode(code);
 
-      for (const match of code.matchAll(regex)) {
-        const importPath = match[2];
-        const isServerImport = importPath.startsWith("wasp/server");
-        if (isServerImport) {
-          const fileName = id.split('/').pop();
-          throw new Error(
-            `${fileName} contains a server import from ${importPath}. This is not allowed in the client code.`
-          );
+      for (const imp of imports) {
+        if (imp.moduleName.startsWith("wasp/server")) {
+          throw new Error(getServerImportErrorMessage(imp, filePath));
         }
       }
     },
   };
+}
+
+type Import = {
+  importStatement: string;
+  moduleName: string;
+}
+
+const importStatementRegex = /\s*import\s+(?:(?:[\w*\s{},]*)\s+from\s+)?(['"`])([^'"`]+)\1\s*/g;
+
+function* getImportsFromCode(code: string): Generator<Import> {
+  const matches = code.matchAll(importStatementRegex);
+  for (const match of matches) {
+    yield {
+      importStatement: match[0].trim(),
+      moduleName: match[2],
+    };
+  }
+}
+
+function getServerImportErrorMessage(imp: Import, filePath: string): string {
+  return  `Client module ${getRelativeFilePath(filePath)} imports server specific module ${imp.moduleName}:
+
+${imp.importStatement}
+
+This is not allowed in the client code.`
+}
+
+// TODO: implement this with Wasp project dir passed to template.
+function getRelativeFilePath(filePath: string): string {
+  return filePath;
 }
