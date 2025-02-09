f290d14c1f65db8ad0e9fbf5583d2ea70431e058
Mihovil Ilakovac <mihovil@ilakovac.com>
2025-02-09 17:00:14 +0100

    Use spaces instead of tabs. Bump dep versions.
    Signed-off-by: Mihovil Ilakovac <mihovil@ilakovac.com>

--- waspc/packages/deploy/src/providers/fly/launch/launch.ts
+++ waspc/packages/deploy/src/providers/fly/launch/launch.ts
@@ -9,33 +9,47 @@ import { clientTomlExistsInProject, getTomlFilePaths, serverTomlExistsInProject 
 import { createFlyDbCommand, flyDeployCommand, flySetupCommand } from '../index.js';
 
 export async function launch(basename: string, region: string, options: LaunchOptions): Promise<void> {
-	waspSays('Launching your Wasp app to Fly.io!');
+  waspSays('Launching your Wasp app to Fly.io!');
 
-	const tomlFilePaths = getTomlFilePaths(options);
-	if (serverTomlExistsInProject(tomlFilePaths) || clientTomlExistsInProject(tomlFilePaths)) {
-		waspSays('You already have Fly toml files. The launch command is intended to be run one time on a new Fly project. Please try a different command.');
-		exit(1);
-	}
+  const tomlFilePaths = getTomlFilePaths(options);
+  if (serverTomlExistsInProject(tomlFilePaths) || clientTomlExistsInProject(tomlFilePaths)) {
+    waspSays(
+      'You already have Fly toml files. The launch command is intended to be run one time on a new Fly project. Please try a different command.'
+    );
+    exit(1);
+  }
 
-	try {
-		await setup(basename, region, options);
-	} catch (e) {
-		waspSays(`There was an error running "${getCommandHelp(flySetupCommand)}". Please review the error and try again (if appropriate).`);
-		exit(1);
-	}
+  try {
+    await setup(basename, region, options);
+  } catch (e) {
+    waspSays(
+      `There was an error running "${getCommandHelp(
+        flySetupCommand
+      )}". Please review the error and try again (if appropriate).`
+    );
+    exit(1);
+  }
 
-	try {
-		await createDb(region, options);
-	} catch (e) {
-		waspSays(`There was an error running "${getCommandHelp(createFlyDbCommand)}". Please review the error and try again (if appropriate).`);
-		exit(1);
-	}
+  try {
+    await createDb(region, options);
+  } catch (e) {
+    waspSays(
+      `There was an error running "${getCommandHelp(
+        createFlyDbCommand
+      )}". Please review the error and try again (if appropriate).`
+    );
+    exit(1);
+  }
 
-	try {
-		const deployOptions: DeployOptions = { ...options, skipBuild: true };
-		await deploy(deployOptions);
-	} catch (e) {
-		waspSays(`There was an error running "${getCommandHelp(flyDeployCommand)}". Please review the error and try again (if appropriate).`);
-		exit(1);
-	}
+  try {
+    const deployOptions: DeployOptions = { ...options, skipBuild: true };
+    await deploy(deployOptions);
+  } catch (e) {
+    waspSays(
+      `There was an error running "${getCommandHelp(
+        flyDeployCommand
+      )}". Please review the error and try again (if appropriate).`
+    );
+    exit(1);
+  }
 }
