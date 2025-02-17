f982380ff4d9860d309391ba7d55481994d0861f
Mihovil Ilakovac <mihovil@ilakovac.com>
2025-02-17 10:56:29 +0100

    Fixes MDX components to have correct links in versioned docs
    Signed-off-by: Mihovil Ilakovac <mihovil@ilakovac.com>

--- web/docs/auth/social-auth/SocialAuthGrid.tsx
+++ web/docs/auth/social-auth/SocialAuthGrid.tsx
@@ -9,22 +9,22 @@ export function SocialAuthGrid({
     {
       title: 'Google',
       description: 'Users sign in with their Google account.',
-      linkToDocs: '/docs/auth/social-auth/google' + pagePart,
+      linkToDocs: './google' + pagePart,
     },
     {
       title: 'Github',
       description: 'Users sign in with their Github account.',
-      linkToDocs: '/docs/auth/social-auth/github' + pagePart,
+      linkToDocs: './github' + pagePart,
     },
     {
       title: 'Keycloak',
       description: 'Users sign in with their Keycloak account.',
-      linkToDocs: '/docs/auth/social-auth/keycloak' + pagePart,
+      linkToDocs: './keycloak' + pagePart,
     },
     {
       title: 'Discord',
       description: 'Users sign in with their Discord account.',
-      linkToDocs: '/docs/auth/social-auth/discord' + pagePart,
+      linkToDocs: './discord' + pagePart,
     },
   ]
   return (
