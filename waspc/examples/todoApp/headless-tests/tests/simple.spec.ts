9977e27be94816c9375c57d95341355142341393
Carlos Precioso <carlos@wasp-lang.dev>
2025-03-25 13:29:25 +0100

    Add resolve alias to Vite to find Prisma's client (#2565)
    Closes #2320

--- waspc/examples/todoApp/headless-tests/tests/simple.spec.ts
+++ waspc/examples/todoApp/headless-tests/tests/simple.spec.ts
@@ -67,5 +67,9 @@ test.describe('signup and login', () => {
     await page.locator(`text=${fullTaskText}`).click()
     const taskPageText = `description: ${randomTask}`
     await page.waitForSelector(`text=${taskPageText}`)
+
+    // Checking if Prisma enums work on the client
+    const taskVisibilityText = `who can see this task: only you`
+    await expect(page.locator('body')).toContainText(taskVisibilityText)
   })
 })
