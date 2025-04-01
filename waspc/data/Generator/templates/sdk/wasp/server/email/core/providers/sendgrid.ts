2703770d4f4d8d766e45e93167640dc59911d582
Carlos Precioso <github@precioso.design>
2025-04-01 12:56:02 +0200

    Surface SendGrid errors from its response

--- waspc/data/Generator/templates/sdk/wasp/server/email/core/providers/sendgrid.ts
+++ waspc/data/Generator/templates/sdk/wasp/server/email/core/providers/sendgrid.ts
@@ -1,6 +1,6 @@
-import SendGrid from "@sendgrid/mail";
-import { getDefaultFromField } from "../helpers.js";
-import type { SendGridProvider, EmailSender } from "../types";
+import SendGrid from "@sendgrid/mail"
+import { getDefaultFromField } from "../helpers.js"
+import type { EmailSender, SendGridProvider } from "../types"
 
 // PRIVATE API
 export function initSendGridEmailSender(
@@ -22,6 +22,22 @@ export function initSendGridEmailSender(
         subject: email.subject,
         text: email.text,
         html: email.html,
+      }).catch((error) => {
+        // SendGrid can return an array of errors in its response,
+        // but the client doesn't surface them appropiately.
+        // If we find it, we will throw an AggregateError with all the errors
+        // so that they are all printed. Otherwise, we will throw the
+        // original error.
+
+        const responseErrors = error?.response?.body?.errors;
+        if (responseErrors && Array.isArray(responseErrors)) {
+          throw new AggregateError(
+            [...responseErrors, error],
+            `SendGrid error: ${error.message}`
+          );
+        } else {
+          throw error;
+        }
       });
     },
   };
