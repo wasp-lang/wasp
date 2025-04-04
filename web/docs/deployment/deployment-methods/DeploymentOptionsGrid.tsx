0bbc9571665ee6c21e6d353d9c68682ee0c9507d
Carlos Precioso <carlos@wasp-lang.dev>
2025-04-04 18:09:22 +0200

    Remake custom Auth UI docs (#2577)

--- web/docs/deployment/deployment-methods/DeploymentOptionsGrid.tsx
+++ web/docs/deployment/deployment-methods/DeploymentOptionsGrid.tsx
@@ -1,56 +1,29 @@
+import { LinkGrid } from '@site/src/components/LinkGrid'
 import React from 'react'
-import Link from '@docusaurus/Link'
-import './DeploymentOptionsGrid.css'
+
+const deploymentMethods = [
+  {
+    title: 'Wasp CLI',
+    description: 'One command deployment & redeployment',
+    linkTo: './cli',
+  },
+  {
+    title: 'Platform as a Service (PaaS)',
+    description: 'Deploy your app manually to the cloud',
+    linkTo: './paas',
+  },
+  {
+    title: 'Self-hosting',
+    description: 'Use your own servers to host your app',
+    linkTo: './self-hosted',
+  },
+]
 
 export function DeploymentOptionsGrid() {
-  const deploymentMethods = [
-    {
-      title: 'Wasp CLI',
-      description: 'One command deployment & redeployment',
-      linkToDocs: './cli',
-    },
-    {
-      title: 'Platform as a Service (PaaS)',
-      description: 'Deploy your app manually to the cloud',
-      linkToDocs: './paas',
-    },
-    {
-      title: 'Self-hosting',
-      description: 'Use your own servers to host your app',
-      linkToDocs: './self-hosted',
-    },
-  ]
   return (
-    <>
-      <div className="deployment-methods-grid">
-        {deploymentMethods.map((deploymentMethod) => (
-          <DeploymentOptionBox
-            title={deploymentMethod.title}
-            description={deploymentMethod.description}
-            linkToDocs={deploymentMethod.linkToDocs}
-          />
-        ))}
-      </div>
-      <p className="deployment-methods-info">
-        <small>Click on each deployment method for more details.</small>
-      </p>
-    </>
-  )
-}
-
-function DeploymentOptionBox({
-  linkToDocs,
-  title,
-  description,
-}: {
-  linkToDocs: string
-  title: string
-  description: string
-}) {
-  return (
-    <Link to={linkToDocs} className="deployment-method-box">
-      <h3>{title} »</h3>
-      <p>{description}</p>
-    </Link>
+    <LinkGrid
+      links={deploymentMethods}
+      caption="Click on each deployment method for more details."
+    />
   )
 }
