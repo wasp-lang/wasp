c34b6473f6d742aeb477098b591d8056df351171
0xTaneja <rushabmtaneja@gmail.com>
2025-04-05 14:23:32 +0000

    Used Wasp.Util Functions and removed redundant code

--- waspc/cli/src/Wasp/Cli/Command/CreateNewProject/StarterTemplates/Templating.hs
+++ waspc/cli/src/Wasp/Cli/Command/CreateNewProject/StarterTemplates/Templating.hs
@@ -3,28 +3,19 @@ module Wasp.Cli.Command.CreateNewProject.StarterTemplates.Templating
   )
 where
 
-import Data.Char (isUpper, toLower)
 import Data.List (foldl')
 import Data.Text (Text)
 import qualified Data.Text as T
 import StrongPath (Abs, Dir, File, Path')
 import Wasp.Cli.Command.CreateNewProject.Common (defaultWaspVersionBounds)
-import Wasp.Cli.Command.CreateNewProject.ProjectDescription (NewProjectAppName, NewProjectName)
+import Wasp.Cli.Command.CreateNewProject.ProjectDescription (NewProjectAppName(..), NewProjectName)
+import Wasp.Util (camelToKebabCase)
 import Wasp.NodePackageFFI (InstallablePackage (WaspConfigPackage), getPackageInstallationPath)
 import Wasp.Project.Analyze (WaspFilePath (..))
 import Wasp.Project.Common (WaspProjectDir)
 import Wasp.Project.ExternalConfig.PackageJson (findPackageJsonFile)
 import Wasp.Project.WaspFile (findWaspFile)
 import qualified Wasp.Util.IO as IOUtil
-
-camelToKebabCase :: String -> String
-camelToKebabCase [] = []
-camelToKebabCase (c : cs) = toLower c : camelToRestKebabCase cs
-  where
-    camelToRestKebabCase [] = []
-    camelToRestKebabCase (c : cs)
-      | isUpper c = '-' : toLower c : camelToRestKebabCase cs
-      | otherwise = c : camelToRestKebabCase cs
 
 replaceTemplatePlaceholdersInTemplateFiles :: NewProjectAppName -> NewProjectName -> Path' Abs (Dir WaspProjectDir) -> IO ()
 replaceTemplatePlaceholdersInTemplateFiles appName projectName projectDir = do
@@ -52,24 +43,9 @@ replaceTemplatePlaceholdersInPackageJsonFile ::
 replaceTemplatePlaceholdersInPackageJsonFile appName projectName projectDir =
   findPackageJsonFile projectDir >>= \case
     Nothing -> return ()
-    Just absPackageJsonFile -> do
-      waspConfigPackagePath <- getPackageInstallationPath WaspConfigPackage
-      let kebabCaseAppName = camelToKebabCase (show appName)
-      let waspTemplateReplacements =
-            [ ("__waspConfigPath__", waspConfigPackagePath),
-              ("__waspAppName__", kebabCaseAppName),
-              ("__waspProjectName__", show projectName),
-              ("__waspVersion__", defaultWaspVersionBounds)
-            ]
-      updateFileContentWith (replacePlaceholders waspTemplateReplacements) absPackageJsonFile
-  where
-    updateFileContentWith :: (Text -> Text) -> Path' Abs (File f) -> IO ()
-    updateFileContentWith updateFn absFilePath = IOUtil.readFileStrict absFilePath >>= IOUtil.writeFileFromText absFilePath . updateFn
-
-    replacePlaceholders :: [(String, String)] -> Text -> Text
-    replacePlaceholders replacements content = foldl' replacePlaceholder content replacements
-      where
-        replacePlaceholder content' (placeholder, value) = T.replace (T.pack placeholder) (T.pack value) content'
+    Just absPackageJsonFile -> do 
+     let kebabCaseAppName = NewProjectAppName . camelToKebabCase . show $ appName
+     replaceTemplatePlaceholdersInFileOnDisk kebabCaseAppName projectName absPackageJsonFile
 
 replaceTemplatePlaceholdersInFileOnDisk :: NewProjectAppName -> NewProjectName -> Path' Abs (File f) -> IO ()
 replaceTemplatePlaceholdersInFileOnDisk appName projectName file = do
