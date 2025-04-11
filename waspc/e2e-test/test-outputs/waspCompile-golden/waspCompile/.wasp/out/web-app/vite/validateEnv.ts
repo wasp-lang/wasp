1fac9d1b7447ffa2f07cb7b17697eebb5011bfc4
Mihovil Ilakovac <mihovil@ilakovac.com>
2025-04-11 13:02:01 +0200

    Build-time validation for client env (#2392)

---
+++ waspc/e2e-test/test-outputs/waspCompile-golden/waspCompile/.wasp/out/web-app/vite/validateEnv.ts
@@ -0,0 +1,44 @@
+import { type Plugin, loadEnv } from 'vite'
+
+import {
+  getValidatedEnvOrError,
+  formatZodEnvErrors,
+} from 'wasp/env/validation'
+import { clientEnvSchema } from 'wasp/client/env/schema'
+
+const redColor = '\x1b[31m'
+
+export function validateEnv(): Plugin {
+  let validationResult: ReturnType<typeof getValidatedEnvOrError> | null = null
+  return {
+    name: 'wasp-validate-env',
+    configResolved: (config) => {
+      const env = loadEnv(config.mode, process.cwd(), config.envPrefix)
+      validationResult = getValidatedEnvOrError(env, clientEnvSchema)
+
+      // Exit if we are in build mode, because we can't show the error in the browser.
+      if (config.command === 'build' && !validationResult.success) {
+        const message = formatZodEnvErrors(validationResult.error.issues)
+        console.error(`${redColor}${message}`)
+        process.exit(1)
+      }
+    },
+    configureServer: (server) => {
+      if (validationResult === null || validationResult.success) {
+        return
+      }
+
+      // Send the error to the browser.
+      const message = formatZodEnvErrors(validationResult.error.issues)
+      server.ws.on('connection', () => {
+        server.ws.send({
+          type: 'error',
+          err: {
+            message,
+            stack: '',
+          },
+        })
+      })
+    },
+  }
+}
