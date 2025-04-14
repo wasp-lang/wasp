52f484ad707941f5f60881ee5bdbecaf40d00aa5
Filip Sodić <filip.sodic@fer.hr>
2025-04-14 15:38:45 +0200

    Change ts config validation to account for esnext

--- waspc/src/Wasp/Generator/ExternalConfig/TsConfig.hs
+++ waspc/src/Wasp/Generator/ExternalConfig/TsConfig.hs
@@ -40,8 +40,8 @@ validateSrcTsConfig tsConfig =
 validateCompilerOptions :: T.CompilerOptions -> [ErrorMsg]
 validateCompilerOptions compilerOptions =
   concat
-    [ validateRequiredFieldInCompilerOptions "module" T._module "preserve",
-      validateRequiredFieldInCompilerOptions "target" T.target "es2022",
+    [ validateRequiredFieldInCompilerOptions "module" T._module "esnext",
+      validateRequiredFieldInCompilerOptions "target" T.target "esnext",
       validateRequiredFieldInCompilerOptions "moduleResolution" T.moduleResolution "bundler",
       validateRequiredFieldInCompilerOptions "moduleDetection" T.moduleDetection "force",
       validateRequiredFieldInCompilerOptions "isolatedModules" T.isolatedModules True,
