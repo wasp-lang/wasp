65162a4bcbc2d9ad604716b41a031d9e7048682b
Carlos Precioso <carlos@wasp-lang.dev>
2025-04-17 17:28:14 +0200

    Snapshots

--- waspc/e2e-test/test-outputs/waspBuild-golden/waspBuild/.wasp/build/sdk/wasp/dist/server/utils.d.ts
+++ waspc/e2e-test/test-outputs/waspBuild-golden/waspBuild/.wasp/build/sdk/wasp/dist/server/utils.d.ts
@@ -1,13 +1,15 @@
-import { Request, Response, NextFunction } from 'express';
-type RequestWithExtraFields = Request & {};
+import { Response, RequestHandler } from 'express';
+declare global {
+    namespace Express {
+        interface Request {
+        }
+    }
+}
 /**
- * Decorator for async express middleware that handles promise rejections.
- * @param {Func} middleware - Express middleware function.
- * @returns Express middleware that is exactly the same as the given middleware but,
- *   if given middleware returns promise, reject of that promise will be correctly handled,
- *   meaning that error will be forwarded to next().
+ * Simple helper to give the correct types for Express handlers.
+ * We define it in the same file as our extension to Request
+ * so that it is picked up by TypeScript.
  */
-export declare const handleRejection: (middleware: (req: RequestWithExtraFields, res: Response, next: NextFunction) => any) => (req: RequestWithExtraFields, res: Response, next: NextFunction) => Promise<void>;
+export declare const defineHandler: (middleware: RequestHandler) => RequestHandler<import("express-serve-static-core").ParamsDictionary, any, any, import("qs").ParsedQs, Record<string, any>>;
 export declare const sleep: (ms: number) => Promise<unknown>;
 export declare function redirect(res: Response, redirectUri: string): Response<any, Record<string, any>>;
-export {};
