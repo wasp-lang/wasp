2942df1a5ace7a6eb8e743133df14159a352ba19
Carlos Precioso <carlos@wasp-lang.dev>
2025-04-22 18:31:09 +0200

    Change /auth/me to `null`

--- waspc/data/Generator/templates/server/src/routes/auth/me.ts
+++ waspc/data/Generator/templates/server/src/routes/auth/me.ts
@@ -1,11 +1,10 @@
 import { serialize as superjsonSerialize } from 'superjson'
 import { handleRejection } from 'wasp/server/utils'
-import { createInvalidCredentialsError } from 'wasp/auth/utils'
 
 export default handleRejection(async (req, res) => {
   if (req.user) {
     return res.json(superjsonSerialize(req.user))
   } else {
-    throw createInvalidCredentialsError()
+    return res.json(null)
   }
 })
