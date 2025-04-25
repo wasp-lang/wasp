fc083f78a4ce37609e48362c4f12a5dbe0379f7c
Carlos Precioso <carlos@wasp-lang.dev>
2025-04-25 10:53:54 +0200

    Snapshots

--- waspc/e2e-test/test-outputs/waspComplexTest-golden/waspComplexTest/.wasp/out/web-app/src/index.tsx
+++ waspc/e2e-test/test-outputs/waspComplexTest-golden/waspComplexTest/.wasp/out/web-app/src/index.tsx
@@ -1,5 +1,5 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
 import { QueryClientProvider } from '@tanstack/react-query'
 
 import { router } from './router'
@@ -22,11 +22,11 @@ async function startApp() {
 
 async function render() {
   const queryClient = await queryClientInitialized
-  ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
-    <React.StrictMode>
+  createRoot(document.getElementById('root') as HTMLElement).render(
+    <StrictMode>
       <QueryClientProvider client={queryClient}>
         {router}
       </QueryClientProvider>
-    </React.StrictMode>
+    </StrictMode>
   )
 }
