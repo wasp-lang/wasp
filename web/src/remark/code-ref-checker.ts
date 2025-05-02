3abd5425afed86fb5943e3a73f09e9892e081457
Martin Sosic <sosic.martin@gmail.com>
2025-05-02 09:40:07 +0200

    Refined the design of code ref checker.

--- web/src/remark/code-ref-checker.ts
+++ web/src/remark/code-ref-checker.ts
@@ -17,6 +17,10 @@ const plugin: Plugin<[], Root> = () => {
       //   the whole node.meta).
       // - every code block needs to have a `ref`, otherwise we throw an error, but, they can do
       //   `ref=null` if they want to skip it.
+      //   - what about the versioned_docs and other stuff? Who will add ref=null to all of that.
+      // - Actually this is better:
+      //   - `ref="waspc/examples/todoApp/src/operations.ts:L7-42"`
+      //   - and if we want something extra, we can try to also cram it in or just go like `ref-awk="<awk_command>"`.
 
       // 2. Fetch code block from the referenced file.
 
@@ -24,7 +28,7 @@ const plugin: Plugin<[], Root> = () => {
       // - When comparing code blocks, be robust about indentation shift and trim at start and end
       //   (whitespace, newlines). We can even minify it maybe (without changing the names
       //   probably). I guess what we really want to do is to normalize them, and the question is
-      //   how.
+      //   how. Don't minify.
 
       // IDEAS:
       // - Is it too ugly to have all this coderef data in the first line of code block?
@@ -35,6 +39,8 @@ const plugin: Plugin<[], Root> = () => {
       // - To shorten the file paths, we can look into offering a couple of common replacements.
       //   For example if most of the paths will start with "wasp/waspc/examples/", we can introduce
       //   `"$exs" -> "wasp/waspc/examples/"` replacement.
+      //   - Bad side is that it is kind of magical, so e.g. we loose copy-pastability.
+      //   - let's do it only if we really get annoyed with it.
     })
   }
 }
