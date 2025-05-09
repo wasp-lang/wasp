3a5128c42fd56340364219910fde376c5276d9bc
Franjo Mindek <franjo.mindek@gmail.com>
2025-05-09 16:24:00 +0200

    move main to run

--- waspc/packages/wasp-config/src/cli.ts
+++ waspc/packages/wasp-config/src/cli.ts
@@ -1,24 +1,3 @@
-import { writeFileSync } from 'fs'
-import { analyzeUserApp } from './appAnalyzer.js'
-
-/**
- * Main function that processes command line arguments, analyzes the user app,
- * and writes the output to a file.
- */
-export async function main(args: string[]): Promise<void> {
-  const { waspTsSpecPath, outputFilePath, entityNames } =
-    parseProcessArgsOrThrow(args)
-
-  const declsJsonResult = await analyzeUserApp(waspTsSpecPath, entityNames)
-
-  if (declsJsonResult.status === 'error') {
-    console.error(declsJsonResult.error)
-    process.exit(1)
-  }
-
-  writeFileSync(outputFilePath, declsJsonResult.value)
-}
-
 export function parseProcessArgsOrThrow(args: string[]): {
   waspTsSpecPath: string
   outputFilePath: string
--- waspc/packages/wasp-config/src/run.ts
+++ waspc/packages/wasp-config/src/run.ts
@@ -1,5 +1,26 @@
 #!/usr/bin/env node
-import { main } from './cli.js'
+
+import { writeFileSync } from 'fs'
+import { analyzeUserApp } from './appAnalyzer.js'
+import { parseProcessArgsOrThrow } from './cli.js'
+
+/**
+ * Main function that processes command line arguments, analyzes the user app,
+ * and writes the output to a file.
+ */
+export async function main(args: string[]): Promise<void> {
+  const { waspTsSpecPath, outputFilePath, entityNames } =
+    parseProcessArgsOrThrow(args)
+
+  const declsJsonResult = await analyzeUserApp(waspTsSpecPath, entityNames)
+
+  if (declsJsonResult.status === 'error') {
+    console.error(declsJsonResult.error)
+    process.exit(1)
+  }
+
+  writeFileSync(outputFilePath, declsJsonResult.value)
+}
 
 main(process.argv).catch((error) => {
   console.error(error)
