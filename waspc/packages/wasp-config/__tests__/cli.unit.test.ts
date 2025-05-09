7cf687e388172c37a9220d1027fbbac0f291b113
Franjo Mindek <franjo.mindek@gmail.com>
2025-05-09 17:20:48 +0200

    cli improvements

--- waspc/packages/wasp-config/__tests__/cli.unit.test.ts
+++ waspc/packages/wasp-config/__tests__/cli.unit.test.ts
@@ -3,9 +3,7 @@ import { parseProcessArgsOrThrow } from '../src/cli'
 
 describe('parseProcessArgsOrThrow', () => {
   test('should parse arguments correctly', () => {
-    testParseProcessArgsOrThrow([
-      'node',
-      'run.js',
+    expectParseProcessArgsToSucceed([
       'main.wasp.js',
       'output.json',
       JSON.stringify(['entity1']),
@@ -13,98 +11,68 @@ describe('parseProcessArgsOrThrow', () => {
   })
 
   test('should parse 0 entities correctly', () => {
-    testParseProcessArgsOrThrow([
-      'node',
-      'run.js',
-      'main.wasp.js',
-      'output.json',
-      '[]',
-    ])
+    expectParseProcessArgsToSucceed(['main.wasp.js', 'output.json', '[]'])
   })
 
   test('should throw an error if less than 5 arguments', () => {
-    testParseProcessArgsOrThrow(
-      ['node', 'run.js', 'main.wasp.js', 'output.json'],
-      { shouldError: true }
-    )
+    expectParseProcessArgsToError(['main.wasp.js', 'output.json'])
   })
 
   test('should throw an error if more than 5 arguments', () => {
-    testParseProcessArgsOrThrow(
-      ['node', 'run.js', 'main.wasp.js', 'output.json', '[]', 'extraArg'],
-      { shouldError: true }
-    )
+    expectParseProcessArgsToError([
+      'main.wasp.js',
+      'output.json',
+      '[]',
+      'extraArg',
+    ])
   })
 
   test('should throw an error if waspTsSpecPath is not a string', () => {
-    testParseProcessArgsOrThrow(
-      [
-        'node',
-        'run.js',
-        undefined,
-        'output.json',
-        JSON.stringify(['entity1']),
-      ] as string[],
-      { shouldError: true }
-    )
+    expectParseProcessArgsToError([
+      undefined,
+      'output.json',
+      JSON.stringify(['entity1']),
+    ] as string[])
   })
 
   test('should throw an error if outputFilePath is not a string', () => {
-    testParseProcessArgsOrThrow(
-      [
-        'node',
-        'run.js',
-        'main.wasp.js',
-        undefined,
-        JSON.stringify(['entity1']),
-      ] as string[],
-      { shouldError: true }
-    )
+    expectParseProcessArgsToError([
+      'main.wasp.js',
+      undefined,
+      JSON.stringify(['entity1']),
+    ] as string[])
   })
 
   test('should throw an error if any entityNames is not a string', () => {
-    testParseProcessArgsOrThrow(
-      ['node', 'run.js', 'main.wasp.js', 'output.json', undefined] as string[],
-      { shouldError: true }
-    )
+    expectParseProcessArgsToError([
+      'main.wasp.js',
+      'output.json',
+      undefined,
+    ] as string[])
   })
 
   test('should throw an error if the entity names JSON is not an array', () => {
-    testParseProcessArgsOrThrow(
-      [
-        'node',
-        'run.js',
-        'main.wasp.js',
-        'output.json',
-        JSON.stringify({ entity1: 'entity1' }),
-      ],
-      { shouldError: true }
-    )
+    expectParseProcessArgsToError([
+      'main.wasp.js',
+      'output.json',
+      JSON.stringify({ entity1: 'entity1' }),
+    ])
   })
 
-  function testParseProcessArgsOrThrow(
-    args: string[],
-    options:
-      | {
-          shouldError?: boolean
-        }
-      | undefined = {
-      shouldError: false,
-    }
-  ) {
-    const { shouldError } = options
+  function expectParseProcessArgsToSucceed(args: string[]) {
+    const result = parseProcessArgsOrThrow(['node', 'run.js', ...args])
 
-    if (shouldError) {
-      expect(() => parseProcessArgsOrThrow(args)).toThrowError()
-      return
-    }
-
-    const result = parseProcessArgsOrThrow(args)
-
+    const [waspTsSpecPath, outputFilePath, entityNames] = args
     expect(result).toEqual({
-      waspTsSpecPath: args.at(2),
-      outputFilePath: args.at(3),
-      entityNames: args.at(4) && JSON.parse(args[4]),
+      waspTsSpecPath,
+      outputFilePath,
+      entityNames: entityNames && JSON.parse(entityNames),
     })
+  }
+
+  function expectParseProcessArgsToError(args: string[]) {
+    expect(() =>
+      parseProcessArgsOrThrow(['node', 'run.js', ...args])
+    ).toThrowError()
   }
 })
