d61b10e5c450076344ffbecedbcbfc77c4da9a8d
Mihovil Ilakovac <mihovil@ilakovac.com>
2025-05-13 10:44:25 +0200

    Remove auth required for streaming page

--- waspc/examples/todoApp/headless-tests/tests/streaming.spec.ts
+++ waspc/examples/todoApp/headless-tests/tests/streaming.spec.ts
@@ -1,36 +1,9 @@
 import { test, expect } from '@playwright/test'
-import {
-  generateRandomCredentials,
-  performLogin,
-  performSignup,
-} from './helpers'
 
 test.describe('streaming', () => {
-  const { email, password } = generateRandomCredentials()
-
   test.describe.configure({ mode: 'serial' })
 
-  test.beforeAll(async ({ browser }) => {
-    const page = await browser.newPage()
-
-    await performSignup(page, {
-      email,
-      password,
-    })
-
-    await expect(page.locator('body')).toContainText(
-      `You've signed up successfully! Check your email for the confirmation link.`
-    )
-  })
-
   test('text streams over time', async ({ page }) => {
-    await performLogin(page, {
-      email,
-      password,
-    })
-
-    await expect(page).toHaveURL('/profile')
-
     await page.goto('/streaming')
 
     // Verify initial text appears first
