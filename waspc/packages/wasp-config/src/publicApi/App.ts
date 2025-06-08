759806be8408281cc9a33ff151300ac19cf70add
Franjo Mindek <franjo.mindek@gmail.com>
2025-06-08 12:14:55 +0200

    refactoring a bit

--- waspc/packages/wasp-config/src/publicApi/App.ts
+++ waspc/packages/wasp-config/src/publicApi/App.ts
@@ -1,111 +1,119 @@
 /** This module defines the user-facing API for defining a Wasp app.
  */
 import { GET_TS_APP_SPEC } from "../_private.js";
-import {
-  ActionConfig,
-  ApiConfig,
-  ApiNamespaceConfig,
-  AppConfig,
-  AuthConfig,
-  ClientConfig,
-  CrudConfig,
-  DbConfig,
-  EmailSenderConfig,
-  JobConfig,
-  PageConfig,
-  PageName,
-  QueryConfig,
-  RouteConfig,
-  ServerConfig,
-  TsAppSpec,
-  WebsocketConfig,
-} from "./tsAppSpec.js";
+import * as TsAppSpec from "./tsAppSpec.js";
 
 export class App {
-  #tsAppSpec: TsAppSpec;
+  #tsAppSpec: AppData;
 
   // NOTE: Using a non-public symbol gives us a package-private property.
   // It's not that important to hide it from the users, but we still don't want
   // user's IDE to suggest it during autocompletion.
-  [GET_TS_APP_SPEC](): TsAppSpec {
+  [GET_TS_APP_SPEC](): AppData {
     return this.#tsAppSpec;
   }
 
-  constructor(name: string, config: AppConfig) {
+  constructor(name: string, config: TsAppSpec.AppConfig) {
     this.#tsAppSpec = {
       app: { name, config },
-      actions: new Map<string, ActionConfig>(),
-      apiNamespaces: new Map<string, ApiNamespaceConfig>(),
-      apis: new Map<string, ApiConfig>(),
+      actions: new Map<string, TsAppSpec.ActionConfig>(),
+      apiNamespaces: new Map<string, TsAppSpec.ApiNamespaceConfig>(),
+      apis: new Map<string, TsAppSpec.ApiConfig>(),
       auth: undefined,
       client: undefined,
-      cruds: new Map<string, CrudConfig>(),
+      cruds: new Map<string, TsAppSpec.CrudConfig>(),
       db: undefined,
       emailSender: undefined,
-      jobs: new Map<string, JobConfig>(),
-      pages: new Map<string, PageConfig>(),
-      queries: new Map<string, QueryConfig>(),
-      routes: new Map<string, RouteConfig>(),
+      jobs: new Map<string, TsAppSpec.JobConfig>(),
+      pages: new Map<string, TsAppSpec.PageConfig>(),
+      queries: new Map<string, TsAppSpec.QueryConfig>(),
+      routes: new Map<string, TsAppSpec.RouteConfig>(),
       server: undefined,
       websocket: undefined,
     };
   }
 
   // TODO: Enforce that all methods are covered in compile time
-  action(this: App, name: string, config: ActionConfig): void {
+  action(this: App, name: string, config: TsAppSpec.ActionConfig): void {
     this.#tsAppSpec.actions.set(name, config);
   }
 
-  apiNamespace(this: App, name: string, config: ApiNamespaceConfig): void {
+  apiNamespace(
+    this: App,
+    name: string,
+    config: TsAppSpec.ApiNamespaceConfig,
+  ): void {
     this.#tsAppSpec.apiNamespaces.set(name, config);
   }
 
-  api(this: App, name: string, config: ApiConfig): void {
+  api(this: App, name: string, config: TsAppSpec.ApiConfig): void {
     this.#tsAppSpec.apis.set(name, config);
   }
 
-  auth(this: App, config: AuthConfig): void {
+  auth(this: App, config: TsAppSpec.AuthConfig): void {
     this.#tsAppSpec.auth = config;
   }
 
-  client(this: App, config: ClientConfig): void {
+  client(this: App, config: TsAppSpec.ClientConfig): void {
     this.#tsAppSpec.client = config;
   }
 
-  crud(this: App, name: string, config: CrudConfig): void {
+  crud(this: App, name: string, config: TsAppSpec.CrudConfig): void {
     this.#tsAppSpec.cruds.set(name, config);
   }
 
-  db(this: App, config: DbConfig): void {
+  db(this: App, config: TsAppSpec.DbConfig): void {
     this.#tsAppSpec.db = config;
   }
 
-  emailSender(this: App, config: EmailSenderConfig): void {
+  emailSender(this: App, config: TsAppSpec.EmailSenderConfig): void {
     this.#tsAppSpec.emailSender = config;
   }
 
-  job(this: App, name: string, config: JobConfig): void {
+  job(this: App, name: string, config: TsAppSpec.JobConfig): void {
     this.#tsAppSpec.jobs.set(name, config);
   }
 
-  page(this: App, name: string, config: PageConfig): PageName {
+  page(
+    this: App,
+    name: string,
+    config: TsAppSpec.PageConfig,
+  ): TsAppSpec.PageName {
     this.#tsAppSpec.pages.set(name, config);
-    return name as PageName;
+    return name as TsAppSpec.PageName;
   }
 
-  query(this: App, name: string, config: QueryConfig): void {
+  query(this: App, name: string, config: TsAppSpec.QueryConfig): void {
     this.#tsAppSpec.queries.set(name, config);
   }
 
-  route(this: App, name: string, config: RouteConfig): void {
+  route(this: App, name: string, config: TsAppSpec.RouteConfig): void {
     this.#tsAppSpec.routes.set(name, config);
   }
 
-  server(this: App, config: ServerConfig): void {
+  server(this: App, config: TsAppSpec.ServerConfig): void {
     this.#tsAppSpec.server = config;
   }
 
-  webSocket(this: App, config: WebsocketConfig) {
+  webSocket(this: App, config: TsAppSpec.WebsocketConfig) {
     this.#tsAppSpec.websocket = config;
   }
 }
+
+export type AppData = {
+  app: { name: string; config: TsAppSpec.AppConfig };
+  actions: Map<string, TsAppSpec.ActionConfig>;
+  apiNamespaces: Map<string, TsAppSpec.ApiNamespaceConfig>;
+  apis: Map<string, TsAppSpec.ApiConfig>;
+  auth?: TsAppSpec.AuthConfig;
+  client?: TsAppSpec.ClientConfig;
+  cruds: Map<string, TsAppSpec.CrudConfig>;
+  db?: TsAppSpec.DbConfig;
+  emailSender?: TsAppSpec.EmailSenderConfig;
+  jobs: Map<string, TsAppSpec.JobConfig>;
+  pages: Map<string, TsAppSpec.PageConfig>;
+  queries: Map<string, TsAppSpec.QueryConfig>;
+  routes: Map<string, TsAppSpec.RouteConfig>;
+  server?: TsAppSpec.ServerConfig;
+  websocket?: TsAppSpec.WebsocketConfig;
+};
