0d062cc5dfcd15c94f2fbd6eec9fc338db87c3ad
Franjo Mindek <franjo.mindek@gmail.com>
2025-06-16 08:49:07 +0200

    revert

--- waspc/e2e-test/GoldenTest.hs
+++ waspc/e2e-test/GoldenTest.hs
@@ -66,7 +66,7 @@ runGoldenTest goldenTest = do
   filesForGoldenTestAbsFps <- listFilesForGoldenTest currentOutputDirAbsFp
   reformatPackageJsonFiles filesForGoldenTestAbsFps
 
-  let manifestAbsFp = currentOutputDirAbsFp FP.</> fileManifestFileName
+  let manifestAbsFp = currentOutputDirAbsFp FP.</> manifestFileName
   writeFileManifest currentOutputDirAbsFp filesForManifestAbsFps manifestAbsFp
 
   let remapCurrentPathToGolden fp = unpack $ replace (pack currentOutputDirAbsFp) (pack goldenOutputDirAbsFp) (pack fp)
@@ -84,8 +84,8 @@ runGoldenTest goldenTest = do
           let goldenOutputAbsFp = remapCurrentPathToGolden currentOutputAbsFp
       ]
   where
-    fileManifestFileName :: String
-    fileManifestFileName = "files.manifest"
+    manifestFileName :: String
+    manifestFileName = "files.manifest"
 
     -- Lists files that should be included in the `files.manifest`.
     listFilesForManifest :: FilePath -> IO [FilePath]
