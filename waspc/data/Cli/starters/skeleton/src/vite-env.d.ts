3489d1182b567e37687dcf4702b4dde95cc985f7
Franjo Mindek <franjo.mindek@gmail.com>
2025-07-01 09:44:52 +0200

    formatting

--- waspc/data/Cli/starters/skeleton/src/vite-env.d.ts
+++ waspc/data/Cli/starters/skeleton/src/vite-env.d.ts
@@ -4,4 +4,4 @@
 // Types for jest-dom are not recognized automatically and Typescript complains
 // about missing types e.g. when using `toBeInTheDocument` and other matchers.
 // Reference: https://github.com/testing-library/jest-dom/issues/546#issuecomment-1889884843
-import '@testing-library/jest-dom';
+import "@testing-library/jest-dom";
