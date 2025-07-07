036ab6c8923fb3c4cf75968b2dc019e161fc768a
0xTaneja <rushabmtaneja@gmail.com>
2025-07-07 22:55:25 +0000

    feat(devtools): worked on .wasp configuration

--- waspc/src/Wasp/Generator/WebAppGenerator/Start.hs
+++ waspc/src/Wasp/Generator/WebAppGenerator/Start.hs
@@ -7,9 +7,11 @@ import StrongPath (Abs, Dir, Path', (</>))
 import Wasp.Generator.Common (ProjectRootDir)
 import qualified Wasp.Generator.WebAppGenerator.Common as Common
 import qualified Wasp.Job as J
-import Wasp.Job.Process (runNodeCommandAsJob)
+import qualified StrongPath as SP
+import Wasp.Job.Process (runNodeCommandAsJobWithExtraEnv)
 
 startWebApp :: Path' Abs (Dir ProjectRootDir) -> J.Job
 startWebApp projectDir = do
   let webAppDir = projectDir </> Common.webAppRootDirInProjectRootDir
-  runNodeCommandAsJob webAppDir "npm" ["start"] J.WebApp
+      envVars = [("WASP_PROJECT_ROOT", SP.fromAbsDir projectDir)]
+  runNodeCommandAsJobWithExtraEnv envVars webAppDir "npm" ["start"] J.WebApp
