346ab664f769a3948cd34ca84540123d25247582
Mihovil Ilakovac <mihovil@ilakovac.com>
2025-07-14 14:55:58 +0200

    Updates for Wasp 0.17.0
    Signed-off-by: Mihovil Ilakovac <mihovil@ilakovac.com>

--- deploy.sh
+++ deploy.sh
@@ -22,22 +22,7 @@ fi
 
 cd web-app
 npm install && REACT_APP_API_URL=$BACKEND_URL npm run build
-cp -r build dist
-
-dockerfile_contents=$(cat <<EOF
-FROM pierrezemb/gostatic
-CMD [ "-fallback", "index.html" ]
-COPY ./dist/ /srv/http/
-EOF
-)
-
-dockerignore_contents=$(cat <<EOF
-node_modules/
-EOF
-)
-
-echo "$dockerfile_contents" > Dockerfile
-echo "$dockerignore_contents" > .dockerignore
+cd build
 
 echo -e "\n\033[1;33mDeploying client to Railway...\033[0m\n"
 railway up --service client --detach
