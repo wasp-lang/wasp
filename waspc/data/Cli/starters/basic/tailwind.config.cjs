0c7ab3177acae5a4b0891cdcfd160f30c02da7ae
Franjo Mindek <franjo.mindek@gmail.com>
2025-07-17 10:33:21 +0200

    quickfix

--- waspc/data/Cli/starters/basic/tailwind.config.cjs
+++ waspc/data/Cli/starters/basic/tailwind.config.cjs
@@ -1,4 +1,5 @@
-import { resolveProjectPath } from "wasp/dev";
+// eslint-disable-next-line @typescript-eslint/no-require-imports
+const { resolveProjectPath } = require('wasp/dev')
 
 /** @type {import('tailwindcss').Config} */
 module.exports = {
