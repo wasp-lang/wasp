885164988bac86e57a92e19cb4a83c8e56417ae7
Mihovil Ilakovac <mihovil@ilakovac.com>
2025-08-14 18:52:06 +0200

    WaspLibs comment

--- waspc/src/Wasp/Generator/WaspLibs.hs
+++ waspc/src/Wasp/Generator/WaspLibs.hs
@@ -5,5 +5,7 @@ where
 
 import qualified Wasp.Generator.WaspLibs.WaspLib as WaspLib
 
+-- NOTE: The package names of the libs should match the names in the
+-- `package.json` files of the libs in the ./libs directory.
 getWaspLibs :: IO [WaspLib.WaspLib]
 getWaspLibs = sequence [WaspLib.makeWaspLib "@wasp.sh/libs-auth"]
