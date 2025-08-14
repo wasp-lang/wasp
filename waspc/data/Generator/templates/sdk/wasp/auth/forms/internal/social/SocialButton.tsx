84f846f3f3f56b41019d486d2c6afbe788ea612d
Carlos Precioso <carlos@wasp-lang.dev>
2025-08-14 15:40:25 +0200

    forward refs

--- waspc/data/Generator/templates/sdk/wasp/auth/forms/internal/social/SocialButton.tsx
+++ waspc/data/Generator/templates/sdk/wasp/auth/forms/internal/social/SocialButton.tsx
@@ -1,11 +1,14 @@
-import type { ComponentProps } from 'react'
-import '../auth-styles.css'
-import { clsx } from '../util'
-import styles from './SocialButton.module.css'
+import { ComponentPropsWithoutRef, ComponentRef, forwardRef } from "react";
+import "../auth-styles.css";
+import { clsx } from "../util";
+import styles from "./SocialButton.module.css";
 
 // PRIVATE API
-export const SocialButton = ({ children, className, ...props }: ComponentProps<'a'>) => (
-  <a className={clsx(styles.socialButton, className)} {...props}>
+export const SocialButton = forwardRef<
+  ComponentRef<"a">,
+  ComponentPropsWithoutRef<"a">
+>(({ children, className, ...props }, ref) => (
+  <a className={clsx(styles.socialButton, className)} ref={ref} {...props}>
     {children}
   </a>
-)
+));
