24064fd173dea1a3e476fdfd4ec2b47c30afebbd
Carlos Precioso <carlos@wasp-lang.dev>
2025-08-14 16:01:13 +0200

    Update snaphsot

--- waspc/e2e-test/test-outputs/waspComplexTest-golden/waspComplexTest/.wasp/out/sdk/wasp/auth/forms/internal/Form.tsx
+++ waspc/e2e-test/test-outputs/waspComplexTest-golden/waspComplexTest/.wasp/out/sdk/wasp/auth/forms/internal/Form.tsx
@@ -1,49 +1,70 @@
-import type { ComponentProps } from 'react'
-import styles from './Form.module.css'
-import './auth-styles.css'
-import { clsx } from './util'
+import { ComponentPropsWithoutRef, ComponentRef, forwardRef } from "react";
+import styles from "./Form.module.css";
+import "./auth-styles.css";
+import { clsx } from "./util";
 
 // PRIVATE API
-export const Form = ({ children, className, ...props }: ComponentProps<'form'>) => (
+export const Form = forwardRef<
+  ComponentRef<"form">,
+  ComponentPropsWithoutRef<"form">
+>(({ children, className, ...props }) => (
   <form className={clsx(styles.form, className)} {...props}>
     {children}
   </form>
-)
+));
 
 // PUBLIC API
-export const FormItemGroup = ({ children, className, ...props }: ComponentProps<'div'>) => (
+export const FormItemGroup = forwardRef<
+  ComponentRef<"div">,
+  ComponentPropsWithoutRef<"div">
+>(({ children, className, ...props }) => (
   <div className={clsx(styles.formItemGroup, className)} {...props}>
     {children}
   </div>
-)
+));
 
 // PUBLIC API
-export const FormLabel = ({ children, className, ...props }: ComponentProps<'label'>) => (
+export const FormLabel = forwardRef<
+  ComponentRef<"label">,
+  ComponentPropsWithoutRef<"label">
+>(({ children, className, ...props }) => (
   <label className={clsx(styles.formLabel, className)} {...props}>
     {children}
   </label>
-)
+));
 
 // PUBLIC API
-export const FormInput = ({ className, ...props }: ComponentProps<'input'>) => (
+export const FormInput = forwardRef<
+  ComponentRef<"input">,
+  ComponentPropsWithoutRef<"input">
+>(({ className, ...props }) => (
   <input className={clsx(styles.formInput, className)} {...props} />
-)
+));
 
-// PUBLIC API
-export const FormTextarea = ({ className, ...props }: ComponentProps<'textarea'>) => (
+// PUBLIC API)
+export const FormTextarea = forwardRef<
+  ComponentRef<"textarea">,
+  ComponentPropsWithoutRef<"textarea">
+>(({ className, ...props }) => (
   <textarea className={clsx(styles.formTextarea, className)} {...props} />
-)
+));
 
-// PUBLIC API
-export const FormError = ({ children, className, ...props }: ComponentProps<'div'>) => (
+// PUBLIC API)
+export const FormError = forwardRef<
+  ComponentRef<"div">,
+  ComponentPropsWithoutRef<"div">
+>(({ children, className, ...props }) => (
   <div className={clsx(styles.formError, className)} {...props}>
     {children}
   </div>
-)
+));
 
 // PRIVATE API
-export const SubmitButton = ({ children, className, ...props }: ComponentProps<'button'>) => (
+export const SubmitButton = forwardRef<
+  ComponentRef<"button">,
+  ComponentPropsWithoutRef<"button">
+>(({ children, className, ...props }) => (
   <button className={clsx(styles.submitButton, className)} {...props}>
     {children}
   </button>
-)
+));
--- waspc/e2e-test/test-outputs/waspComplexTest-golden/waspComplexTest/.wasp/out/sdk/wasp/auth/forms/internal/Message.tsx
+++ waspc/e2e-test/test-outputs/waspComplexTest-golden/waspComplexTest/.wasp/out/sdk/wasp/auth/forms/internal/Message.tsx
@@ -1,25 +1,34 @@
-import type { ComponentProps } from 'react'
-import styles from './Message.module.css'
-import './auth-styles.css'
-import { clsx } from './util'
+import { ComponentPropsWithoutRef, ComponentRef, forwardRef } from "react";
+import styles from "./Message.module.css";
+import "./auth-styles.css";
+import { clsx } from "./util";
 
 // PRIVATE API
-export const Message = ({ children, className, ...props }: ComponentProps<'div'>) => (
+export const Message = forwardRef<
+  ComponentRef<"div">,
+  ComponentPropsWithoutRef<"div">
+>(({ children, className, ...props }) => (
   <div className={clsx(styles.message, className)} {...props}>
     {children}
   </div>
-)
+));
 
 // PRIVATE API
-export const MessageError = ({ children, className, ...props }: ComponentProps<'div'>) => (
+export const MessageError = forwardRef<
+  ComponentRef<"div">,
+  ComponentPropsWithoutRef<"div">
+>(({ children, className, ...props }) => (
   <div className={clsx(styles.messageError, className)} {...props}>
     {children}
   </div>
-)
+));
 
 // PRIVATE API
-export const MessageSuccess = ({ children, className, ...props }: ComponentProps<'div'>) => (
+export const MessageSuccess = forwardRef<
+  ComponentRef<"div">,
+  ComponentPropsWithoutRef<"div">
+>(({ children, className, ...props }) => (
   <div className={clsx(styles.messageSuccess, className)} {...props}>
     {children}
   </div>
-)
+));
