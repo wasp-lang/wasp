11730b79618677d7437052a24402ee50826798bb
Mihovil Ilakovac <mihovil@ilakovac.com>
2025-09-10 16:24:21 +0200

    Update tutorial TS app
    Signed-off-by: Mihovil Ilakovac <mihovil@ilakovac.com>

--- examples/tutorials/TodoAppTs/src/queries.ts
+++ examples/tutorials/TodoAppTs/src/queries.ts
@@ -1,6 +1,6 @@
-import { Task } from "wasp/entities";
+import type { Task } from "wasp/entities";
 import { HttpError } from "wasp/server";
-import { GetTasks } from "wasp/server/operations";
+import type { GetTasks } from "wasp/server/operations";
 
 export const getTasks: GetTasks<void, Task[]> = async (args, context) => {
   if (!context.user) {
