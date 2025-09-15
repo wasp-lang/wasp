b7f6616b455317ec5b8a32467d38e81ca4bdf741
Carlos Precioso <carlos@wasp-lang.dev>
2025-09-15 15:34:33 +0200

    Undo plumbing

--- waspc/src/Wasp/Generator/ServerGenerator.hs
+++ waspc/src/Wasp/Generator/ServerGenerator.hs
@@ -131,7 +131,6 @@ genPackageJson spec waspDependencies = do
           object
             [ "depsChunk" .= N.getDependenciesPackageJsonEntry combinedDependencies,
               "devDepsChunk" .= N.getDevDependenciesPackageJsonEntry combinedDependencies,
-              "peerDepsChunk" .= N.getPeerDependenciesPackageJsonEntry combinedDependencies,
               "nodeVersionRange" .= (">=" <> show NodeVersion.oldestWaspSupportedNodeVersion),
               "startProductionScript"
                 .= ( (if hasEntities then "npm run db-migrate-prod && " else "")
