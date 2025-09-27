a695db14896211d3cb04d05b03fc7f3affed854e
Mihovil Ilakovac <mihovil@ilakovac.com>
2025-09-27 14:56:50 +0200

    Update package copying scripts safety

--- waspc/tools/install_packages_to_data_dir.sh
+++ waspc/tools/install_packages_to_data_dir.sh
@@ -7,7 +7,7 @@
 script_dir=$(CDPATH="" cd -- "$(dirname -- "$0")" && pwd)
 waspc_dir=$script_dir/..
 
-for package_dir in "$waspc_dir"/packages/*; do
+for package_dir in "${waspc_dir:?}"/packages/*; do
   if [[ -d "$package_dir" ]]; then
     package=$(basename "$package_dir")
     # We're only installing the dependencies here to verify that the build
@@ -21,6 +21,6 @@ for package_dir in "$waspc_dir"/packages/*; do
   fi
 done
 
-cd "$waspc_dir"
+cd "${waspc_dir:?}"
 rm -rf ./data/packages
 cp -R ./packages ./data
