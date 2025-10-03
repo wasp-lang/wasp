38fdbe07e3fd52960fed01c416b72b0bf4e55f6a
Mihovil Ilakovac <mihovil@ilakovac.com>
2025-10-03 15:47:41 +0200

    Update cookies import and formatting

--- waspc/data/Generator/templates/server/src/auth/providers/oauth/cookies.ts
+++ waspc/data/Generator/templates/server/src/auth/providers/oauth/cookies.ts
@@ -1,13 +1,13 @@
-import { parseCookies } from "@wasp.sh/lib-auth/server";
 import {
   Request as ExpressRequest,
   Response as ExpressResponse,
-} from "express";
+} from 'express';
+import { parseCookies } from '@wasp.sh/lib-auth/server';
 
-import type { ProviderConfig } from "wasp/auth/providers/types";
-import { config } from "wasp/server";
+import type { ProviderConfig } from 'wasp/auth/providers/types';
+import { config } from 'wasp/server';
 
-import type { OAuthStateFieldName } from "./state";
+import type { OAuthStateFieldName } from './state';
 
 export function setOAuthCookieValue(
   provider: ProviderConfig,
