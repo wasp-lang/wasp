8ff00d59f19553884ca70e5572b8daa78547940d
Franjo Mindek <franjo.mindek@gmail.com>
2025-10-22 13:13:52 +0200

    add entity for now
    # Conflicts:
    #	waspc/packages/studio/client/src/Flow.tsx

---
+++ waspc/packages/studio/client/src/ELK.ts
@@ -0,0 +1,99 @@
+import ELK, { type ElkNode } from "elkjs/lib/elk.bundled.js";
+import {
+  Edge,
+  Node
+} from "reactflow";
+
+const elk = new ELK();
+
+export const getLayoutedElements = (nodes: Node[], edges: Edge[]) => {
+  const graph = {
+    id: "root",
+    // Elk has a *huge* amount of options to configure. To see everything you can
+    // tweak check out:
+    //
+    // - https://www.eclipse.org/elk/reference/algorithms.html
+    // - https://www.eclipse.org/elk/reference/options.html
+    layoutOptions: {
+      // Alternative layout:
+      // "elk.spacing.nodeNode": "30.0",
+      // "elk.algorithm": "elk.layered",
+      // "elk.layered.spacing.nodeNodeBetweenLayers": "100.0",
+      // "elk.layered.thoroughness": "7",
+      // "elk.direction": "RIGHT",
+      // "elk.edgeRouting": "POLYLINE",
+      // "elk.aspectRatio": "1.0f",
+      "elk.algorithm": "layered",
+      "elk.direction": "RIGHT",
+      "elk.edgeRouting": "POLYLINE",
+      // "elk.hierarchyHandling": "INCLUDE_CHILDREN",
+      "elk.layered.crossingMinimization.semiInteractive": true,
+    },
+    children: nodes.map((node: Node) => ({
+      ...node,
+      width: getNodeWidth(node),
+      height: getNodeHeight(node),
+    })),
+    edges: edges,
+  };
+
+  return (
+    elk
+      // Hack
+      .layout(graph as unknown as ElkNode)
+      .then((layoutedGraph) => {
+        if (!layoutedGraph.children) {
+          return null;
+        }
+        return {
+          nodes: layoutedGraph.children.map((node) => ({
+            ...node,
+            position: { x: node.x, y: node.y },
+          })),
+
+          edges: layoutedGraph.edges,
+        };
+      })
+      .catch(console.error)
+  );
+};
+
+function getNodeHeight(node: Node) {
+  if (node.type === "apiNode") {
+    return 100;
+  }
+  if (node.type === "jobNode" && node.data.schedule) {
+    return 100;
+  }
+  if (node.type === "appNode") {
+    const authMethods = node.data.auth?.methods ?? [];
+    return 100 + authMethods.length * 50;
+  }
+  return 50;
+}
+
+function getNodeWidth(node: Node) {
+  const textCandidates = [
+    node.data?.label,
+    node.data?.name,
+    node.data?.path,
+    node.data?.schedule,
+    // Auth methods
+    ...getAuthMethods(node),
+  ]
+    .filter(Boolean)
+    .map((text) => text.length);
+
+  const longestText = Math.max(...textCandidates);
+  const width = Math.max(150, longestText * 10 + 40);
+  return width;
+}
+
+function getAuthMethods(node: Node) {
+  if (node.type !== "appNode") {
+    return [];
+  }
+  return (
+    node.data?.auth?.methods.map((method: string) => `Auth: ${method}`) ?? []
+  );
+}
--- waspc/packages/studio/client/src/types.ts
+++ waspc/packages/studio/client/src/types.ts
@@ -1,4 +1,4 @@
-export type Data = {
+export type WaspAppData = {
   entities: {
     name: string;
   }[];
