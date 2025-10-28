4df1860f01009dbc9aa47269f958659b4937fdf9
Franjo Mindek <franjo.mindek@gmail.com>
2025-10-28 17:07:24 +0100

    fix home dir resolving

--- waspc/e2e-tests/ContainerTest/WaspInstallContainerTest.hs
+++ waspc/e2e-tests/ContainerTest/WaspInstallContainerTest.hs
@@ -11,41 +11,34 @@ import ShellCommands
     writeToStdErrOnFailureAndExit,
     (~|),
   )
-import StrongPath (Abs, Dir, Path', fromAbsDir, (</>))
 import System.Process (readCreateProcess, shell)
-import Wasp.Cli.FileSystem
-  ( UserHomeDir,
-    getHomeDir,
-    waspInstallationDirInHomeDir,
-  )
 import qualified Wasp.SemanticVersion.Version as SV
 
 -- | Note that Wasp install script lives outside of Wasp CLI,
 -- We should not export functions defined here outside of this module.
 waspInstallContainerTest :: IO ContainerTest
 waspInstallContainerTest = do
-  homeDir <- getHomeDir
   latestWaspVersion <- readCreateProcess (shell curlLatestWaspVersion) ""
-  return $ makeWaspInstallContainerTest homeDir latestWaspVersion
+  return $ makeWaspInstallContainerTest latestWaspVersion
   where
     -- Copied from https://github.com/wasp-lang/get-wasp-sh/blob/994a3f0817aa0d7b1b3001f504a6bad781393da2/installer.sh#L292
     curlLatestWaspVersion :: ShellCommand
     curlLatestWaspVersion = "curl -LIs -o /dev/null -w '%{url_effective}' https://github.com/wasp-lang/wasp/releases/latest | awk -F/ '{print $NF}' | cut -c2-"
 
-    makeWaspInstallContainerTest :: Path' Abs (Dir UserHomeDir) -> String -> ContainerTest
-    makeWaspInstallContainerTest homeDir latestWaspVersion =
+    makeWaspInstallContainerTest :: String -> ContainerTest
+    makeWaspInstallContainerTest latestWaspVersion =
       makeContainerTest
         "wasp-install"
         [ writeToStdErrOnFailureAndExit
             (installWaspCliVersion specificWaspVersion)
-            "Installer should suppoert installing a specific Wasp version",
+            "Installer should support installing a specific Wasp version",
           writeToStdErrOnFailureAndExit
             (assertWaspCliVerionDirectoryExists specificWaspVersion)
-            ("Installing Wasp version should create the " ++ fromAbsDir waspInstallationDir ++ "<version> directory"),
+            ("Installing Wasp version " ++ show specificWaspVersion ++ " should create the $HOME/.local/share/wasp-lang/" ++ show specificWaspVersion ++ " directory"),
           installLatestWaspCli,
           writeToStdErrOnFailureAndExit
             assertLatestWaspCliVerionDirectoryExists
-            ("Installing latest Wasp version should create the " ++ fromAbsDir waspInstallationDir ++ "/<version> directory"),
+            ("Installing latest Wasp version should create the $HOME/.local/share/wasp-lang/" ++ latestWaspVersion ++ " directory"),
           writeToStdErrOnFailureAndExit
             (assertWaspCliVerionDirectoryExists specificWaspVersion)
             "Installing multiple Wasp versions should not remove the previously installed versions directories",
@@ -55,11 +48,13 @@ waspInstallContainerTest = do
             "Installing already installed version should reuse the existing installation on the disk"
         ]
       where
+        -- We use $HOME because 'getHomeDirectory' would resolve outside of container.
         assertLatestWaspCliVerionDirectoryExists :: ShellCommandBuilder context ShellCommand
-        assertLatestWaspCliVerionDirectoryExists = return $ "[ -d " ++ fromAbsDir waspInstallationDir ++ latestWaspVersion ++ " ]"
+        assertLatestWaspCliVerionDirectoryExists = return $ "[ -d \"$HOME/.local/share/wasp-lang/" ++ latestWaspVersion ++ "\" ]"
 
+        -- We use $HOME because 'getHomeDirectory' would resolve outside of container.
         assertWaspCliVerionDirectoryExists :: SV.Version -> ShellCommandBuilder context ShellCommand
-        assertWaspCliVerionDirectoryExists version = return $ "[ -d " ++ fromAbsDir waspInstallationDir ++ show version ++ " ]"
+        assertWaspCliVerionDirectoryExists version = return $ "[ -d \"$HOME/.local/share/wasp-lang/" ++ show version ++ "\" ]"
 
-        waspInstallationDir = homeDir </> waspInstallationDirInHomeDir
+        specificWaspVersion :: SV.Version
         specificWaspVersion = SV.Version 0 17 0
