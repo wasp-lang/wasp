4332f30cd68b8c43e95d6b1243536f828d688fc8
Carlos Precioso <carlos@wasp-lang.dev>
2025-11-03 14:47:07 +0100

    Use $

--- waspc/src/Wasp/Generator/Valid/TsConfig.hs
+++ waspc/src/Wasp/Generator/Valid/TsConfig.hs
@@ -21,35 +21,35 @@ validateSrcTsConfig config =
     validateFile =
       withFileName "tsconfig.json" $
         validateAll_
-          [ valueOfField "include" T.include (eqJust ["src"]),
+          [ valueOfField "include" T.include $ eqJust ["src"],
             valueOfField "compilerOptions" T.compilerOptions validateCompilerOptions
           ]
 
     validateCompilerOptions =
       validateAll_
-        [ valueOfField "module" T._module (eqJust "esnext"),
-          valueOfField "target" T.target (eqJust "esnext"),
+        [ valueOfField "module" T._module $ eqJust "esnext",
+          valueOfField "target" T.target $ eqJust "esnext",
           -- Since Wasp ends up bundling the user code, `bundler` is the most
           -- appropriate `moduleResolution` option.
-          valueOfField "moduleResolution" T.moduleResolution (eqJust "bundler"),
-          valueOfField "moduleDetection" T.moduleDetection (eqJust "force"),
+          valueOfField "moduleResolution" T.moduleResolution $ eqJust "bundler",
+          valueOfField "moduleDetection" T.moduleDetection $ eqJust "force",
           -- `isolatedModules` prevents users from using features that don't work
           -- with transpilers and would fail when Wasp bundles the code with rollup
           -- (e.g., const enums)
-          valueOfField "isolatedModules" T.isolatedModules (eqJust True),
-          valueOfField "jsx" T.jsx (eqJust "preserve"),
-          valueOfField "strict" T.strict (eqJust True),
-          valueOfField "esModuleInterop" T.esModuleInterop (eqJust True),
-          valueOfField "lib" T.lib (eqJust ["dom", "dom.iterable", "esnext"]),
-          valueOfField "allowJs" T.allowJs (eqJust True),
+          valueOfField "isolatedModules" T.isolatedModules $ eqJust True,
+          valueOfField "jsx" T.jsx $ eqJust "preserve",
+          valueOfField "strict" T.strict $ eqJust True,
+          valueOfField "esModuleInterop" T.esModuleInterop $ eqJust True,
+          valueOfField "lib" T.lib $ eqJust ["dom", "dom.iterable", "esnext"],
+          valueOfField "allowJs" T.allowJs $ eqJust True,
           -- Wasp internally uses TypeScript's project references to compile the
           -- code. Referenced projects may not disable emit, so we must specify an
           -- `outDir`.
-          valueOfField "outDir" T.outDir (eqJust ".wasp/out/user"),
+          valueOfField "outDir" T.outDir $ eqJust ".wasp/out/user",
           -- The composite flag is required because Wasp uses project references
           -- (i.e., web app and server reference user code as a subproject)
-          valueOfField "composite" T.composite (eqJust True),
-          valueOfField "skipLibCheck" T.skipLibCheck (eqJust True)
+          valueOfField "composite" T.composite $ eqJust True,
+          valueOfField "skipLibCheck" T.skipLibCheck $ eqJust True
         ]
 
 eqJust :: (Eq a, Show a) => a -> Maybe a -> Validation ()
