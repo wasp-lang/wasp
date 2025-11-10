f5ae87aad190712ed4643f9ff92e38fc37f11eaf
Carlos Precioso <carlos@wasp-lang.dev>
2025-11-10 17:29:33 +0100

    Split StarterTemplates to AvailableTemplates

---
+++ waspc/cli/src/Wasp/Cli/Command/CreateNewProject/AvailableTemplates.hs
@@ -0,0 +1,113 @@
+{-# LANGUAGE TupleSections #-}
+
+module Wasp.Cli.Command.CreateNewProject.AvailableTemplates
+  ( availableStarterTemplates,
+    defaultStarterTemplate,
+  )
+where
+
+import StrongPath (reldir)
+import qualified System.FilePath as FP
+import Wasp.Cli.Command.CreateNewProject.StarterTemplates (DirBasedTemplateMetadata (..), StarterTemplate (..))
+import qualified Wasp.Cli.GithubRepo as GhRepo
+import Wasp.Util.Terminal (styleCode, styleText)
+
+availableStarterTemplates :: [StarterTemplate]
+availableStarterTemplates =
+  [ basicStarterTemplate,
+    minimalStarterTemplate,
+    openSaasStarterTemplate,
+    AiGeneratedStarterTemplate
+  ]
+
+defaultStarterTemplate :: StarterTemplate
+defaultStarterTemplate = basicStarterTemplate
+
+{- HLINT ignore minimalStarterTemplate "Redundant $" -}
+
+minimalStarterTemplate :: StarterTemplate
+minimalStarterTemplate =
+  BundledStarterTemplate $
+    DirBasedTemplateMetadata
+      { _path = [reldir|minimal|],
+        _name = "minimal",
+        _description = "A minimal starter template that features just a single page.",
+        _buildStartingInstructions = \projectDirName ->
+          unlines
+            [ styleText $ "To run your new app, do:",
+              styleCode $ "    cd " <> projectDirName,
+              styleCode $ "    wasp start"
+            ]
+      }
+
+{- HLINT ignore basicStarterTemplate "Redundant $" -}
+
+basicStarterTemplate :: StarterTemplate
+basicStarterTemplate =
+  BundledStarterTemplate $
+    DirBasedTemplateMetadata
+      { _path = [reldir|basic|],
+        _name = "basic",
+        _description = "A basic starter template designed to help you get up and running quickly. It features examples covering the most common use cases.",
+        _buildStartingInstructions = \projectDirName ->
+          unlines
+            [ styleText $ "To run your new app, do:",
+              styleCode $ "    cd " <> projectDirName,
+              styleCode $ "    wasp db migrate-dev",
+              styleCode $ "    wasp start",
+              styleText $ "",
+              styleText $ "Check the " <> styleCode "README.md" <> " for additional guidance!"
+            ]
+      }
+
+{- HLINT ignore openSaasStarterTemplate "Redundant $" -}
+
+openSaasStarterTemplate :: StarterTemplate
+openSaasStarterTemplate =
+  GhRepoReleaseArchiveTemplate
+    ( GhRepo.GithubRepoRef
+        { GhRepo._repoOwner = "wasp-lang",
+          GhRepo._repoName = "open-saas",
+          GhRepo._repoReferenceName = waspVersionTemplateGitTag
+        }
+    )
+    "template.tar.gz"
+    ( DirBasedTemplateMetadata
+        { _name = "saas",
+          _description =
+            "Everything a SaaS needs! Comes with Auth, ChatGPT API, Tailwind, Stripe payments and more."
+              <> " Check out https://opensaas.sh/ for more details.",
+          -- We assume that the archive contains files at its root.
+          _path = [reldir|.|],
+          _buildStartingInstructions = \projectDirName ->
+            unlines
+              [ styleText $ "To run your new app, follow the instructions below:",
+                styleText $ "",
+                styleText $ "  1. Position into app's root directory:",
+                styleCode $ "    cd " <> projectDirName FP.</> "app",
+                styleText $ "",
+                styleText $ "  2. Run the development database (and leave it running):",
+                styleCode $ "    wasp db start",
+                styleText $ "",
+                styleText $ "  3. Open new terminal window (or tab) in that same dir and continue in it.",
+                styleText $ "",
+                styleText $ "  4. Apply initial database migrations:",
+                styleCode $ "    wasp db migrate-dev",
+                styleText $ "",
+                styleText $ "  5. Create initial dot env file from the template:",
+                styleCode $ "    cp .env.server.example .env.server",
+                styleText $ "",
+                styleText $ "  6. Last step: run the app!",
+                styleCode $ "    wasp start",
+                styleText $ "",
+                styleText $ "Check the README for additional guidance and the link to docs!"
+              ]
+        }
+    )
+
+-- NOTE: As version of Wasp CLI changes, so we should update this tag name here,
+--   and also create it on gh repos of templates.
+--   By tagging templates for each version of Wasp CLI, we ensure that each release of
+--   Wasp CLI uses correct version of templates, that work with it.
+waspVersionTemplateGitTag :: String
+waspVersionTemplateGitTag = "wasp-v0.19-template"
