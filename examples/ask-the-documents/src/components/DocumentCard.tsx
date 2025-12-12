ae748e5fc437b7b47b64044606b38b997c7a8e90
Filip Sodić <filip.sodic@fer.hr>
2025-12-12 10:16:17 +0100

    Format documents

--- examples/ask-the-documents/src/components/DocumentCard.tsx
+++ examples/ask-the-documents/src/components/DocumentCard.tsx
@@ -49,7 +49,9 @@ export function DocumentCard({
       <Divider />
       <CardBody>
         <ScrollShadow className="max-h-[200px]">
-          <div className="markdown"><Markdown>{document.content}</Markdown></div>
+          <div className="markdown">
+            <Markdown>{document.content}</Markdown>
+          </div>
         </ScrollShadow>
       </CardBody>
       {footerContent && (
