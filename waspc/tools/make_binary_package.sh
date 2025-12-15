03e872e5ae9f4e3fa7c1f2469c43cc65d215ac53
Carlos Precioso <carlos@wasp-lang.dev>
2025-12-15 18:01:57 +0100

    Comments and such

--- waspc/tools/make_binary_package.sh
+++ waspc/tools/make_binary_package.sh
@@ -18,12 +18,18 @@ CABAL_PROJECT_ROOT_PATH="$(cabal list-bin wasp-cli | sed s/\\/dist-newstyle.*//)
 WASP_BINARY_PATH="$(cabal list-bin wasp-cli)"
 cp "$WASP_BINARY_PATH" "$TMP_DIR/wasp-bin"
 
-cabal sdist --list-only \
-  | grep "^./data" \
-  | while read -r data_file_path; do
-    mkdir -p "$TMP_DIR/$(dirname "$data_file_path")"
-    cp "$CABAL_PROJECT_ROOT_PATH/$data_file_path" "$TMP_DIR/$data_file_path"
-  done
+CABAL_DATA_FILE_PATHS=$(
+  # First we list all the files declared in the .cabal file
+  # Then we only take the ones in the "data" directory
+  cabal sdist --list-only | grep "^./data/"
+)
+for data_file_path in $CABAL_DATA_FILE_PATHS; do
+  source_path="$CABAL_PROJECT_ROOT_PATH/$data_file_path"
+  dest_path="$TMP_DIR/$data_file_path"
+
+  mkdir -p "$(dirname "$dest_path")" # Make sure the parent directories in the output exist.
+  cp "$source_path" "$dest_path"
+done
 
 tar -czf "$DST" -C "$TMP_DIR" .
 
