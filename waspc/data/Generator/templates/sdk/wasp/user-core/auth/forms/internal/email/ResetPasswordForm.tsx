b80c2e93706a37cb36976827e14a54cfaec16f1e
Franjo Mindek <franjo.mindek@gmail.com>
2025-12-16 17:27:09 +0100

    revert

--- waspc/data/Generator/templates/sdk/wasp/user-core/auth/forms/internal/email/ResetPasswordForm.tsx
+++ waspc/data/Generator/templates/sdk/wasp/user-core/auth/forms/internal/email/ResetPasswordForm.tsx
@@ -1,7 +1,7 @@
 import { useContext } from 'react'
 import { useForm } from 'react-hook-form'
-import { useLocation } from 'react-router-dom'
 import { resetPassword } from '../../../email/actions/passwordReset'
+import { useLocation } from 'react-router-dom'
 import { Form, FormItemGroup, FormLabel, FormInput, SubmitButton, FormError } from '../Form'
 import { AuthContext } from '../../Auth'
 
