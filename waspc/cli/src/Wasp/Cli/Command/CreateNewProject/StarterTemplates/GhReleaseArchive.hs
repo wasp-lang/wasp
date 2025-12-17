39d8081f3ade1ff2c7d9c700dc4fcc84c89ccd65
Carlos Precioso <carlos@wasp-lang.dev>
2025-12-17 17:52:49 +0100

    Add URL check

--- waspc/cli/src/Wasp/Cli/Command/CreateNewProject/StarterTemplates/GhReleaseArchive.hs
+++ waspc/cli/src/Wasp/Cli/Command/CreateNewProject/StarterTemplates/GhReleaseArchive.hs
@@ -3,16 +3,24 @@ module Wasp.Cli.Command.CreateNewProject.StarterTemplates.GhReleaseArchive
   )
 where
 
+import Control.Monad (unless)
 import Control.Monad.IO.Class (liftIO)
+import Data.List (intercalate)
 import StrongPath (Abs, Dir, Dir', Path', Rel')
 import Wasp.Cli.Command (Command)
 import Wasp.Cli.Command.CreateNewProject.Common (throwProjectCreationError)
 import Wasp.Cli.Command.CreateNewProject.ProjectDescription (NewProjectAppName, NewProjectName)
 import Wasp.Cli.Command.CreateNewProject.StarterTemplates.Templating (replaceTemplatePlaceholdersInTemplateFiles)
-import Wasp.Cli.GithubRepo (GithubReleaseArchiveName, GithubRepoRef, fetchFolderFromGithubReleaseArchiveToDisk)
+import Wasp.Cli.GithubRepo
+  ( GithubReleaseArchiveName,
+    GithubRepoRef (_repoName, _repoOwner),
+    checkGitHubReleaseExists,
+    fetchFolderFromGithubReleaseArchiveToDisk,
+  )
 import Wasp.Project (WaspProjectDir)
 
 createProjectOnDiskFromGhReleaseArchiveTemplate ::
+  String ->
   Path' Abs (Dir WaspProjectDir) ->
   NewProjectName ->
   NewProjectAppName ->
@@ -20,7 +28,11 @@ createProjectOnDiskFromGhReleaseArchiveTemplate ::
   GithubReleaseArchiveName ->
   Path' Rel' Dir' ->
   Command ()
-createProjectOnDiskFromGhReleaseArchiveTemplate absWaspProjectDir projectName appName ghRepoRef assetName templatePathInRepo = do
+createProjectOnDiskFromGhReleaseArchiveTemplate templateName absWaspProjectDir projectName appName ghRepoRef assetName templatePathInRepo = do
+  releaseExists <- liftIO (checkGitHubReleaseExists ghRepoRef)
+
+  unless releaseExists throwOutdatedTagError
+
   fetchTemplateFromGhToWaspProjectDir
     >>= either throwProjectCreationError (const replaceTemplatePlaceholders)
   where
@@ -29,3 +41,18 @@ createProjectOnDiskFromGhReleaseArchiveTemplate absWaspProjectDir projectName ap
 
     replaceTemplatePlaceholders =
       liftIO $ replaceTemplatePlaceholdersInTemplateFiles appName projectName absWaspProjectDir
+
+    throwOutdatedTagError =
+      throwProjectCreationError $
+        unlines
+          [ "Template " ++ templateName ++ " doesn't yet have a version compatible with the current Wasp version.",
+            "You can try again with an older Wasp version, or choose a different template.",
+            "",
+            "If you want to use an older Wasp version:",
+            "Visit " ++ releasesUrl ++ " to see available releases,",
+            "and install the Wasp version that matches the latest release tag available by running:",
+            "  curl -sSL https://get.wasp.sh/installer.sh | sh -s -- -v x.y.z",
+            "Then you can try creating your project again."
+          ]
+
+    releasesUrl = intercalate "/" ["https://github.com", _repoOwner ghRepoRef, _repoName ghRepoRef, "releases"]
--- waspc/cli/src/Wasp/Cli/GithubRepo.hs
+++ waspc/cli/src/Wasp/Cli/GithubRepo.hs
@@ -3,6 +3,7 @@ module Wasp.Cli.GithubRepo where
 import Data.List (intercalate)
 import StrongPath (Abs, Dir, Path', Rel)
 import Wasp.Cli.Archive (fetchArchiveAndCopySubdirToDisk)
+import Wasp.Cli.Util.Http (checkUrlExists)
 
 data GithubRepoRef = GithubRepoRef
   { _repoOwner :: GithubRepoOwner,
@@ -19,6 +20,19 @@ type GithubRepoName = String
 type GithubRepoReferenceName = String
 
 type GithubReleaseArchiveName = String
+
+checkGitHubReleaseExists :: GithubRepoRef -> IO Bool
+checkGitHubReleaseExists githubRepoRef =
+  checkUrlExists $ getGithubReleaseURL githubRepoRef
+  where
+    getGithubReleaseURL :: GithubRepoRef -> String
+    getGithubReleaseURL
+      GithubRepoRef
+        { _repoName = repoName,
+          _repoOwner = repoOwner,
+          _repoTagName = repoTagName
+        } =
+        intercalate "/" ["https://github.com", repoOwner, repoName, "releases", "tag", repoTagName]
 
 fetchFolderFromGithubReleaseArchiveToDisk ::
   GithubRepoRef ->
