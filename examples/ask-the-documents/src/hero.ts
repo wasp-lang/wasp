5a8f3dd081321cbbcce9d874ada3414a97144fed
Carlos Precioso <carlos@wasp-lang.dev>
2026-01-02 11:17:59 +0100

    Migrate ATD

---
+++ examples/ask-the-documents/src/hero.ts
@@ -0,0 +1,6 @@
+import { heroui } from "@heroui/react";
+
+// This export relies on a hidden type of a transitive dependency, so TypeScript
+// cannot properly export the inferred type here. We don't use ever this export
+// directly (we just pass it to Tailwind), so it's fine to cast it to any.
+export default heroui() as any;
