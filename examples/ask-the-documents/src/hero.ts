4ac213d3e9c118f68b2266ddf123b5411d136a9e
Carlos Precioso <carlos@wasp-lang.dev>
2026-01-05 16:09:56 +0100

    Better comment

--- examples/ask-the-documents/src/hero.ts
+++ examples/ask-the-documents/src/hero.ts
@@ -1,6 +1,8 @@
 import { heroui } from "@heroui/react";
 
 // This export relies on a hidden type of a transitive dependency, so TypeScript
-// cannot properly export the inferred type here. We don't use ever this export
-// directly (we just pass it to Tailwind), so it's fine to cast it to any.
-export default heroui() as any;
+// cannot properly export the inferred type here. It complains in `composite:
+// true` mode.
+// https://github.com/heroui-inc/heroui/issues/6083
+// As a workaround, we cast to `unknown`.
+export default heroui() as unknown;
