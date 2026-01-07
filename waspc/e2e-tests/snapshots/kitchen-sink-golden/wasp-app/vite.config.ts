591b935f56c571a17b41be18d282cff4d52cd8f9
Carlos Precioso <carlos@wasp-lang.dev>
2026-01-07 12:01:08 +0100

    Update snapshot

--- waspc/e2e-tests/snapshots/kitchen-sink-golden/wasp-app/vite.config.ts
+++ waspc/e2e-tests/snapshots/kitchen-sink-golden/wasp-app/vite.config.ts
@@ -1,9 +1,12 @@
 import tailwindcss from "@tailwindcss/vite";
-import { defineConfig } from "vite";
+import { defineConfig } from "vitest/config";
 
 export default defineConfig({
   server: {
     open: false,
   },
   plugins: [tailwindcss()],
+  test: {
+    exclude: ["./e2e-tests/**"],
+  },
 });
