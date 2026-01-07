42bcbf218deda3e72bed180e1620f1834cfbb2f3
Carlos Precioso <carlos@wasp-lang.dev>
2026-01-07 10:01:58 +0100

    Bump version

--- examples/waspello/main.wasp.ts
+++ examples/waspello/main.wasp.ts
@@ -2,7 +2,7 @@ import { ActionConfig, App, ExtImport } from "wasp-config";
 
 const app = new App("waspello", {
   title: "Waspello",
-  wasp: { version: "^0.20.0" },
+  wasp: { version: "^0.21.0" },
 });
 
 app.auth({
