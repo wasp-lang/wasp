19f13921bd10d7f0f442fce04157cf8554f7220e
Franjo Mindek <franjo.mindek@gmail.com>
2026-01-15 17:29:56 +0100

    imports

--- waspc/src/Wasp/Generator/SdkGenerator/Core/ClientG.hs
+++ waspc/src/Wasp/Generator/SdkGenerator/Core/ClientG.hs
@@ -3,18 +3,11 @@ module Wasp.Generator.SdkGenerator.Core.ClientG
   )
 where
 
-import Data.Aeson (object, (.=))
 import StrongPath (relfile)
 import Wasp.AppSpec (AppSpec)
-import qualified Wasp.AppSpec.App as AS.App
-import qualified Wasp.AppSpec.App.Client as AS.App.Client
-import Wasp.AppSpec.Valid (getApp)
 import Wasp.Generator.FileDraft (FileDraft)
 import Wasp.Generator.Monad (Generator)
-import Wasp.Generator.SdkGenerator.Core.Common (mkTmplFd, mkTmplFdWithData)
-import Wasp.Generator.SdkGenerator.JsImport (extImportToImportJson)
-import qualified Wasp.Generator.ServerGenerator.Common as Server
-import qualified Wasp.Generator.WebAppGenerator.Common as WebApp
+import Wasp.Generator.SdkGenerator.Core.Common (mkTmplFd)
 
 genClient :: AppSpec -> Generator [FileDraft]
 genClient _spec =
