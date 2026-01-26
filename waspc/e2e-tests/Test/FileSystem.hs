775b2a6e43913116376f78d22e75686da6f1e108
Franjo Mindek <franjo.mindek@gmail.com>
2026-01-26 15:17:05 +0100

    forgot to commit

--- waspc/e2e-tests/Test/FileSystem.hs
+++ waspc/e2e-tests/Test/FileSystem.hs
@@ -1,46 +1,29 @@
 module Test.FileSystem
   ( TestDir,
-    testDirInE2eTestsDir,
     getTestDir,
-    TestWaspProjectDir,
-    asWaspProjectDir,
-    testWaspProjectDirInTestDir,
-    testsTempDirInTestsDir,
-    getTestsTempDir,
+    testsOutputsDirInTestsDir,
+    getTestOutputsDir,
   )
 where
 
 import Data.Maybe (fromJust)
 import FileSystem (E2eTestsDir, getE2eTestsDir)
-import StrongPath (Abs, Dir, Path, Path', Rel, castDir, parseRelDir, reldir, (</>))
-import Wasp.Project.Common (WaspProjectDir)
+import StrongPath (Abs, Dir, Path', Rel, parseRelDir, reldir, (</>))
 
--- | The directory where all e2e tests' artifacts are stored.
-data TestsTempDir
-
--- | The directory inside a 'TestsTempDir' where artifacts for a specific e2e test are stored.
--- It is named after the e2e test name.
+-- | A directory inside the 'TestOutputsDir' where outputs of a specific e2e test are stored.
 data TestDir
 
--- | The Wasp app directory inside of a 'TestDir'.
-data TestWaspProjectDir
-
--- | Converts a 'TestWaspProjectDir' to a 'Wasp.Project.Common.WaspProjectDir'.
--- This is safe because every e2e Wasp project directory is also a Wasp project directory.
-asWaspProjectDir :: Path s a (Dir TestWaspProjectDir) -> Path s a (Dir WaspProjectDir)
-asWaspProjectDir = castDir
-
-getTestsTempDir :: IO (Path' Abs (Dir TestsTempDir))
-getTestsTempDir = (</> testsTempDirInTestsDir) <$> getE2eTestsDir
+-- | The directory where all 'TestDir' are created.
+data TestOutputsDir
 
 getTestDir :: String -> IO (Path' Abs (Dir TestDir))
-getTestDir testName = (</> testDirInE2eTestsDir testName) <$> getE2eTestsDir
+getTestDir testName = (</> (testsOutputsDirInTestsDir </> testDirInTestOutputsDir testName)) <$> getE2eTestsDir
 
-testDirInE2eTestsDir :: String -> Path' (Rel E2eTestsDir) (Dir TestDir)
-testDirInE2eTestsDir testName = testsTempDirInTestsDir </> (fromJust . parseRelDir $ testName)
+getTestOutputsDir :: IO (Path' Abs (Dir TestOutputsDir))
+getTestOutputsDir = (</> testsOutputsDirInTestsDir) <$> getE2eTestsDir
 
-testsTempDirInTestsDir :: Path' (Rel E2eTestsDir) (Dir TestsTempDir)
-testsTempDirInTestsDir = [reldir|Test/temp|]
+testDirInTestOutputsDir :: String -> Path' (Rel TestOutputsDir) (Dir TestDir)
+testDirInTestOutputsDir = fromJust . parseRelDir
 
-testWaspProjectDirInTestDir :: String -> Path' (Rel TestDir) (Dir TestWaspProjectDir)
-testWaspProjectDirInTestDir testWaspProjectName = fromJust . parseRelDir $ testWaspProjectName
+testsOutputsDirInTestsDir :: Path' (Rel E2eTestsDir) (Dir TestOutputsDir)
+testsOutputsDirInTestsDir = [reldir|Test/test-outputs|]
