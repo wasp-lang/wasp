8e7f339b52816731702a81f01916ebe99da0f738
Filip Sodić <filip.sodic@fer.hr>
2026-01-27 10:35:56 +0100

    Change `react-router-dom` to `react-router` (#3658)

--- waspc/data/Cli/starters/basic/src/auth/email/PasswordResetPage.tsx
+++ waspc/data/Cli/starters/basic/src/auth/email/PasswordResetPage.tsx
@@ -1,4 +1,4 @@
-import { Link } from "react-router-dom";
+import { Link } from "react-router";
 import { ResetPasswordForm } from "wasp/client/auth";
 import { AuthLayout } from "../AuthLayout";
 
--- waspc/e2e-tests/snapshots/kitchen-sink-golden/wasp-app/.wasp/out/sdk/wasp/src/pages/CatchAllPage.tsx
+++ waspc/e2e-tests/snapshots/kitchen-sink-golden/wasp-app/.wasp/out/sdk/wasp/src/pages/CatchAllPage.tsx
@@ -1,4 +1,4 @@
-import { useLocation } from "react-router-dom";
+import { useLocation } from "react-router";
 import { FeatureContainer } from "../components/FeatureContainer";
 
 export function CatchAllPage() {
