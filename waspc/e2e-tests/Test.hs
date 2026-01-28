223430118517ef66c1dc0e72368603c686fcc900
Franjo Mindek <franjo.mindek@gmail.com>
2026-01-28 18:05:08 +0100

    init

--- waspc/e2e-tests/Test.hs
+++ waspc/e2e-tests/Test.hs
@@ -5,17 +5,18 @@
 module Test
   ( Test (..),
     TestCase (..),
-    runTest,
+    runTests,
   )
 where
 
+import Control.Concurrent.Async (mapConcurrently)
 import Data.Maybe (fromJust)
 import FileSystem (TestCaseDir, getTestCaseDir, getTestOutputsDir)
 import ShellCommands (ShellCommand, ShellCommandBuilder, TestContext (..), WaspProjectContext (..), buildShellCommand, (~&&))
 import StrongPath (Abs, Dir, Path', fromAbsDir, parseRelDir, (</>))
 import System.Exit (ExitCode (..))
 import System.Process (CreateProcess (..), StdStream (..), callCommand, createProcess, shell, waitForProcess)
-import Test.Hspec (Spec, expectationFailure, it, runIO)
+import Test.Hspec (Spec, expectationFailure, it)
 import Test.Tasty (TestTree)
 import Test.Tasty.Hspec (testSpec)
 
@@ -30,18 +31,49 @@ data TestCase = TestCase
     shellCommandBuilder :: ShellCommandBuilder TestContext [ShellCommand]
   }
 
--- | Runs a 'Test' by executing all test cases' shell commands and then checking their exit code.
--- Each test case runs in its own isolated directory.
-runTest :: Test -> IO TestTree
-runTest test = do
+-- | Prepares a list of 'Test's in parallel (executing shell commands and capturing exit codes),
+--  and then creates test trees that check the results.
+runTests :: [Test] -> IO [TestTree]
+runTests tests = do
   testOutputsDir <- getTestOutputsDir
   callCommand $ "mkdir -p " ++ fromAbsDir testOutputsDir
 
-  testSpec test.name (mapM_ (createAndExecuteTestCase test.name) test.testCases)
+  mapConcurrently prepareTest tests >>= mapM createTestTree
 
-createAndExecuteTestCase :: String -> TestCase -> Spec
-createAndExecuteTestCase testName testCase = do
-  testCaseDir <- runIO $ getTestCaseDir testName testCase.name
+-- | Data needed to create a test tree.
+data TestData = TestData
+  { name :: String,
+    testCaseResults :: [TestCaseResult]
+  }
+
+-- | Result of executing a single test case.
+data TestCaseResult = TestCaseResult
+  { name :: String,
+    command :: String,
+    exitCode :: ExitCode
+  }
+
+-- | Prepares a test by executing all its test cases sequentially and capturing results.
+prepareTest :: Test -> IO TestData
+prepareTest test = do
+  testCaseResults <- mapM (executeTestCase test.name) test.testCases
+  return TestData {name = test.name, testCaseResults}
+
+createTestTree :: TestData -> IO TestTree
+createTestTree testData =
+  testSpec testData.name (mapM_ checkTestCaseResult testData.testCaseResults)
+  where
+    checkTestCaseResult :: TestCaseResult -> Spec
+    checkTestCaseResult result =
+      it result.name $ do
+        case result.exitCode of
+          ExitFailure _ -> expectationFailure $ "Command failed: " ++ result.command
+          ExitSuccess -> return ()
+
+-- | Executes a single test case and captures its result.
+executeTestCase :: String -> TestCase -> IO TestCaseResult
+executeTestCase testName testCase = do
+  testCaseDir <- getTestCaseDir testName testCase.name
   let waspProjectContext =
         WaspProjectContext
           { waspProjectDir = testCaseDir </> (fromJust . parseRelDir $ "wasp-app"),
@@ -50,24 +82,20 @@ createAndExecuteTestCase testName testCase = do
       testContext = TestContext {testCaseDir, waspProjectContext}
       testCaseCommand = foldr1 (~&&) $ buildShellCommand testContext testCase.shellCommandBuilder
 
-  runIO $ setupTestCase testCaseDir
-  runIO $ putStrLn $ "Executing test case: " ++ testName ++ "/" ++ testCase.name
-  runIO $ putStrLn $ "Command: " ++ testCaseCommand
+  setupTestCase testCaseDir
+  putStrLn $ "Executing test case: " ++ testName ++ "/" ++ testCase.name
+  putStrLn $ "Command: " ++ testCaseCommand
   (_, _, _, processHandle) <-
-    runIO $
-      createProcess
-        (shell testCaseCommand)
-          { cwd = Just $ fromAbsDir testCaseDir,
-            std_in = Inherit,
-            std_out = Inherit,
-            std_err = Inherit
-          }
-  exitCode <- runIO $ waitForProcess processHandle
+    createProcess
+      (shell testCaseCommand)
+        { cwd = Just $ fromAbsDir testCaseDir,
+          std_in = Inherit,
+          std_out = Inherit,
+          std_err = Inherit
+        }
+  exitCode <- waitForProcess processHandle
 
-  it testCase.name $ do
-    case exitCode of
-      ExitFailure _ -> expectationFailure $ "Command failed: " ++ testCaseCommand
-      ExitSuccess -> return ()
+  return TestCaseResult {name = testCase.name, command = testCaseCommand, exitCode}
 
 setupTestCase :: Path' Abs (Dir TestCaseDir) -> IO ()
 setupTestCase testCaseDir = do
