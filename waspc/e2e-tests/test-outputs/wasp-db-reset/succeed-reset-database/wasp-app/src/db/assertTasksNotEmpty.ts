713033df53f51012ba4c6802408878335ff6f46c
Franjo Mindek <franjo.mindek@gmail.com>
2026-01-28 15:46:44 +0100

    remove extras

--- waspc/e2e-tests/test-outputs/wasp-db-reset/succeed-reset-database/wasp-app/src/db/assertTasksNotEmpty.ts
+++
@@ -1,8 +0,0 @@
-import { prisma } from 'wasp/server'
-
-export async function assertTasksNotEmpty() {
-  const taskCount = await prisma.task.count()
-  if (taskCount === 0) {
-    throw new Error('Expected tasks table to have data, but it was empty')
-  }
-}
--- waspc/e2e-tests/test-outputs/wasp-new/create-basic/wasp-app/src/tags/components/colors.ts
+++
@@ -1,8 +0,0 @@
-export function generateBrightColor(
-  hue = Math.floor(Math.random() * 360),
-): string {
-  const saturation = 90;
-  const lightness = 80;
-
-  return `hsl(${hue}, ${saturation}%, ${lightness}%)`;
-}
