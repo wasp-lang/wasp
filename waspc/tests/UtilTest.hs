d084f11d0355f64821a75f8043277fde6296d44c
Filip Sodić <filip.sodic@fer.hr>
2026-01-29 11:15:44 +0100

    Clean up

--- waspc/tests/UtilTest.hs
+++ waspc/tests/UtilTest.hs
@@ -227,26 +227,23 @@ spec_isOlderThanNHours :: Spec
 spec_isOlderThanNHours = do
   describe "isOlderThanNHours" $ do
     it "returns True when time is older than N hours" $ do
-      now <- T.getCurrentTime
-      let _3h0m1s_ago = T.addUTCTime (negate $ 3 * 3600 + 1) now
+      _3h0m1s_ago <- secondsFromNow (negate $ 3 * 3600 + 1)
       isOlderThanNHours 3 _3h0m1s_ago `shouldReturn` True
 
     it "returns False when time is newer than N hours" $ do
-      now <- T.getCurrentTime
-      let _2h59m59s_ago = T.addUTCTime (negate $ 2 * 3600 + 59 * 60 + 59) now
+      _2h59m59s_ago <- secondsFromNow (negate $ 2 * 3600 + 59 * 60 + 59)
       isOlderThanNHours 3 _2h59m59s_ago `shouldReturn` False
 
     it "returns False when time is in the future" $ do
-      now <- T.getCurrentTime
-      let oneSecondFromNow = T.addUTCTime 1 now
+      oneSecondFromNow <- secondsFromNow 1
       isOlderThanNHours 1 oneSecondFromNow `shouldReturn` False
 
     it "returns True for past time when N is 0" $ do
-      now <- T.getCurrentTime
-      let oneSecondAgo = T.addUTCTime (negate 1) now
+      oneSecondAgo <- secondsFromNow (negate 1)
       isOlderThanNHours 0 oneSecondAgo `shouldReturn` True
 
     it "returns False for future time when N is 0" $ do
-      now <- T.getCurrentTime
-      let oneSecondFromNow = T.addUTCTime 1 now
+      oneSecondFromNow <- secondsFromNow 1
       isOlderThanNHours 0 oneSecondFromNow `shouldReturn` False
+  where
+    secondsFromNow nSeconds = T.addUTCTime nSeconds <$> T.getCurrentTime
