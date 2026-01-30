507ce28a79f66f07da8eae38e32806a34311dce0
Franjo Mindek <84568328+FranjoMindek@users.noreply.github.com>
2026-01-30 13:57:51 +0100

    Parallelize e2e tests creation (#3678)

--- waspc/e2e-tests/Main.hs
+++ waspc/e2e-tests/Main.hs
@@ -1,6 +1,6 @@
 import SnapshotTest (runSnapshotTest)
 import System.Info (os)
-import Test (runTest)
+import Test (testTreeFromTest)
 import Test.Tasty (TestTree, defaultMain, testGroup)
 import Tests.SnapshotTests.KitchenSinkSnapshotTest (kitchenSinkSnapshotTest)
 import Tests.SnapshotTests.WaspBuildSnapshotTest (waspBuildSnapshotTest)
@@ -41,9 +41,9 @@ e2eTests = do
         waspMigrateSnapshotTest,
         kitchenSinkSnapshotTest
       ]
-  tests <-
+  testTrees <-
     mapM
-      runTest
+      testTreeFromTest
       [ -- general Wasp commads
         waspNewTest,
         waspTelemetryTest,
@@ -74,5 +74,5 @@ e2eTests = do
     testGroup
       "E2E tests"
       [ testGroup "Snapshot Tests" snapshotTests,
-        testGroup "Tests" tests
+        testGroup "Tests" testTrees
       ]
