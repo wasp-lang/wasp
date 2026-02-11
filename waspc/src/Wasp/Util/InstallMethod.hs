8c2e83faeda0bcb255a1a16338b33c0aa47e66b3
Carlos Precioso <carlos@wasp-lang.dev>
2026-02-11 14:09:40 +0100

    Remove unnecessary switch

--- waspc/src/Wasp/Util/InstallMethod.hs
+++ waspc/src/Wasp/Util/InstallMethod.hs
@@ -1,46 +1,20 @@
 module Wasp.Util.InstallMethod
-  ( CliInstallMethod (..),
-    waspCliInstallMethod,
-    getInstallationCommand,
-    getInstallationCommandFromInstallMethod,
-    npmPackageName,
+  ( getInstallationCommand,
+    uninstallationCommand,
   )
 where
 
-import System.Environment (lookupEnv)
-import System.IO.Unsafe (unsafePerformIO)
+import Data.Maybe (fromMaybe)
 
-data CliInstallMethod
-  = BinaryInstaller
-  | NpmPackage
+getInstallationCommand :: Maybe String -> String
+getInstallationCommand version =
+  "npm i -g " ++ npmPackageName ++ "@" ++ versionString
+  where
+    versionString = fromMaybe "latest" version
 
--- Keep the environment variable name and value in sync with
--- scripts/make-npm-packages/templates/main-package/bin.js
-installMethodEnvVarName :: String
-installMethodEnvVarName = "WASP_CLI_INSTALL_METHOD"
-
-{-# NOINLINE waspCliInstallMethod #-}
-waspCliInstallMethod :: CliInstallMethod
-waspCliInstallMethod =
-  -- We're using `unsafePerformIO` here to read the environment variable only once,
-  -- since we're treating it as a static value it's fine to treat this as a compiled-in
-  -- value.
-  case unsafePerformIO $ lookupEnv installMethodEnvVarName of
-    Just "npm" -> NpmPackage
-    _ -> BinaryInstaller
+uninstallationCommand :: String
+uninstallationCommand =
+  "npm uninstall -g " ++ npmPackageName
 
 npmPackageName :: String
 npmPackageName = "@wasp.sh/wasp-cli"
-
-getInstallationCommand :: Maybe String -> String
-getInstallationCommand = getInstallationCommandFromInstallMethod waspCliInstallMethod
-
-getInstallationCommandFromInstallMethod :: CliInstallMethod -> Maybe String -> String
-getInstallationCommandFromInstallMethod NpmPackage (Just version) =
-  "npm i -g " ++ npmPackageName ++ "@" ++ version
-getInstallationCommandFromInstallMethod NpmPackage Nothing =
-  "npm i -g " ++ npmPackageName ++ "@latest"
-getInstallationCommandFromInstallMethod BinaryInstaller (Just version) =
-  "curl -sSL https://get.wasp.sh/installer.sh | sh -s -- -v " ++ version
-getInstallationCommandFromInstallMethod BinaryInstaller Nothing =
-  "curl -sSL https://get.wasp.sh/installer.sh | sh -s"
