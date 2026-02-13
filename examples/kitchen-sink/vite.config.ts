a7699624e59a0e1e8c142b78c3d5fc50804e8dcb
Mihovil Ilakovac <mihovil@ilakovac.com>
2026-02-13 17:24:06 +0100

    Update Wasp config plugin to force certain options

--- examples/kitchen-sink/vite.config.ts
+++ examples/kitchen-sink/vite.config.ts
@@ -4,6 +4,10 @@ import { wasp } from "wasp/client/vite";
 
 export default defineConfig({
   server: {
+    // Keep this option to 4000, this makes sure our Vite option
+    // forcing works since e2e tests rely on client being served
+    // on port 3000.
+    port: 4000,
     open: false,
   },
   plugins: [wasp(), tailwindcss()],
