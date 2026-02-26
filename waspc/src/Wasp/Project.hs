0bbd55cf1c7dc41e796627976aab2ca47fbd4e9b
Filip Sodić <filip.sodic@fer.hr>
2026-02-26 12:38:44 +0100

    Install wasp-config from the .wasp dir

--- waspc/src/Wasp/Project.hs
+++ waspc/src/Wasp/Project.hs
@@ -11,6 +11,7 @@ module Wasp.Project
 where
 
 import Control.Arrow (ArrowChoice (left), first)
+import Control.Concurrent (newChan)
 import Data.List.NonEmpty (toList)
 import Data.Maybe (maybeToList)
 import Data.Text (Text)
@@ -19,6 +20,8 @@ import qualified Wasp.AppSpec as AS
 import Wasp.CompileOptions (CompileOptions (generatorWarningsFilter), sendMessage)
 import qualified Wasp.Generator as Generator
 import qualified Wasp.Generator.DockerGenerator as DockerGenerator
+import Wasp.Generator.NpmInstall (installProjectNpmDependencies)
+import Wasp.NodePackageFFI (InstallablePackage (WaspConfigPackage), ensurePackageAtInstallationPathInProject)
 import Wasp.Project.Analyze (analyzeWaspProject)
 import Wasp.Project.Common (CompileError, CompileWarning, WaspProjectDir)
 import qualified Wasp.Project.Env as Project.Env
@@ -29,17 +32,22 @@ compile ::
   CompileOptions ->
   IO ([CompileWarning], [CompileError])
 compile waspDir outDir options = do
-  compileWarningsAndErrors <-
-    analyzeWaspProject waspDir options >>= \case
-      (Left analyzerErrors, analyzerWarnings) -> return (analyzerWarnings, analyzerErrors)
-      (Right appSpec, analyzerWarnings) ->
-        first (<> analyzerWarnings) <$> generateCode appSpec outDir options
-  dotEnvWarnings <- maybeToList <$> Project.Env.warnIfTheDotEnvPresent waspDir
-  return $
-    mconcat
-      [ compileWarningsAndErrors,
-        (dotEnvWarnings, [])
-      ]
+  ensurePackageAtInstallationPathInProject waspDir WaspConfigPackage
+  chan <- newChan
+  installProjectNpmDependencies chan waspDir >>= \case
+    Left err -> return ([], [err])
+    Right () -> do
+      compileWarningsAndErrors <-
+        analyzeWaspProject waspDir options >>= \case
+          (Left analyzerErrors, analyzerWarnings) -> return (analyzerWarnings, analyzerErrors)
+          (Right appSpec, analyzerWarnings) ->
+            first (<> analyzerWarnings) <$> generateCode appSpec outDir options
+      dotEnvWarnings <- maybeToList <$> Project.Env.warnIfTheDotEnvPresent waspDir
+      return $
+        mconcat
+          [ compileWarningsAndErrors,
+            (dotEnvWarnings, [])
+          ]
 
 generateCode ::
   AS.AppSpec ->
