693b0761e33044035854cc7d8eb3b1d9b09cf3f2
Franjo Mindek <franjo.mindek@gmail.com>
2026-02-27 15:21:06 +0100

    simplify

--- waspc/tests/SemanticVersion/ComparatorSetTest.hs
+++ waspc/tests/SemanticVersion/ComparatorSetTest.hs
@@ -8,7 +8,13 @@ spec_SemanticVersion_ComparatorSet :: Spec
 spec_SemanticVersion_ComparatorSet = do
   describe "show" $ do
     it "shows single comparator comparator set" $ do
-      show (ComparatorSet $ NE.fromList [XRange [pv|1.2|]]) `shouldBe` "1.2"
+      show
+        ( ComparatorSet $
+            NE.fromList
+              [ XRange [pv|1.2|]
+              ]
+        )
+        `shouldBe` "1.2"
     it "shows multiple comparator comparator set" $ do
       show
         ( ComparatorSet $
@@ -21,42 +27,30 @@ spec_SemanticVersion_ComparatorSet = do
         `shouldBe` ">=1.2.0 <2.0.0 ^2.1.3"
 
   describe "versionBounds" $ do
-    it ">=1.0.0 <2.0.0 intersects to [1.0.0, 2.0.0)" $
-      versionBounds
-        ( ComparatorSet $
-            NE.fromList
-              [ PrimitiveComparator GreaterThanOrEqual [pv|1.0.0|],
-                PrimitiveComparator LessThan [pv|2.0.0|]
-              ]
-        )
-        `shouldBe` [vi| [1.0.0, 2.0.0) |]
-    it ">1.0.0 <=2.0.0 intersects to (1.0.0, 2.0.0]" $
-      versionBounds
-        ( ComparatorSet $
-            NE.fromList
-              [ PrimitiveComparator GreaterThan [pv|1.0.0|],
-                PrimitiveComparator LessThanOrEqual [pv|2.0.0|]
-              ]
-        )
-        `shouldBe` [vi| (1.0.0, 2.0.0] |]
+    let comps ~> expectedInterval =
+          let compSet = ComparatorSet (NE.fromList comps)
+           in it (show compSet) $ versionBounds compSet `shouldBe` expectedInterval
 
-  describe "isVersionInInterval" $ do
-    it ">=1.0.0 <2.0.0 matches versions in range" $
-      testComparatorSet
-        ( ComparatorSet $
-            NE.fromList
-              [ PrimitiveComparator GreaterThanOrEqual [pv|1.0.0|],
-                PrimitiveComparator LessThan [pv|2.0.0|]
-              ]
-        )
-        [ ([v|0.9.9|], False),
-          ([v|1.0.0|], True),
-          ([v|1.5.0|], True),
-          ([v|1.99.99|], True),
-          ([v|2.0.0|], False)
-        ]
-  where
-    testComparatorSet :: ComparatorSet -> [(Version, Bool)] -> Expectation
-    testComparatorSet compSet versionsWithResults =
-      map (\(ver, _) -> isVersionInInterval (versionBounds compSet) ver) versionsWithResults
-        `shouldBe` map snd versionsWithResults
+    -- Basic inclusive/exclusive combinations
+    [PrimitiveComparator GreaterThanOrEqual [pv|1.0.0|], PrimitiveComparator LessThan [pv|2.0.0|]]
+      ~> [vi| [1.0.0, 2.0.0) |]
+    [PrimitiveComparator GreaterThan [pv|1.0.0|], PrimitiveComparator LessThanOrEqual [pv|2.0.0|]]
+      ~> [vi| (1.0.0, 2.0.0] |]
+
+    -- Caret narrows an open upper bound
+    [PrimitiveComparator GreaterThanOrEqual [pv|1.0.0|], BackwardsCompatibleWith [pv|1.2.0|]]
+      ~> [vi| [1.2.0, 2.0.0) |]
+
+    -- Tilde narrows further than caret
+    [BackwardsCompatibleWith [pv|1.0.0|], ApproximatelyEquvivalentTo [pv|1.2.0|]]
+      ~> [vi| [1.2.0, 1.3.0) |]
+
+    -- Three comparators: the tightest wins on each side
+    [PrimitiveComparator GreaterThan [pv|0.5.0|], PrimitiveComparator LessThan [pv|3.0.0|], BackwardsCompatibleWith [pv|1.0.0|]]
+      ~> [vi| [1.0.0, 2.0.0) |]
+
+    -- Exclusive beats inclusive at the same version
+    [PrimitiveComparator GreaterThan [pv|1.0.0|], PrimitiveComparator GreaterThanOrEqual [pv|1.0.0|]]
+      ~> [vi| (1.0.0, inf) |]
+    [PrimitiveComparator LessThan [pv|2.0.0|], PrimitiveComparator LessThanOrEqual [pv|2.0.0|]]
+      ~> [vi| (inf, 2.0.0) |]
