c9fdf0e7edc77aa6241521d40823823a730f7742
Carlos Precioso <carlos@wasp-lang.dev>
2026-03-03 12:26:31 +0100

    Remove unused constants

--- waspc/data/Generator/libs/vite-ssr/src/common.ts
+++ waspc/data/Generator/libs/vite-ssr/src/common.ts
@@ -2,9 +2,6 @@ import { name as PLUGIN_NAME } from "../package.json" with { type: "json" };
 
 export { PLUGIN_NAME };
 
-export const CLIENT_APP_ENTRY_ID = `/${PLUGIN_NAME}/client-app-entry`;
-export const CLIENT_HTML_ENTRY_ID = "index.html";
-
 export interface Options {
   clientEntrySrc: string;
   ssrEntrySrc: string;
