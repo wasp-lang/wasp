d6f6617a357655651798fc25c7daae93d18ffa25
Franjo Mindek <franjo.mindek@gmail.com>
2026-03-09 15:48:51 +0100

    oops revert

--- waspc/e2e-tests/test-outputs/snapshots/kitchen-sink-golden/wasp-app/src/env.ts
+++ waspc/e2e-tests/test-outputs/snapshots/kitchen-sink-golden/wasp-app/src/env.ts
@@ -5,16 +5,12 @@ export const serverEnvValidationSchema = defineEnvValidationSchema(
   z.object({
     TEST_ENV_VAR: z.string({
       required_error: "TEST_ENV_VAR is required.",
-    }).min(50),
+    }),
   }),
 );
 
 export const clientEnvValidationSchema = defineEnvValidationSchema(
   z.object({
     REACT_APP_NAME: z.string().default("Kitchen Sink App"),
-    REACT_APP_MUST_BE_DEFINED: z.string({
-      required_error: "REACT_APP_MUST_BE_DEFINED string is required.",
-    }),
-    REACT_APP_MUST_BE_LONG: z.string().min(10),
   }),
 );
