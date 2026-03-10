58ce5c58c333c5034ecd7b16319a4cbcdbacef4e
Franjo Mindek <franjo.mindek@gmail.com>
2026-03-10 14:30:31 +0100

    typo

--- waspc/data/Generator/templates/sdk/wasp/types/index.ts
+++ waspc/data/Generator/templates/sdk/wasp/types/index.ts
@@ -1,26 +1,30 @@
 /**
  * The interfaces in this module are augmented by types from `.wasp/out/types/`.
  * They are agumented with user declarations types.
- * 
+ *
  * E.g., a user defined prisma client.
-*/
+ */
 
 export interface Registry {}
-export type FromRegistry<K extends string, Default> =
-K extends keyof Registry
-? Registry[K]
-: Default
-
-export interface CrudOverridesRegistry {}
-export type FromOperationsRegistry<Operation extends string, Default> =
-Operation extends keyof OperationsRegistry
-? OperationsRegistry[Operation]
-: Default
+export type FromRegistry<K extends string, Default> = K extends keyof Registry
+  ? Registry[K]
+  : Default;
 
 export interface OperationsRegistry {}
-export type FromCudOverridesRegistry<CrudName extends string, CrudOperation extends string, Default> =
-  CrudName extends keyof CrudOverridesRegistry
-    ? CrudOperation extends keyof CrudOverridesRegistry[CrudName]
-      ? CrudOverridesRegistry[CrudName][CrudOperation]
-      : Default
+export type FromOperationsRegistry<
+  Operation extends string,
+  Default,
+> = Operation extends keyof OperationsRegistry
+  ? OperationsRegistry[Operation]
+  : Default;
+
+export interface CrudOverridesRegistry {}
+export type FromCrudOverridesRegistry<
+  CrudName extends string,
+  CrudOperation extends string,
+  Default,
+> = CrudName extends keyof CrudOverridesRegistry
+  ? CrudOperation extends keyof CrudOverridesRegistry[CrudName]
+    ? CrudOverridesRegistry[CrudName][CrudOperation]
     : Default
+  : Default;
