8125f5169f972595cedab5e6e24aaec0fa51de44
Franjo Mindek <franjo.mindek@gmail.com>
2026-03-10 15:58:43 +0100

    change

--- waspc/data/Generator/templates/sdk/wasp/client/env.ts
+++ waspc/data/Generator/templates/sdk/wasp/client/env.ts
@@ -1,11 +1,14 @@
-import * as z from 'zod'
-import { ensureEnvSchema } from '../env/validation.js'
-import { FromRegistry } from '../types/index.js'
-import { getClientEnvSchema } from './env/schema.js'
+import * as z from "zod";
+import { ensureEnvSchema } from "../env/validation.js";
+import { FromRegistry } from "../types/index.js";
+import { getClientEnvSchema } from "./env/schema.js";
 
-const _env = ensureEnvSchema(import.meta.env, getClientEnvSchema(import.meta.env.MODE))
-type ClientEnv = typeof _env & z.infer<FromRegistry<'clientEnvSchema', z.ZodObject<{}>>>
-export const env = _env as ClientEnv
+const _env = ensureEnvSchema(
+  import.meta.env,
+  getClientEnvSchema(import.meta.env.MODE),
+);
+type ClientEnv = typeof _env &
+  z.infer<FromRegistry<"clientEnvSchema", z.ZodObject<{}>>>;
 
 // PUBLIC API
-// export const env = ensureEnvSchema(import.meta.env, clientEnvSchema)
+export const env = _env as ClientEnv;
