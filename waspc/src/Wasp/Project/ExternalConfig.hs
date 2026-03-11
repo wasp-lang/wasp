75511b0a46bd195a1b1c70a373fdc2b7616a540a
Filip Sodić <filip.sodic@fer.hr>
2026-03-11 17:02:25 +0100

    Move TS config validation to the appropriate module

--- waspc/src/Wasp/Project/ExternalConfig.hs
+++ waspc/src/Wasp/Project/ExternalConfig.hs
@@ -11,16 +11,14 @@ import Control.Monad.Except (ExceptT (ExceptT), runExceptT)
 import StrongPath (Abs, Dir, File, Path', Rel)
 import Wasp.ExternalConfig.Npm.PackageJson (PackageJson)
 import Wasp.ExternalConfig.TsConfig (TsConfig)
-import qualified Wasp.ExternalConfig.TsConfig as T
 import Wasp.Project.Common
   ( CompileError,
     SrcTsConfigFile,
     WaspProjectDir,
   )
 import Wasp.Project.ExternalConfig.PackageJson (readPackageJsonFile)
-import Wasp.Project.ExternalConfig.TsConfig (readSrcTsConfigFile)
+import Wasp.Project.ExternalConfig.TsConfig (readSrcTsConfigFile, validateSrcTsConfig)
 import Wasp.Project.ExternalConfig.ViteConfig (validateViteConfig)
-import qualified Wasp.Validator as V
 
 data ExternalConfigs = ExternalConfigs
   { _packageJson :: PackageJson,
@@ -58,49 +56,3 @@ readExternalConfigs waspDir srcTsConfigPath = runExceptT $ do
 validateExternalConfigs :: ExternalConfigs -> [CompileError]
 validateExternalConfigs configs =
   validateSrcTsConfig (_srcTsConfig configs)
-
-validateSrcTsConfig :: T.TsConfig -> [CompileError]
-validateSrcTsConfig config =
-  show <$> V.execValidator tsConfigValidator config
-  where
-    -- References for understanding the required compiler options:
-    --   - The comments in templates/sdk/wasp/tsconfig.json
-    --   - https://www.typescriptlang.org/docs/handbook/modules/introduction.html
-    --   - https://www.totaltypescript.com/tsconfig-cheat-sheet
-    --   - https://www.typescriptlang.org/tsconfig/
-
-    tsConfigValidator :: V.Validator T.TsConfig
-    tsConfigValidator =
-      V.withFileName "tsconfig.json" $
-        V.all
-          [ V.inField ("include", T.include) $ V.eqJust ["src"],
-            V.inField ("compilerOptions", T.compilerOptions) compilerOptionsValidator
-          ]
-
-    compilerOptionsValidator :: V.Validator T.CompilerOptions
-    compilerOptionsValidator =
-      V.all
-        [ V.inField ("module", T._module) $ V.eqJust "esnext",
-          V.inField ("target", T.target) $ V.eqJust "esnext",
-          -- Since Wasp ends up bundling the user code, `bundler` is the most
-          -- appropriate `moduleResolution` option.
-          V.inField ("moduleResolution", T.moduleResolution) $ V.eqJust "bundler",
-          V.inField ("moduleDetection", T.moduleDetection) $ V.eqJust "force",
-          -- `isolatedModules` prevents users from using features that don't work
-          -- with transpilers and would fail when Wasp bundles the code with rollup
-          -- (e.g., const enums)
-          V.inField ("isolatedModules", T.isolatedModules) $ V.eqJust True,
-          V.inField ("jsx", T.jsx) $ V.eqJust "preserve",
-          V.inField ("strict", T.strict) $ V.eqJust True,
-          V.inField ("esModuleInterop", T.esModuleInterop) $ V.eqJust True,
-          V.inField ("lib", T.lib) $ V.eqJust ["dom", "dom.iterable", "esnext"],
-          V.inField ("allowJs", T.allowJs) $ V.eqJust True,
-          -- Wasp internally uses TypeScript's project references to compile the
-          -- code. Referenced projects may not disable emit, so we must specify an
-          -- `outDir`.
-          V.inField ("outDir", T.outDir) $ V.eqJust ".wasp/out/user",
-          -- The composite flag is required because Wasp uses project references
-          -- (i.e., web app and server reference user code as a subproject)
-          V.inField ("composite", T.composite) $ V.eqJust True,
-          V.inField ("skipLibCheck", T.skipLibCheck) $ V.eqJust True
-        ]
--- waspc/src/Wasp/Project/ExternalConfig/TsConfig.hs
+++ waspc/src/Wasp/Project/ExternalConfig/TsConfig.hs
@@ -1,5 +1,6 @@
 module Wasp.Project.ExternalConfig.TsConfig
   ( readSrcTsConfigFile,
+    validateSrcTsConfig,
   )
 where
 
@@ -10,13 +11,15 @@ import Data.Either.Extra (maybeToEither)
 import StrongPath (Abs, Dir, File, Path', Rel, basename, fromRelFile, toFilePath)
 import qualified Wasp.ExternalConfig.TsConfig as T
 import Wasp.Project.Common
-  ( SrcTsConfigFile,
+  ( CompileError,
+    SrcTsConfigFile,
     WaspProjectDir,
     findFileInWaspProjectDir,
   )
 import Wasp.Util (indent)
 import qualified Wasp.Util.IO as IOUtil
 import Wasp.Util.Json (parseJsonWithComments)
+import qualified Wasp.Validator as V
 
 readSrcTsConfigFile ::
   Path' Abs (Dir WaspProjectDir) ->
@@ -37,3 +40,49 @@ readTsConfigFile tsConfigFile = do
   where
     errorMessagePrefix = "Failed to parse '" ++ baseTsConfigFilePath ++ "':\n"
     baseTsConfigFilePath = fromRelFile (basename tsConfigFile)
+
+validateSrcTsConfig :: T.TsConfig -> [CompileError]
+validateSrcTsConfig config =
+  show <$> V.execValidator tsConfigValidator config
+  where
+    -- References for understanding the required compiler options:
+    --   - The comments in templates/sdk/wasp/tsconfig.json
+    --   - https://www.typescriptlang.org/docs/handbook/modules/introduction.html
+    --   - https://www.totaltypescript.com/tsconfig-cheat-sheet
+    --   - https://www.typescriptlang.org/tsconfig/
+
+    tsConfigValidator :: V.Validator T.TsConfig
+    tsConfigValidator =
+      V.withFileName "tsconfig.json" $
+        V.all
+          [ V.inField ("include", T.include) $ V.eqJust ["src"],
+            V.inField ("compilerOptions", T.compilerOptions) compilerOptionsValidator
+          ]
+
+    compilerOptionsValidator :: V.Validator T.CompilerOptions
+    compilerOptionsValidator =
+      V.all
+        [ V.inField ("module", T._module) $ V.eqJust "esnext",
+          V.inField ("target", T.target) $ V.eqJust "esnext",
+          -- Since Wasp ends up bundling the user code, `bundler` is the most
+          -- appropriate `moduleResolution` option.
+          V.inField ("moduleResolution", T.moduleResolution) $ V.eqJust "bundler",
+          V.inField ("moduleDetection", T.moduleDetection) $ V.eqJust "force",
+          -- `isolatedModules` prevents users from using features that don't work
+          -- with transpilers and would fail when Wasp bundles the code with rollup
+          -- (e.g., const enums)
+          V.inField ("isolatedModules", T.isolatedModules) $ V.eqJust True,
+          V.inField ("jsx", T.jsx) $ V.eqJust "preserve",
+          V.inField ("strict", T.strict) $ V.eqJust True,
+          V.inField ("esModuleInterop", T.esModuleInterop) $ V.eqJust True,
+          V.inField ("lib", T.lib) $ V.eqJust ["dom", "dom.iterable", "esnext"],
+          V.inField ("allowJs", T.allowJs) $ V.eqJust True,
+          -- Wasp internally uses TypeScript's project references to compile the
+          -- code. Referenced projects may not disable emit, so we must specify an
+          -- `outDir`.
+          V.inField ("outDir", T.outDir) $ V.eqJust ".wasp/out/user",
+          -- The composite flag is required because Wasp uses project references
+          -- (i.e., web app and server reference user code as a subproject)
+          V.inField ("composite", T.composite) $ V.eqJust True,
+          V.inField ("skipLibCheck", T.skipLibCheck) $ V.eqJust True
+        ]
