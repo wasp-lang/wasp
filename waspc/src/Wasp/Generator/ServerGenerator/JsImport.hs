765e071a20fc01596c4cafa80514095e56ab2ece
Franjo Mindek <franjo.mindek@gmail.com>
2026-03-17 17:49:59 +0100

    js import

--- waspc/src/Wasp/Generator/ServerGenerator/JsImport.hs
+++ waspc/src/Wasp/Generator/ServerGenerator/JsImport.hs
@@ -2,7 +2,7 @@ module Wasp.Generator.ServerGenerator.JsImport where
 
 import qualified Data.Aeson as Aeson
 import Data.Maybe (fromJust)
-import StrongPath (Dir, Path, Posix, Rel, relDirToPosix, reldir, (</>))
+import StrongPath (Dir, Path, Posix, Rel, castDir, relDirToPosix, reldir, (</>))
 import qualified Wasp.AppSpec.ExtImport as EI
 import qualified Wasp.Generator.JsImport as GJI
 import Wasp.Generator.ServerGenerator.Common (ServerSrcDir, serverSrcDirInProjectRootDir)
@@ -13,7 +13,7 @@ import Wasp.JsImport
     JsImportStatement,
   )
 import qualified Wasp.JsImport as JI
-import Wasp.Project.Common (generatedCodeDirInWaspProjectDir)
+import Wasp.Project.Common (generatedCodeDirInWaspProjectDir, srcDirInWaspProjectDir)
 import Wasp.Util.StrongPath (invertRelDir)
 
 extImportToImportJson ::
@@ -48,17 +48,17 @@ getAliasedJsImportStmtAndIdentifier ::
 getAliasedJsImportStmtAndIdentifier importAlias pathFromImportLocationToExtCodeDir =
   JI.getJsImportStmtAndIdentifier . JI.applyJsImportAlias (Just importAlias) . extImportToJsImport pathFromImportLocationToExtCodeDir
 
+-- NOTE: We have to cast dir because the 'GJI' expects path to the `sdk/wasp/src` dir.
 extImportToJsImport ::
   Path Posix (Rel importLocation) (Dir ServerSrcDir) ->
   EI.ExtImport ->
   JsImport
-extImportToJsImport = GJI.extImportToJsImport $ fromJust (relDirToPosix waspProjectSrcDirFromServerSrcDir)
+extImportToJsImport = GJI.extImportToJsImport $ fromJust . relDirToPosix $ castDir waspProjectSrcDirFromServerSrcDir
   where
     -- NOTE: Instead of generating the `src` folder with the user's code and
     -- referencing that, we reference user code directly. This gives us proper
     -- error messages (with user's file names and line numbers). It works great
     -- with Vite (Vite outputs absolute file paths), but less great on the
     -- server (TS outputs relative paths, resulting in ../../src/something).
-    -- NOTE: When changing this, also update WebAppGenerator/JsImport.hs
-    waspProjectSrcDirFromServerSrcDir = waspProjectDirFromServerSrcDir </> [reldir|src|]
+    waspProjectSrcDirFromServerSrcDir = waspProjectDirFromServerSrcDir </> srcDirInWaspProjectDir
     waspProjectDirFromServerSrcDir = invertRelDir (generatedCodeDirInWaspProjectDir </> serverSrcDirInProjectRootDir)
