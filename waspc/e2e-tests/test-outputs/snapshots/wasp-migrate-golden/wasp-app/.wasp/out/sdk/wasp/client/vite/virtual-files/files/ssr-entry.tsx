0c09438e3d1be16229bb18260b91a3f67c914b7d
Carlos Precioso <carlos@wasp-lang.dev>
2026-03-23 19:15:51 +0100

    snapshots

--- waspc/e2e-tests/test-outputs/snapshots/wasp-migrate-golden/wasp-app/.wasp/out/sdk/wasp/client/vite/virtual-files/files/ssr-entry.tsx
+++ waspc/e2e-tests/test-outputs/snapshots/wasp-migrate-golden/wasp-app/.wasp/out/sdk/wasp/client/vite/virtual-files/files/ssr-entry.tsx
@@ -46,7 +46,7 @@ async function appToHtml(
     isFallbackPage,
     children,
   }: { isFallbackPage: boolean; children?: ReactNode },
-  { clientEntrySrc, transformIndexHtml }: PrerenderContext,
+  { clientEntrySrc }: PrerenderContext,
 ) {
   const app = (
     <Layout isFallbackPage={isFallbackPage} clientEntrySrc={clientEntrySrc}>
@@ -58,7 +58,6 @@ async function appToHtml(
 
   const html = await reactPrerender(app)
     .then((result) => streamConsumers.text(result.prelude))
-    .then((html) => transformIndexHtml(html));
 
   return html;
 };
