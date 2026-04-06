06a18c5ea4932dc26f8ec3551c65cb361754698d
Franjo Mindek <franjo.mindek@gmail.com>
2026-04-06 19:57:18 +0200

    websockets and crud

--- waspc/data/Generator/templates/sdk/wasp/client/crud/_crud.ts
+++ waspc/data/Generator/templates/sdk/wasp/client/crud/_crud.ts
@@ -1,89 +1,118 @@
 {{={= =}=}}
-import { createAction } from "../operations/actions/core.js";
-import { createQuery } from "../operations/queries/core.js";
-import { makeUseActionFor, makeUseQueryFor } from "./operationsHelpers.js";
+import { createAction, type ActionFor } from "../operations/actions/core.js";
+import { createQuery, type QueryFor } from "../operations/queries/core.js";
+import { makeUseActionFor, makeUseQueryFor, type UseActionFor, type UseQueryFor } from "./operationsHelpers.js";
 import { 
-    {=# operations.Get =}
-    GetQueryResolved,
-    {=/ operations.Get =}
-    {=# operations.GetAll =}
-    GetAllQueryResolved,
-    {=/ operations.GetAll =}
-    {=# operations.Create =}
-    CreateActionResolved,
-    {=/ operations.Create =}
-    {=# operations.Update =}
-    UpdateActionResolved,
-    {=/ operations.Update =}
-    {=# operations.Delete =}
-    DeleteActionResolved,
-    {=/ operations.Delete =}
+  {=# operations.Get =}
+  GetQueryResolved,
+  {=/ operations.Get =}
+  {=# operations.GetAll =}
+  GetAllQueryResolved,
+  {=/ operations.GetAll =}
+  {=# operations.Create =}
+  CreateActionResolved,
+  {=/ operations.Create =}
+  {=# operations.Update =}
+  UpdateActionResolved,
+  {=/ operations.Update =}
+  {=# operations.Delete =}
+  DeleteActionResolved,
+  {=/ operations.Delete =}
 } from 'wasp/server/crud/{= name =}'
 
-function createCrud() {
-    {=# operations.Get =}
-    const crudGetQuery = createQuery<GetQueryResolved>(
-        '{= fullPath =}',
-        {=& entitiesArray =}
-    )
-    {=/ operations.Get =}
-    {=# operations.GetAll =}
-    const crudGetAllQuery = createQuery<GetAllQueryResolved>(
-        '{= fullPath =}',
-        {=& entitiesArray =}
-    )
-    {=/ operations.GetAll =}
-    {=# operations.Create =}
-    const crudCreateAction = createAction<CreateActionResolved>(
-        '{= fullPath =}',
-        {=& entitiesArray =}
-    )
-    {=/ operations.Create =}
-    {=# operations.Update =}
-    const crudUpdateAction = createAction<UpdateActionResolved>(
-        '{= fullPath =}',
-        {=& entitiesArray =}
-    )
-    {=/ operations.Update =}
-    {=# operations.Delete =}
-    const crudDeleteAction = createAction<DeleteActionResolved>(
-        '{= fullPath =}',
-        {=& entitiesArray =}
-    )
-    {=/ operations.Delete =}
-    return {
-        {=# operations.Get =}
-        get: {
-            query: crudGetQuery,
-            useQuery: makeUseQueryFor(crudGetQuery)
-        },
-        {=/ operations.Get =}
-        {=# operations.GetAll =}
-        getAll: {
-            query: crudGetAllQuery,
-            useQuery: makeUseQueryFor(crudGetAllQuery)
-        },
-        {=/ operations.GetAll =}
-        {=# operations.Create =}
-        create: {
-            action: crudCreateAction,
-            useAction: makeUseActionFor(crudCreateAction)
-        },
-        {=/ operations.Create =}
-        {=# operations.Update =}
-        update: {
-            action: crudUpdateAction,
-            useAction: makeUseActionFor(crudUpdateAction)
-        },
-        {=/ operations.Update =}
-        {=# operations.Delete =}
-        delete: {
-            action: crudDeleteAction,
-            useAction: makeUseActionFor(crudDeleteAction)
-        },
-        {=/ operations.Delete =}
-    }
-}
+type Crud = {
+  get: {
+    query: GetQuery,
+    useQuery: UseQueryFor<GetQuery>
+  },
+  getAll: {
+    query: GetAllQuery,
+    useQuery: UseQueryFor<GetAllQuery>
+  },
+  create: {
+    action: CreateAction,
+    useAction: UseActionFor<CreateAction>,
+  },
+  update: {
+    action: UpdateAction,
+    useAction: UseActionFor<UpdateAction>,
+  },
+  delete: {
+    action: DeleteAction,
+    useAction: UseActionFor<DeleteAction>,
+  }
+};
+
+type GetQuery = QueryFor<GetQueryResolved>;
+type GetAllQuery = QueryFor<GetAllQueryResolved>;
+type CreateAction = ActionFor<CreateActionResolved>;
+type UpdateAction = ActionFor<UpdateActionResolved>;
+type DeleteAction = ActionFor<DeleteActionResolved>;
 
 // PUBLIC API
-export const {= name =} = createCrud();
+export const {= name =}: Crud = createCrud();
+
+function createCrud(): Crud {
+  {=# operations.Get =}
+  const crudGetQuery = createQuery<GetQueryResolved>(
+    '{= fullPath =}',
+    {=& entitiesArray =}
+  )
+  {=/ operations.Get =}
+  {=# operations.GetAll =}
+  const crudGetAllQuery = createQuery<GetAllQueryResolved>(
+    '{= fullPath =}',
+    {=& entitiesArray =}
+  )
+  {=/ operations.GetAll =}
+  {=# operations.Create =}
+  const crudCreateAction = createAction<CreateActionResolved>(
+    '{= fullPath =}',
+    {=& entitiesArray =}
+  )
+  {=/ operations.Create =}
+  {=# operations.Update =}
+  const crudUpdateAction = createAction<UpdateActionResolved>(
+    '{= fullPath =}',
+    {=& entitiesArray =}
+  )
+  {=/ operations.Update =}
+  {=# operations.Delete =}
+  const crudDeleteAction = createAction<DeleteActionResolved>(
+    '{= fullPath =}',
+    {=& entitiesArray =}
+  )
+  {=/ operations.Delete =}
+  return {
+    {=# operations.Get =}
+    get: {
+      query: crudGetQuery,
+      useQuery: makeUseQueryFor(crudGetQuery)
+    },
+    {=/ operations.Get =}
+    {=# operations.GetAll =}
+    getAll: {
+      query: crudGetAllQuery,
+      useQuery: makeUseQueryFor(crudGetAllQuery)
+    },
+    {=/ operations.GetAll =}
+    {=# operations.Create =}
+    create: {
+      action: crudCreateAction,
+      useAction: makeUseActionFor(crudCreateAction)
+    },
+    {=/ operations.Create =}
+    {=# operations.Update =}
+    update: {
+      action: crudUpdateAction,
+      useAction: makeUseActionFor(crudUpdateAction)
+    },
+    {=/ operations.Update =}
+    {=# operations.Delete =}
+    delete: {
+      action: crudDeleteAction,
+      useAction: makeUseActionFor(crudDeleteAction)
+    },
+    {=/ operations.Delete =}
+  }
+}
--- waspc/data/Generator/templates/sdk/wasp/client/crud/operationsHelpers.ts
+++ waspc/data/Generator/templates/sdk/wasp/client/crud/operationsHelpers.ts
@@ -1,6 +1,13 @@
+import type { UseQueryResult } from "@tanstack/react-query"
+import type { Tail, _Awaited, _ReturnType } from "../../universal/types"
 import { useAction, useQuery } from "../operations"
-import type { Query, Action } from "../operations/rpc"
-import type { Tail } from "../../universal/types"
+import type { OptimisticUpdateDefinition } from "../operations/hooks"
+import type { Action, GenericBackendOperation, OperationRpcFor, Query } from "../operations/rpc"
+
+// PRIVATE API
+export type UseQueryFor<BackendQuery extends GenericBackendOperation> =
+  (queryFnArgs?: Parameters<BackendQuery>[0], options?: any) =>
+    UseQueryResult<_Awaited<_ReturnType<BackendQuery>>, Error>
 
 // PRIVATE API
 export function makeUseQueryFor<Input, Output>(
@@ -10,6 +17,12 @@ export function makeUseQueryFor<Input, Output>(
     ...rest: Tail<Parameters<typeof useQuery<Input, Output>>>
   ) => useQuery<Input, Output>(query, ...rest);
 }
+
+// PRIVATE API
+export type UseActionFor<BackendAction extends GenericBackendOperation> =
+  (actionOptions?: {
+    optimisticUpdates: OptimisticUpdateDefinition<Parameters<BackendAction>[0], any>[]
+  }) => OperationRpcFor<BackendAction>
 
 // PRIVATE API
 export function makeUseActionFor<Input = unknown, Output = unknown>(
