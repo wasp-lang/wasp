cee9d154960b8a60daab7d426afacc0e5f21f0d2
Franjo Mindek <franjo.mindek@gmail.com>
2026-04-17 16:56:10 +0200

    init

--- waspc/src/Wasp/SemanticVersion/ComparatorSet.hs
+++ waspc/src/Wasp/SemanticVersion/ComparatorSet.hs
@@ -8,24 +8,24 @@ module Wasp.SemanticVersion.ComparatorSet
     comparatorSetParser,
     simpleRangeExpressionParser,
     hyphenRangeParser,
+    Comparator (..),
+    PrimitiveOperator (..),
+    comparatorParser,
+    toXRangeUpperBound,
+    toXRangeLowerBound,
   )
 where
 
 import qualified Data.List.NonEmpty as NE
 import qualified Language.Haskell.TH.Syntax as TH
 import qualified Text.Parsec as P
-import Wasp.SemanticVersion.Comparator
-  ( Comparator (..),
-    comparatorParser,
-    toXRangeLowerBound,
-    toXRangeUpperBound,
-  )
 import Wasp.SemanticVersion.PartialVersion (PartialVersion (..), partialVersionParser)
 import Wasp.SemanticVersion.Version (Version (..))
 import Wasp.SemanticVersion.VersionBound
   ( HasVersionBounds (versionBounds),
     VersionBound (..),
     intervalIntersection,
+    noVersionInterval,
   )
 
 -- | A comparator set is either a sequence of simple range expressions or a hyphen range.
@@ -143,3 +143,79 @@ hyphenRangeParser = do
     -- Must must exactly 1 white space character around the hyphen.
     hyphenRangeSeparatorParser :: P.Parsec String () Char
     hyphenRangeSeparatorParser = P.space *> P.char '-' <* P.space
+
+-- | A comparator composed of an operator and a partial version.
+-- It represents a single version constraint in a range.
+data Comparator
+  = Comparator PrimitiveOperator PartialVersion
+  deriving (Eq, TH.Lift)
+
+data PrimitiveOperator
+  = Equal
+  | LessThan
+  | LessThanOrEqual
+  | GreaterThan
+  | GreaterThanOrEqual
+  deriving (Eq, TH.Lift)
+
+-- | We rely on this 'show' implementation to produce valid `node-semver` comparator.
+instance Show Comparator where
+  show (Comparator op pv) = show op ++ show pv
+
+-- | We rely on this 'show' implementation to produce valid `node-semver` comparator.
+instance Show PrimitiveOperator where
+  -- Equal shows as "" because both "=1.2.3" and "1.2.3" are valid,
+  -- and the canonical form omits the "=".
+  show Equal = ""
+  show LessThan = "<"
+  show LessThanOrEqual = "<="
+  show GreaterThan = ">"
+  show GreaterThanOrEqual = ">="
+
+instance HasVersionBounds Comparator where
+  versionBounds (Comparator primOp pv) = case primOp of
+    Equal -> (toXRangeLowerBound pv, toXRangeUpperBound pv)
+    LessThan -> case pv of
+      Any -> noVersionInterval
+      (Major mjr) -> (Inclusive $ Version 0 0 0, Exclusive $ Version mjr 0 0)
+      (MajorMinor mjr mnr) -> (Inclusive $ Version 0 0 0, Exclusive $ Version mjr mnr 0)
+      (MajorMinorPatch mjr mnr ptc) -> (Inclusive $ Version 0 0 0, Exclusive $ Version mjr mnr ptc)
+    LessThanOrEqual -> (Inclusive $ Version 0 0 0, toXRangeUpperBound pv)
+    GreaterThan -> case pv of
+      Any -> noVersionInterval
+      (Major mjr) -> (Inclusive $ Version (mjr + 1) 0 0, Inf)
+      (MajorMinor mjr mnr) -> (Inclusive $ Version mjr (mnr + 1) 0, Inf)
+      (MajorMinorPatch mjr mnr ptc) -> (Exclusive $ Version mjr mnr ptc, Inf)
+    GreaterThanOrEqual -> (toXRangeLowerBound pv, Inf)
+
+toXRangeUpperBound :: PartialVersion -> VersionBound
+toXRangeUpperBound Any = Inf
+toXRangeUpperBound (Major mjr) = Exclusive (Version (mjr + 1) 0 0)
+toXRangeUpperBound (MajorMinor mjr mnr) = Exclusive (Version mjr (mnr + 1) 0)
+toXRangeUpperBound (MajorMinorPatch mjr mnr ptc) = Inclusive (Version mjr mnr ptc)
+
+toXRangeLowerBound :: PartialVersion -> VersionBound
+toXRangeLowerBound Any = Inclusive $ Version 0 0 0
+toXRangeLowerBound (Major mjr) = Inclusive $ Version mjr 0 0
+toXRangeLowerBound (MajorMinor mjr mnr) = Inclusive $ Version mjr mnr 0
+toXRangeLowerBound (MajorMinorPatch mjr mnr ptc) = Inclusive $ Version mjr mnr ptc
+
+-- | Parses a single comparator.
+-- See `primitive` definition here: https://github.com/npm/node-semver#ran`ge-grammar
+comparatorParser :: P.Parsec String () Comparator
+comparatorParser = primitiveComparatorParser
+  where
+    primitiveComparatorParser :: P.Parsec String () Comparator
+    primitiveComparatorParser =
+      Comparator <$> primitiveOperatorParser <* P.spaces <*> partialVersionParser
+
+    primitiveOperatorParser :: P.Parsec String () PrimitiveOperator
+    primitiveOperatorParser =
+      P.choice
+        [ LessThanOrEqual <$ P.try (P.string "<="),
+          GreaterThanOrEqual <$ P.try (P.string ">="),
+          LessThan <$ P.char '<',
+          GreaterThan <$ P.char '>',
+          Equal <$ P.char '=',
+          Equal <$ P.string ""
+        ]
