0adb627b92d823837383e44be9534e813dbb1f6e
Franjo Mindek <franjo.mindek@gmail.com>
2026-04-17 17:17:12 +0200

    reorder

--- waspc/src/Wasp/SemanticVersion/ComparatorSet.hs
+++ waspc/src/Wasp/SemanticVersion/ComparatorSet.hs
@@ -50,6 +50,20 @@ data SimpleRangeExpression
     CaretRange PartialVersion
   deriving (Eq, TH.Lift)
 
+-- | A comparator composed of an operator and a partial version.
+-- It represents a single version constraint in a range.
+data Comparator
+  = Comparator PrimitiveOperator PartialVersion
+  deriving (Eq, TH.Lift)
+
+data PrimitiveOperator
+  = Equal
+  | LessThan
+  | LessThanOrEqual
+  | GreaterThan
+  | GreaterThanOrEqual
+  deriving (Eq, TH.Lift)
+
 -- | We rely on this 'show' implementation to produce valid `node-semver` comparator set.
 instance Show ComparatorSet where
   show (SimpleComparatorSet simpleRangeExpressions) = unwords $ show <$> NE.toList simpleRangeExpressions
@@ -60,6 +74,20 @@ instance Show SimpleRangeExpression where
   show (Primitive comparator) = show comparator
   show (TildeRange pv) = "~" ++ show pv
   show (CaretRange pv) = "^" ++ show pv
+
+-- | We rely on this 'show' implementation to produce valid `node-semver` comparator.
+instance Show Comparator where
+  show (Comparator op pv) = show op ++ show pv
+
+-- | We rely on this 'show' implementation to produce valid `node-semver` comparator.
+instance Show PrimitiveOperator where
+  -- Equal shows as "" because both "=1.2.3" and "1.2.3" are valid,
+  -- and the canonical form omits the "=".
+  show Equal = ""
+  show LessThan = "<"
+  show LessThanOrEqual = "<="
+  show GreaterThan = ">"
+  show GreaterThanOrEqual = ">="
 
 -- | We define concatenation of two comparator sets as a union of their range expressions.
 -- Hyphen Ranges can't be combined with other comparator sets.
@@ -77,6 +105,22 @@ instance HasVersionBounds SimpleRangeExpression where
   versionBounds (Primitive comp) = versionBounds comp
   versionBounds (TildeRange pv) = (toXRangeLowerBound pv, toTildeRangeUpperBound pv)
   versionBounds (CaretRange pv) = (toXRangeLowerBound pv, toCareRangetUpperBound pv)
+
+instance HasVersionBounds Comparator where
+  versionBounds (Comparator primOp pv) = case primOp of
+    Equal -> (toXRangeLowerBound pv, toXRangeUpperBound pv)
+    LessThan -> case pv of
+      Any -> noVersionInterval
+      (Major mjr) -> (Inclusive $ Version 0 0 0, Exclusive $ Version mjr 0 0)
+      (MajorMinor mjr mnr) -> (Inclusive $ Version 0 0 0, Exclusive $ Version mjr mnr 0)
+      (MajorMinorPatch mjr mnr ptc) -> (Inclusive $ Version 0 0 0, Exclusive $ Version mjr mnr ptc)
+    LessThanOrEqual -> (Inclusive $ Version 0 0 0, toXRangeUpperBound pv)
+    GreaterThan -> case pv of
+      Any -> noVersionInterval
+      (Major mjr) -> (Inclusive $ Version (mjr + 1) 0 0, Inf)
+      (MajorMinor mjr mnr) -> (Inclusive $ Version mjr (mnr + 1) 0, Inf)
+      (MajorMinorPatch mjr mnr ptc) -> (Exclusive $ Version mjr mnr ptc, Inf)
+    GreaterThanOrEqual -> (toXRangeLowerBound pv, Inf)
 
 -- | Tilde range allows patch-level changes if minor is specified.
 toTildeRangeUpperBound :: PartialVersion -> VersionBound
@@ -96,6 +140,18 @@ toCareRangetUpperBound (MajorMinor mjr _) = Exclusive (Version (mjr + 1) 0 0)
 toCareRangetUpperBound (MajorMinorPatch 0 0 ptc) = Exclusive (Version 0 0 (ptc + 1))
 toCareRangetUpperBound (MajorMinorPatch 0 mnr _) = Exclusive (Version 0 (mnr + 1) 0)
 toCareRangetUpperBound (MajorMinorPatch mjr _ _) = Exclusive (Version (mjr + 1) 0 0)
+
+toXRangeUpperBound :: PartialVersion -> VersionBound
+toXRangeUpperBound Any = Inf
+toXRangeUpperBound (Major mjr) = Exclusive (Version (mjr + 1) 0 0)
+toXRangeUpperBound (MajorMinor mjr mnr) = Exclusive (Version mjr (mnr + 1) 0)
+toXRangeUpperBound (MajorMinorPatch mjr mnr ptc) = Inclusive (Version mjr mnr ptc)
+
+toXRangeLowerBound :: PartialVersion -> VersionBound
+toXRangeLowerBound Any = Inclusive $ Version 0 0 0
+toXRangeLowerBound (Major mjr) = Inclusive $ Version mjr 0 0
+toXRangeLowerBound (MajorMinor mjr mnr) = Inclusive $ Version mjr mnr 0
+toXRangeLowerBound (MajorMinorPatch mjr mnr ptc) = Inclusive $ Version mjr mnr ptc
 
 -- | Parses a comparator set.
 -- See `range` definition here: https://github.com/npm/node-semver#range-grammar
@@ -143,62 +199,6 @@ hyphenRangeParser = do
     -- Must must exactly 1 white space character around the hyphen.
     hyphenRangeSeparatorParser :: P.Parsec String () Char
     hyphenRangeSeparatorParser = P.space *> P.char '-' <* P.space
-
--- | A comparator composed of an operator and a partial version.
--- It represents a single version constraint in a range.
-data Comparator
-  = Comparator PrimitiveOperator PartialVersion
-  deriving (Eq, TH.Lift)
-
-data PrimitiveOperator
-  = Equal
-  | LessThan
-  | LessThanOrEqual
-  | GreaterThan
-  | GreaterThanOrEqual
-  deriving (Eq, TH.Lift)
-
--- | We rely on this 'show' implementation to produce valid `node-semver` comparator.
-instance Show Comparator where
-  show (Comparator op pv) = show op ++ show pv
-
--- | We rely on this 'show' implementation to produce valid `node-semver` comparator.
-instance Show PrimitiveOperator where
-  -- Equal shows as "" because both "=1.2.3" and "1.2.3" are valid,
-  -- and the canonical form omits the "=".
-  show Equal = ""
-  show LessThan = "<"
-  show LessThanOrEqual = "<="
-  show GreaterThan = ">"
-  show GreaterThanOrEqual = ">="
-
-instance HasVersionBounds Comparator where
-  versionBounds (Comparator primOp pv) = case primOp of
-    Equal -> (toXRangeLowerBound pv, toXRangeUpperBound pv)
-    LessThan -> case pv of
-      Any -> noVersionInterval
-      (Major mjr) -> (Inclusive $ Version 0 0 0, Exclusive $ Version mjr 0 0)
-      (MajorMinor mjr mnr) -> (Inclusive $ Version 0 0 0, Exclusive $ Version mjr mnr 0)
-      (MajorMinorPatch mjr mnr ptc) -> (Inclusive $ Version 0 0 0, Exclusive $ Version mjr mnr ptc)
-    LessThanOrEqual -> (Inclusive $ Version 0 0 0, toXRangeUpperBound pv)
-    GreaterThan -> case pv of
-      Any -> noVersionInterval
-      (Major mjr) -> (Inclusive $ Version (mjr + 1) 0 0, Inf)
-      (MajorMinor mjr mnr) -> (Inclusive $ Version mjr (mnr + 1) 0, Inf)
-      (MajorMinorPatch mjr mnr ptc) -> (Exclusive $ Version mjr mnr ptc, Inf)
-    GreaterThanOrEqual -> (toXRangeLowerBound pv, Inf)
-
-toXRangeUpperBound :: PartialVersion -> VersionBound
-toXRangeUpperBound Any = Inf
-toXRangeUpperBound (Major mjr) = Exclusive (Version (mjr + 1) 0 0)
-toXRangeUpperBound (MajorMinor mjr mnr) = Exclusive (Version mjr (mnr + 1) 0)
-toXRangeUpperBound (MajorMinorPatch mjr mnr ptc) = Inclusive (Version mjr mnr ptc)
-
-toXRangeLowerBound :: PartialVersion -> VersionBound
-toXRangeLowerBound Any = Inclusive $ Version 0 0 0
-toXRangeLowerBound (Major mjr) = Inclusive $ Version mjr 0 0
-toXRangeLowerBound (MajorMinor mjr mnr) = Inclusive $ Version mjr mnr 0
-toXRangeLowerBound (MajorMinorPatch mjr mnr ptc) = Inclusive $ Version mjr mnr ptc
 
 -- | Parses a single comparator.
 -- See `primitive` definition here: https://github.com/npm/node-semver#ran`ge-grammar
