69f4f1e9199f571787f54e4938811ad7c0aa76cb
Matija Sosic <matija.sosic@gmail.com>
2026-04-27 15:30:42 +0200

    made blog nicer.

---
+++ web/src/theme/BlogPostPage/index.tsx
@@ -0,0 +1,64 @@
+import React, { type ReactNode } from "react";
+import clsx from "clsx";
+import { HtmlClassNameProvider, ThemeClassNames } from "@docusaurus/theme-common";
+import {
+  BlogPostProvider,
+  useBlogPost,
+} from "@docusaurus/plugin-content-blog/client";
+import BlogLayout from "@theme/BlogLayout";
+import BlogPostItem from "@theme/BlogPostItem";
+import BlogPostPaginator from "@theme/BlogPostPaginator";
+import BlogPostPageMetadata from "@theme/BlogPostPage/Metadata";
+import BlogPostPageStructuredData from "@theme/BlogPostPage/StructuredData";
+import TOC from "@theme/TOC";
+import ContentVisibility from "@theme/ContentVisibility";
+import type { Props } from "@theme/BlogPostPage";
+
+function BlogPostPageContent({ children }: { children: ReactNode }): ReactNode {
+  const { metadata, toc } = useBlogPost();
+  const { nextItem, prevItem, frontMatter } = metadata;
+  const {
+    hide_table_of_contents: hideTableOfContents,
+    toc_min_heading_level: tocMinHeadingLevel,
+    toc_max_heading_level: tocMaxHeadingLevel,
+  } = frontMatter;
+  return (
+    <BlogLayout
+      toc={
+        !hideTableOfContents && toc.length > 0 ? (
+          <TOC
+            toc={toc}
+            minHeadingLevel={tocMinHeadingLevel}
+            maxHeadingLevel={tocMaxHeadingLevel}
+          />
+        ) : undefined
+      }
+    >
+      <ContentVisibility metadata={metadata} />
+      <BlogPostItem>{children}</BlogPostItem>
+      {(nextItem || prevItem) && (
+        <BlogPostPaginator nextItem={nextItem} prevItem={prevItem} />
+      )}
+    </BlogLayout>
+  );
+}
+
+export default function BlogPostPage(props: Props): ReactNode {
+  const BlogPostContent = props.content;
+  return (
+    <BlogPostProvider content={props.content} isBlogPostPage>
+      <HtmlClassNameProvider
+        className={clsx(
+          ThemeClassNames.wrapper.blogPages,
+          ThemeClassNames.page.blogPostPage
+        )}
+      >
+        <BlogPostPageMetadata />
+        <BlogPostPageStructuredData />
+        <BlogPostPageContent>
+          <BlogPostContent />
+        </BlogPostPageContent>
+      </HtmlClassNameProvider>
+    </BlogPostProvider>
+  );
+}
