92d47eb31eac4b7dfca5e7729ae79ae9734a7b04
Mihovil Ilakovac <mihovil@ilakovac.com>
2026-05-07 16:51:41 +0200

    Fix full ExtImport alias fixture

--- waspc/data/packages/wasp-config/__tests__/legacy/testFixtures.ts
+++ waspc/data/packages/wasp-config/__tests__/legacy/testFixtures.ts
@@ -954,6 +954,7 @@ export function getExtImport(
       return {
         ...importObject,
         from: "@src/external",
+        alias: "externalAlias",
       } satisfies FullConfig<TsAppSpec.ExtImport>;
     default:
       assertUnreachable(scope);
