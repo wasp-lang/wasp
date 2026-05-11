59d9b0225146c3c18cfebbe50faf0d45844754e2
Mihovil Ilakovac <mihovil@ilakovac.com>
2026-05-11 13:56:14 +0200

    Simplify ExtImport invalid input handling

--- waspc/data/packages/wasp-config/__tests__/spec/testFixtures.ts
+++ waspc/data/packages/wasp-config/__tests__/spec/testFixtures.ts
@@ -14,6 +14,7 @@ import {
   route,
 } from "../../src/spec/publicApi/index.js";
 import * as TsAppSpec from "../../src/spec/publicApi/tsAppSpec.js";
+import type { AnyFunction } from "../../src/typeUtils.js";
 
 export function getMinimalApp(): TsAppSpec.App {
   return app({
@@ -203,5 +204,3 @@ type ConfigScope = (typeof CONFIG_SCOPES)[number];
 function assertUnreachable(value: never): never {
   throw new Error(`Unhandled case: ${value}`);
 }
-
-type AnyFunction = (...args: never[]) => unknown;
