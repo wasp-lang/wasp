6fc311e95eceac323b1ace95b647cf863d4a439b
Franjo Mindek <franjo.mindek@gmail.com>
2026-05-12 11:18:15 +0200

    hide stack trace

--- waspc/data/packages/wasp-config/src/spec/tsSpecError.ts
+++ waspc/data/packages/wasp-config/src/spec/tsSpecError.ts
@@ -2,5 +2,7 @@ export class TsSpecError extends Error {
   constructor(message: string, options?: ErrorOptions) {
     super(message, options);
     this.name = "Wasp Spec Error";
+    // We want to hide the stack trace for user-facing errors.
+    this.stack = undefined;
   }
 }
