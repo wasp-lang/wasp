faff5679edfa8d5d0e6d2c66b61251398eca8379
Franjo Mindek <franjo.mindek@gmail.com>
2026-06-12 09:10:02 +0200

    Init

--- waspc/data/Cli/starters/basic/src/shared/components/Header.tsx
+++ waspc/data/Cli/starters/basic/src/shared/components/Header.tsx
@@ -1,6 +1,6 @@
 import { logout, useAuth } from "wasp/client/auth";
 import { Link } from "wasp/client/router";
-import Logo from "../../assets/logo.svg";
+import Logo from "../../assets/wasp-logo.svg";
 import { Button, ButtonLink } from "./Button";
 
 export function Header() {
@@ -10,7 +10,7 @@ export function Header() {
     <header className="sticky top-0 z-10 flex justify-center border-b border-neutral-200 bg-white shadow-sm">
       <div className="flex w-full max-w-(--breakpoint-lg) items-center justify-between p-4 px-12">
         <Link to="/" className="flex items-center gap-2">
-          <img src={Logo} alt="Todo App Logo" className="h-10 w-10" />
+          <img src={Logo} alt="Wasp logo" className="h-10 w-10" />
           <h1 className="text-2xl font-semibold">Todo App</h1>
         </Link>
         <nav>
