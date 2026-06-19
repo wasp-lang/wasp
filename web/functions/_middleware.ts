5b772cad027bc9bafbe32ba24c1693d94b1959dd
Franjo Mindek <84568328+FranjoMindek@users.noreply.github.com>
2026-06-19 11:58:09 +0200

    Update web/functions/_middleware.ts
    Co-authored-by: Carlos Precioso <carlos@wasp-lang.dev>

--- web/functions/_middleware.ts
+++ web/functions/_middleware.ts
@@ -48,7 +48,7 @@ function wantsMarkdownContent(request: Request): boolean {
     return false;
   }
   const acceptHeader = request.headers.get("Accept") ?? "";
-  // We don't really want to borther with order of formats and their q-values.
+  // We don't really want to bother with order of formats and their q-values.
   // Requesting `text/markdown` is a deliberate choice, so we assume priority.
   return acceptHeader.includes("text/markdown");
 }
