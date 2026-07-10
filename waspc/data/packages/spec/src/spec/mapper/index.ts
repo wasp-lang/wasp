c49fef1d6d32db8bc400c2ee3d1b9dcf4db2d93d
Carlos Precioso <carlos@wasp-lang.dev>
2026-07-10 10:16:31 +0200

    Refactor mapApp into mapper module with emit-based decl registration (#4446)

---
+++ waspc/data/packages/spec/src/spec/mapper/index.ts
@@ -0,0 +1,37 @@
+import * as AppSpec from "../../appSpec.js";
+import * as WaspSpec from "../publicApi/waspSpec.js";
+import { mapAppSpec } from "./app.js";
+import { makeAppMapperContext } from "./context.js";
+
+export function convertWaspSpecToAppSpec(
+  app: WaspSpec.App,
+  {
+    projectRootDir,
+    entityNames,
+  }: {
+    projectRootDir: string;
+    entityNames: string[];
+  },
+): AppSpec.Decl[] {
+  const specElements = flattenSpecElements(app.spec);
+
+  const { ctx, collectedDeclsByKey } = makeAppMapperContext({
+    entityNames,
+    projectRootDir,
+    specElements,
+  });
+
+  for (const specElement of specElements) {
+    ctx.collectSpecElement(specElement);
+  }
+
+  const appDecl = mapAppSpec(app, ctx);
+
+  return [appDecl, ...collectedDeclsByKey.values()];
+}
+
+function flattenSpecElements(spec: WaspSpec.Spec): WaspSpec.SpecElement[] {
+  // We assert the `[spec]` as a `SpecElement[]` to avoid
+  // infinite recursion of the `WaspSpec.Spec` type.
+  return ([spec] as WaspSpec.SpecElement[]).flat(Infinity);
+}
