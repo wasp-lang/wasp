4a7d2bca0b68dbda53544ccaa00139b457df6ec5
Franjo Mindek <franjo.mindek@gmail.com>
2026-07-13 09:34:55 +0200

    beef up the explanation

--- waspc/data/Generator/templates/sdk/wasp/types/register.ts
+++ waspc/data/Generator/templates/sdk/wasp/types/register.ts
@@ -1,13 +1,32 @@
 /**
- * This module acts as a bridge between the SDK and user-defined types.
+ * This module acts as a bridge between SDK and user project types.
  *
  * The SDK defines and exports empty {@link Register} interface.
- * During compliation, Wasp generate additional type declarations
- * in `.wasp/out/types/runtime/` (which is part of user project) that extend
- * the empty {@link Register} interface via module augmentation.
- *
- * As a result, users can "see" user-defined types in SDK without SDK 
- * directly depending on the user code.
+ * 
+ * During compilation, Wasp generate additional type declarations
+ * in `.wasp/out/types/runtime/sdk` (which is part of user project) that 
+ * extend (augment) empty {@link Register} interface via module augmentation:
+ * ```ts
+ * declare module "wasp/types" {
+ *   interface Register {
+ *     prismaSetupFn: typeof import('../../../../../src/features/db/prisma').setUpPrisma
+ *     // ...
+ *   }
+ * }
+ * ```
+ * This essentially pushes user project types into SDK.
+ * 
+ * On the SDK side, all user project dependent types are resolved through
+ * the {@link Register} interface:
+ * ```ts
+ * type RegisteredPrismaSetupFn = FromRegister<'prismaSetupFn', () => InternalPrismaClient>;
+ * export type PrismaClient = ReturnType<RegisteredPrismaSetupFn>;
+ * ```
+ * This ensures user project types are reflected in the SDK.
+ * E.g., operation clients will accept parameters and have return types that the user defined.
+ * 
+ * As a result, users can see their own user-defined types in SDK,
+ * without SDK directly depending on the user code.
  */
 
 /**
