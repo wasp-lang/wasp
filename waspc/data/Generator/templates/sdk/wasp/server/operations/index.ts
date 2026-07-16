b6929a49add50cf354007e254a84c5a51ebcc8cb
Franjo Mindek <franjo.mindek@gmail.com>
2026-07-16 12:44:35 +0200

    fix

--- waspc/data/Generator/templates/sdk/wasp/server/operations/index.ts
+++ waspc/data/Generator/templates/sdk/wasp/server/operations/index.ts
@@ -30,23 +30,23 @@ export { {= operationName =} } from './actions/index.js'
  * dependency package, `tsc` must synthesize a portable reference to
  * that symbol.
  * E.g., an authenticated operations context contains a type symbol for
- * `AuthUser`. So we must be able to create a portable reference to
- * `AuthUser`.
+ * {@link AuthUser}. So we must be able to create a portable reference to
+ * {@link AuthUser}.
  * 
  * Creating a portable reference is possible if either:
  * 
  * 1. The symbol is imported locally.
  *     E.g., the same module that defines the operation, imports
- *    `AuthUser`: `import { type AuthUser } from "wasp/auth"`.
+ *    {@link AuthUser}: `import { type AuthUser } from "wasp/auth"`.
  * 
  * 2. The symbol is re-exported through an already loaded module.
  *    E.g., some other module has: `import { X } from "auth/wasp"`.
- *    Because of that, `tsc` can reach `AuthUser` through "auth/wasp".
+ *    Because of that, `tsc` can reach {@link AuthUser} through "auth/wasp".
  * 
  * 3. The file where the symbol is declared is reachable through the
  *    packages `exports` map.
  *    E.g., Wasp SDK does not export `"server/operations/user"` module,
- *    where `AuthUser` is declared, so this condition always fails.
+ *    where {@link AuthUser} is declared, so this condition always fails.
  * 
  * So we must depend on either the first or the second condition
  * being true for types to work properly.
@@ -58,10 +58,9 @@ export { {= operationName =} } from './actions/index.js'
  * situations where this is not true. Example being our very own
  * `ask-the-documents` project. It has zero `"wasp/auth"` imports,
  * but defines operations via the `satisfies` keyword. This results
- * in an error, because the `AuthUser` symbol is not portable.
+ * in an error, because the {@link AuthUser} symbol is not portable.
  * 
- * To avoid this problem, `"wasp/server/operations"` re-export `AuthUser`.
+ * To avoid this problem, `"wasp/server/operations"` re-export {@link AuthUser}.
  * That way, `tsc` can always create a portable reference to it.
- * 
  */
 export { type AuthUser } from '../auth/user.js'
