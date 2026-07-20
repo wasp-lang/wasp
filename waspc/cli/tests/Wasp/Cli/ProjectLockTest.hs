7d90db9cd57f31a6a29d3c0b408bed8e6957f1bf
Carlos Precioso <carlos@wasp-lang.dev>
2026-07-20 15:00:20 +0200

    Remove req test

--- waspc/cli/tests/Wasp/Cli/ProjectLockTest.hs
+++ waspc/cli/tests/Wasp/Cli/ProjectLockTest.hs
@@ -1,18 +1,11 @@
 module Wasp.Cli.ProjectLockTest where
 
-import Control.Exception (try)
-import Control.Monad.Error.Class (throwError)
-import Control.Monad.IO.Class (liftIO)
 import Data.Maybe (fromJust)
 import StrongPath (Abs, Dir, File, Path', (</>))
 import qualified StrongPath as SP
 import qualified System.Directory as Directory
-import System.Exit (ExitCode (ExitFailure))
 import System.IO.Temp (withSystemTempDirectory)
 import Test.Hspec
-import Wasp.Cli.Command (CommandError (CommandError), runCommand)
-import Wasp.Cli.Command.Clean (clean)
-import Wasp.Cli.Command.Require (InLockedWaspProject (InLockedWaspProject), require)
 import Wasp.Cli.ProjectLock
   ( ProjectLockError (..),
     acquireProjectLock,
@@ -58,43 +51,6 @@ spec_projectLock = do
         releaseProjectLock (projectLockFilePath waspProjectDir)
         _ <- expectAcquired =<< acquireProjectLock (projectLockFilePath waspProjectDir)
         releaseProjectLock (projectLockFilePath waspProjectDir)
-
-  describe "InLockedWaspProject requirement" $ do
-    -- These cases change the process-wide current directory, so they must remain
-    -- in a single test to prevent tasty from running them concurrently.
-    it "handles command cleanup and clean" $ do
-      withTempWaspProject $ \waspProjectDir -> do
-        Directory.withCurrentDirectory (SP.fromAbsDir waspProjectDir) $
-          runCommand $ do
-            InLockedWaspProject lockedProjectDir <- require
-            liftIO $ do
-              lockedProjectDir `shouldBe` waspProjectDir
-              Directory.doesFileExist (lockFilePath waspProjectDir)
-                `shouldReturn` True
-
-        Directory.doesFileExist (lockFilePath waspProjectDir) `shouldReturn` False
-
-      withTempWaspProject $ \waspProjectDir -> do
-        result <-
-          Directory.withCurrentDirectory (SP.fromAbsDir waspProjectDir) $
-            try $
-              runCommand $ do
-                InLockedWaspProject _ <- require
-                throwError $ CommandError "Test error" "Error message"
-
-        result `shouldBe` Left (ExitFailure 1)
-        Directory.doesFileExist (lockFilePath waspProjectDir) `shouldReturn` False
-
-      withTempWaspProject $ \waspProjectDir -> do
-        let dotWaspDir = SP.fromAbsDir waspProjectDir ++ ".wasp"
-        let nodeModulesDir = SP.fromAbsDir waspProjectDir ++ "node_modules"
-        Directory.createDirectoryIfMissing True dotWaspDir
-        Directory.createDirectoryIfMissing True nodeModulesDir
-
-        Directory.withCurrentDirectory (SP.fromAbsDir waspProjectDir) $ runCommand clean
-
-        Directory.doesDirectoryExist dotWaspDir `shouldReturn` False
-        Directory.doesDirectoryExist nodeModulesDir `shouldReturn` False
 
 expectAcquired :: Either ProjectLockError processId -> IO processId
 expectAcquired = \case
