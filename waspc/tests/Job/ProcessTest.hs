5a4d98df04b295847d440e12d16ad455ec6c9633
Mihovil Ilakovac <mihovil@ilakovac.com>
2026-07-21 10:53:41 +0200

    Share process output forwarding

--- waspc/tests/Job/ProcessTest.hs
+++ waspc/tests/Job/ProcessTest.hs
@@ -15,17 +15,17 @@ spec_runProcessAsJob :: Spec
 spec_runProcessAsJob =
   describe "runProcessAsJob" $ do
     it "decodes split and incomplete UTF-8 on stdout" $
-      runSplitUtf8Process "stdout" `shouldReturn` "€�"
+      runSplitUtf8Process "stdout" J.Stdout `shouldReturn` "€�"
 
     it "decodes split and incomplete UTF-8 on stderr" $
-      runSplitUtf8Process "stderr" `shouldReturn` "€�"
+      runSplitUtf8Process "stderr" J.Stderr `shouldReturn` "€�"
 
-runSplitUtf8Process :: String -> IO T.Text
-runSplitUtf8Process streamName = do
+runSplitUtf8Process :: String -> J.JobOutputType -> IO T.Text
+runSplitUtf8Process streamName expectedOutputType = do
   chan <- newChan
   exitCode <- Process.runProcessAsJob (P.proc "node" ["-e", splitUtf8Script streamName]) J.Wasp chan
   exitCode `shouldBe` ExitSuccess
-  output <- collectOutputUntilExit chan
+  output <- collectOutputUntilExit expectedOutputType chan
   remainingMessage <- timeout (secondsToMicroSeconds 0.1) $ readChan chan
   remainingMessage `shouldSatisfy` isNothing
   return output
@@ -38,14 +38,16 @@ splitUtf8Script streamName =
     <> streamName
     <> ".write(Buffer.from([0x82, 0xac, 0xe2])), 200);"
 
-collectOutputUntilExit :: Chan J.JobMessage -> IO T.Text
-collectOutputUntilExit chan = go []
+collectOutputUntilExit :: J.JobOutputType -> Chan J.JobMessage -> IO T.Text
+collectOutputUntilExit expectedOutputType chan = go []
   where
     go collected = do
       message <- readChan chan
       J._jobType message `shouldBe` J.Wasp
       case J._data message of
-        J.JobOutput output _ -> go (output : collected)
+        J.JobOutput output outputType -> do
+          outputType `shouldBe` expectedOutputType
+          go (output : collected)
         J.JobExit exitCode -> do
           exitCode `shouldBe` ExitSuccess
           return $ T.concat $ reverse collected
