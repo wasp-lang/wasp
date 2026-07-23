15158ffb45dc1d8c922218148ec8c60cce7e69ec
Carlos Precioso <carlos@wasp-lang.dev>
2026-07-23 11:21:37 +0200

    Delete Wasp.AppSpec.Entity.Field, use PSL field types directly
    The semantic Field/FieldType/Scalar layer duplicated Psl.Ast.Model.FieldType
    constructor-for-constructor via a boilerplate identity mapping, but the only
    consumer (doesUserEntityContainField) reads just the field name.
    
    - Add getModelFields helper to Wasp.Psl.Util; reuse it in findIdField.
    - Entity is now a newtype over pslModelBody; getFields returns [Psl.Model.Field].
    - Rewrite Valid.findFieldByName to match on Psl.Model._name.
    - Delete the module and drop it from waspc.cabal.

--- waspc/src/Wasp/AppSpec/Entity.hs
+++ waspc/src/Wasp/AppSpec/Entity.hs
@@ -13,16 +13,12 @@ where
 import Data.Aeson (FromJSON (parseJSON))
 import Data.Data (Data)
 import Wasp.AppSpec.Core.IsDecl (IsDecl)
-import Wasp.AppSpec.Entity.Field (Field)
-import qualified Wasp.AppSpec.Entity.Field as Field
 import qualified Wasp.Psl.Ast.Attribute as Psl.Attribute
 import qualified Wasp.Psl.Ast.Model as Psl.Model
-import qualified Wasp.Psl.Ast.WithCtx as Psl.WithCtx
-import Wasp.Psl.Util (findIdBlockAttribute, findIdField)
+import Wasp.Psl.Util (findIdBlockAttribute, findIdField, getModelFields)
 
-data Entity = Entity
-  { fields :: ![Field],
-    pslModelBody :: !Psl.Model.Body
+newtype Entity = Entity
+  { pslModelBody :: Psl.Model.Body
   }
   deriving (Show, Eq, Data)
 
@@ -32,21 +28,10 @@ instance FromJSON Entity where
   parseJSON = const $ fail "Entity declarations in wasp are deprecated, entities are now defined via prisma.schema file."
 
 makeEntity :: Psl.Model.Body -> Entity
-makeEntity body =
-  Entity
-    { fields = makeEntityFieldsFromPslBody body,
-      pslModelBody = body
-    }
-  where
-    makeEntityFieldsFromPslBody :: Psl.Model.Body -> [Field]
-    makeEntityFieldsFromPslBody (Psl.Model.Body pslElements) =
-      Field.pslFieldToEntityField
-        <$> [ field
-            | (Psl.Model.ElementField field) <- Psl.WithCtx.getNode <$> pslElements
-            ]
+makeEntity body = Entity {pslModelBody = body}
 
-getFields :: Entity -> [Field]
-getFields = fields
+getFields :: Entity -> [Psl.Model.Field]
+getFields = getModelFields . getPslModelBody
 
 getPslModelBody :: Entity -> Psl.Model.Body
 getPslModelBody = pslModelBody
