0bf6ee896ce3b3d8800a859025036dca5d212d35
Carlos Precioso <carlos@wasp-lang.dev>
2026-07-24 09:53:02 +0200

    Feedback

--- waspc/data/Cli/starters/basic/main.wasp.ts
+++ waspc/data/Cli/starters/basic/main.wasp.ts
@@ -1,15 +1,14 @@
-import { app, route } from "@wasp.sh/spec";
+import { app, page, route } from "@wasp.sh/spec";
 
-import {
-  authSpec,
-  emailVerificationRoute,
-  loginRoute,
-  passwordResetRoute,
-} from "./src/auth/auth.wasp";
 import { tagsSpec } from "./src/tags/tags.wasp";
 import { tasksPage, tasksSpec } from "./src/tasks/tasks.wasp";
 
 import { App } from "./src/App" with { type: "ref" };
+import { EmailVerificationPage } from "./src/auth/email/EmailVerificationPage" with { type: "ref" };
+import { LoginPage } from "./src/auth/email/LoginPage" with { type: "ref" };
+import { PasswordResetPage } from "./src/auth/email/PasswordResetPage" with { type: "ref" };
+import { RequestPasswordResetPage } from "./src/auth/email/RequestPasswordResetPage" with { type: "ref" };
+import { SignupPage } from "./src/auth/email/SignupPage" with { type: "ref" };
 import { userSignupFields } from "./src/auth/email/userSignupFields" with { type: "ref" };
 
 const mainRoute = route("TasksRoute", "/", tasksPage);
@@ -29,15 +28,23 @@ export default app({
         },
         userSignupFields,
         emailVerification: {
-          clientRoute: emailVerificationRoute,
+          clientRoute: route(
+            "EmailVerificationRoute",
+            "/email-verification",
+            page(EmailVerificationPage),
+          ),
         },
         passwordReset: {
-          clientRoute: passwordResetRoute,
+          clientRoute: route(
+            "PasswordResetRoute",
+            "/password-reset",
+            page(PasswordResetPage),
+          ),
         },
       },
     },
     onAuthSucceededRedirectTo: mainRoute,
-    onAuthFailedRedirectTo: loginRoute,
+    onAuthFailedRedirectTo: route("LoginRoute", "/login", page(LoginPage)),
   },
   emailSender: {
     provider: "Dummy",
@@ -45,5 +52,15 @@ export default app({
   client: {
     rootComponent: App,
   },
-  spec: [mainRoute, tasksSpec, tagsSpec, authSpec],
+  spec: [
+    mainRoute,
+    tasksSpec,
+    tagsSpec,
+    route("SignupRoute", "/signup", page(SignupPage)),
+    route(
+      "RequestPasswordResetRoute",
+      "/request-password-reset",
+      page(RequestPasswordResetPage),
+    ),
+  ],
 });
