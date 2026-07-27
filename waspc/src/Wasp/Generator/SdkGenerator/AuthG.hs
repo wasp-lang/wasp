3e0667187d1bcebe6c679538bb22aa9501e36669
Franjo Mindek <franjo.mindek@gmail.com>
2026-07-27 18:23:25 +0200

    mirror sdk template structure in generator modules

--- waspc/src/Wasp/Generator/SdkGenerator/AuthG.hs
+++ waspc/src/Wasp/Generator/SdkGenerator/AuthG.hs
@@ -11,11 +11,9 @@ import qualified Wasp.AppSpec.App as AS.App
 import qualified Wasp.AppSpec.App.Auth as AS.Auth
 import Wasp.AppSpec.Valid (getApp)
 import Wasp.Generator.Common (makeJsArrayFromHaskellList)
-import qualified Wasp.Generator.DbGenerator.Auth as DbAuth
 import Wasp.Generator.FileDraft (FileDraft)
 import Wasp.Generator.Monad (Generator)
 import Wasp.Generator.SdkGenerator.Auth.AuthFormsG (genAuthForms)
-import Wasp.Generator.SdkGenerator.Auth.Common (getOnAuthSucceededRedirectToOrDefault)
 import Wasp.Generator.SdkGenerator.Auth.EmailAuthG (genEmailAuth)
 import Wasp.Generator.SdkGenerator.Auth.LocalAuthG (genLocalAuth)
 import Wasp.Generator.SdkGenerator.Auth.OAuthAuthG (genOAuthAuth)
@@ -24,10 +22,7 @@ import Wasp.Generator.SdkGenerator.Common
     genFileCopy,
     mkTmplFdWithData,
   )
-import Wasp.Generator.SdkGenerator.JsImport (extImportToImportJson)
-import Wasp.Generator.SdkGenerator.Server.OAuthG (genOAuth)
 import Wasp.Util ((<++>))
-import qualified Wasp.Util as Util
 
 genAuth :: AppSpec -> Generator [FileDraft]
 genAuth spec =
@@ -52,18 +47,6 @@ genAuth spec =
         <++> genLocalAuth auth
         <++> genOAuthAuth auth
         <++> genEmailAuth auth
-        -- server stuff
-        <++> sequence
-          [ genFileCopy [relfile|server/core/auth.ts|],
-            genFileCopyInServerAuth [relfile|password.ts|],
-            genFileCopyInServerAuth [relfile|jwt.ts|],
-            genSessionTs auth,
-            genLuciaTs auth,
-            genUtils auth,
-            genProvidersTypes auth,
-            genProvdersIndex auth
-          ]
-        <++> genOAuth auth
   where
     maybeAuth = AS.App.auth $ snd $ getApp spec
 
@@ -78,61 +61,6 @@ genUseAuth auth =
       tmplData
   where
     tmplData = object ["entitiesGetMeDependsOn" .= makeJsArrayFromHaskellList [userEntityName]]
-    userEntityName = AS.refName $ AS.Auth.userEntity auth
-
-genLuciaTs :: AS.Auth.Auth -> Generator FileDraft
-genLuciaTs auth =
-  return $
-    mkTmplFdWithData
-      (serverAuthDirInSdkTemplatesDir </> [relfile|lucia.ts|])
-      tmplData
-  where
-    tmplData =
-      object
-        [ "sessionEntityLower" .= (Util.toLowerFirst DbAuth.sessionEntityName :: String),
-          "authEntityLower" .= (Util.toLowerFirst DbAuth.authEntityName :: String),
-          "userEntityUpper" .= (userEntityName :: String)
-        ]
-
-    userEntityName = AS.refName $ AS.Auth.userEntity auth
-
-genSessionTs :: AS.Auth.Auth -> Generator FileDraft
-genSessionTs auth =
-  return $
-    mkTmplFdWithData
-      (serverAuthDirInSdkTemplatesDir </> [relfile|session.ts|])
-      tmplData
-  where
-    tmplData =
-      object
-        [ "userEntityUpper" .= userEntityName,
-          "userEntityLower" .= Util.toLowerFirst userEntityName,
-          "authFieldOnUserEntityName" .= DbAuth.authFieldOnUserEntityName,
-          "identitiesFieldOnAuthEntityName" .= DbAuth.identitiesFieldOnAuthEntityName
-        ]
-    userEntityName = AS.refName $ AS.Auth.userEntity auth
-
-genUtils :: AS.Auth.Auth -> Generator FileDraft
-genUtils auth =
-  return $
-    mkTmplFdWithData
-      (serverAuthDirInSdkTemplatesDir </> [relfile|utils.ts|])
-      tmplData
-  where
-    tmplData =
-      object
-        [ "userEntityUpper" .= (userEntityName :: String),
-          "userEntityLower" .= (Util.toLowerFirst userEntityName :: String),
-          "authEntityUpper" .= (DbAuth.authEntityName :: String),
-          "authEntityLower" .= (Util.toLowerFirst DbAuth.authEntityName :: String),
-          "userFieldOnAuthEntityName" .= (DbAuth.userFieldOnAuthEntityName :: String),
-          "authIdentityEntityUpper" .= (DbAuth.authIdentityEntityName :: String),
-          "authIdentityEntityLower" .= (Util.toLowerFirst DbAuth.authIdentityEntityName :: String),
-          "authFieldOnUserEntityName" .= (DbAuth.authFieldOnUserEntityName :: String),
-          "identitiesFieldOnAuthEntityName" .= (DbAuth.identitiesFieldOnAuthEntityName :: String),
-          "failureRedirectPath" .= AS.Auth.onAuthFailedRedirectTo auth,
-          "successRedirectPath" .= getOnAuthSucceededRedirectToOrDefault auth
-        ]
     userEntityName = AS.refName $ AS.Auth.userEntity auth
 
 genIndexTs :: AS.Auth.Auth -> Generator FileDraft
@@ -150,50 +78,9 @@ genIndexTs auth =
     isEmailAuthEnabled = AS.Auth.isEmailAuthEnabled auth
     isLocalAuthEnabled = AS.Auth.isUsernameAndPasswordAuthEnabled auth
 
-genProvdersIndex :: AS.Auth.Auth -> Generator FileDraft
-genProvdersIndex auth =
-  return $
-    mkTmplFdWithData
-      (serverAuthDirInSdkTemplatesDir </> [relfile|providers/index.ts|])
-      tmplData
-  where
-    tmplData =
-      object
-        [ "emailUserSignupFields" .= extImportToImportJson userEmailSignupFields,
-          "usernameAndPasswordUserSignupFields" .= extImportToImportJson userUsernameAndPassowrdSignupFields
-        ]
-    userEmailSignupFields = AS.Auth.email authMethods >>= AS.Auth.userSignupFieldsForEmailAuth
-    userUsernameAndPassowrdSignupFields = AS.Auth.usernameAndPassword authMethods >>= AS.Auth.userSignupFieldsForUsernameAuth
-    authMethods = AS.Auth.methods auth
-
-genProvidersTypes :: AS.Auth.Auth -> Generator FileDraft
-genProvidersTypes auth =
-  return $
-    mkTmplFdWithData
-      (serverAuthDirInSdkTemplatesDir </> [relfile|providers/types.ts|])
-      tmplData
-  where
-    tmplData =
-      object
-        [ "userEntityUpper" .= (userEntityName :: String),
-          "emailUserSignupFields" .= extImportToImportJson userEmailSignupFields,
-          "usernameAndPasswordUserSignupFields" .= extImportToImportJson userUsernameAndPassowrdSignupFields
-        ]
-    userEntityName = AS.refName $ AS.Auth.userEntity auth
-    userEmailSignupFields = AS.Auth.email authMethods >>= AS.Auth.userSignupFieldsForEmailAuth
-    userUsernameAndPassowrdSignupFields = AS.Auth.usernameAndPassword authMethods >>= AS.Auth.userSignupFieldsForUsernameAuth
-    authMethods = AS.Auth.methods auth
-
 authDirInSdkTemplatesDir :: Path' (Rel SdkTemplatesDir) Dir'
 authDirInSdkTemplatesDir = [reldir|auth|]
-
-serverAuthDirInSdkTemplatesDir :: Path' (Rel SdkTemplatesDir) Dir'
-serverAuthDirInSdkTemplatesDir = [reldir|server/auth|]
 
 genFileCopyInAuth :: Path' Rel' File' -> Generator FileDraft
 genFileCopyInAuth =
   genFileCopy . (authDirInSdkTemplatesDir </>)
-
-genFileCopyInServerAuth :: Path' Rel' File' -> Generator FileDraft
-genFileCopyInServerAuth =
-  genFileCopy . (serverAuthDirInSdkTemplatesDir </>)
