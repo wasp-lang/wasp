1284e896b867035c3cbc84638d4fcf452a0725c4
Carlos Precioso <carlos@wasp-lang.dev>
2026-08-18 16:13:38 +0200

    Remove unneeded port

--- waspc/cli/src/Wasp/Cli/Command/BuildStart/Client.hs
+++ waspc/cli/src/Wasp/Cli/Command/BuildStart/Client.hs
@@ -7,7 +7,6 @@ where
 import Data.Function ((&))
 import Wasp.Cli.Command.BuildStart.Config (BuildStartConfig (..))
 import Wasp.Env (getEnvVars)
-import Wasp.Generator.WebAppGenerator.RunConfig (WebAppRunConfig (..))
 import qualified Wasp.Job as J
 import Wasp.Job.Except (ExceptJob, toExceptJob)
 import Wasp.Job.Process (runNodeCommandAsJob, runNodeCommandAsJobWithExtraEnv)
@@ -32,12 +31,9 @@ startClient config =
     "npx"
     [ "vite",
       "preview", -- `preview` launches a static file server for the built client.
-      "--port",
-      clientPort,
       "--strictPort" -- This will make it fail if the port is already in use.
     ]
     J.WebApp
     & toExceptJob (("Serving the client failed with exit code: " <>) . show)
   where
-    clientPort = show config.clientRunConfig.port
     projectDir = config.projectDir
--- waspc/src/Wasp/Generator/WebAppGenerator/RunConfig.hs
+++ waspc/src/Wasp/Generator/WebAppGenerator/RunConfig.hs
@@ -4,14 +4,12 @@ module Wasp.Generator.WebAppGenerator.RunConfig
   )
 where
 
-import Network.Socket (PortNumber)
 import Wasp.AppComponentUrl (AppComponentUrl (..))
 import Wasp.Env (EnvVar, HasEnvVars (..))
 import qualified Wasp.Generator.WebAppGenerator.Common as Common
 
-data WebAppRunConfig = WebAppRunConfig
-  { port :: PortNumber,
-    -- These might not hold all the environment variables that the web app uses,
+newtype WebAppRunConfig = WebAppRunConfig
+  { -- These might not hold all the environment variables that the web app uses,
     -- as it also reads from `.env.client` files and the current environment,
     -- autonomously. This holds the necessary environment variables for the app
     -- components to know where to run and where to communicate with server.
@@ -26,7 +24,6 @@ instance HasEnvVars WebAppRunConfig where
 makeWebAppRunConfig :: AppComponentUrl -> String -> WebAppRunConfig
 makeWebAppRunConfig expectedUrl serverUrl =
   WebAppRunConfig
-    expectedUrl.port
     [ (Common.serverUrlEnvVarName, serverUrl),
       (Common.clientPortEnvVarName, show expectedUrl.port)
     ]
