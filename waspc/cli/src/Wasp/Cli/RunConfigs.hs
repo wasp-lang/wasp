a85b0fe10aebf4b87b71249679cc7a4ae3acb5cd
Carlos Precioso <carlos@wasp-lang.dev>
2026-08-18 16:38:07 +0200

    Split

--- waspc/cli/src/Wasp/Cli/RunConfigs.hs
+++ waspc/cli/src/Wasp/Cli/RunConfigs.hs
@@ -1,38 +1,21 @@
 module Wasp.Cli.RunConfigs
   ( makeDefaultDevRunConfigs,
-    makeDefaultUrls,
     makeRunConfigs,
-    makeDefaultDevClientUrl,
-    defaultDevServerUrl,
   )
 where
 
 import Wasp.AppComponentUrl (AppComponentUrl (..))
 import qualified Wasp.AppComponentUrl as AppComponentUrl
 import Wasp.AppSpec (AppSpec)
+import Wasp.Cli.DefaultDevAppComponentUrls (makeDefaultUrls)
 import Wasp.Generator.ServerGenerator.RunConfig (ServerRunConfig, makeServerRunConfig)
-import qualified Wasp.Generator.WebAppGenerator.Common as WebAppG
 import Wasp.Generator.WebAppGenerator.RunConfig (WebAppRunConfig, makeWebAppRunConfig)
 
 makeDefaultDevRunConfigs :: AppSpec -> (WebAppRunConfig, ServerRunConfig)
 makeDefaultDevRunConfigs appSpec = makeRunConfigs $ makeDefaultUrls appSpec
-
-makeDefaultUrls :: AppSpec -> (AppComponentUrl, AppComponentUrl)
-makeDefaultUrls appSpec = (clientUrl, serverUrl)
-  where
-    clientUrl = makeDefaultDevClientUrl appSpec
-    serverUrl = defaultDevServerUrl
 
 makeRunConfigs :: (AppComponentUrl, AppComponentUrl) -> (WebAppRunConfig, ServerRunConfig)
 makeRunConfigs (clientUrl, serverUrl) = (clientRunConfig, serverRunConfig)
   where
     clientRunConfig = makeWebAppRunConfig clientUrl (AppComponentUrl.url serverUrl)
     serverRunConfig = makeServerRunConfig serverUrl (AppComponentUrl.url clientUrl)
-
-makeDefaultDevClientUrl :: AppSpec -> AppComponentUrl
-makeDefaultDevClientUrl spec =
-  Local {port = 3000, path = Just $ WebAppG.getBaseDir spec}
-
-defaultDevServerUrl :: AppComponentUrl
-defaultDevServerUrl =
-  Local {port = 3001, path = Nothing}
