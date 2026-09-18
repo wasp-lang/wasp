c337045c481255551d66e50cd1f9001431958199
Mihovil Ilakovac <mihovil@ilakovac.com>
2026-09-18 10:27:53 +0200

    Move browser session state and API error helpers into core

--- waspc/data/Generator/libs/sdk-core/src/browser/index.ts
+++ waspc/data/Generator/libs/sdk-core/src/browser/index.ts
@@ -1,1 +1,9 @@
-export {};
+export { apiEventsEmitter } from "./auth/events.js";
+export {
+  clearSessionId,
+  getSessionId,
+  handleApiError,
+  removeLocalUserData,
+  setSessionId,
+} from "./auth/session.js";
+export { storage, type DataStore } from "./storage.js";
