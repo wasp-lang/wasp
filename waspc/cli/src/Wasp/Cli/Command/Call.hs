aa47143794b515a73af7c467e207aab592b4e919
Vardhman Bengani <161213233+flcrom@users.noreply.github.com>
2026-09-18 13:57:32 +0530

    Add Help constructor and parseCall (#3116)

--- waspc/cli/src/Wasp/Cli/Command/Call.hs
+++ waspc/cli/src/Wasp/Cli/Command/Call.hs
@@ -13,6 +13,7 @@ data Call
   | BuildStart Arguments
   | Version
   | Doctor
+  | Help
   | Telemetry
   | Deps
   | Dockerfile
@@ -24,6 +25,37 @@ data Call
   | Deploy Arguments -- deploy cmd passthrough args
   | Test Arguments -- "client" | "server", then test cmd passthrough args
   | Unknown Arguments -- all args
-  deriving (Eq)
+  deriving (Eq, Show)
 
 type Arguments = [String]
+
+-- | Parses CLI arguments into the command to run.
+parseCall :: [String] -> Call
+parseCall args = case args of
+  ("new" : newArgs) -> New newArgs
+  ("start" : "db" : startDbArgs) -> StartDb startDbArgs
+  ("start" : startArgs) -> Start startArgs
+  ["clean"] -> Clean
+  ["install"] -> Install
+  ["compile"] -> Compile
+  ("db" : dbArgs) -> Db dbArgs
+  ["uninstall"] -> Uninstall
+  ["version"] -> Version
+  ["doctor"] -> Doctor
+  ["build"] -> Build
+  ("build" : "start" : buildStartArgs) -> BuildStart buildStartArgs
+  ["help"] -> Help
+  ["--help"] -> Help
+  ["-h"] -> Help
+  ["telemetry"] -> Telemetry
+  ["deps"] -> Deps
+  ["dockerfile"] -> Dockerfile
+  ("show" : showArgs) -> Show showArgs
+  ["news"] -> News
+  ["studio"] -> Studio
+  ["completion"] -> PrintBashCompletionInstruction
+  ["completion:list"] -> BashCompletionListCommands
+  ("deploy" : deployArgs) -> Deploy deployArgs
+  ("test" : testArgs) -> Test testArgs
+  [] -> Help
+  _unknownCommand -> Unknown args
